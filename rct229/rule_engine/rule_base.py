4bc364c45637ea0e86edb1c78fb1877054c6eaa3
holl695 <charlie.holly@pnnl.com>
2021-07-14 18:35:52 -0400

    Check that list_contexts are actually lists

--- rct229/rule_engine/rule_base.py
+++ rct229/rule_engine/rule_base.py
@@ -715,19 +715,23 @@ class RuleDefinitionListIndexedBase(RuleDefinitionListBase):
             raise ValueError(UNUSED_INDEX_RMR_MSG)
 
         # Get the list contexts
-        list_trio = UserBaselineProposedVals(
-            find_all(list_path, context.user) if rmrs_used.user else None,
-            find_all(list_path, context.baseline) if rmrs_used.baseline else None,
-            find_all(list_path, context.proposed) if rmrs_used.proposed else None,
+        user_list = find_all(list_path, context.user) if rmrs_used.user else None
+        baseline_list = (
+            find_all(list_path, context.baseline) if rmrs_used.baseline else None
+        )
+        proposed_list = (
+            find_all(list_path, context.proposed) if rmrs_used.proposed else None
         )
 
-        # # This implementation assumes the used lists contexts are in fact lists
-        # if (
-        #     (rmrs_used.user and not isinstance(list_context_trio.user, list))
-        #     or (rmrs_used.baseline and not isinstance(list_context_trio.baseline, list))
-        #     or (rmrs_used.proposed and not isinstance(list_context_trio.proposed, list))
-        # ):
-        #     raise ValueError(CONTEXT_NOT_LIST)
+        # This implementation assumes the used lists contexts are in fact lists
+        if (
+            (rmrs_used.user and not isinstance(user_list, list))
+            or (rmrs_used.baseline and not isinstance(baseline_list, list))
+            or (rmrs_used.proposed and not isinstance(proposed_list, list))
+        ):
+            raise ValueError(CONTEXT_NOT_LIST)
+
+        list_trio = UserBaselineProposedVals(user_list, baseline_list, proposed_list)
 
         user_list = None
         baseline_list = None
