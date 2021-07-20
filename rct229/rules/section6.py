3715b22de6f74f3cc1a299f799009d77aa1f01e7
holl695 <charlie.holly@pnnl.com>
2021-07-20 13:16:56 -0400

    Reverting to old rule file organization

--- rct229/rules/section6.py
+++ rct229/rules/section6.py
@@ -1,5 +1,4 @@
 from numpy import sum
-
 from rct229.data.schema_enums import schema_enums
 from rct229.rule_engine.rule_base import (
     RuleDefinitionBase,
@@ -35,6 +34,76 @@ class Section6Rule1(RuleDefinitionListIndexedBase):
                 index_rmr="proposed",
                 list_path="$..spaces[*]",  # All spaces inside the building
             )
+
+        class SpaceRule(RuleDefinitionBase):
+            def __init__(self):
+                super(Section6Rule1.BuildingRule.SpaceRule, self,).__init__(
+                    required_fields={
+                        "$": ["interior_lighting", "floor_area"],
+                        "interior_lighting[*]": ["power_per_area"],
+                    },
+                    rmrs_used=UserBaselineProposedVals(True, False, True),
+                )
+
+            def get_calc_vals(self, context, data=None):
+                space_lighting_power_per_area_user = sum(
+                    find_all("interior_lighting[*].power_per_area", context.user)
+                )
+                space_lighting_power_per_area_proposed = sum(
+                    find_all("interior_lighting[*].power_per_area", context.proposed)
+                )
+
+                return {
+                    "space_lighting_power_user": space_lighting_power_per_area_user
+                    * context.user["floor_area"],
+                    "space_lighting_power_proposed": space_lighting_power_per_area_proposed
+                    * context.proposed["floor_area"],
+                }
+
+            def rule_check(self, context, calc_vals, data=None):
+                return (
+                    calc_vals["space_lighting_power_user"]
+                    == calc_vals["space_lighting_power_proposed"]
+                )
+
+
+# ------------------------
+
+
+class Section6Rule2(RuleDefinitionListIndexedBase):
+    """Rule 2 of ASHRAE 90.1-2019 Appendix G Section 6 (Lighting)"""
+
+    def __init__(self):
+        super(Section6Rule1, self).__init__(
+            rmrs_used=UserBaselineProposedVals(False, False, True),
+            each_rule=Section6Rule2.BuildingRule(),
+            index_rmr="proposed",
+            id="6-2",
+            description="The total building interior lighting power shall not exceed the interior lighting power allowance determined using either Table G3.7 or G3.8",
+            rmr_context="buildings",
+        )
+
+    class BuildingRule(RuleDefinitionListIndexedBase):
+        def __init__(self):
+            super(Section6Rule1.BuildingRule, self).__init__(
+                rmrs_used=UserBaselineProposedVals(True, False, True),
+                each_rule=Section6Rule1.BuildingRule.BuildingSeqmentRule(),
+                index_rmr="proposed",
+                rmr_context="building_segments",
+            )
+
+        class BuildingSegmentRule(RuleDefinitionListIndexedBase):
+            def __init__(self):
+                super(Section6Rule1.BuildingRule, self).__init__(
+                    rmrs_used=UserBaselineProposedVals(True, False, True),
+                    each_rule=Section6Rule1.BuildingRule.BuildingSeqmentRule(),
+                    index_rmr="proposed",
+                    rmr_context="building_segments",
+                )
+
+            def create_data(self, context, data):
+                # Get the lighting_building_area_type
+                return find_all("[*].name", context.proposed)
 
         class SpaceRule(RuleDefinitionBase):
             def __init__(self):
