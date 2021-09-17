1d0d28313329d2cdfdc217299c6ce269d1971862
charliepnnl <charlie.holly@pnnl.com>
2021-09-17 10:11:26 -0700

    get_opaque_surface_type()

---
+++ rct229/ruleset_functions/get_opaque_surface_type.py
@@ -0,0 +1,51 @@
+from rct229.utils.jsonpath_utils import find_all
+
+
+def get_opaque_surface_type(surface):
+    """Determines a surface's opaque surface type
+
+    Parameters
+    ----------
+    surface : dict
+        A dictionary representing a surface as defined by the ASHRAE229 schema.
+        It is assumed to have at least the minimal structure:
+        {
+            ajacent_to,
+            construction: {
+                has_radiant_heating
+            },
+            tilt
+        }
+
+    Returns
+    -------
+    str
+        One of the following surface types: "ABOVE-GRADE WALL", "BELOW-GRADE WALL",
+        "FLOOR", "GROUND", "HEATED SLAB-ON=GRADE", "ROOF", "UNHEATED SLAB-ON-GRADE"
+    """
+    surface_type = None
+    surface_tilt = surface["tilt"]
+
+    # Check for roof
+    if 0 <= surface_tilt < 60:
+        surface_type = "ROOF"
+
+    # Check for floor
+    elif 120 <= surface_tilt <= 180:
+        if (
+            surface["construction"]["has_radiant_heating"]
+            and surface["adjacent_to"] == "GROUND"
+        ):
+            surface_type = "HEATED SLAB-ON=GRADE"
+        elif surface["adjacent_to"] == "GROUND":
+            surface_type = "UNHEATED SLAB-ON-GRADE"
+        else:
+            surface_type = "FLOOR"
+
+    # Is a wall
+    elif surface["adjacent_to"] == "GROUND":
+        surface_type = "BELOW-GRADE WALL"
+    else:
+        surface_type = "ABOVE-GRADE WALL"
+
+    return surface_type
