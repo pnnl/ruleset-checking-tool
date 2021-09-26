201acf39f764cfaeaf234356f5b8ff96fdf0df51
charliepnnl <charlie.holly@pnnl.com>
2021-09-26 13:58:29 -0700

    Rule 15-6 using pint

--- rct229/rules/section15.py
+++ rct229/rules/section15.py
@@ -252,13 +252,12 @@ class Section15Rule6(RuleDefinitionListIndexedBase):
             )
 
         def is_applicable(self, context, data=None):
-            # Provide conversion from VA to kVA
-            user_transformer_kVA = context.user["capacity"] / 1000
+            user_transformer_capacity = context.user["capacity"]
 
             user_transformer_type = context.user["type"]
             user_transformer_phase = context.user["phase"]
             user_transformer_capacity_in_range = table_8_4_4_in_range(
-                phase=user_transformer_phase, kVA=user_transformer_kVA
+                phase=user_transformer_phase, capacity=user_transformer_capacity
             )
 
             return (
@@ -268,13 +267,12 @@ class Section15Rule6(RuleDefinitionListIndexedBase):
 
         def get_calc_vals(self, context, data=None):
             user_transformer_phase = context.user["phase"]
-            # Provide conversion from VA to kVA
-            user_transformer_kVA = context.user["capacity"] / 1000
+            user_transformer_capacity = context.user["capacity"]
 
             return {
                 "user_transformer_efficiency": context.user["efficiency"],
                 "required_user_transformer_min_efficiency": table_8_4_4_lookup(
-                    phase=user_transformer_phase, kVA=user_transformer_kVA
+                    phase=user_transformer_phase, capacity=user_transformer_capacity
                 )["efficiency"],
             }
 
