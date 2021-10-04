ee60f4abab8dc11c65efcc3da11348d30b171cd9
charliepnnl <charlie.holly@pnnl.com>
2021-10-04 12:29:39 -0700

    Fixing merged in develop code

--- rct229/rules/section5.py
+++ rct229/rules/section5.py
@@ -4,6 +4,7 @@ from rct229.rule_engine.rule_base import (
 )
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.match_lists import match_lists_exactly_by_id
 
 # Rule Definitions for Section 5 of 90.1-2019 Appendix G
 
@@ -41,7 +42,7 @@ class Section5Rule2(RuleDefinitionListIndexedBase):
             user_surfaces = find_all("$..surfaces[*]", context.user)
 
             # This assumes that the surfaces all match
-            matched_user_surfaces = match_lists_exectly_by_id(
+            matched_user_surfaces = match_lists_exactly_by_id(
                 proposed_surfaces, user_surfaces
             )
             proposed_user_surface_pairs = zip(proposed_surfaces, matched_user_surfaces)
