1f1e24fe1d794e5207907ef5485b1c3f8b9e3fe2
Tadimeti, Vijay <vijay.tadimeti@pnnl.gov>
2021-10-06 23:52:19 -0700

    black

--- rct229/data_fns/table_G3_6_fns.py
+++ rct229/data_fns/table_G3_6_fns.py
@@ -19,14 +19,15 @@ building_exterior_enumeration_to_lpd_space_type_map = {
     "OUTDOOR_SALES_OPEN_AREAS": "Outdoor sales - open areas",
     "STREET_FRONTAGE": "Street frontage",
     "NON_TRADABLE_FACADE": "Non-tradable facade",
-    "BUILDING_FACADE":"Building facade",
-    "AUTOMATED_TELLER_MACHINES":"Automated teller machines",
-    "NIGHT_DEPOSITORIES":"Night depositories",
-    "ENTRANCE_AND_GATEHOUSE":"Entrance and gatehouses",
-    "EMERGENCY_VEHICLE_LOADING_AREA":"Emergency vehicle loading area",
-    "DRIVE_UP_WINDOWS_FAST_FOOD":"Drive-up windows at fast-food restaurants",
-    "PARKING_NEAR_24HR_RETAIL_ENTRANCES":"Parking near 24-hour retail entrances"
+    "BUILDING_FACADE": "Building facade",
+    "AUTOMATED_TELLER_MACHINES": "Automated teller machines",
+    "NIGHT_DEPOSITORIES": "Night depositories",
+    "ENTRANCE_AND_GATEHOUSE": "Entrance and gatehouses",
+    "EMERGENCY_VEHICLE_LOADING_AREA": "Emergency vehicle loading area",
+    "DRIVE_UP_WINDOWS_FAST_FOOD": "Drive-up windows at fast-food restaurants",
+    "PARKING_NEAR_24HR_RETAIL_ENTRANCES": "Parking near 24-hour retail entrances",
 }
+
 
 def table_G3_6_lookup(building_exterior_type_enum_val):
     """Returns the lighting power density for a building_exterior as
@@ -56,8 +57,13 @@ def table_G3_6_lookup(building_exterior_type_enum_val):
     watts_per_linear_ft = osstd_entry["w/ft"]
     watt_per_location = osstd_entry["w/location"]
     lpd = watts_per_ft2 * ureg("watt / foot**2") if watts_per_ft2 is not None else None
-    linear_lpd = watts_per_linear_ft * ureg("watt / foot") if watts_per_linear_ft is not None else None
-    location_lpd = watt_per_location * ureg("watt") if watt_per_location is not None else None
-
+    linear_lpd = (
+        watts_per_linear_ft * ureg("watt / foot")
+        if watts_per_linear_ft is not None
+        else None
+    )
+    location_lpd = (
+        watt_per_location * ureg("watt") if watt_per_location is not None else None
+    )
 
     return {"lpd": lpd, "linear_lpd": linear_lpd, "location_lpd": location_lpd}
