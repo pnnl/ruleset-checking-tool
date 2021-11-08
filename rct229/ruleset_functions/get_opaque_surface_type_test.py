d72489fd5fac141eb287eb4018bd6ea348fc4e25
charliepnnl <charlie.holly@pnnl.com>
2021-11-08 10:50:54 -0800

    Update unit tests for get_opaque_surface_type to use pint

--- rct229/ruleset_functions/get_opaque_surface_type_test.py
+++ rct229/ruleset_functions/get_opaque_surface_type_test.py
@@ -1,36 +1,40 @@
 import pytest
 from rct229.ruleset_functions.get_opaque_surface_type import get_opaque_surface_type
+from rct229.schema.config import ureg
+
+# Constants
+DEGREES = ureg("degrees")
 
 TEST_SURFACES = {
     "ABOVE-GRADE WALL": {
         "adjacent_to": "INTERIOR",
         "construction": {"has_radiant_heating": False},
-        "tilt": 80,
+        "tilt": 80 * DEGREES,
     },
     "BELOW-GRADE WALL": {
         "adjacent_to": "GROUND",
         "construction": {"has_radiant_heating": True},
-        "tilt": 100,
+        "tilt": 100 * DEGREES,
     },
     "FLOOR": {
         "adjacent_to": "EXTERIOR",
         "construction": {"has_radiant_heating": False},
-        "tilt": 160,
+        "tilt": 160 * DEGREES,
     },
     "HEATED SLAB-ON-GRADE": {
         "adjacent_to": "GROUND",
         "construction": {"has_radiant_heating": True},
-        "tilt": 120,
+        "tilt": 120 * DEGREES,
     },
     "ROOF": {
         "adjacent_to": "EXTERIOR",
         "construction": {"has_radiant_heating": False},
-        "tilt": 30,
+        "tilt": 30 * DEGREES,
     },
     "UNHEATED SLAB-ON-GRADE": {
         "adjacent_to": "GROUND",
         "construction": {"has_radiant_heating": False},
-        "tilt": 180,
+        "tilt": 180 * DEGREES,
     },
 }
 
