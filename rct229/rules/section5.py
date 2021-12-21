77013d3b6852151f85d8a4b4f25613c5b207f971
charliepnnl <charlie.holly@pnnl.com>
2021-12-21 12:42:22 -0800

    Start of Rule 5-5

--- rct229/rules/section5.py
+++ rct229/rules/section5.py
@@ -3,6 +3,10 @@ from rct229.rule_engine.rule_base import (
     RuleDefinitionListIndexedBase,
 )
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.ruleset_functions.get_opaque_surface_type import (
+    get_opaque_surface_type,
+    ROOF,
+)
 from rct229.utils.jsonpath_utils import find_all
 from rct229.utils.match_lists import match_lists_exactly_by_id
 
@@ -84,6 +88,57 @@ class Section5Rule3(RuleDefinitionListIndexedBase):
 
         def get_calc_vals(self, context, data=None):
             baseline_surfaces_casting_shade_ids = []
+            for surface in find_all(EXTERIOR_SURFACES_JSONPATH, context.baseline):
+                if surface["does_cast_shade"]:
+                    baseline_surfaces_casting_shade_ids.append(surface["id"])
+
+            return {
+                "baseline_surfaces_casting_shade_ids": baseline_surfaces_casting_shade_ids
+            }
+
+        def rule_check(self, context, calc_vals, data=None):
+            return len(calc_vals["baseline_surfaces_casting_shade_ids"]) == 0
+
+
+# ------------------------
+
+
+class Section5Rule5(RuleDefinitionListIndexedBase):
+    """Rule 5 of ASHRAE 90.1-2019 Appendix G Section 5 (Envelope)"""
+
+    def __init__(self):
+        super(Section5Rule3, self).__init__(
+            rmrs_used=UserBaselineProposedVals(False, True, False),
+            required_fields={
+                "weather": ["climate_zone"],
+            },
+            each_rule=Section5Rule3.BuildingRule(),
+            index_rmr="baseline",
+            id="5-5",
+            description="Baseline roof assemblies must match the appropriate assembly maximum U-factors in Tables G3.4-1 through G3.4-8.",
+            list_path="buildings[*]",
+        )
+
+        def create_data(context, data=None):
+            return {"climate_zone": context.baseline["weather"]["climate_zone"]}
+
+    class BuildingRule(RuleDefinitionListIndexedBase):
+        def __init__(self):
+            super(Section5Rule3.BuildingRule, self).__init__(
+                rmrs_used=UserBaselineProposedVals(False, True, False),
+                required_fields={},
+            )
+
+            def create_context_list(context, data=None):
+                context_list = []
+                for surface in find_all("$..surfaces[*]"):
+                    if get_opaque_surface_type(surface) == ROOF:
+                        UserBaselineProposedVals(None, surface, None)
+
+        def get_calc_vals(self, context, data=None):
+            surface_conditioning_category_dict = get_surface_conditioning_category_dict(
+                data["climate_zone"], context.baseline
+            )
             for surface in find_all(EXTERIOR_SURFACES_JSONPATH, context.baseline):
                 if surface["does_cast_shade"]:
                     baseline_surfaces_casting_shade_ids.append(surface["id"])
