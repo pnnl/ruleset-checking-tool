27283913b2eda600f87455a858d5aa87eb6991de
charliepnnl <charlie.holly@pnnl.com>
2022-02-19 10:41:43 -0800

    Updated pint_sum() to handle empty lists

--- rct229/utils/pint_utils.py
+++ rct229/utils/pint_utils.py
@@ -2,5 +2,8 @@ import functools
 import operator
 
 
-def pint_sum(qty_list, initializer=None):
-    return functools.reduce(operator.add, qty_list, initializer)
+def pint_sum(qty_list, default=None):
+    if len(qty_list) == 0:
+        assert default is not None
+
+    return functools.reduce(operator.add, qty_list) if len(qty_list) > 0 else default
