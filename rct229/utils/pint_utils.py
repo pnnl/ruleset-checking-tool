8c73a55918ea79c391082c1806e44fb77c20defb
charliepnnl <charlie.holly@pnnl.com>
2022-02-19 09:47:51 -0800

    Allow pint_sum() to specify an initializer

--- rct229/utils/pint_utils.py
+++ rct229/utils/pint_utils.py
@@ -2,5 +2,5 @@ import functools
 import operator
 
 
-def pint_sum(qty_list):
-    return functools.reduce(operator.add, qty_list)
+def pint_sum(qty_list, initializer=None):
+    return functools.reduce(operator.add, qty_list, initializer)
