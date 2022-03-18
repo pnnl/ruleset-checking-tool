b242ec88ad76b892b01d5580e55734513bfadd6d
Weili Xu <weili.xu@techprefab.com>
2022-03-18 10:40:00 -0400

    Add assertions

--- rct229/ruleset_functions/get_area_type_window_wall_area.py
+++ rct229/ruleset_functions/get_area_type_window_wall_area.py
@@ -2,6 +2,12 @@ from rct229.data.schema_enums import schema_enums
 from rct229.ruleset_functions.get_surface_conditioning_category_dict import get_surface_conditioning_category_dict, SurfaceConditioningCategory as SCC
 from rct229.ruleset_functions.get_opaque_surface_type import get_opaque_surface_type, ABOVE_GRADE_WALL
 from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.assertions import (
+    assert_,
+    assert_nonempty_lists,
+    assert_required_fields,
+    getattr_,
+)
 
 DOOR = schema_enums["SubsurfaceClassificationType"].DOOR.name
 
@@ -25,11 +31,14 @@ def get_area_type_window_wall_area(climate_zone, building):
                                                         SCC.EXTERIOR_NON_RESIDENTIAL,
                                                        SCC.EXTERIOR_MIXED,
                                                        SCC.SEMI_EXTERIOR]):
+                assert_("area" in surface,  f"surface:{surface['id']} has no area")
                 window_wall_areas_dictionary[area_type]["TOTAL_WALL_AREA"] += surface["area"]
 
                 # add sub-surfaces
                 for subsurface in find_all("subsurfaces[*]", surface):
-                    # TODO convert DOOR to enum
+                    assert_("classification" in subsurface, f"subsurface:{subsurface['id']} has no classification")
+                    assert_("glazed_area" in subsurface, f"subsurface:{subsurface['id']} has no glazed_area")
+                    assert_("opaque_area" in subsurface, f"subsurface:{subsurface['id']} has no opaque_area")
                     if (subsurface["classification"] == DOOR) and (subsurface["glazed_area"] > subsurface["opaque_area"]):
                         window_wall_areas_dictionary[area_type]["TOTAL_WINDOW_AREA"] += (subsurface["glazed_area"] + subsurface["opaque_area"])
                     else:
