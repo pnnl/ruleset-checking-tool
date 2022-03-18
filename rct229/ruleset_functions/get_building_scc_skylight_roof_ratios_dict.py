b12a42a96d10feec8c9e8894bea9c2198a4152e6
Weili Xu <weili.xu@pnnl.gov>
2022-03-18 13:54:43 -0700

    start frame

---
+++ rct229/ruleset_functions/get_building_scc_skylight_roof_ratios_dict.py
@@ -0,0 +1,7 @@
+from rct229.ruleset_functions.get_surface_conditioning_category_dict import (
+    get_surface_conditioning_category_dict
+)
+
+def get_building_scc_skylight_roof_ratios_dict(climate_zone, building):
+    # required fields for this function are coming from the nested functions
+    scc_dictionary = None
