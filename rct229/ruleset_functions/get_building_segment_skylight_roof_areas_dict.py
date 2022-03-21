6690661de482b9b84b14e15fba76a6235e179a17
charliepnnl <charlie.holly@pnnl.com>
2022-03-21 14:18:40 -0700

    Fixed buliding element pluralities

--- rct229/ruleset_functions/get_building_segment_skylight_roof_areas_dict.py
+++ rct229/ruleset_functions/get_building_segment_skylight_roof_areas_dict.py
@@ -21,8 +21,10 @@ from rct229.utils.pint_utils import ZERO
 # NOTE: There are no required fields at this function's level; the ruleset_functions
 # called do have required fields
 
+import pdb
 
-def get_building_segment_roof_areas_dict(climate_zone, building):
+
+def get_building_segment_skylight_roof_areas_dict(climate_zone, building):
     """Gets a dictionary mapping building segment id to a dictionary of (total area of
     skylights) and (total area of envelope roofs) for the building_segment
 
@@ -44,28 +46,30 @@ def get_building_segment_roof_areas_dict(climate_zone, building):
             }
         }
     """
-
+    print("Started get_building_segment_skylight_roof_areas_dict()")
     zcc_dict = get_zone_conditioning_category_dict(climate_zone, building)
     scc_dict = get_surface_conditioning_category_dict(climate_zone, building)
-
+    # pdb.set_trace()
     building_segment_roof_areas_dict = {}
 
-    for building_segment in find_all("building_segment[*]", building):
+    for building_segment in find_all("building_segments[*]", building):
         building_segment_roof_areas_dict[
             building_segment["id"]
         ] = building_segment_roof_areas = {
             "total_envelope_roof_area": ZERO.AREA,
             "total_skylight_area": ZERO.AREA,
         }
-
-        for zone in find_all("zone[*]", building_segment):
+        print(f"segment {building_segment['id']}")
+        for zone in find_all("zones[*]", building_segment):
             if zcc_dict[zone["id"]] in [
                 ZCC.CONDITIONED_RESIDENTIAL,
                 ZCC.CONDITIONED_NON_RESIDENTIAL,
                 ZCC.CONDITIONED_MIXED,
                 ZCC.SEMI_HEATED,
             ]:
-                for surface in find_all("surface[*]", zone):
+                print(f"zone {zone['id']}")
+                for surface in find_all("surfaces[*]", zone):
+                    print(f"surface {surface['id']}")
                     if get_opaque_surface_type(surface) == OST.ROOF and scc_dict[
                         surface["id"]
                     ] in [
@@ -75,12 +79,13 @@ def get_building_segment_roof_areas_dict(climate_zone, building):
                         SCC.SEMI_EXTERIOR,
                     ]:
 
+                        print(f"surface {surface['id']} is ROOF+")
                         building_segment_roof_areas[
                             "total_envelope_roof_area"
                         ] += getattr_(surface, "surface", "area")
 
                         building_segment_roof_areas["total_skylight_area"] += pint_sum(
-                            find_all("subsurface[*].glazed_area"), surface
-                        ) + pint_sum(find_all("subsurface[*].opaque_area", surface))
+                            find_all("subsurfaces[*].glazed_area"), surface
+                        ) + pint_sum(find_all("subsurfaces[*].opaque_area", surface))
 
-    return building_segment_roof_areas_dict
+    # return building_segment_roof_areas_dict
