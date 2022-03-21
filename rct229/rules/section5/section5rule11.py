c5eca4640c9de16a246c09950b2fe4d7767efb5b
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-03-21 11:16:58 -0700

    adds rule5-11

--- rct229/rules/section5/section5rule11.py
+++ rct229/rules/section5/section5rule11.py
@@ -1,3 +1,4 @@
+from rct229.data.schema_enums import schema_enums
 from rct229.data_fns.table_G3_4_fns import table_G34_lookup
 from rct229.rule_engine.rule_base import (
     RuleDefinitionBase,
@@ -6,9 +7,15 @@ from rct229.rule_engine.rule_base import (
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.ruleset_functions.get_opaque_surface_type import (
     BELOW_GRADE_WALL,
+    ABOVE_GRADE_WALL,
     get_opaque_surface_type,
 )
 from rct229.ruleset_functions.get_surface_conditioning_category_dict import (
+    EXTERIOR_MIXED,
+    EXTERIOR_NON_RESIDENTIAL,
+    EXTERIOR_RESIDENTIAL,
+    SEMI_EXTERIOR,
+    UNREGULATED,
     get_surface_conditioning_category_dict,
 )
 from rct229.utils.jsonpath_utils import find_all
@@ -20,100 +27,103 @@ class Section5Rule11(RuleDefinitionListIndexedBase):
     def __init__(self):
         super(Section5Rule11, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, True, False),
-            each_rule=Section5Rule11.BuildingRule(),
-            index_rmr="baseline",
-            list_path="buildings[*]",
-            id="5-11",
-            description="Baseline below-grade walls shall match the appropriate assembly maximum C-factors in Table G3.4-1 through G3.4-8.",
             required_fields={
                 "$": ["weather"],
                 "weather": ["climate_zone"],
             },
+            each_rule=Section5Rule11.BuildingRule(),
+            index_rmr="baseline",
+            id="5-11",
+            description="Baseline above-grade wall assemblies must match the appropriate assembly maximum U-factors in Tables G3.4-1 through G3.4-8.",
+            list_path="buildings[*]",
         )
 
     def create_data(self, context, data=None):
-        return {"climate_zone": context.baseline["weather"]["climate_zone"]}
+        rmr_baseline = context.baseline
+        return {"climate_zone": rmr_baseline["weather"]["climate_zone"]}
 
-    class BuildingRule(RuleDefinitionBase):
+    class BuildingRule(RuleDefinitionListIndexedBase):
         def __init__(self):
             super(Section5Rule11.BuildingRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, False),
-                required_fields={"$..zones[*]": ["surfaces"]},
+                required_fields={},
+                each_rule=Section5Rule11.BuildingRule.AboveGradeWallRule(),
+                index_rmr="baseline",
             )
 
-        def get_calc_vals(self, context, data=None):
-            # Climate zone should have only one in RMR
-            climate_zone = data["climate_zone"]
-            # Becasue list_path is building, the scope of the context.baseline here is building
-            scc_dictionary_b = get_surface_conditioning_category_dict(
-                climate_zone, context.baseline
-            )
-            # Retrieve all surfaces under the building
-            surfaces_b = find_all("$..zones[*].surfaces[*]", context.baseline)
-            # build-in manual check flag.
-            calc_val = {}
-            failing_surface_c_factor_ids = []
-            mix_surface_c_factor_ids = []
+        def create_context_list(self, context, data=None):
+            building = context.baseline
+            # List of all baseline above-grade wall surfaces to become the context for AboveGradeWallRule
+            return [
+                UserBaselineProposedVals(None, surface, None)
+                for surface in find_all("$..surfaces[*]", building)
+                if get_opaque_surface_type(surface) == ABOVE_GRADE_WALL
+            ]
 
-            for surface_b in surfaces_b:
-                if get_opaque_surface_type(surface_b) == BELOW_GRADE_WALL:
-                    # construction info
-                    surface_construction_b = surface_b["construction"]
-                    scc_b = scc_dictionary_b[surface_b["id"]]
-                    # QNS a good number to estimate?
-                    target_c_factor = None
-                    # TODO All these need to change to enum later
-                    if scc_b in [
-                        "EXTERIOR RESIDENTIAL",
-                        "EXTERIOR NON-RESIDENTIAL",
-                        "SEMI-EXTERIOR",
-                    ]:
-                        target = table_G34_lookup(climate_zone, scc_b, BELOW_GRADE_WALL)
-                        target_c_factor = target["c_factor"]
-
-                    elif scc_b == "EXTERIOR MIXED":
-                        target = table_G34_lookup(
-                            climate_zone, "EXTERIOR RESIDENTIAL", BELOW_GRADE_WALL
-                        )
-                        target_c_factor_res = target["c_factor"]
-
-                        target = table_G34_lookup(
-                            climate_zone, "EXTERIOR NON-RESIDENTIAL", BELOW_GRADE_WALL
-                        )
-                        target_c_factor_nonres = target["c_factor"]
-
-                        if target_c_factor_res != target_c_factor_nonres:
-                            mix_surface_c_factor_ids.append(surface_b["id"])
-                        else:
-                            target_c_factor = target_c_factor_res
-                    # convert values to IP unit to compare with standard.
-                    model_c_factor_magnitude = round(
-                        surface_construction_b["c_factor"]
-                        .to("Btu_h / square_foot / delta_degF")
-                        .magnitude,
-                        2,
-                    )
-                    target_c_factor_magnitude = target_c_factor.magnitude
-                    if model_c_factor_magnitude != target_c_factor_magnitude:
-                        failing_surface_c_factor_ids.append(surface_b["id"])
-
+        def create_data(self, context, data=None):
+            building = context.baseline
+            # Merge into the existing data dict
             return {
-                "failed_c_factor_surface_id": failing_surface_c_factor_ids,
-                "mix_surface_c_factor_ids": mix_surface_c_factor_ids,
+                **data,
+                "surface_conditioning_category_dict": get_surface_conditioning_category_dict(
+                    data["climate_zone"], building
+                ),
             }
 
-        def manual_check_required(self, context, calc_vals=None, data=None):
-            """
-            Return True or False if the results shall depends on manual check
-            Parameters
-            ----------
-            context
-            calc_vals
-            data
-            Returns
-            -------
-            """
-            return len(calc_vals["mix_surface_c_factor_ids"]) > 0
+        class AboveGradeWallRule(RuleDefinitionBase):
+            def __init__(self):
+                super(Section5Rule11.BuildingRule.AboveGradeWallRule, self).__init__(
+                    rmrs_used=UserBaselineProposedVals(False, True, False),
+                    required_fields={},
+                )
 
-        def rule_check(self, context, calc_vals, data=None):
-            return len(calc_vals["failed_c_factor_surface_id"]) == 0
+            def get_calc_vals(self, context, data=None):
+                climate_zone: str = data["climate_zone"]
+                above_grade_wall = context.baseline
+                scc: str = data["surface_conditioning_category_dict"][above_grade_wall["id"]]
+                above_grade_wall_u_factor = above_grade_wall["construction"]["u_factor"]
+
+                target_u_factor = None
+                target_u_factor_res = None
+                target_u_factor_nonres = None
+
+                if scc in [
+                    EXTERIOR_RESIDENTIAL,
+                    EXTERIOR_NON_RESIDENTIAL,
+                    SEMI_EXTERIOR,
+                ]:
+                    target_u_factor = table_G34_lookup(climate_zone, scc, ABOVE_GRADE_WALL)[
+                        "u_value"
+                    ]
+                elif scc == EXTERIOR_MIXED:
+                    target_u_factor_res = table_G34_lookup(
+                        climate_zone, EXTERIOR_RESIDENTIAL, ABOVE_GRADE_WALL
+                    )["u_value"]
+                    target_u_factor_nonres = table_G34_lookup(
+                        climate_zone, EXTERIOR_NON_RESIDENTIAL, ABOVE_GRADE_WALL
+                    )["u_value"]
+                    if target_u_factor_res == target_u_factor_nonres:
+                        target_u_factor = target_u_factor_res
+
+                return {
+                    "above_grade_wall_u_factor": above_grade_wall_u_factor,
+                    "target_u_factor": target_u_factor,
+                    "target_u_factor_res": target_u_factor_res,
+                    "target_u_factor_nonres": target_u_factor_nonres,
+                }
+
+            def manaul_check_required(self, context, calc_vals, data=None):
+                target_u_factor_res = calc_vals["target_u_factor_res"]
+                target_u_factor_nonres = calc_vals["target_u_factor_nonres"]
+
+                return (
+                    target_u_factor_res is not None
+                    and target_u_factor_nonres is not None
+                    and target_u_factor_res != target_u_factor_nonres
+                )
+
+            def rule_check(self, context, calc_vals, data=None):
+                above_grade_wall_u_factor = calc_vals["above_grade_wall_u_factor"]
+                target_u_factor = calc_vals["target_u_factor"]
+
+                return above_grade_wall_u_factor == target_u_factor
