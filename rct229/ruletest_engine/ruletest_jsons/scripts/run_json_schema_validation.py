15767b88b1ae5d63d8d059e9169f8db59aa19d3c
Gonzalez, Juan <juan.gonzalez@pnnl.gov>
2022-03-24 17:31:56 -0400

    Small fix to ensure check schema validation is decoupled from excel_to_test_json script. Long term, both the excel to test JSON and validation should be handled separately in a CLI type call, not explicitly ran inside the python files.

--- rct229/ruletest_engine/ruletest_jsons/scripts/run_json_schema_validation.py
+++ rct229/ruletest_engine/ruletest_jsons/scripts/run_json_schema_validation.py
@@ -1,8 +1,6 @@
-import excel_to_test_json
-
 from rct229.ruletest_engine.ruletest_engine import *
 
-test_json_name = excel_to_test_json.json_name
+test_json_name = 'envelope_tests.json'
 
 json_dir = os.path.join(os.path.dirname(__file__), "..", "..", "ruletest_jsons")
 test_json_path = os.path.join(json_dir, test_json_name)
