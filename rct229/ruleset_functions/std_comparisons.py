7bdeb298f48417f50548b35695e2fe41d52964b2
charliepnnl <charlie.holly@pnnl.com>
2022-03-28 16:02:05 -0700

    std_equality() utility function

---
+++ rct229/ruleset_functions/std_comparisons.py
@@ -0,0 +1,25 @@
+def std_equality(std_val, val, units, ndigits=0):
+    """Determines whether two pint quantities are considered equal, in the eyes of
+    the standard being used
+
+    To determine equality, both quantities are converted to the given units and rounded
+    with the given number of digits precision.
+
+    Parameters
+    ----------
+    std_val : Quantity
+        A pint quantity considered the standard value
+    val : Quantity
+        A pint quantity to be compared to the standard value
+    units : str
+        A pint units string
+    ndigits : number
+        The number of digits precision after the decimal point
+    Returns
+    -------
+    bool
+        True iff the values are considered equal by the standard
+    """
+    return round(std_val.to(units).magnitude, ndigits) == round(
+        val.to(units).magnitude, ndigits
+    )
