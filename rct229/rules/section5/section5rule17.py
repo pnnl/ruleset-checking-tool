ca28af03052eed9369f61be5a6420db2a74a20b1
Weili Xu <weili.xu@pnnl.gov>
2022-03-31 06:55:36 -0700

    Update based on the comments

--- rct229/rules/section5/section5rule17.py
+++ rct229/rules/section5/section5rule17.py
@@ -27,7 +27,7 @@ class Section5Rule17(RuleDefinitionListIndexedBase):
                 "weather": ["climate_zone"],
             },
             each_rule=Section5Rule17.BuildingRule(),
-            index_rmr="proposed",
+            index_rmr="baseline",
             id="5-17",
             description="Opaque surfaces that are not regulated (not part of opaque building envelope) must be modeled the same in the baseline as in the proposed design. ",
             list_path="ruleset_model_instances[0].buildings[*]",
@@ -46,26 +46,6 @@ class Section5Rule17(RuleDefinitionListIndexedBase):
                 index_rmr="baseline",
             )
 
-        def create_context_list(self, context, data=None):
-            # List of all baseline roof surfaces to become the context for RoofRule
-            scc = data["surface_conditioning_category_dict"]
-
-            baseline_surfaces = find_all("$..surfaces[*]", context.baseline)
-            proposed_surfaces = find_all("$..surfaces[*]", context.proposed)
-
-            # This assumes that the surfaces matched by IDs between proposed and baseline
-            matched_proposed_surfaces = match_lists_exactly_by_id(
-                baseline_surfaces, proposed_surfaces
-            )
-
-            proposed_baseline_surface_pairs = zip(baseline_surfaces, matched_proposed_surfaces)
-
-            return [
-                UserBaselineProposedVals(None, surface_b, surface_p)
-                for surface_b, surface_p in proposed_baseline_surface_pairs
-                if scc[surface_b["id"]] == SCC.UNREGULATED
-            ]
-
         def create_data(self, context, data=None):
             building = context.baseline
             # Merge into the existing data dict
@@ -75,6 +55,26 @@ class Section5Rule17(RuleDefinitionListIndexedBase):
                     data["climate_zone"], building
                 ),
             }
+
+        def create_context_list(self, context, data=None):
+            # List of all baseline unregulated surfaces to become the context for Unregulated Surfaces
+            scc = data["surface_conditioning_category_dict"]
+
+            baseline_surfaces = find_all("$..surfaces[*]", context.baseline)
+            proposed_surfaces = find_all("$..surfaces[*]", context.proposed)
+
+            # This assumes that the surfaces matched by IDs between proposed and baseline
+            matched_proposed_surfaces = match_lists_exactly_by_id(
+                proposed_surfaces, baseline_surfaces
+            )
+
+            proposed_baseline_surface_pairs = zip(baseline_surfaces, matched_proposed_surfaces)
+
+            return [
+                UserBaselineProposedVals(None, surface_b, surface_p)
+                for surface_b, surface_p in proposed_baseline_surface_pairs
+                if scc[surface_b["id"]] == SCC.UNREGULATED and surface_p is not None
+            ]
 
         class UnregulatedSurfaceRule(RuleDefinitionBase):
             def __init__(self):
@@ -89,51 +89,37 @@ class Section5Rule17(RuleDefinitionListIndexedBase):
 
                 surface_b_type = get_opaque_surface_type(surface_b)
                 surface_b_construction = surface_b["construction"]
-                surface_p_type = None
-                surface_p_construction = None
+                surface_p_type = get_opaque_surface_type(surface_p)
+                surface_p_construction = surface_p["construction"]
 
-                if surface_p is not None:
-                    surface_p_type = get_opaque_surface_type(surface_p)
-                    surface_p_construction = surface_p["construction"]
+                calc_vals = {
+                    "baseline_surface_type": surface_b_type,
+                    "proposed_surface_type": surface_p_type
+                }
 
                 if surface_b_type in [OST.ABOVE_GRADE_WALL, OST.FLOOR, OST.ROOF]:
                     return {
-                        "id": surface_b["id"],
-                        "baseline_surface_type": surface_b_type,
-                        "proposed_surface_type": surface_p_type,
+                        **calc_vals,
                         "baseline_surface_u_factor": surface_b_construction.get("u_factor"),
-                        "proposed_surface_u_factor": surface_p_construction if surface_p_construction is None else
-                        surface_p_construction.get("u_factor")
+                        "proposed_surface_u_factor": surface_p_construction.get("u_factor")
                     }
                 elif surface_b_type in [OST.UNHEATED_SOG, OST.HEATED_SOG]:
                     return {
-                        "id": surface_b["id"],
-                        "baseline_surface_type": surface_b_type,
-                        "proposed_surface_type": surface_p_type,
+                        **calc_vals,
                         "baseline_surface_f_factor": surface_b_construction.get("f_factor"),
-                        "proposed_surface_f_factor": surface_p_construction if surface_p_construction is None else
-                        surface_p_construction.get("f_factor")
+                        "proposed_surface_f_factor": surface_p_construction.get("f_factor")
                     }
                 elif surface_b_type == OST.BELOW_GRADE_WALL:
                     return {
-                        "id": surface_b["id"],
-                        "baseline_surface_type": surface_b_type,
-                        "proposed_surface_type": surface_p_type,
+                        **calc_vals,
                         "baseline_surface_c_factor": surface_b_construction.get("c_factor"),
-                        "proposed_surface_c_factor": surface_p_construction if surface_p_construction is None else
-                        surface_p_construction.get("c_factor")
+                        "proposed_surface_c_factor": surface_p_construction.get("c_factor")
                     }
                 else:
                     # Will never reach this line
                     # The OST defaults all unidentifiable surfaces to above wall grade
                     # Serve code completeness
-                    return {
-                        "id": surface_b["id"],
-                        "baseline_surface_type": None,
-                        "proposed_surface_type": None,
-                        "baseline_surface_c_factor": None,
-                        "proposed_surface_c_factor": None
-                    }
+                    raise Exception(f"Unrecognized surface type: {surface_b_type}")
 
             def rule_check(self, context, calc_vals, data=None):
                 baseline_surface_type = calc_vals["baseline_surface_type"]
@@ -143,16 +129,16 @@ class Section5Rule17(RuleDefinitionListIndexedBase):
                     return False
 
                 if baseline_surface_type in [OST.ABOVE_GRADE_WALL, OST.FLOOR, OST.ROOF]:
-                    return calc_vals["proposed_surface_u_factor"] is not None or \
-                            calc_vals["baseline_surface_u_factor"] is not None or \
+                    return calc_vals["proposed_surface_u_factor"] is not None and \
+                            calc_vals["baseline_surface_u_factor"] is not None and \
                             calc_vals["baseline_surface_u_factor"] == calc_vals["proposed_surface_u_factor"]
                 elif baseline_surface_type in [OST.UNHEATED_SOG, OST.HEATED_SOG]:
-                    return calc_vals["proposed_surface_f_factor"] is not None or \
-                            calc_vals["baseline_surface_f_factor"] is not None or \
+                    return calc_vals["proposed_surface_f_factor"] is not None and \
+                            calc_vals["baseline_surface_f_factor"] is not None and \
                             calc_vals["baseline_surface_f_factor"] == calc_vals["proposed_surface_f_factor"]
                 elif baseline_surface_type == OST.BELOW_GRADE_WALL:
-                    return calc_vals["proposed_surface_c_factor"] is not None or \
-                            calc_vals["baseline_surface_c_factor"] is not None or \
+                    return calc_vals["proposed_surface_c_factor"] is not None and \
+                            calc_vals["baseline_surface_c_factor"] is not None and \
                             calc_vals["baseline_surface_c_factor"] == calc_vals["proposed_surface_c_factor"]
                 else:
                     return False
