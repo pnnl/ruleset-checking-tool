f54696226a83364c54f33a517dc8cc5d98e9a99e
Weili Xu <weili.xu@techprefab.com>
2022-03-31 09:52:17 -0400

    Rule 5-21 with exception handling

---
+++ rct229/rules/section5/section5rule21.py
@@ -0,0 +1,177 @@
+from rct229.data.schema_enums import schema_enums
+from rct229.rule_engine.rule_base import RuleDefinitionListIndexedBase, RuleDefinitionBase
+from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.ruleset_functions.get_area_type_window_wall_area_dict import get_area_type_window_wall_area_dict
+from rct229.ruleset_functions.get_opaque_surface_type import (OpaqueSurfaceType as OST, get_opaque_surface_type)
+from rct229.utils.assertions import getattr_, MissingKeyException
+from rct229.utils.jsonpath_utils import find_all
+from rct229.ruleset_functions.get_surface_conditioning_category_dict import (SurfaceConditioningCategory as SCC, get_surface_conditioning_category_dict)
+from rct229.utils.match_lists import match_lists_exactly_by_id
+
+
+DOOR = schema_enums["SubsurfaceClassificationType"].DOOR.name
+
+
+class Section5Rule21(RuleDefinitionListIndexedBase):
+    """Rule 5 of ASHRAE 90.1-2019 Appendix G Section 5 (Envelope)"""
+
+    def __init__(self):
+        super(Section5Rule21, self).__init__(
+            rmrs_used=UserBaselineProposedVals(False, True, True),
+            required_fields={
+                "$": ["weather"],
+                "weather": ["climate_zone"],
+            },
+            each_rule=Section5Rule21.BuildingRule(),
+            index_rmr="baseline",
+            id="5-21",
+            description="The vertical fenestration shall be distributed on each face of the building in the same proportion as in the proposed design.",
+            list_path="ruleset_model_instances[0].buildings[*]",
+        )
+
+    def create_data(self, context, data=None):
+        rmr_baseline = context.baseline
+        return {"climate_zone": rmr_baseline["weather"]["climate_zone"]}
+
+    class BuildingRule(RuleDefinitionListIndexedBase):
+        def __init__(self):
+            super(Section5Rule21.BuildingRule, self).__init__(
+                rmrs_used=UserBaselineProposedVals(False, True, True),
+                required_fields={},
+                each_rule=Section5Rule21.BuildingRule.AboveGradeWallRule(),
+                index_rmr="baseline",
+            )
+
+        def create_data(self, context, data=None):
+            building_b = context.baseline
+            building_p = context.proposed
+            climate_zone = data["climate_zone"]
+
+            window_wall_areas_dictionary_b = get_area_type_window_wall_area_dict(climate_zone, building_b)
+            window_wall_areas_dictionary_p = get_area_type_window_wall_area_dict(climate_zone, building_p)
+
+            return {
+                **data,
+                "total_fenestration_area_b": sum([window_wall_areas_dictionary_b[key]["total_window_area"]
+                                                  for key in window_wall_areas_dictionary_b.keys()]),
+                "total_fenestration_area_p": sum([window_wall_areas_dictionary_p[key]["total_window_area"]
+                                                  for key in window_wall_areas_dictionary_p.keys()]),
+                "surface_conditioning_category_dict_b": get_surface_conditioning_category_dict(
+                    data["climate_zone"], building_b
+                ),
+            }
+
+        def create_context_list(self, context, data=None):
+            building_b = context.baseline
+            building_p = context.proposed
+
+            scc = data["surface_conditioning_category_dict_b"]
+
+            baseline_surfaces = find_all("$..surfaces[*]", building_b)
+            proposed_surfaces = find_all("$..surfaces[*]", building_p)
+
+            matched_proposed_surfaces = match_lists_exactly_by_id(baseline_surfaces, proposed_surfaces)
+            proposed_baseline_surface_pairs = zip(baseline_surfaces, matched_proposed_surfaces)
+            # List of all baseline roof surfaces to become the context for RoofRule
+            return [
+                UserBaselineProposedVals(None, surface_b, surface_p)
+                for surface_b, surface_p in proposed_baseline_surface_pairs
+                if get_opaque_surface_type(surface_b) == OST.ABOVE_GRADE_WALL and scc[surface_b["id"]] != SCC.UNREGULATED
+            ]
+
+        class AboveGradeWallRule(RuleDefinitionBase):
+            def __init__(self):
+                super(Section5Rule21.BuildingRule.AboveGradeWallRule, self).__init__(
+                    rmrs_used=UserBaselineProposedVals(False, True, True),
+                    required_fields={
+                        "$": ["construction"],
+                        "construction": ["u_factor"],
+                    },
+                )
+
+            @staticmethod
+            def _helper_calc_val(fenestration_area_surface, surface):
+                for subsurface in surface["subsurfaces"]:
+                    subsurface_classification = getattr_(subsurface, "subsurface", "classification")
+                    glazed_area = getattr_(subsurface, "subsurface", "glazed_area")
+                    opaque_area = getattr_(subsurface, "subsurface", "opaque_area")
+                    if subsurface_classification == DOOR and glazed_area > opaque_area:
+                        fenestration_area_surface += (glazed_area + opaque_area)
+                    else:
+                        fenestration_area_surface += (glazed_area + opaque_area)
+
+            def get_calc_vals(self, context, data=None):
+                above_grade_wall_b = context.baseline
+                above_grade_wall_p = context.proposed
+
+                if (above_grade_wall_b.get["subsurfaces"] ^ above_grade_wall_p.get["subsurfaces"]) or \
+                        (data["total_fenestration_area_b"] == 0.0 ^ data["total_fenestration_area_p"] == 0.0):
+                    # baseline & proposed mismatch:
+                    # baseline has subsurface xor proposed has subsurface or
+                    # baseline fenestration area == 0 xor proposed fenestration area == 0
+                    return {
+                        "total_fenestration_area_surface_b": None,
+                        "total_fenestration_area_b": None,
+                        "total_fenestration_area_surface_p": None,
+                        "total_fenestration_area_p": None
+                    }
+
+                total_fenestration_area_surface_b = 0.0
+                total_fenestration_area_b = data["total_fenestration_area_b"]
+                total_fenestration_area_surface_p = 0.0
+                total_fenestration_area_p = data["total_fenestration_area_p"]
+
+                calc_val = {
+                    "total_fenestration_area_surface_b": total_fenestration_area_surface_b,
+                    "total_fenestration_area_b": total_fenestration_area_b,
+                    "total_fenestration_area_surface_p": total_fenestration_area_surface_p,
+                    "total_fenestration_area_p": total_fenestration_area_p
+                }
+
+                if above_grade_wall_b.get["subsurfaces"] is None:
+                    # wall has no fenestration surfaces
+                    return {
+                        **calc_val
+                    }
+
+                try:
+                    self._helper_calc_val(total_fenestration_area_surface_b, above_grade_wall_b)
+                    self._helper_calc_val(total_fenestration_area_surface_p, above_grade_wall_p)
+                except MissingKeyException as e:
+                    error = f"Missing key in object: {str(e)}, rule check stopped."
+                    return {
+                        "error": error
+                    }
+
+                return {
+                    **calc_val
+                }
+
+            def manual_check_required(self, context, calc_vals=None, data=None):
+                return "error" in calc_vals.keys()
+
+            def rule_check(self, context, calc_vals=None, data=None):
+                if calc_vals["total_fenestration_area_surface_b"] is None and calc_vals["total_fenestration_area_surface_p"] is None \
+                        and calc_vals["total_fenestration_area_b"] is None and calc_vals["total_fenestration_area_p"] is None:
+                    # Failed case where baseline mismatch proposed
+                    return False
+                else:
+                    total_fenestration_area_surface_b = calc_vals["total_fenestration_area_surface_b"]
+                    total_fenestration_area_surface_p = calc_vals["total_fenestration_area_surface_p"]
+                    total_fenestration_area_b = calc_vals["total_fenestration_area_b"]
+                    total_fenestration_area_p = calc_vals["total_fenestration_area_p"]
+
+                    if total_fenestration_area_b == 0.0 and total_fenestration_area_p == 0.0:
+                        # No fenestration in this building, avoid 0.0 division
+                        return True
+
+                    return (total_fenestration_area_surface_b / total_fenestration_area_b) == \
+                           (total_fenestration_area_surface_p / total_fenestration_area_p)
+
+
+
+
+
+
+
+
--- rct229/utils/assertions.py
+++ rct229/utils/assertions.py
@@ -1,6 +1,12 @@
 from rct229.utils.jsonpath_utils import find_all
 
 
+class MissingKeyException(Exception):
+    def __init__(self, object_name, obj_id, first_key):
+        message = f"{object_name}:{obj_id} is missing {first_key} field"
+        super().__init__(message)
+
+
 def assert_(bool, err_msg):
     assert bool, err_msg
 
@@ -16,7 +22,7 @@ def assert_required_fields(req_fields, obj):
         for element in find_all(jpath, obj):
             for field in fields:
                 assert (
-                    field in element
+                        field in element
                 ), f"Missing {field} in {jpath} id:{element.get('id')}"
 
 
@@ -45,7 +51,9 @@ def getattr_(obj, obj_name: str, first_key, *remaining_keys):
     AssertionError if the key path does not exist. The error message indicates what
     field was missing.
     """
-    assert first_key in obj, f"{obj_name}:{obj['id']} is missing {first_key} field"
+    if first_key not in obj:
+        raise MissingKeyException(obj_name, obj["id"], first_key)
+    # assert (first_key in obj, f"{obj_name}:{obj['id']} is missing {first_key} field")
     val = obj[first_key]
 
     return (
