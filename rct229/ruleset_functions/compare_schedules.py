eff92c47ba8885e40e333dac02ec8c89d0dcdd0b
Weili Xu <weili.xu@techprefab.com>
2022-04-13 19:15:47 -0400

    add some more explaination to compare schedule

--- rct229/ruleset_functions/compare_schedules.py
+++ rct229/ruleset_functions/compare_schedules.py
@@ -1,15 +1,25 @@
+from rct229.utils.assertions import RCTFailureException
 
-def compare_schedules(schedule_1, schedule_2, mask_schedule, comparison_factor, is_leap_year):
+REGULAR_YEAR_HOUR = 8760
+LEAP_YEAR_HOUR = 8784
+
+
+def compare_schedules(schedule_1: list, schedule_2: list, mask_schedule: list, comparison_factor: float, is_leap_year: bool):
     """compare two schedules and determine if they match with or without a comparison factor when applicable
         NOTE: The function only works with hourly schedule for now.
 
         Parameters
         ----------
-        schedule_1: First schedule
-        schedule_2: Second schedule
-        mask_schedule: The schedule that defines comparison mode for all hours (8760 or 8784) in a year, i.e. if hourly value is 1, schedule_1 is evaluated to be equal to schedule_2; if hourly value is 2, schedule_1 is evaluated to be equal to schedule_2 times the comparison factor; if hourly value is 0, comparison was skipped for that particular hour (example when evaluating shut off controls, only he building closed hrs are evaluated).
-        comparison_factor: The target multiplier number for schedule_1 compared to schedule_2, i.e. when applicable, the hourly value in schedule_1 shall be equal to that in schedule_2 times the comparison_factor.
-        is_leap_year: boolean, indicate whether the comparison is in a leap year or not.
+        schedule_1: list, First schedule [0.187, 0.187, 0.187, ... ]
+        schedule_2: list, Second schedule [0.187, 0.187, 0.187, ...]
+        mask_schedule: list, The schedule that defines comparison mode for all hours (8760 or 8784) in a year, i.e.
+            if hourly value is 1, schedule_1 is evaluated to be equal to schedule_2;
+            if hourly value is 2, schedule_1 is evaluated to be equal to schedule_2 times the comparison factor;
+            if hourly value is 0, comparison was skipped for that particular hour
+            (example when evaluating shut off controls, only he building closed hrs are evaluated) [1,1,1,1,1...]
+        comparison_factor: float, The target multiplier number for schedule_1 compared to schedule_2, i.e. when applicable,
+            the hourly value in schedule_1 shall be equal to that in schedule_2 times the comparison_factor. 1.0
+        is_leap_year: bool, indicate whether the comparison is in a leap year or not. True / False
 
         Returns
         -------
@@ -18,7 +28,13 @@ def compare_schedules(schedule_1, schedule_2, mask_schedule, comparison_factor, 
         EFLH difference for schedule_1 and schedule_2,
         i.e. {
             "TOTAL_HOURS_COMPARED": total_hours_compared,
-             "TOTAL_HOURS_MATCH": total_hours_match,
-              "EFLH_DIFFERENCE: EFLH_difference
+            "TOTAL_HOURS_MATCH": total_hours_match,
+            "EFLH_DIFFERENCE: EFLH_difference
             }
         """
+    num_hours = REGULAR_YEAR_HOUR
+    if is_leap_year:
+        num_hours = LEAP_YEAR_HOUR
+
+    if len(schedule_1) != len(schedule_2) and len(schedule_1) != len(mask_schedule) and len(schedule_1) != num_hours:
+        raise RCTFailureException(f"Hourly schedules has number of hours mismatched or do not meet the target number of hours. schedule_1 : {len(schedule_1)}; schedlue_2: {len(schedule_2)}")
