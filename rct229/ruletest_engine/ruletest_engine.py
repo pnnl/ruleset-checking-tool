f1a2d01039ada4a737acc293e561ee8aedc069a0
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-05-18 15:52:40 -0700

    no message

--- rct229/ruletest_engine/ruletest_engine.py
+++ rct229/ruletest_engine/ruletest_engine.py
@@ -500,5 +500,5 @@ def run_envelope_tests():
     """
 
     envelope_test_json = "envelope_tests.json"
-    envelope_test_json = "section5/rule_5_49.json"
+
     return run_section_tests(envelope_test_json)
