97e402e00cd5e644818d8e1fe10117379ffddd6d
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-05-18 15:47:30 -0700

    no message

--- rct229/ruletest_engine/ruletest_engine.py
+++ rct229/ruletest_engine/ruletest_engine.py
@@ -249,9 +249,7 @@ def run_section_tests(test_json_name):
         import pprint
         # Evaluate rule and check for invalid RMRs
         evaluation_dict = evaluate_rule(rule, rmr_trio)
-        # print(json.dumps(evaluation_dict))
         pprint.pprint(evaluation_dict)
-        # pprint.pprint(evaluation_dict)
         invalid_rmrs_dict = evaluation_dict["invalid_rmrs"]
 
         # If invalid RMRs exist, fail this rule and append failed message
