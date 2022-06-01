246df2630eb56587801268b68cae014551783b59
Weili Xu <weili.xu@pnnl.gov>
2022-06-01 06:21:04 -0700

    update section 6 rule 3 according to the comments

--- rct229/rules/section6/section6rule3.py
+++ rct229/rules/section6/section6rule3.py
@@ -5,25 +5,26 @@ from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedV
 
 GUEST_ROOM = schema_enums["LightingSpaceType2019ASHRAE901TG37"].GUEST_ROOM.name
 DORMITORY_LIVING_QUARTERS = schema_enums["LightingSpaceType2019ASHRAE901TG37"].DORMITORY_LIVING_QUARTERS.name
-DWELLING_UNITS = schema_enums["LightingSpaceType2019ASHRAE901TG37"].DWELLING_UNIT.name
+DWELLING_UNIT = schema_enums["LightingSpaceType2019ASHRAE901TG37"].DWELLING_UNIT.name
+
 
 class Section6Rule3(RuleDefinitionListIndexedBase):
     """Rule 3 of ASHRAE 90.1-2019 Appendix G Section 6 (Lighting)"""
 
     def __init__(self):
         super(Section6Rule3, self).__init__(
-            rmrs_used=UserBaselineProposedVals(True, True, False),
+            rmrs_used=UserBaselineProposedVals(True, False, True),
             each_rule=Section6Rule3.SpaceRule(),
             index_rmr="proposed",
             id="6-3",
-            description="Spaces in proposed building with hardwired lighting, including Hotel/Motel Guest Rooms, Dormitory Living Quarters, Interior Lighting Power >= Table 9.6.1; For Dwelling Units, Interior Lighting Power >= 0.6W/sq.ft.",
+            description="Spaces in proposed building with hardwired lighting, including Hotel/Motel Guest Rooms, Dormitory Living Quarters, Interior Lighting Power >= Table 9.6.1 or the lighting design in the user model, whichever is greater; For Dwelling Units, Interior Lighting Power >= 0.6W/sq.ft. or the lighting design in the user model, whichever is greater",
             list_path="ruleset_model_instances[0].buildings[*].zones[*].spaces[*]"
         )
 
     class SpaceRule(RuleDefinitionBase):
         def __init__(self):
             super(Section6Rule3.SpaceRule, self).__init__(
-                rmrs_used=UserBaselineProposedVals(True, True, False),
+                rmrs_used=UserBaselineProposedVals(True, False, True),
                 required_fields={
                     "$": ["lighting_space_type", "interior_lighting"],
                     "interior_lighting": ["power_per_area"],
@@ -33,7 +34,7 @@ class Section6Rule3(RuleDefinitionListIndexedBase):
         def is_applicable(self, context, data=None):
             space_p = context.proposed
             lighting_space_type_p = space_p["lighting_space_type"]
-            return lighting_space_type_p in [GUEST_ROOM, DWELLING_UNITS, DORMITORY_LIVING_QUARTERS]
+            return lighting_space_type_p in [GUEST_ROOM, DWELLING_UNIT, DORMITORY_LIVING_QUARTERS]
 
         def get_calc_vals(self, context, data=None):
             space_p = context.proposed
@@ -43,19 +44,19 @@ class Section6Rule3(RuleDefinitionListIndexedBase):
             lighting_power_allowance_p = table_9_6_1_lookup(space_p["lighting_space_type"])
 
             # sum of space_p lighting power density
-            interior_lighting_space_p = sum(interior_lighting["power_per_area"] for interior_lighting in space_p["interior_lighting"])
-            interior_lighting_space_u = sum(interior_lighting["power_per_area"] for interior_lighting in space_u["interior_lighting"])
+            space_lighting_power_per_area_p = sum(interior_lighting["power_per_area"] for interior_lighting in space_p["interior_lighting"])
+            space_lighting_power_per_area_u = sum(interior_lighting["power_per_area"] for interior_lighting in space_u["interior_lighting"])
 
             return {
                 "lighting_power_allowance_p": lighting_power_allowance_p,
-                "interior_lighting_space_p": interior_lighting_space_p,
-                "interior_lighting_space_u": interior_lighting_space_u
+                "space_lighting_power_per_area_p": space_lighting_power_per_area_p,
+                "space_lighting_power_per_area_u": space_lighting_power_per_area_u
             }
 
         def rule_check(self, context, calc_vals=None, data=None):
             lighting_power_allowance_p = calc_vals["lighting_power_allowance_p"]
-            interior_lighting_space_p = calc_vals["interior_lighting_space_p"]
-            interior_lighting_space_u = calc_vals["interior_lighting_space_u"]
+            space_lighting_power_per_area_p = calc_vals["space_lighting_power_per_area_p"]
+            space_lighting_power_per_area_u = calc_vals["space_lighting_power_per_area_u"]
 
-            return interior_lighting_space_p == max(lighting_power_allowance_p, interior_lighting_space_u)
+            return space_lighting_power_per_area_p == max(lighting_power_allowance_p, space_lighting_power_per_area_u)
 
