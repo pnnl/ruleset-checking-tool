8947f376c5637ba59c23a0ccaffd8175475e9918
charliepnnl <charlie.holly@pnnl.com>
2022-06-21 18:02:55 -0700

    Debugged Rule6-4

--- rct229/rules/section6/section6rule4.py
+++ rct229/rules/section6/section6rule4.py
@@ -5,7 +5,7 @@ from rct229.rule_engine.rule_base import (
     RuleDefinitionListIndexedBase,
 )
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
-from rct229.ruleset_functions.get_avg_zone_ht import get_avg_zone_ht
+from rct229.ruleset_functions.get_avg_zone_height import get_avg_zone_height
 from rct229.ruleset_functions.get_building_segment_lighting_status_type_dict import (
     LightingStatusType,
     get_building_segment_lighting_status_type_dict,
@@ -24,7 +24,7 @@ class Section6Rule4(RuleDefinitionListIndexedBase):
     def __init__(self):
         super(Section6Rule4, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, True, True),
-            each_rule=Section6Rule7.BuildingSegmentRule(),
+            each_rule=Section6Rule4.BuildingSegmentRule(),
             index_rmr="baseline",
             id="6-4",
             description='Where a complete lighting system exists and where a lighting system has been designed and submitted with design documents, the baseline LPD is equal to expected value in Table G3.7. Where lighting neither exists nor is submitted with design documents, baseline LPD shall be determined in accordance with Table G3-7 for "Office-Open Plan" space type.',
@@ -33,7 +33,7 @@ class Section6Rule4(RuleDefinitionListIndexedBase):
 
     class BuildingSegmentRule(RuleDefinitionListIndexedBase):
         def __init__(self):
-            super(Section6Rule4.BuildingRule.BuildingSegmentRule, self).__init__(
+            super(Section6Rule4.BuildingSegmentRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, True),
                 each_rule=Section6Rule4.BuildingSegmentRule.ZoneRule(),
                 index_rmr="baseline",
@@ -51,12 +51,9 @@ class Section6Rule4(RuleDefinitionListIndexedBase):
 
         class ZoneRule(RuleDefinitionListIndexedBase):
             def __init__(self):
-                super(
-                    Section6Rule4.BuildingRule.BuildingSegmentRule.ZoneRule,
-                    self,
-                ).__init__(
+                super(Section6Rule4.BuildingSegmentRule.ZoneRule, self,).__init__(
                     rmrs_used=UserBaselineProposedVals(False, True, True),
-                    each_rule=Section6Rule4.BuildingRule.BuildingSegmentRule.ZoneRule.SpaceRule(),
+                    each_rule=Section6Rule4.BuildingSegmentRule.ZoneRule.SpaceRule(),
                     index_rmr="baseline",
                     list_path="spaces[*]",
                 )
@@ -67,75 +64,79 @@ class Section6Rule4(RuleDefinitionListIndexedBase):
                 # We will need this after Weili's update to table_G3_7_lookup()
                 return {"avg_zone_ht_b": get_avg_zone_ht(zone_b)}
 
-        class SpaceRule(RuleDefinitionBase):
-            def __init__(self):
-                super(
-                    Section6Rule4.BuildingRule.BuildingSegmentRule.SpaceRule,
-                    self,
-                ).__init__(
-                    fail_msg="P_RMR lighting status type is as-designed or as-existing. But lighting space type in B_RMR is not specified.",
-                    rmrs_used=UserBaselineProposedVals(False, True, True),
-                )
-
-            def get_calc_vals(self, context, data=None):
-                space_b = context.baseline
-                space_p = context.proposed
-                total_space_lpd_b = sum(
-                    find_all("interior_lighting[*].power_per_area", space_b)
-                )
-                space_lighting_status_type_p = data[
-                    "building_segment_lighting_status_type_dict_p"
-                ][space_p["id"]]
-                lpd_allowance_b = (
-                    table_G3_7_lookup(
-                        space_b["lighting_space_type"], data["avg_zone_ht_b"]
+            class SpaceRule(RuleDefinitionBase):
+                def __init__(self):
+                    super(
+                        Section6Rule4.BuildingSegmentRule.ZoneRule.SpaceRule,
+                        self,
+                    ).__init__(
+                        fail_msg="P_RMR lighting status type is as-designed or as-existing. But lighting space type in B_RMR is not specified.",
+                        rmrs_used=UserBaselineProposedVals(False, True, True),
                     )
-                    if "lighting_space_type" in space_b
-                    else table_G3_7_lookup(OFFICE_OPEN_PLAN, data["avg_zone_ht_b"])
-                )
 
-                return {
-                    "total_space_lpd_b": total_space_lpd_b,
-                    "space_lighting_status_type_p": space_lighting_status_type_p,
-                    "lpd_allowance_b": lpd_allowance_b,
-                }
+                def get_calc_vals(self, context, data=None):
+                    space_b = context.baseline
+                    space_p = context.proposed
+                    total_space_lpd_b = sum(
+                        find_all("interior_lighting[*].power_per_area", space_b)
+                    )
+                    space_lighting_status_type_p = data[
+                        "building_segment_lighting_status_type_dict_p"
+                    ][space_p["id"]]
+                    lpd_allowance_b = (
+                        table_G3_7_lookup(
+                            space_b["lighting_space_type"], data["avg_zone_ht_b"]
+                        )
+                        if "lighting_space_type" in space_b
+                        else table_G3_7_lookup(OFFICE_OPEN_PLAN, data["avg_zone_ht_b"])
+                    )
 
-            def rule_check(self, context, calc_vals=None, data=None):
-                space_b = context.baseline
-                lighting_space_type_b = space_b.get("lighting_space_type")
+                    return {
+                        "total_space_lpd_b": total_space_lpd_b,
+                        "space_lighting_status_type_p": space_lighting_status_type_p,
+                        "lpd_allowance_b": lpd_allowance_b,
+                    }
 
-                space_lighting_status_type_p = calc_vals["space_lighting_status_type_p"]
-                total_space_lpd_b = calc_vals["total_space_lpd_b"]
-                lpd_allowance_b = calc_vals["lpd_allowance_b"]
+                def rule_check(self, context, calc_vals=None, data=None):
+                    space_b = context.baseline
+                    lighting_space_type_b = space_b.get("lighting_space_type")
 
-                return (
-                    # Not Case 1
-                    not (
-                        space_lighting_status_type_p
+                    space_lighting_status_type_p = calc_vals[
+                        "space_lighting_status_type_p"
+                    ]
+                    total_space_lpd_b = calc_vals["total_space_lpd_b"]
+                    lpd_allowance_b = calc_vals["lpd_allowance_b"]
+
+                    return (
+                        # Not Case 1
+                        not (
+                            space_lighting_status_type_p
+                            == LightingStatusType.AS_DESIGNED_OR_AS_EXISTING
+                            and not lighting_space_type_b
+                        )
+                        # Passes for both values of space_lighting_status_type_p
+                        and (
+                            space_lighting_status_type_p
+                            in [
+                                LightingStatusType.AS_DESIGNED_OR_AS_EXISTING,
+                                LightingStatusType.NOT_YET_DESIGNED_OR_MATCH_TABLE_9_5_1,
+                            ]
+                            and std_equal(total_space_lpd_b, lpd_allowance_b)
+                        )
+                    )
+
+                def get_fail_msg(self, context, calc_vals=None, data=None):
+                    space_b = context.baseline
+                    lighting_space_type_b = space_b.get("lighting_space_type")
+
+                    space_lighting_status_type_p = calc_vals[
+                        "space_lighting_status_type_p"
+                    ]
+
+                    return (
+                        "P_RMR lighting status type is as-designed or as-existing. But lighting space type in B_RMR is not specified."
+                        if space_lighting_status_type_p
                         == LightingStatusType.AS_DESIGNED_OR_AS_EXISTING
                         and not lighting_space_type_b
+                        else ""
                     )
-                    # Passes for both values of space_lighting_status_type_p
-                    and (
-                        space_lighting_status_type_p
-                        in [
-                            LightingStatusType.AS_DESIGNED_OR_AS_EXISTING,
-                            LightingStatusType.NOT_YET_DESIGNED_OR_MATCH_TABLE_9_5_1,
-                        ]
-                        and std_equal(total_space_lpd_b, lpd_allowance_b)
-                    )
-                )
-
-            def get_fail_msg(self, context, calc_vals=None, data=None):
-                space_b = context.baseline
-                lighting_space_type_b = space_b.get("lighting_space_type")
-
-                space_lighting_status_type_p = calc_vals["space_lighting_status_type_p"]
-
-                return (
-                    "P_RMR lighting status type is as-designed or as-existing. But lighting space type in B_RMR is not specified."
-                    if space_lighting_status_type_p
-                    == LightingStatusType.AS_DESIGNED_OR_AS_EXISTING
-                    and not lighting_space_type_b
-                    else ""
-                )
