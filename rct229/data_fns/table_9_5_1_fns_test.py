5ed24d36e24dd0eea23e9d3158561b54aa8d9a1a
charliepnnl <charlie.holly@pnnl.com>
2022-06-29 15:43:00 -0700

    Remove unused schema_enum imports

--- rct229/data_fns/table_9_5_1_fns_test.py
+++ rct229/data_fns/table_9_5_1_fns_test.py
@@ -1,7 +1,6 @@
 import pytest
 
 from rct229.data import data
-from rct229.data.schema_enums import schema_enums
 from rct229.data_fns.table_9_5_1_fns import (
     lighting_space_type_enumeration_to_lpd_map,
     table_9_5_1_lookup,
