21f62b70f2367c9eac529e3f6b8bfcf1bec21e02
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-08-01 17:42:14 -0700

    Corrected accidentally changed files

--- rct229/rules/section6/section6rule2.py
+++ rct229/rules/section6/section6rule2.py
@@ -3,8 +3,6 @@ from rct229.data_fns.table_9_6_1_fns import table_9_6_1_lookup
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
-from rct229.utils.jsonpath_utils import find_all
-from rct229.utils.pint_utils import ZERO, pint_sum
 
 GUEST_ROOM = schema_enums["LightingSpaceType2019ASHRAE901TG37"].GUEST_ROOM
 DORMITORY_LIVING_QUARTERS = schema_enums[
@@ -22,8 +20,11 @@ class Section6Rule2(RuleDefinitionListIndexedBase):
             each_rule=Section6Rule2.SpaceRule(),
             index_rmr="proposed",
             id="6-2",
-            description="SSpaces in proposed building with hardwired lighting, including Hotel/Motel Guest Rooms, Dormitory Living Quarters, Interior Lighting Power >= Table 9.6.1; For Dwelling Units, Interior Lighting Power >= 0.6W/sq.ft.",
-            list_path="ruleset_model_instances[0].buildings[*].building_segments[*].zones[*].spaces[*]",
+            description="Spaces in proposed building with hardwired lighting, including Hotel/Motel Guest Rooms, "
+            "Dormitory Living Quarters, Interior Lighting Power >= Table 9.6.1 or the lighting design in "
+            "the user model, whichever is greater; For Dwelling Units, Interior Lighting Power >= "
+            "0.6W/sq.ft. or the lighting design in the user model, whichever is greater",
+            list_path="ruleset_model_instances[0].buildings[*].zones[*].spaces[*]",
         )
 
     class SpaceRule(RuleDefinitionBase):
@@ -32,14 +33,13 @@ class Section6Rule2(RuleDefinitionListIndexedBase):
                 rmrs_used=UserBaselineProposedVals(True, False, True),
                 required_fields={
                     "$": ["lighting_space_type", "interior_lighting"],
-                    "$.interior_lighting[*]": ["power_per_area"],
+                    "interior_lighting": ["power_per_area"],
                 },
             )
 
         def is_applicable(self, context, data=None):
             space_p = context.proposed
             lighting_space_type_p = space_p["lighting_space_type"]
-
             return lighting_space_type_p in [
                 GUEST_ROOM,
                 DWELLING_UNIT,
@@ -51,23 +51,18 @@ class Section6Rule2(RuleDefinitionListIndexedBase):
             space_u = context.user
 
             # get allowance
-            if space_p["lighting_space_type"] in [
-                GUEST_ROOM,
-                DORMITORY_LIVING_QUARTERS,
-            ]:
-                lighting_power_allowance_p = table_9_6_1_lookup(
-                    space_p["lighting_space_type"]
-                )["lpd"]
-            elif space_p["lighting_space_type"] == DWELLING_UNIT:
-                lighting_power_allowance_p = 0.6
-
-            space_lighting_power_per_area_p = pint_sum(
-                find_all("$..interior_lighting[*].power_per_area", space_p),
-                ZERO.POWER_PER_AREA,
+            lighting_power_allowance_p = table_9_6_1_lookup(
+                space_p["lighting_space_type"]
             )
-            space_lighting_power_per_area_u = pint_sum(
-                find_all("$..interior_lighting[*].power_per_area", space_u),
-                ZERO.POWER_PER_AREA,
+
+            # sum of space_p lighting power density
+            space_lighting_power_per_area_p = sum(
+                interior_lighting["power_per_area"]
+                for interior_lighting in space_p["interior_lighting"]
+            )
+            space_lighting_power_per_area_u = sum(
+                interior_lighting["power_per_area"]
+                for interior_lighting in space_u["interior_lighting"]
             )
 
             return {
