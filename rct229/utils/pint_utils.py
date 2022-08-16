e3019e3e24d5ecde428a1cdbf0cc889b9be1118f
charliepnnl <charlie.holly@pnnl.com>
2022-08-16 12:56:25 -0700

    UNIT_SYSTEM, CalcQ, calcq_to_q

--- rct229/utils/pint_utils.py
+++ rct229/utils/pint_utils.py
@@ -1,7 +1,17 @@
+from dataclasses import dataclass
 import functools
 import operator
 
+from pint import Quantity
+
 from rct229.schema.config import ureg
+
+
+class UNIT_SYSTEM:
+    """Class representing the two unit systems"""
+
+    IP = "IP"
+    SI = "SI"
 
 
 class ZERO:
@@ -18,6 +28,47 @@ class ZERO:
     U_FACTOR = ureg("Btu/(hr*ft2*degR)")
     UA = U_FACTOR * AREA
     FLOW = VOLUME / ureg("minute")
+
+
+@dataclass(frozen=True)
+class CalcQ:
+    """Class that encodes the type of quantity along with a pint Quantity"""
+
+    q_type: str
+    q: Quantity
+
+    def to_str(self, unit_system=UNIT_SYSTEM.IP) -> str:
+        return f"{self.q_type}: {unit_system}: {q}"
+
+
+def calcq_to_q(obj):
+    """Replaces a CalcQ object with its quantity. It will also walk any combination
+    of dicts and lists, replacing any CalcQ objects.
+
+    Any object other than a CalcQ, list, or dict is simply passed through unchanged.
+    This function does not mutate the passed obj.
+
+    Parameters
+    ----------
+    obj : any
+        The object to be converted
+
+    Returns
+    -------
+    any
+        The processed value.
+
+    """
+    if isinstance(obj, CalcQ):
+        retval = obj.q
+    elif isinstance(obj, list):
+        retval = [calcq_to_q(item) for item in obj]
+    elif isinstance(obj, dict):
+        retval = {key: calcq_to_q(value) for key, value in obj.items()}
+    else:
+        retval = obj
+
+    return retval
 
 
 def pint_sum(qty_list, default=None):
