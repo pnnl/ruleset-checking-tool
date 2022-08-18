77ee831ca844319748afd0d04b85f39627619316
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-08-18 15:59:02 -0700

    Reflected Weili's comments

--- rct229/rules/section6/section6rule7.py
+++ rct229/rules/section6/section6rule7.py
@@ -2,12 +2,15 @@ from rct229.data.schema_enums import schema_enums
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
-from rct229.utils.assertions import getattr_
 from rct229.utils.jsonpath_utils import find_all
 
 MSG_WARN_DAYLIGHT_NO_SCHEDULE = "Some of the spaces in zone are modeled with window(s) and/or skylight(s) and have daylighting controls modeled explicitly in the simulation tool. Verify that the mandatory lighting control requirements are met."
 MSG_WARN_DAYLIGHT = "Some of the spaces in zone are modeled with window(s) and/or skylight(s) and have daylighting controls modeled via schedule adjustment. Verify that the mandatory lighting control requirements are met, and that the supporting documentation is provided for the schedule adjustment."
 MSG_WARN_NO_DAYLIGHT = "Some of the spaces in zone are modeled with fenestration but no daylighting controls. The design must include mandatory daylighting controls unless any of the exceptions to 90.1 section 9.4.1.1€ apply."
+
+DOOR = schema_enums["SubsurfaceClassificationOptions"].DOOR
+EXTERIOR = schema_enums["SurfaceAdjacentToOptions"].EXTERIOR
+MONE = schema_enums["LightingDaylightingControlOptions"].NONE
 
 
 class Section6Rule7(RuleDefinitionListIndexedBase):
@@ -36,7 +39,7 @@ class Section6Rule7(RuleDefinitionListIndexedBase):
             daylight_flag_p = (
                 len(
                     find_all(
-                        "$..surfaces[?adjacent_to = 'EXTERIOR'].subsurfaces[?classification != 'DOOR']",
+                        "$..surfaces[?adjacent_to = EXTERIOR].subsurfaces[?classification != DOOR]",
                         zone_p,
                     )
                 )
@@ -46,21 +49,18 @@ class Section6Rule7(RuleDefinitionListIndexedBase):
             has_daylight_control_flag = (
                 len(
                     find_all(
-                        "$..spaces[*].interior_lighting[?daylighting_control_type!='NONE']",
+                        "$..spaces[*].interior_lighting[?daylighting_control_type!= NONE]",
                         zone_p,
                     )
                 )
                 > 0
             )
 
-            daylight_schedule_adjustment_flag = (
-                len(
-                    find_all(
-                        "$..spaces[*].interior_lighting[?are_schedules_used_for_modeling_daylighting_control == true]",
-                        zone_p,
-                    )
+            daylight_schedule_adjustment_flag = any(
+                find_all(
+                    "$..spaces[*].interior_lighting[?are_schedules_used_for_modeling_daylighting_control == true]",
+                    zone_p,
                 )
-                > 0
             )
 
             return {
@@ -76,19 +76,15 @@ class Section6Rule7(RuleDefinitionListIndexedBase):
             return daylight_flag_p and has_daylight_control_flag
 
         def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
-            daylight_flag_p = calc_vals["daylight_flag_p"]
-            has_daylight_control_flag = calc_vals["has_daylight_control_flag"]
             daylight_schedule_adjustment_flag = calc_vals[
                 "daylight_schedule_adjustment_flag"
             ]
 
-            if daylight_flag_p and has_daylight_control_flag:
-                if daylight_schedule_adjustment_flag:
-                    manual_check_msg = MSG_WARN_DAYLIGHT
-                else:
-                    manual_check_msg = MSG_WARN_DAYLIGHT_NO_SCHEDULE
-
-            return manual_check_msg
+            return (
+                MSG_WARN_DAYLIGHT
+                if daylight_schedule_adjustment_flag
+                else MSG_WARN_DAYLIGHT_NO_SCHEDULE
+            )
 
         def rule_check(self, context, calc_vals, data=None):
             daylight_flag_p = calc_vals["daylight_flag_p"]
