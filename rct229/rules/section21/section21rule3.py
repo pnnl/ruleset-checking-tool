234f7b9343067972d075f501a3b564fc48c75e9d
Weili Xu <weili.xu@pnnl.gov>
2022-08-22 12:14:01 -0700

    address comments

--- rct229/rules/section21/section21rule3.py
+++ rct229/rules/section21/section21rule3.py
@@ -5,8 +5,8 @@ from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedV
 from rct229.ruleset_functions.get_baseline_system_types import mock_get_baseline_system_types
 from rct229.utils.assertions import getattr_
 
-APPLICABLE_SYS_TYPE = ["SYS-1", "SYS-5", "SYS-7", "SYS-11.2", "SYS-12", "SYS-1A", "SYS-7A", "SYS-11.2A", "SYS-12A"]
-HEATING = schema_enums["FluidLoopOptions"].HEATING
+APPLICABLE_SYS_TYPES = ["SYS-1", "SYS-5", "SYS-7", "SYS-11.2", "SYS-12", "SYS-1A", "SYS-7A", "SYS-11.2A", "SYS-12A"]
+FLUID_LOOP = schema_enums["FluidLoopOptions"]
 
 
 class Section21Rule3(RuleDefinitionListIndexedBase):
@@ -18,18 +18,20 @@ class Section21Rule3(RuleDefinitionListIndexedBase):
             index_rmr="baseline",
             id="21-3",
             description="Heating hot water plant capacity shall be based on coincident loads.",
-            rmr_context="ruleset_model_instances/0/fluid_loops",
+            list_path="ruleset_model_instances[0].fluid_loops[*]",
         )
 
     def is_applicable(self, context, data=None):
         rmd_b = context.baseline
-        baseline_system_types = mock_get_baseline_system_types(rmd_b)
-        # if any system type found in the APPLICABLE_SYS_TYPE then return applicable.
-        return any([key in APPLICABLE_SYS_TYPE for key in baseline_system_types.keys()])
+        # FIXME: replace with baseline_system_types = get_baseline_system_types(rmd_b) when get_baseline_system_types
+        #  is ready.
+        baseline_system_types = {"SYS-7A": ["hvac_sys_7_a"], "SYS-11A": ["hvac_sys_11_a"]}
+        # if any system type found in the APPLICABLE_SYS_TYPES then return applicable.
+        return any([key in APPLICABLE_SYS_TYPES for key in baseline_system_types.keys()])
 
     def list_filter(self, context_item, data):
         fluid_loop_b = context_item.baseline
-        return getattr_(fluid_loop_b, "FluidLoop", "type") == HEATING
+        return getattr_(fluid_loop_b, "FluidLoop", "type") == FLUID_LOOP.HEATING
 
     class HeatingFluidLoopRule(RuleDefinitionBase):
         def __init__(self):
@@ -43,9 +45,9 @@ class Section21Rule3(RuleDefinitionListIndexedBase):
 
         def get_calc_vals(self, context, data=None):
             heating_fluid_loop_b = context.baseline
-            heating_design_and_control_b = heating_fluid_loop_b["heating_design_and_control"]
+            is_sized_using_coincident_load = heating_fluid_loop_b["heating_design_and_control"]["is_sized_using_coincident_load"]
             return {
-                "is_sized_using_coincident_load": heating_design_and_control_b["is_sized_using_coincident_load"]
+                "is_sized_using_coincident_load": is_sized_using_coincident_load
             }
 
         def rule_check(self, context, calc_vals=None, data=None):
--- rct229/ruleset_functions/get_baseline_system_types.py
+++ rct229/ruleset_functions/get_baseline_system_types.py
@@ -1,4 +1,5 @@
-def mock_get_baseline_system_types(rmd):
+def get_baseline_system_types(rmd):
+    # dummy function - more will be added.
     return {
         "SYS-7A": ["hvac_sys_7_a"], "SYS-11A": ["hvac_sys_11_a"]
     }
