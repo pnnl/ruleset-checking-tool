e2fae7c9865f3b56b327bde528bfc7703ffef3dc
Weili Xu <weili.xu@pnnl.gov>
2022-09-02 08:48:53 -0700

    Complete the implementation of heating system fluid loop

--- rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/is_hvac_sys_heating_type_fluid_loop.py
+++ rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/is_hvac_sys_heating_type_fluid_loop.py
@@ -1,6 +1,4 @@
 from rct229.data.schema_enums import schema_enums
-from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_there_only_one_hvac_sys_heating_system import \
-    is_there_only_one_hvac_sys_heating_system
 from rct229.utils.assertions import getattr_
 from rct229.utils.jsonpath_utils import find_exactly_one_with_field_value
 
@@ -26,18 +24,16 @@ def is_hvac_sys_heating_type_fluid_loop(rmi_b, hvac_b_id):
     """
     is_hvac_sys_heating_type_fluid_loop_flag = False
 
-    if is_there_only_one_hvac_sys_heating_system(rmi_b, hvac_b_id):
-        hvac_b = find_exactly_one_with_field_value(
-            "$.buildings[*].building_segments[*].heating_ventilation_air_conditioning_systems",
-            "id",
-            hvac_b_id,
-            rmi_b,
-        )
-        heating_system = hvac_b.get("heating_system")
-        if heating_system:
-            if heating_system.get("hot_water_loop") is not None and getattr_(heating_system, "heating_system",
-                                                                             "heating_system_type") == HEATING_SYSTEM.FLUID_LOOP:
-                is_hvac_sys_heating_type_fluid_loop_flag = True
+    hvac_b = find_exactly_one_with_field_value(
+        "$.buildings[*].building_segments[*].heating_ventilation_air_conditioning_systems",
+        "id",
+        hvac_b_id,
+        rmi_b,
+    )
+    heating_system = hvac_b.get("heating_system")
+    if heating_system:
+        if heating_system.get("hot_water_loop") is not None and getattr_(heating_system, "heating_system", "heating_system_type") == HEATING_SYSTEM.FLUID_LOOP:
+            is_hvac_sys_heating_type_fluid_loop_flag = True
 
     return is_hvac_sys_heating_type_fluid_loop_flag
 
