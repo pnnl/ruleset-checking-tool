b9a9920d37ed60ef957e7979d61c05713a5f3410
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-09-08 08:31:18 -0700

    Add/Update rule 22-1

--- rct229/rules/section22/section22rule1.py
+++ rct229/rules/section22/section22rule1.py
@@ -1,13 +1,37 @@
-from rct229.data.schema_enums import schema_enums
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.schema.config import ureg
 from rct229.utils.assertions import getattr_
 from rct229.utils.jsonpath_utils import find_all
-from rct229.utils.std_comparisons import std_equal
 
-APPLICABLE_SYS_TYPES = ["SYS-7", "SYS-8", "SYS-11.1", "SYS-11.2", "SYS-12", "SYS-13", "SYS-1A", "SYS-3A", "SYS-7A", "SYS-8A", "SYS-11.1A", "SYS-11.2A", "SYS-12A", "SYS-13A", "SYS-7B", "SYS-8B", "SYS-11B", "SYS-12B", "SYS-13B", "SYS-1C", "SYS-3C", "SYS-7C", "SYS-11C", "SYS-12C", "SYS-13C"]
+APPLICABLE_SYS_TYPES = [
+    "SYS-7",
+    "SYS-8",
+    "SYS-11.1",
+    "SYS-11.2",
+    "SYS-12",
+    "SYS-13",
+    "SYS-1A",
+    "SYS-3A",
+    "SYS-7A",
+    "SYS-8A",
+    "SYS-11.1A",
+    "SYS-11.2A",
+    "SYS-12A",
+    "SYS-13A",
+    "SYS-7B",
+    "SYS-8B",
+    "SYS-11B",
+    "SYS-12B",
+    "SYS-13B",
+    "SYS-1C",
+    "SYS-3C",
+    "SYS-7C",
+    "SYS-11C",
+    "SYS-12C",
+    "SYS-13C",
+]
 DESIGN_SUPPLY_TEMP = 44 * ureg("degF")
 
 
@@ -30,7 +54,7 @@ class Section22Rule1(RuleDefinitionListIndexedBase):
         chillers = find_all("$.chillers[*]", rmi_b)
         loop_chiller_dict = {}
         for chiller_b in chillers:
-            loop_id = getattr_(chiller_b, "chiller", "loop")
+            loop_id = getattr_(chiller_b, "chillers", "cooling_loop")
             if not loop_id in loop_chiller_dict.keys():
                 loop_chiller_dict[loop_id] = []
             loop_chiller_dict[loop_id].append(chiller_b)
@@ -62,12 +86,11 @@ class Section22Rule1(RuleDefinitionListIndexedBase):
 
         def get_calc_vals(self, context, data=None):
             fluid_loop_b = context.baseline
-            design_supply_temperature = fluid_loop_b["heating_design_and_control"][
-                "design_supply_temperature"
-            ]
+            design_supply_temperature = fluid_loop_b[
+                "cooling_or_condensing_design_and_control"
+            ]["design_supply_temperature"]
 
             return {"design_supply_temperature": design_supply_temperature}
-
 
         def rule_check(self, context, calc_vals=None, data=None):
             design_supply_temperature = calc_vals["design_supply_temperature"]
