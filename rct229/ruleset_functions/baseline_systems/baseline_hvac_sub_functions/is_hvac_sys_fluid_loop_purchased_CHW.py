8d37d3c63f0e1b5ab8c332b79e9cd5b661e50465
Weili Xu <weili.xu@pnnl.gov>
2022-09-09 13:39:06 -0700

    Update addressing the comments

--- rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/is_hvac_sys_fluid_loop_purchased_CHW.py
+++ rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/is_hvac_sys_fluid_loop_purchased_CHW.py
@@ -18,17 +18,15 @@ def is_hvac_sys_fluid_loop_purchased_chw(rmi_b, hvac_b_id):
         True: the fluid loop associated with the cooling system associated with the HVAC system is attached to an external purchased cooling loop
         False: otherwise
     """
-    is_hvac_sys_fluid_loop_purchased_chw_flag = False
-    purchased_cooling_loop_list_b = []
+    purchased_cooling_loop_id_list_b = []
 
     external_fluid_sources = rmi_b.get("external_fluid_source")
     if external_fluid_sources:
         for external_fluid_source in external_fluid_sources:
             if (
                 external_fluid_source.get("type") == EXTERNAL_FLUID_SOURCE.CHILLED_WATER
-                and external_fluid_source.get("loop") is not None
             ):
-                purchased_cooling_loop_list_b.append(external_fluid_source["loop"])
+                purchased_cooling_loop_id_list_b.append(external_fluid_source["loop"])
 
     # Get the hvac system
     hvac_b = find_exactly_one_with_field_value(
@@ -37,11 +35,10 @@ def is_hvac_sys_fluid_loop_purchased_chw(rmi_b, hvac_b_id):
         hvac_b_id,
         rmi_b,
     )
-    # Check if hvac_b has preheat system
     cooling_system = hvac_b.get("cooling_system")
     is_hvac_sys_fluid_loop_purchased_chw_flag = (
         cooling_system is not None
-        and cooling_system.get("chilled_water_loop") in purchased_cooling_loop_list_b
+        and cooling_system.get("chilled_water_loop") in purchased_cooling_loop_id_list_b
     )
 
     return is_hvac_sys_fluid_loop_purchased_chw_flag
