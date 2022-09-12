2956503598388f10381d9de36a54b6051248cee8
Weili Xu <weili.xu@pnnl.gov>
2022-09-12 01:13:47 -0400

    Update are_all_terminal_heating_loops_attached_to_boiler.py

--- rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/are_all_terminal_heating_loops_attached_to_boiler.py
+++ rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/are_all_terminal_heating_loops_attached_to_boiler.py
@@ -1,6 +1,11 @@
 from rct229.data.schema_enums import schema_enums
 from rct229.utils.assertions import getattr_
-from rct229.utils.jsonpath_utils import find_all, find_exactly_one_with_field_value
+from rct229.utils.jsonpath_utils import (
+    find_all,
+    find_exactly_one_with_field_value,
+    find_one,
+    find_one_with_field_value,
+)
 
 FLUID_LOOP_TYPE = schema_enums["FluidLoopOptions"]
 
@@ -22,13 +27,10 @@ def are_all_terminal_heating_loops_attached_to_boiler(rmi_b, terminal_unit_id_li
         False: otherwise
     """
     are_all_terminal_heating_loops_attached_to_boiler_flag = True
-    boilers = find_all("$.boilers[*]", rmi_b)
-    loop_boiler_dict = dict()
-    for boiler_b in boilers:
-        loop_id = getattr_(boiler_b, "boiler", "loop")
-        if not loop_id in loop_boiler_dict.keys():
-            loop_boiler_dict[loop_id] = []
-        loop_boiler_dict[loop_id].append(boiler_b)
+    loop_boiler_id_list = [
+        getattr_(boiler_b, "boiler", "loop")
+        for boiler_b in find_all("$.boilers[*]", rmi_b)
+    ]
 
     for terminal_b_id in terminal_unit_id_list:
         terminal_b = find_exactly_one_with_field_value(
@@ -37,21 +39,20 @@ def are_all_terminal_heating_loops_attached_to_boiler(rmi_b, terminal_unit_id_li
             terminal_b_id,
             rmi_b,
         )
-        heating_from_loop = terminal_b.get("heating_from_loop")
-        if heating_from_loop:
-            fluid_loop = find_exactly_one_with_field_value(
-                "$.fluid_loops",
-                "id",
-                heating_from_loop,
-                rmi_b,
-            )
-            if (
-                getattr_(fluid_loop, "fluid loop", "type") != FLUID_LOOP_TYPE.HEATING
-                or heating_from_loop not in loop_boiler_dict.keys()
-            ):
-                are_all_terminal_heating_loops_attached_to_boiler_flag = False
-                break
-        else:
+        heating_from_loop_id = terminal_b.get("heating_from_loop")
+        # return None if terminal_b has no heating_from_loop field, or
+        # the heating_from_loop id cannot be found in fluid_loops
+        fluid_loop = find_one_with_field_value(
+            "$.fluid_loops",
+            "id",
+            heating_from_loop_id,
+            rmi_b,
+        )
+        # cond 1 allows heating_from_loop_id to be None, cond 2 allows fluid_loop to be None.
+        if (
+            heating_from_loop_id not in loop_boiler_id_list
+            or find_one("type", fluid_loop) != FLUID_LOOP_TYPE.HEATING
+        ):
             are_all_terminal_heating_loops_attached_to_boiler_flag = False
             break
 
