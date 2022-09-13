34d079ca45dace42d823c15534ce36b8a3c1d446
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-09-12 22:47:05 -0700

    reformat

--- rct229/rules/section21/section21rule7.py
+++ rct229/rules/section21/section21rule7.py
@@ -1,9 +1,7 @@
 from rct229.data.schema_enums import schema_enums
 from rct229.rule_engine.rule_base import RuleDefinitionBase
-from rct229.rule_engine.rule_list_indexed_base import \
-    RuleDefinitionListIndexedBase
-from rct229.rule_engine.user_baseline_proposed_vals import \
-    UserBaselineProposedVals
+from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
+from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.schema.config import ureg
 from rct229.utils.jsonpath_utils import find_all
 from rct229.utils.pint_utils import CalcQ
@@ -83,8 +81,12 @@ class Section21Rule7(RuleDefinitionListIndexedBase):
                 "design_return_temperature"
             ]
             return {
-                "design_supply_temperature": CalcQ("temperature", design_supply_temperature),
-                "design_return_temperature": CalcQ("temperature", design_return_temperature),
+                "design_supply_temperature": CalcQ(
+                    "temperature", design_supply_temperature
+                ),
+                "design_return_temperature": CalcQ(
+                    "temperature", design_return_temperature
+                ),
             }
 
         def rule_check(self, context, calc_vals=None, data=None):
