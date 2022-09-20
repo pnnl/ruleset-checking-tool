9ca95089470325628577b4e14178178d30f09d54
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-09-20 14:44:42 -0700

    Update the 6-1 JSON file

--- rct229/ruletest_engine/run_ruletests.py
+++ rct229/ruletest_engine/run_ruletests.py
@@ -1,8 +1,8 @@
 from rct229.ruletest_engine.ruletest_engine import *
 
 # run_transformer_tests()
-run_lighting_tests()
+# run_lighting_tests()
 # run_lighting_tests()
 # run_boiler_tests()
-# run_chiller_tests()
+run_chiller_tests()
 # run_envelope_tests()
