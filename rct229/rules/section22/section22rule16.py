4cd1c1628c887f5e66290de8aaf5d6fbc0fca9ce
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-09-26 18:25:53 -0700

    Finish 22-16

--- rct229/rules/section22/section22rule16.py
+++ rct229/rules/section22/section22rule16.py
@@ -2,7 +2,10 @@ from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.schema.config import ureg
-from rct229.utils.jsonpath_utils import find_all, find_exactly_one_with_field_value, find_one
+from rct229.utils.jsonpath_utils import (
+    find_all,
+    find_exactly_one_with_field_value,
+)
 from rct229.utils.pint_utils import CalcQ
 from rct229.utils.std_comparisons import std_equal
 
@@ -45,7 +48,6 @@ class Section22Rule16(RuleDefinitionListIndexedBase):
             "SYS-7": ["hvac_sys_7"],
             "SYS-11B": ["hvac_sys_11_b"],
         }
-
         # if any system type found in the APPLICABLE_SYS_TYPES and wet-bulb temp is in the correct range then return applicable.
         return any(
             [key in APPLICABLE_SYS_TYPES for key in baseline_system_types.keys()]
@@ -60,17 +62,21 @@ class Section22Rule16(RuleDefinitionListIndexedBase):
 
     def create_data(self, context, data):
         rmi_b = context.baseline
-        chiller_loop_ids = find_all("chillers[*].cooling_loop", rmi_b)
-        b = find_one("$..fluid_loops[*].id", "Chiller Loop 1")
-        a = find_exactly_one_with_field_value("$..fluid_loops[*].cooling_or_condensing_design_and_control", "design_supply_temperature", "Chiller Loop 1", rmi_b)
-        return {"loop_chiller_dict": chiller_loop_ids}
+        supply_temperature_b_dict = {
+            heat_rejection_loop: find_exactly_one_with_field_value(
+                "$..fluid_loops[*]", "id", heat_rejection_loop, rmi_b
+            )["cooling_or_condensing_design_and_control"]["design_supply_temperature"]
+            for heat_rejection_loop in find_all("heat_rejections[*].loop", rmi_b)
+        }
+
+        return {"supply_temperature_b_dict": supply_temperature_b_dict}
 
     class ChillerHeatRejectionRule(RuleDefinitionBase):
         def __init__(self):
             super(Section22Rule16.ChillerHeatRejectionRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, False),
                 required_fields={
-                    "$": ["range"],
+                    "$": ["design_wetbulb_temperature", "approach", "loop"],
                 },
             )
 
@@ -78,22 +84,19 @@ class Section22Rule16(RuleDefinitionListIndexedBase):
             heat_rejection_b = context.baseline
             wbt_b = heat_rejection_b["design_wetbulb_temperature"]
             approach_b = heat_rejection_b["approach"]
-
-            cw_loop_id_b = heat_rejection_b["loop"]
-            supply_temperature_b = 1
-
-
+            loop_b = heat_rejection_b["loop"]
             return {
                 "wbt_b": CalcQ("temperature", wbt_b),
                 "approach_b": CalcQ("temperature", approach_b),
-                "supply_temperature_b": CalcQ("temperature", supply_temperature_b),
+                "loop_b": loop_b,
             }
 
         def rule_check(self, context, calc_vals=None, data=None):
             wbt_b = calc_vals["wbt_b"]
             approach_b = calc_vals["approach_b"]
-            supply_temperature_b = calc_vals["supply_temperature_b"]
+            loop_b = calc_vals["loop_b"]
+            supply_temperature_b = data["supply_temperature_b_dict"][loop_b]
             return std_equal(
                 supply_temperature_b.to(ureg.kelvin),
-                wbt_b.to(ureg.kelvin) + approach_b.to(ureg.kelvin),
+                (wbt_b + approach_b).to(ureg.kelvin),
             )
