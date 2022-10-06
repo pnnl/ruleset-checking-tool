a567e62d70879d5acdadc45d8be0e85210c7900a
Weili Xu <weili.xu@pnnl.gov>
2022-10-06 15:31:28 -0400

    update to address comments

--- rct229/data_fns/table_3_5_3_fns.py
+++ rct229/data_fns/table_3_5_3_fns.py
@@ -20,7 +20,7 @@ water_chiller_compressor_type_map = {
 
 # minimum capacity and maximum capacity in the ashrae_90_1_table_3_5_3
 minimum_capacity_threshold_list = [0, 150, 300]
-maximum_capacity_threshold_list = [9999.99, 299.99, 149.99]
+maximum_capacity_threshold_list = [9999.99, 300, 150]
 
 
 def table_3_5_3_lookup(compressor_type, capacity):
@@ -46,21 +46,25 @@ def table_3_5_3_lookup(compressor_type, capacity):
         if capacity > min_threshold:
             minimum_capacity = min_threshold
 
-    maximum_capacity = maximum_capacity_threshold_list[-1]
+    maximum_capacity = maximum_capacity_threshold_list[0]
     for max_threshold in maximum_capacity_threshold_list:
         if capacity < max_threshold:
             maximum_capacity = max_threshold
 
     osstd_entry = find_osstd_table_entry(
         [
-            ("cooling_type", compressor_category),
-            ("minimum_capacity", minimum_capacity),
-            ("maximum_capacity", maximum_capacity),
+            ("equipment_type", compressor_category),
+            ("inclusive_minimum_capacity_tons", minimum_capacity),
+            ("exclusive_maximum_capacity_tons", maximum_capacity),
         ],
         osstd_table=data["ashrae_90_1_table_G3_5_3"],
     )
-    minimum_full_load_efficiency_val = osstd_entry["minimum_full_load_efficiency"]
-    minimum_integrated_part_load_val = osstd_entry["minimum_integrated_part_load_value"]
+    minimum_full_load_efficiency_val = osstd_entry[
+        "minimum_full_load_efficiency_kw_per_ton"
+    ]
+    minimum_integrated_part_load_val = osstd_entry[
+        "minimum_integrated_part_load_kw_per_ton"
+    ]
 
     minimum_full_load_efficiency = minimum_full_load_efficiency_val * ureg(
         "kilowatt / ton"
@@ -70,6 +74,6 @@ def table_3_5_3_lookup(compressor_type, capacity):
     )
 
     return {
-        "minimum_full_load_efficiency": minimum_full_load_efficiency,
-        "minimum_integrated_part_load": minimum_integrated_part_load,
+        "minimum_full_load_efficiency_kw_per_ton": minimum_full_load_efficiency,
+        "minimum_integrated_part_load_kw_per_ton": minimum_integrated_part_load,
     }
