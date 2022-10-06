ad4cf944eb19337699570d2e56241f026cccb590
Weili Xu <weili.xu@pnnl.gov>
2022-10-06 16:42:59 -0400

    Update addressing the comments

--- rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/are_all_terminal_heat_sources_electric.py
+++ rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/are_all_terminal_heat_sources_electric.py
@@ -1,6 +1,6 @@
 from rct229.data.schema_enums import schema_enums
 from rct229.ruleset_functions.baseline_systems.baseline_system_util import (
-    find_exact_one_terminal_unit,
+    find_exactly_one_terminal_unit,
 )
 
 HEATING_SOURCE = schema_enums["HeatingSourceOptions"]
@@ -23,11 +23,8 @@ def are_all_terminal_heat_sources_electric(rmi_b, terminal_unit_id_list):
         True: heat source associated with all terminal units input to this function are electric
         False: any terminal unit has a heat source other than electric
     """
-    are_all_terminal_heat_sources_electric_flag = True
-    for terminal_b_id in terminal_unit_id_list:
-        terminal_b = find_exact_one_terminal_unit(rmi_b, terminal_b_id)
-        if terminal_b.get("heating_source") != HEATING_SOURCE.ELECTRIC:
-            are_all_terminal_heat_sources_electric_flag = False
-            break
-
-    return are_all_terminal_heat_sources_electric_flag
+    return all(
+        find_exactly_one_terminal_unit(rmi_b, terminal_b_id).get("heating_source")
+        == HEATING_SOURCE.ELECTRIC
+        for terminal_b_id in terminal_unit_id_list
+    )
--- rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/test_are_all_terminal_heat_sources_electric.py
+++ rct229/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/test_are_all_terminal_heat_sources_electric.py
@@ -29,13 +29,13 @@ TEST_RMD = {
 TEST_RMD_FULL = {"id": "229_01", "ruleset_model_instances": [TEST_RMD]}
 
 
-def test__all_terminal_heat_source_hot_water():
+def test__all_terminal_heat_source__hot_water():
     assert are_all_terminal_heat_sources_electric(TEST_RMD, ["terminal_2"]) == False
 
 
-def test__all_terminal_heat_source_electric():
+def test__all_terminal_heat_source__electric():
     assert are_all_terminal_heat_sources_electric(TEST_RMD, ["terminal_3"]) == True
 
 
-def test__all_terminal_heat_source_none():
+def test__all_terminal_heat_source__none():
     assert are_all_terminal_heat_sources_electric(TEST_RMD, ["terminal_1"]) == False
