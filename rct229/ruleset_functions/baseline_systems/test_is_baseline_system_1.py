0c3e79374c120fbfc4284399cf398bf33889596c
Weili Xu <weili.xu@pnnl.gov>
2022-10-19 16:27:18 -0400

    Done system type 1 test file

--- rct229/ruleset_functions/baseline_systems/test_is_baseline_system_1.py
+++ rct229/ruleset_functions/baseline_systems/test_is_baseline_system_1.py
@@ -23,6 +23,59 @@ rmd_model = {
                                             "served_by_heating_ventilation_air_conditioning_system": "PTAC 1",
                                         }
                                     ],
+                                },
+                                {
+                                    "id": "Thermal Zone 1b",
+                                    "thermostat_cooling_setpoint_schedule": "Required Cooling Schedule 1",
+                                    "thermostat_heating_setpoint_schedule": "Required Heating Schedule 1",
+                                    "terminals": [
+                                        {
+                                            "id": "PTAC Terminal 1b",
+                                            "is_supply_ducted": False,
+                                            "type": "CONSTANT_AIR_VOLUME",
+                                            "served_by_heating_ventilation_air_conditioning_system": "PTAC 1b",
+                                        }
+                                    ],
+                                },
+                                {
+                                    "id": "Thermal Zone 1a",
+                                    "thermostat_cooling_setpoint_schedule": "Required Cooling Schedule 1",
+                                    "thermostat_heating_setpoint_schedule": "Required Heating Schedule 1",
+                                    "terminals": [
+                                        {
+                                            "id": "PTAC Terminal 1a",
+                                            "is_supply_ducted": False,
+                                            "type": "CONSTANT_AIR_VOLUME",
+                                            "heating_source": "HOT_WATER",
+                                            "heating_from_loop": "Boiler Loop 1",
+                                            "cooling_source": "CHILLED_WATER",
+                                            "cooling_from_loop": "Purchased CHW Loop 1",
+                                            # this is necessary in the current setup.
+                                            # This parameter connects hvac_b with specific terminal so that the
+                                            # function is_baseline_system_1 can execute within the correct zone list.
+                                            "served_by_heating_ventilation_air_conditioning_system": "PTAC 1a",
+                                        }
+                                    ],
+                                },
+                                {
+                                    "id": "Thermal Zone 1c",
+                                    "thermostat_cooling_setpoint_schedule": "Required Cooling Schedule 1",
+                                    "thermostat_heating_setpoint_schedule": "Required Heating Schedule 1",
+                                    "terminals": [
+                                        {
+                                            "id": "PTAC Terminal 1c",
+                                            "is_supply_ducted": False,
+                                            "type": "CONSTANT_AIR_VOLUME",
+                                            "heating_source": "HOT_WATER",
+                                            "heating_from_loop": "Purchased HW Loop 1",
+                                            "cooling_source": "CHILLED_WATER",
+                                            "cooling_from_loop": "Purchased CHW Loop 1",
+                                            # this is necessary in the current setup.
+                                            # This parameter connects hvac_b with specific terminal so that the
+                                            # function is_baseline_system_1 can execute within the correct zone list.
+                                            "served_by_heating_ventilation_air_conditioning_system": "PTAC 1c",
+                                        }
+                                    ],
                                 }
                             ],
                             "heating_ventilation_air_conditioning_systems": [
@@ -42,10 +95,40 @@ rmd_model = {
                                         "fan_control": "CONSTANT",
                                         "supply_fans": [{"id": "Supply Fan 1"}],
                                     },
+                                },
+                                {
+                                    "id": "PTAC 1b",
+                                    "cooling_system": {
+                                        "id": "DX Coil 1b",
+                                        "cooling_system_type": "DIRECT_EXPANSION",
+                                    },
+                                    "heating_system": {
+                                        "id": "HHW Coil 1b",
+                                        "heating_system_type": "FLUID_LOOP",
+                                        "hot_water_loop": "Purchased HW Loop 1",
+                                    },
+                                    "fan_system": {
+                                        "id": "CAV Fan System 1b",
+                                        "fan_control": "CONSTANT",
+                                        "supply_fans": [{"id": "Supply Fan 1b"}],
+                                    },
+                                },
+                                {
+                                    "id": "PTAC 1a",
+                                },
+                                {
+                                    "id": "PTAC 1c",
                                 }
                             ],
                         }
                     ],
+                }
+            ],
+            "chillers": [
+                {
+                    "id": "Chiller 1",
+                    "loop": "Chiller Loop 1",
+                    "condensing_loop": "Condenser Loop 1",
                 }
             ],
             "boilers": [
@@ -54,6 +137,40 @@ rmd_model = {
                     "loop": "Boiler Loop 1",
                     "energy_source_type": "NATURAL_GAS",
                 }
+            ],
+            "fluid_loops": [
+                {
+                    "id": "Boiler Loop 1",
+                    "type": "HEATING",
+                    "heating_design_and_control": {
+                        "id": "DAC1",
+                        "minimum_flow_fraction": 0.25,
+                    },
+                },
+                {
+                    "id": "Purchased HW Loop 1",
+                    "type": "HEATING",
+                },
+                {
+                    "id": "Purchased CHW Loop 1",
+                    "type": "COOLING",
+                },
+                {
+                    "id": "Chiller Loop 1",
+                    "type": "COOLING",
+                },
+            ],
+            "external_fluid_source": [
+                {
+                    "id": "Purchased HW",
+                    "loop": "Purchased HW Loop 1",
+                    "type": "HOT_WATER",
+                },
+                {
+                    "id": "Purchased CHW",
+                    "loop": "Purchased CHW Loop 1",
+                    "type": "CHILLED_WATER",
+                },
             ],
         }
     ],
