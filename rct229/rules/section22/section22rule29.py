6172ef48823ea557a236ecba4ee63f438e7fef9f
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-10-20 11:27:04 -0700

    Finished 22-29

--- rct229/rules/section22/section22rule29.py
+++ rct229/rules/section22/section22rule29.py
@@ -1,15 +1,21 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
-from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.schema.config import ureg
-from rct229.utils.jsonpath_utils import find_all
-from rct229.utils.pint_utils import CalcQ
-from rct229.utils.std_comparisons import std_equal
+from rct229.utils.jsonpath_utils import find_all, find_exactly_one_with_field_value
 
-
-APPLICABLE_SYS_TYPES = ["SYS-7", "SYS-8", "SYS-12", "SYS-13", "SYS-7B", "SYS-8B", "SYS-12B", "SYS-13B"]
+APPLICABLE_SYS_TYPES = [
+    "SYS-7",
+    "SYS-8",
+    "SYS-12",
+    "SYS-13",
+    "SYS-7B",
+    "SYS-8B",
+    "SYS-12B",
+    "SYS-13B",
+]
 NOT_APPLICABLE_SYS_TYPES = ["SYS-11.1", "SYS-11.2", "SYS-11B"]
 REQUIRED_PUMP_POWER = 19 * ureg("W/gpm")
+
 
 class Section22Rule29(RuleDefinitionBase):
     """Rule 29 of ASHRAE 90.1-2019 Appendix G Section 22 (Chilled water loop)"""
@@ -22,7 +28,6 @@ class Section22Rule29(RuleDefinitionBase):
             description="For chilled-water systems served by chiller(s) and does not serve baseline System-11, condenser-water pump power shall be 19 W/gpm.",
             rmr_context="ruleset_model_instances/0",
         )
-        # super(Section22Rule29, self).__init__(rmrs_used, id, description, rmr_context)
 
     def is_applicable(self, context, data=None):
         rmi_b = context.baseline
@@ -41,17 +46,17 @@ class Section22Rule29(RuleDefinitionBase):
 
     def get_calc_vals(self, context, data=None):
         rmi_b = context.baseline
-        placeholder = 1
-        condenser_loop_id_array = []
-        for chiller_b in rmi_b["chillers"]:
-            condenser_loop_id_array.append(chiller_b["condensing_loop"])
-        print(condenser_loop_id_array)
-        placeholder = 2
-
-        return 1
+        condenser_loop_pump_power_list = [
+            True
+            if find_exactly_one_with_field_value(
+                "$..fluid_loops[*]", "id", condenser_loop_id, rmi_b
+            ).get("pump_power_per_flow_rate")
+            == REQUIRED_PUMP_POWER
+            else False
+            for condenser_loop_id in find_all("chillers[*].condensing_loop", rmi_b)
+        ]
+        return {"condenser_loop_pump_power_list": condenser_loop_pump_power_list}
 
     def rule_check(self, context, calc_vals=None, data=None):
-        condenser_loop_pump_power_b = calc_vals["pump_power_per_flow_rate"]
-        return condenser_loop_pump_power_b == REQUIRED_PUMP_POWER
-
-
+        condenser_loop_pump_power_list = calc_vals["condenser_loop_pump_power_list"]
+        return all(condenser_loop_pump_power_list)
