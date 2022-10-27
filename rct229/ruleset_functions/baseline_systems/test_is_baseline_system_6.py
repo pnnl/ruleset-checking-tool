686f35405bfc2a5d17b1f31b97e5c64ab899e652
Weili Xu <weili.xu@pnnl.gov>
2022-10-27 11:11:00 -0400

    Update test system 6

--- rct229/ruleset_functions/baseline_systems/test_is_baseline_system_6.py
+++ rct229/ruleset_functions/baseline_systems/test_is_baseline_system_6.py
@@ -1,9 +1,9 @@
 from rct229.schema.validate import schema_validate_rmr
 
-# hvac_id = "System 5" => Sys_5, [Thermal Zone 1], [VAV Air Terminal 1]
-# hvac_id = "System 5b" => Sys_5b, [Thermal Zone 2], [VAV Air Terminal 2]
+# hvac_id = "System 6" => Sys_6, [Thermal Zone 1], [VAV Air Terminal 1]
+# hvac_id = "System 6b" => Sys_6b, [Thermal Zone 2], [VAV Air Terminal 2]
 
-SYS_5_RMD = {
+SYS_6_RMD = {
     "id": "ASHRAE229 1",
     "ruleset_model_instances": [
         {
@@ -25,9 +25,12 @@ SYS_5_RMD = {
                                             "id": "VAV Air Terminal 1",
                                             "is_supply_ducted": True,
                                             "type": "VARIABLE_AIR_VOLUME",
-                                            "served_by_heating_ventilation_air_conditioning_system": "System 5",
-                                            "heating_source": "HOT_WATER",
-                                            "heating_from_loop": "Boiler Loop 1",
+                                            "served_by_heating_ventilation_air_conditioning_system": "System 6",
+                                            "heating_source": "ELECTRIC",
+                                            "fan": {
+                                                "id": "Terminal Fan 1",
+                                            },
+                                            "fan_configuration": "PARALLEL",
                                         }
                                     ],
                                 },
@@ -40,24 +43,27 @@ SYS_5_RMD = {
                                             "id": "VAV Air Terminal 2",
                                             "is_supply_ducted": True,
                                             "type": "VARIABLE_AIR_VOLUME",
-                                            "served_by_heating_ventilation_air_conditioning_system": "System 5B",
+                                            "served_by_heating_ventilation_air_conditioning_system": "System 6B",
                                             "heating_source": "HOT_WATER",
-                                            "heating_from_loop": "Purchased HW Loop 1",
+                                            "heating_from_loop": "Purchased HW 1",
+                                            "fan": {
+                                                "id": "Terminal Fan 2",
+                                            },
+                                            "fan_configuration": "PARALLEL",
                                         }
                                     ],
                                 },
                             ],
                             "heating_ventilation_air_conditioning_systems": [
                                 {
-                                    "id": "System 5",
+                                    "id": "System 6",
                                     "cooling_system": {
                                         "id": "DX Coil 1",
                                         "cooling_system_type": "DIRECT_EXPANSION",
                                     },
                                     "preheat_system": {
                                         "id": "Preheat Coil 1",
-                                        "heating_system_type": "FLUID_LOOP",
-                                        "hot_water_loop": "Boiler Loop 1",
+                                        "heating_system_type": "ELECTRIC_RESISTANCE",
                                     },
                                     "fan_system": {
                                         "id": "VAV Fan System 1",
@@ -67,7 +73,7 @@ SYS_5_RMD = {
                                     },
                                 },
                                 {
-                                    "id": "System 5B",
+                                    "id": "System 6B",
                                     "cooling_system": {
                                         "id": "DX Coil 2",
                                         "cooling_system_type": "DIRECT_EXPANSION",
@@ -75,7 +81,7 @@ SYS_5_RMD = {
                                     "preheat_system": {
                                         "id": "Preheat Coil 2",
                                         "heating_system_type": "FLUID_LOOP",
-                                        "hot_water_loop": "Purchased HW Loop 1",
+                                        "hot_water_loop": "Purchased HW 1",
                                     },
                                     "fan_system": {
                                         "id": "VAV Fan System 2",
@@ -89,24 +95,6 @@ SYS_5_RMD = {
                     ],
                 }
             ],
-            "boilers": [
-                {
-                    "id": "Boiler 1",
-                    "loop": "Boiler Loop 1",
-                    "energy_source_type": "NATURAL_GAS",
-                }
-            ],
-            "pumps": [
-                {
-                    "id": "Boiler Pump 1",
-                    "loop_or_piping": "Boiler Loop 1",
-                    "speed_control": "FIXED_SPEED",
-                }
-            ],
-            "fluid_loops": [
-                {"id": "Boiler Loop 1", "type": "HEATING"},
-                {"id": "Purchased HW Loop 1", "type": "HEATING"},
-            ],
             "external_fluid_source": [
                 {
                     "id": "Purchased HW 1",
@@ -114,13 +102,21 @@ SYS_5_RMD = {
                     "type": "HOT_WATER",
                 }
             ],
+            "pumps": [
+                {
+                    "id": "HW Pump 1",
+                    "loop_or_piping": "Purchased HW Loop 1",
+                    "speed_control": "FIXED_SPEED",
+                }
+            ],
+            "fluid_loops": [{"id": "Purchased HW Loop 1", "type": "HEATING"}],
         }
     ],
 }
 
 
-def test__TEST_RMD_baseline_system_5__is_valid():
-    schema_validation_result = schema_validate_rmr(SYS_5_RMD)
+def test__TEST_RMD_baseline_system_6__is_valid():
+    schema_validation_result = schema_validate_rmr(SYS_6_RMD)
     assert schema_validation_result[
         "passed"
     ], f"Schema error: {schema_validation_result['error']}"
