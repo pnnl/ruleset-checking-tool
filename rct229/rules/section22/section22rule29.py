72270288b6c8105a958907e98782969061a6c1ed
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-10-28 12:45:13 -0700

    Address PR comments

--- rct229/rules/section22/section22rule29.py
+++ rct229/rules/section22/section22rule29.py
@@ -1,20 +1,24 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.ruleset_functions.baseline_systems.baseline_system_util import (
+    HVAC_SYS,
+    find_exactly_one_loop,
+)
+from rct229.ruleset_functions.get_baseline_system_types import get_baseline_system_types
 from rct229.schema.config import ureg
-from rct229.utils.jsonpath_utils import find_all, find_exactly_one_with_field_value
+from rct229.utils.jsonpath_utils import find_all
 from rct229.utils.std_comparisons import std_equal
 
 APPLICABLE_SYS_TYPES = [
-    "SYS-7",
-    "SYS-8",
-    "SYS-12",
-    "SYS-13",
-    "SYS-7B",
-    "SYS-8B",
-    "SYS-12B",
-    "SYS-13B",
+    HVAC_SYS.SYS_7,
+    HVAC_SYS.SYS_8,
+    HVAC_SYS.SYS_12,
+    HVAC_SYS.SYS_13,
+    HVAC_SYS.SYS_7B,
+    HVAC_SYS.SYS_8B,
+    HVAC_SYS.SYS_12B,
 ]
-NOT_APPLICABLE_SYS_TYPES = ["SYS-11.1", "SYS-11.2", "SYS-11B"]
+NOT_APPLICABLE_SYS_TYPES = [HVAC_SYS.SYS_11_1, HVAC_SYS.SYS_11_2, HVAC_SYS.SYS_11_1B]
 REQUIRED_PUMP_POWER = 19 * ureg("W/gpm")
 
 
@@ -31,34 +35,38 @@ class Section22Rule29(RuleDefinitionBase):
 
     def is_applicable(self, context, data=None):
         rmi_b = context.baseline
-        # FIXME: replace with baseline_system_types = get_baseline_system_types(rmi_b) when get_baseline_system_types
-        #  is ready.
-        baseline_system_types = {
-            "SYS-7": ["hvac_sys_7"],
-            "SYS-11": ["hvac_sys_11"],
-        }
-        # if any system type found in the APPLICABLE_SYS_TYPES then return applicable.
+        baseline_system_types_dict = get_baseline_system_types(rmi_b)
+        # create a list contains all HVAC systems that are modeled in the rmi_b
+        available_type_lists = [
+            hvac_type
+            for hvac_type in baseline_system_types_dict.keys()
+            if len(baseline_system_types_dict[hvac_type]) > 0
+        ]
         return any(
-            [key in APPLICABLE_SYS_TYPES for key in baseline_system_types.keys()]
+            [
+                available_type in APPLICABLE_SYS_TYPES
+                for available_type in available_type_lists
+            ]
         ) and not any(
-            [key in NOT_APPLICABLE_SYS_TYPES for key in baseline_system_types.keys()]
+            [
+                available_type in NOT_APPLICABLE_SYS_TYPES
+                for available_type in available_type_lists
+            ]
         )
 
     def get_calc_vals(self, context, data=None):
         rmi_b = context.baseline
         condenser_loop_pump_power_list = [
-            True
-            if std_equal(
-                find_exactly_one_with_field_value(
-                    "$..fluid_loops[*]", "id", condenser_loop_id, rmi_b
-                ).get("pump_power_per_flow_rate"),
-                REQUIRED_PUMP_POWER,
+            find_exactly_one_loop(rmi_b, condenser_loop_id).get(
+                "pump_power_per_flow_rate"
             )
-            else False
             for condenser_loop_id in find_all("chillers[*].condensing_loop", rmi_b)
         ]
         return {"condenser_loop_pump_power_list": condenser_loop_pump_power_list}
 
     def rule_check(self, context, calc_vals=None, data=None):
         condenser_loop_pump_power_list = calc_vals["condenser_loop_pump_power_list"]
-        return all(condenser_loop_pump_power_list)
+        return all([
+            std_equal(pump_power, REQUIRED_PUMP_POWER)
+            for pump_power in condenser_loop_pump_power_list
+        ])
