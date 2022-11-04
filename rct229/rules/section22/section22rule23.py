64a0d933f6b74825047d0c9c1858cfaad2a12e02
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-11-03 22:31:26 -0700

    Address the PR comments

--- rct229/rules/section22/section22rule23.py
+++ rct229/rules/section22/section22rule23.py
@@ -1,80 +1,72 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
-from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.ruleset_functions.baseline_systems.baseline_system_util import HVAC_SYS
+from rct229.ruleset_functions.get_baseline_system_types import get_baseline_system_types
 from rct229.utils.jsonpath_utils import find_all
 
 APPLICABLE_SYS_TYPES = [
-    "SYS-7",
-    "SYS-8",
-    "SYS-11.1",
-    "SYS-11.2",
-    "SYS-12",
-    "SYS-13",
-    "SYS-7B",
-    "SYS-8B",
-    "SYS-11B",
-    "SYS-12B",
-    "SYS-13B",
+    HVAC_SYS.SYS_7,
+    HVAC_SYS.SYS_8,
+    HVAC_SYS.SYS_11_1,
+    HVAC_SYS.SYS_11_2,
+    HVAC_SYS.SYS_12,
+    HVAC_SYS.SYS_13,
+    HVAC_SYS.SYS_7B,
+    HVAC_SYS.SYS_8B,
+    HVAC_SYS.SYS_11B,
+    HVAC_SYS.SYS_12B,
 ]
 
 
-class Section22Rule23(RuleDefinitionListIndexedBase):
+class Section22Rule23(RuleDefinitionBase):
     """Rule 23 of ASHRAE 90.1-2019 Appendix G Section 22 (Hot water loop)"""
 
     def __init__(self):
         super(Section22Rule23, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, True, False),
-            each_rule=Section22Rule23.ChillerRule(),
-            index_rmr="baseline",
             id="22-23",
             description="Each baseline chiller shall be modeled with separate chilled water pump interlocked to operate with the associated chiller.",
             rmr_context="ruleset_model_instances/0",
-            list_path="chillers[*]",
         )
 
     def is_applicable(self, context, data=None):
         rmi_b = context.baseline
-        # FIXME: replace with baseline_system_types = get_baseline_system_types(rmi_b) when get_baseline_system_types
-        #  is ready.
-        baseline_system_types = {
-            "SYS-7": ["hvac_sys_7"],
-            "SYS-11-1": ["hvac_sys_11_1"],
-        }
-        # if any system type found in the APPLICABLE_SYS_TYPES then return applicable.
+        baseline_system_types_dict = get_baseline_system_types(rmi_b)
+        # create a list contains all HVAC systems that are modeled in the rmi_b
+        available_type_lists = [
+            hvac_type
+            for hvac_type in baseline_system_types_dict.keys()
+            if len(baseline_system_types_dict[hvac_type]) > 0
+        ]
         return any(
-            [key in APPLICABLE_SYS_TYPES for key in baseline_system_types.keys()]
+            [
+                available_type in APPLICABLE_SYS_TYPES
+                for available_type in available_type_lists
+            ]
         )
 
-    def create_data(self, context, data):
+    def get_calc_vals(self, context, data=None):
         rmi_b = context.baseline
         no_of_chillers_b = len(find_all("chillers[*]", rmi_b))
-
         primary_chw_loop_id_array = find_all("chillers[*].cooling_loop", rmi_b)
+        interlock_flag = all(
+            find_all("chillers[*].is_chilled_water_pump_interlocked", rmi_b)
+        )
+
         primary_chw_loop_pump_num = 0
         for pump_b in find_all("pumps[*]", rmi_b):
             if pump_b["loop_or_piping"] in primary_chw_loop_id_array:
                 primary_chw_loop_pump_num += 1
+
         return {
             "no_of_chillers_b": no_of_chillers_b,
             "primary_chw_loop_pump_num": primary_chw_loop_pump_num,
+            "interlock_flag": interlock_flag,
         }
 
-    class ChillerRule(RuleDefinitionBase):
-        def __init__(self):
-            super(Section22Rule23.ChillerRule, self).__init__(
-                rmrs_used=UserBaselineProposedVals(False, True, False),
-                required_fields={
-                    "$": ["is_chilled_water_pump_interlocked"],
-                },
-            )
+    def rule_check(self, context, calc_vals=None, data=None):
+        no_of_chillers_b = calc_vals["no_of_chillers_b"]
+        primary_chw_loop_pump_num = calc_vals["primary_chw_loop_pump_num"]
+        interlock_flag = calc_vals["interlock_flag"]
 
-        def get_calc_vals(self, context, data=None):
-            chiller_b = context.baseline
-            interlock_flag = chiller_b["is_chilled_water_pump_interlocked"]
-            return {"interlock_flag": interlock_flag}
-
-        def rule_check(self, context, calc_vals=None, data=None):
-            primary_chw_loop_pump_num = data["primary_chw_loop_pump_num"]
-            interlock_flag = calc_vals["interlock_flag"]
-            no_of_chillers_b = data["no_of_chillers_b"]
-            return no_of_chillers_b == primary_chw_loop_pump_num and interlock_flag
+        return no_of_chillers_b == primary_chw_loop_pump_num and interlock_flag
