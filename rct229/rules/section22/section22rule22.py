f8c7f04a606658a1c1e7ba26ee7f11ffdf56d6b0
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-11-07 16:07:09 -0800

    finish 22-22

--- rct229/rules/section22/section22rule22.py
+++ rct229/rules/section22/section22rule22.py
@@ -2,21 +2,22 @@ from rct229.data_fns.table_3_5_3_fns import table_3_5_3_lookup
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.ruleset_functions.baseline_systems.baseline_system_util import HVAC_SYS
+from rct229.ruleset_functions.get_baseline_system_types import get_baseline_system_types
 from rct229.schema.config import ureg
 from rct229.utils.std_comparisons import std_equal
 
 APPLICABLE_SYS_TYPES = [
-    "SYS-7",
-    "SYS-8",
-    "SYS-11.1",
-    "SYS-11.2",
-    "SYS-12",
-    "SYS-13",
-    "SYS-7B",
-    "SYS-8B",
-    "SYS-11B",
-    "SYS-12B",
-    "SYS-13B",
+    HVAC_SYS.SYS_7,
+    HVAC_SYS.SYS_8,
+    HVAC_SYS.SYS_11_1,
+    HVAC_SYS.SYS_11_2,
+    HVAC_SYS.SYS_12,
+    HVAC_SYS.SYS_13,
+    HVAC_SYS.SYS_7B,
+    HVAC_SYS.SYS_8B,
+    HVAC_SYS.SYS_11B,
+    HVAC_SYS.SYS_12B,
 ]
 
 
@@ -36,15 +37,18 @@ class Section22Rule22(RuleDefinitionListIndexedBase):
 
     def is_applicable(self, context, data=None):
         rmi_b = context.baseline
-        # FIXME: replace with baseline_system_types = get_baseline_system_types(rmi_b) when get_baseline_system_types
-        #  is ready.
-        baseline_system_types = {
-            "SYS-7": ["hvac_sys_7"],
-            "SYS-11.1": ["hvac_sys_11.1"],
-        }
-        # if any system type found in the APPLICABLE_SYS_TYPES and not found in NOT_APPLICABLE_SYS_TYPES then return applicable.
+        baseline_system_types_dict = get_baseline_system_types(rmi_b)
+        # create a list contains all HVAC systems that are modeled in the rmi_b
+        available_type_lists = [
+            hvac_type
+            for hvac_type in baseline_system_types_dict.keys()
+            if len(baseline_system_types_dict[hvac_type]) > 0
+        ]
         return any(
-            [key in APPLICABLE_SYS_TYPES for key in baseline_system_types.keys()]
+            [
+                available_type in APPLICABLE_SYS_TYPES
+                for available_type in available_type_lists
+            ]
         )
 
     class ChillerRule(RuleDefinitionBase):
@@ -52,29 +56,20 @@ class Section22Rule22(RuleDefinitionListIndexedBase):
             super(Section22Rule22.ChillerRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, False),
                 required_fields={
-                    "$": ["rated_capacity"],
+                    "$": ["compressor_type", "rated_capacity"],
                 },
             )
 
         def get_calc_vals(self, context, data=None):
             chiller_b = context.baseline
             compressor_type_b = chiller_b["compressor_type"]
-            rated_capacity_b = chiller_b["rated_capacity"]
-
-            if rated_capacity_b < 150 * ureg("Btu/hr"):
-                size_category = "< 150TONS"
-            elif 150 * ureg("Btu/hr") <= rated_capacity_b < 300 * ureg("Btu/hr"):
-                size_category = ">= 150TONS AND < 300TONS"
-            elif 300 * ureg("Btu/hr") <= rated_capacity_b < 600 * ureg("Btu/hr"):
-                size_category = ">= 300TONS AND < 600TONS"
-            else:
-                size_category = "> 600TONS"
+            rated_capacity_b = chiller_b["rated_capacity"].to(ureg.ton).m
 
             kW_ton_full_load_b = table_3_5_3_lookup(
-                compressor_type_b, size_category, "FL"
+                compressor_type_b, rated_capacity_b
             )["minimum_full_load_efficiency_kw_per_ton"]
+
             return {
-                "size_category": size_category,
                 "kW_ton_full_load_b": kW_ton_full_load_b,
             }
 
@@ -82,4 +77,5 @@ class Section22Rule22(RuleDefinitionListIndexedBase):
             chiller_b = context.baseline
             full_load_efficiency_b = chiller_b["full_load_efficiency"]
             kW_ton_full_load_b = calc_vals["kW_ton_full_load_b"]
+
             return std_equal(full_load_efficiency_b, kW_ton_full_load_b * ureg(""))
