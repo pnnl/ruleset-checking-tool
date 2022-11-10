48f3439b37d71bb3a49ae9b6f0b79c79e179ec45
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-11-10 11:17:09 -0800

    address PR comments

--- rct229/rules/section22/section22rule9.py
+++ rct229/rules/section22/section22rule9.py
@@ -7,7 +7,9 @@ from rct229.ruleset_functions.get_primary_secondary_loops_dict import (
     get_primary_secondary_loops_dict,
 )
 from rct229.schema.config import ureg
+from rct229.utils.assertions import getattr_
 from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.pint_utils import ZERO
 
 APPLICABLE_SYS_TYPES = [
     HVAC_SYS.SYS_7,
@@ -35,12 +37,13 @@ class Section22Rule9(RuleDefinitionListIndexedBase):
             index_rmr="baseline",
             id="22-9",
             description="For Baseline chilled water system with cooling capacity of 300 tons or more, the secondary loop shall be modeled with a minimum flow of 25% of the design flow rate.",
-            rmr_context="ruleset_model_instances/0",
-            list_path="fluid_loops[*]",
+            list_path="ruleset_model_instances[0].fluid_loops[*]",
         )
 
     def is_applicable(self, context, data=None):
-        rmi_b = context.baseline
+        rmr_baseline = context.baseline
+        rmi_b = rmr_baseline["ruleset_model_instances"][0]
+
         baseline_system_types_dict = get_baseline_system_types(rmi_b)
         # create a list contains all HVAC systems that are modeled in the rmi_b
         available_type_lists = [
@@ -48,6 +51,7 @@ class Section22Rule9(RuleDefinitionListIndexedBase):
             for hvac_type in baseline_system_types_dict.keys()
             if len(baseline_system_types_dict[hvac_type]) > 0
         ]
+
         primary_secondary_loop_dictionary = get_primary_secondary_loops_dict(rmi_b)
 
         return (
@@ -61,14 +65,16 @@ class Section22Rule9(RuleDefinitionListIndexedBase):
         )
 
     def create_data(self, context, data):
-        rmi_b = context.baseline
+        rmr_baseline = context.baseline
+        rmi_b = rmr_baseline["ruleset_model_instances"][0]
+
         chw_loop_capacity_dict = {}
         for chiller in find_all("chillers[*]", rmi_b):
             if chiller["cooling_loop"] not in chw_loop_capacity_dict.keys():
-                chw_loop_capacity_dict[chiller["cooling_loop"]] = 0.0 * ureg("W")
-                chw_loop_capacity_dict[chiller["cooling_loop"]] += chiller[
-                    "rated_capacity"
-                ]
+                chw_loop_capacity_dict[chiller["cooling_loop"]] = ZERO.POWER
+            chw_loop_capacity_dict[chiller["cooling_loop"]] += getattr_(
+                chiller, "chiller", "rated_capacity"
+            )
 
         primary_secondary_loop_dictionary = get_primary_secondary_loops_dict(rmi_b)
 
@@ -88,20 +94,37 @@ class Section22Rule9(RuleDefinitionListIndexedBase):
             >= MIN_CHW_PRIMARY_LOOP_COOLING_CAPACITY
         )
 
-    class ChillerFluidLoopRule(RuleDefinitionBase):
+    class ChillerFluidLoopRule(RuleDefinitionListIndexedBase):
         def __init__(self):
             super(Section22Rule9.ChillerFluidLoopRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, False),
+                each_rule=Section22Rule9.ChillerFluidLoopRule.SecondaryChildLoopRule(),
+                index_rmr="baseline",
+                list_path="$..child_loops[*]",
+                required_fields={
+                    "$": ["cooling_or_condensing_design_and_control"],
+                    "cooling_or_condensing_design_and_control": [
+                        "minimum_flow_fraction"
+                    ],
+                },
             )
 
+        class SecondaryChildLoopRule(RuleDefinitionBase):
+            def __init__(self):
+                super(
+                    Section22Rule9.ChillerFluidLoopRule.SecondaryChildLoopRule, self
+                ).__init__(
+                    rmrs_used=UserBaselineProposedVals(False, True, False),
+                )
+
         def get_calc_vals(self, context, data=None):
-            fluid_loop_b = context.baseline
-
-            for secondary_loop_id_b in fluid_loop_b["child_loops"]:
-                secondary_loop_min_flow_rate = secondary_loop_id_b[
-                    "cooling_or_condensing_design_and_control"
-                ]["minimum_flow_fraction"]
-
+            child_loop_b = context.baseline
+            place = 1
+            # for secondary_loop_id_b in child_loop_b["child_loops"]:
+            #     secondary_loop_min_flow_rate = secondary_loop_id_b[
+            #         "cooling_or_condensing_design_and_control"
+            #     ]["minimum_flow_fraction"]
+            secondary_loop_min_flow_rate = 1
             return {"secondary_loop_min_flow_rate": secondary_loop_min_flow_rate}
 
         def rule_check(self, context, calc_vals=None, data=None):
