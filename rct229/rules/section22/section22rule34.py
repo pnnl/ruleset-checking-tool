095c329e9c67b58c46259b3d6a0c1ed282e84b5a
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-11-14 09:32:50 -0800

    address PR comment

--- rct229/rules/section22/section22rule34.py
+++ rct229/rules/section22/section22rule34.py
@@ -38,8 +38,8 @@ class Section22Rule34(RuleDefinitionListIndexedBase):
     def is_applicable(self, context, data=None):
         rmi_b = context.baseline
         baseline_system_types_dict = get_baseline_system_types(rmi_b)
-        # create a list contains all HVAC systems that are modeled in the rmi_b
-        available_type_lists = [
+        # create a list containing all HVAC systems that are modeled in the rmi_b
+        available_type_list = [
             hvac_type
             for hvac_type in baseline_system_types_dict.keys()
             if len(baseline_system_types_dict[hvac_type]) > 0
@@ -47,7 +47,7 @@ class Section22Rule34(RuleDefinitionListIndexedBase):
         return any(
             [
                 available_type in APPLICABLE_SYS_TYPES
-                for available_type in available_type_lists
+                for available_type in available_type_list
             ]
         )
 
@@ -77,17 +77,13 @@ class Section22Rule34(RuleDefinitionListIndexedBase):
 
         def get_calc_vals(self, context, data=None):
             fluid_loop_b = context.baseline
-            is_sized_using_coincident_load_bool = fluid_loop_b[
+            is_sized_using_coincident_load = fluid_loop_b[
                 "cooling_or_condensing_design_and_control"
             ]["is_sized_using_coincident_load"]
 
-            return {
-                "is_sized_using_coincident_load_bool": is_sized_using_coincident_load_bool
-            }
+            return {"is_sized_using_coincident_load": is_sized_using_coincident_load}
 
         def rule_check(self, context, calc_vals=None, data=None):
-            is_sized_using_coincident_load_bool = calc_vals[
-                "is_sized_using_coincident_load_bool"
-            ]
+            is_sized_using_coincident_load = calc_vals["is_sized_using_coincident_load"]
 
-            return is_sized_using_coincident_load_bool
+            return is_sized_using_coincident_load
