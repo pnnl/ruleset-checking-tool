8573574c2f260d02d9a47b396cdbcf8e89f4b441
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-11-14 17:56:34 -0800

    address PR comments

--- rct229/ruleset_functions/check_purchased_chw_hhw.py
+++ rct229/ruleset_functions/check_purchased_chw_hhw.py
@@ -2,6 +2,7 @@ from rct229.data.schema_enums import schema_enums
 from rct229.ruleset_functions.baseline_systems.baseline_system_util import (
     find_exactly_one_fluid_loop,
 )
+from rct229.utils.assertions import getattr_
 from rct229.utils.jsonpath_utils import find_all, find_one
 
 EXTERNAL_FLUID_SOURCE = schema_enums["ExternalFluidSourceOptions"]
@@ -9,41 +10,42 @@ EXTERNAL_FLUID_SOURCE = schema_enums["ExternalFluidSourceOptions"]
 
 def check_purchased_chw_hhw(rmi_b):
     """
-    Check if RMD is modeled with purchased chilled water as space cooling source or purchased hot water/steam as space heating source.
-    If any system in RMD uses purchased chilled water, function shall return True for purchased chilled water as space cooling source.
-    Similarly, if any system in RMD uses purchased hot water or steam, function shall return True for purchased hot water/steam as space heating source.
+    Check if RMI is modeled with purchased chilled water as space cooling source or purchased hot water/steam as space heating source.
+    If any system in RMI uses purchased chilled water, function shall return True for purchased chilled water as space cooling source.
+    Similarly, if any system in RMI uses purchased hot water or steam, function shall return True for purchased hot water/steam as space heating source.
 
     Parameters
     ----------
     rmi_b: json
-        RMD at RuleSetModelInstance level
+        RMI at RuleSetModelInstance level
 
     Returns
     -------
-    purchased_chw_hhw_status_dictionary: A dictionary that saves whether RMD is modeled with purchased chilled water as space cooling source or purchased hot water/steam as space heating source,
-    i.e. {"PURCHASED_COOLING": TRUE, "PURCHASED_HEATING": FALSE}.
+    purchased_chw_hhw_status_dictionary: A dictionary that saves whether RMI is modeled with purchased chilled water as space cooling source or purchased hot water/steam as space heating source,
+    i.e. {"purchased_cooling": True, "purchased_heating": False}.
 
     """
     purchased_chw_hhw_status_dict = {
         "purchased_cooling": False,
         "purchased_heating": False,
     }
-
-    if not find_all("$..external_fluid_source[*]", rmi_b):
+    # rmi_b = rmi_b["ruleset_model_instances"][0]
+    external_fluid_sources = find_all("$..external_fluid_source[*]", rmi_b)
+    if not external_fluid_sources:
         return purchased_chw_hhw_status_dict
 
-    for external_fluid_source in find_all("$..external_fluid_source[*]", rmi_b):
-        if external_fluid_source["type"] == EXTERNAL_FLUID_SOURCE.CHILLED_WATER:
+    for external_fluid_source in external_fluid_sources:
+        if (
+            getattr_(external_fluid_source, "external_fluid_source", "type")
+            == EXTERNAL_FLUID_SOURCE.CHILLED_WATER
+        ):
             cooling_loop = find_exactly_one_fluid_loop(
-                rmi_b, external_fluid_source["loop"]
+                rmi_b, getattr_(external_fluid_source, "external_fluid_source", "loop")
             )
-            for loop in find_one("$.child_loops[*]", cooling_loop):
-                print(loop)
 
-            purchased_chw_loop_array = [
-                find_one("$.child_loops[*]", cooling_loop)["id"]
+            purchased_chw_loop_array = find_all("$.child_loops[*].id", cooling_loop) + [
+                cooling_loop["id"]
             ]
-            purchased_chw_loop_array.append(find_one("$.id", cooling_loop))
 
             for hvac_sys in find_all(
                 "$..heating_ventilation_air_conditioning_systems[*]", rmi_b
@@ -56,14 +58,18 @@ def check_purchased_chw_hhw(rmi_b):
                     break
 
         else:  # HOT_WATER or STEAM type
+            assert external_fluid_source["type"] in [
+                EXTERNAL_FLUID_SOURCE.HOT_WATER,
+                EXTERNAL_FLUID_SOURCE.STEAM,
+            ]
+
             heating_loop = find_exactly_one_fluid_loop(
-                rmi_b, external_fluid_source["loop"]
+                rmi_b, getattr_(external_fluid_source, "external_fluid_source", "loop")
             )
 
-            purchased_hhw_loop_array = [
-                find_one("$.child_loops[*]", heating_loop)["id"]
+            purchased_hhw_loop_array = find_all("$.child_loops[*].id", heating_loop) + [
+                heating_loop["id"]
             ]
-            purchased_hhw_loop_array.append(find_one("$.id", heating_loop))
 
             for hvac_sys in find_all(
                 "$..heating_ventilation_air_conditioning_systems[*]", rmi_b
@@ -74,13 +80,13 @@ def check_purchased_chw_hhw(rmi_b):
                 ):
                     purchased_chw_hhw_status_dict["purchased_heating"] = True
                     break
-                if hvac_sys.get(
-                    "preheat_system"
-                ):  # Prevent an error when preheat system doesn't exist
-                    for preheat_system in hvac_sys["preheat_system"]:
-                        if preheat_system["hot_water_loop"] in purchased_hhw_loop_array:
-                            purchased_chw_hhw_status_dict["purchased_heating"] = True
-                            break
+                if (
+                    hvac_sys.get("preheat_system")
+                    and hvac_sys["preheat_system"]["heating_system_type"]
+                    in purchased_hhw_loop_array
+                ):
+                    purchased_chw_hhw_status_dict["purchased_heating"] = True
+                    break
 
             for terminal in find_all("$..terminals[*]", rmi_b):
                 if terminal.get("heating_from_loop") in purchased_hhw_loop_array:
