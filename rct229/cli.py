88dfa01ce8775b355cbddfe20ede394748dd7eda
Weili Xu <weili.xu@pnnl.gov>
2022-11-30 01:07:52 -0500

    add rule unit tests

--- rct229/cli.py
+++ rct229/cli.py
@@ -6,6 +6,8 @@ from rct229.reports.project_report import (
     print_summary_report,
 )
 from rct229.rule_engine.engine import evaluate_all_rules
+from rct229.ruletest_engine.ruletest_engine import run_envelope_tests, run_lighting_tests, run_transformer_tests, \
+    run_boiler_tests, run_chiller_tests
 from rct229.schema.validate import validate_rmr
 from rct229.utils.file import deserialize_rmr_file
 
@@ -22,6 +24,35 @@ def cli():
     """
     ASHRAE 229 - Ruleset Checking Tool
     """
+
+
+# Software Test Workflow
+test_short_help_text = "Software test workflow, add sections to do test. argument (optional): section string, " \
+                       "currently available: section5, section6, section 21, section 22 "
+
+
+@cli.command("test", short_help=test_short_help_text, help=test_short_help_text, hidden=True)
+@click.argument("section", type=click.STRING, required=False)
+def run_test(section=None):
+    if section:
+        print(f"software test workflow for section {section}")
+        if section == "section5":
+            assert run_envelope_tests(), "Failed section 5 tests"
+        elif section == "section6":
+            assert run_lighting_tests(), "Failed section 6 tests"
+        # elif section == "section15":
+        #    assert run_transformer_tests(), "Failed section 15 tests"
+        elif section == "section21":
+            assert run_boiler_tests(), "Failed section 21 tests"
+        elif section == "section22":
+            assert run_chiller_tests(), "Failed section 22 tests"
+    else:
+        print(f"software test workflow for all tests")
+        # assert run_transformer_tests(), "Failed section 15 tests"
+        assert run_lighting_tests(), "Failed section 6 tests"
+        assert run_envelope_tests(), "Failed section 5 tests"
+        assert run_boiler_tests(), "Failed section 21 tests"
+        assert run_chiller_tests(), "Failed section 22 tests"
 
 
 # Evaluate RMR Triplet
