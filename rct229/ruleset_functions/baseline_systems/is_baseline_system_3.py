4ad8ae32b3fbeb45af12d493c53ba623377e57ac
yunjoonjung <yunjoon.jung@pnnl.gov>
2022-12-08 16:59:21 -0800

    finish system 3

--- rct229/ruleset_functions/baseline_systems/is_baseline_system_3.py
+++ rct229/ruleset_functions/baseline_systems/is_baseline_system_3.py
@@ -8,9 +8,6 @@ from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.are_a
 from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.are_all_terminal_heat_sources_none_or_null import (
     are_all_terminal_heat_sources_none_or_null,
 )
-from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.are_all_terminal_supplies_ducted import (
-    are_all_terminal_supplies_ducted,
-)
 from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.are_all_terminal_types_CAV import (
     are_all_terminal_types_cav,
 )
@@ -23,11 +20,23 @@ from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.does_
 from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_cooling_type_DX import (
     is_hvac_sys_cooling_type_dx,
 )
+from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_cooling_type_fluid_loop import (
+    is_hvac_sys_cooling_type_fluid_loop,
+)
 from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_fan_sys_CV import (
     is_hvac_sys_fan_sys_cv,
 )
-from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_heating_type_heat_pump import (
-    is_hvac_sys_heating_type_heat_pump,
+from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_fluid_loop_purchased_CHW import (
+    is_hvac_sys_fluid_loop_purchased_chw,
+)
+from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_fluid_loop_purchased_heating import (
+    is_hvac_sys_fluid_loop_purchased_heating,
+)
+from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_heating_type_fluid_loop import (
+    is_hvac_sys_heating_type_fluid_loop,
+)
+from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_heating_type_furnace import (
+    is_hvac_sys_heating_type_furnace,
 )
 from rct229.ruleset_functions.baseline_systems.baseline_system_util import (
     HVAC_SYS,
@@ -37,9 +46,10 @@ from rct229.ruleset_functions.baseline_systems.baseline_system_util import (
 HEATING_SYSTEM = schema_enums["HeatingSystemOptions"]
 
 
-def is_baseline_system_2(rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list):
+def is_baseline_system_3(rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list):
     """
-    Get either Sys-2 or Not_Sys_2 string output which indicates whether the HVAC system is ASHRAE 90.1 2019 Appendix G system 2 (PTHP).
+    Get either Sys-3, Sys-3a, Sys-3b, Sys-3c, or Not_Sys_3 string output which indicates whether the HVAC system is ASHRAE 90.1 2019 Appendix G system 3 (PSZ), system 3a (system 3 with purchased CHW),
+    system 3b (system 3 with purchased heating), system 3c (system 3 with purchased CHW and purchased HW).
 
     Parameters
     ----------
@@ -57,12 +67,13 @@ def is_baseline_system_2(rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list):
 
     Returns
     -------
-    The function returns either Sys-2 or Not_Sys_2 string output which indicates whether the HVAC system is ASHRAE 90.1 2019 Appendix G system 2 (PTHP).
+    The function returns either Sys-3, Sys-3a, Sys-3b, Sys-3c, or Not_Sys_3 string output which indicates whether the HVAC system is ASHRAE 90.1 2019 Appendix G system 3 (PSZ),
+    system 3a (system 3 with purchased CHW), system 3b (system 3 with purchased heating), system 3c (system 3 with purchased CHW and purchased HW).
     """
-    is_baseline_system_2 = HVAC_SYS.UNMATCHED
+    is_baseline_system_3 = HVAC_SYS.UNMATCHED
 
     hvac_b = find_exactly_one_hvac_system(rmi_b, hvac_b_id)
-    # check if the hvac system has the required sub systems for system type 2
+    # check if the hvac system has the required sub systems for system type 3
     has_required_sys = (
         hvac_b.get("preheat_system") is None
         or hvac_b["preheat_system"].get("heating_system_type") is None
@@ -73,19 +84,27 @@ def is_baseline_system_2(rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list):
         # short-circuit the logic if no required data is found.
         has_required_sys
         # sub functions handles missing required sys, and return False.
-        and is_hvac_sys_heating_type_heat_pump(rmi_b, hvac_b_id)
-        and is_hvac_sys_cooling_type_dx(rmi_b, hvac_b_id)
         and is_hvac_sys_fan_sys_cv(rmi_b, hvac_b_id)
         and does_hvac_system_serve_single_zone(rmi_b, zone_id_list)
         and does_each_zone_have_only_one_terminal(rmi_b, zone_id_list)
         and are_all_terminal_heat_sources_none_or_null(rmi_b, terminal_unit_id_list)
         and are_all_terminal_cool_sources_none_or_null(rmi_b, terminal_unit_id_list)
         and are_all_terminal_fans_null(rmi_b, terminal_unit_id_list)
-        and are_all_terminal_supplies_ducted(rmi_b, terminal_unit_id_list)
         and are_all_terminal_types_cav(rmi_b, terminal_unit_id_list)
     )
 
     if are_sys_data_matched:
-        is_baseline_system_2 = HVAC_SYS.SYS_2
+        if is_hvac_sys_cooling_type_dx(rmi_b, hvac_b_id):
+            if is_hvac_sys_heating_type_furnace(rmi_b, hvac_b_id):
+                is_baseline_system_3 = HVAC_SYS.SYS_3
+            elif is_hvac_sys_heating_type_fluid_loop(rmi_b, hvac_b_id):
+                is_baseline_system_3 = HVAC_SYS.SYS_3B
+        elif is_hvac_sys_cooling_type_fluid_loop(
+            rmi_b, hvac_b_id
+        ) and is_hvac_sys_fluid_loop_purchased_chw(rmi_b, hvac_b_id):
+            if is_hvac_sys_heating_type_furnace(rmi_b, hvac_b_id):
+                is_baseline_system_3 = HVAC_SYS.SYS_3A
+            elif is_hvac_sys_fluid_loop_purchased_heating(rmi_b, hvac_b_id):
+                is_baseline_system_3 = HVAC_SYS.SYS_3C
 
-    return is_baseline_system_2
+    return is_baseline_system_3
