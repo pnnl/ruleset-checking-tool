7667b232f660ede93a62b673303caf89c08bcce7
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-01-09 09:32:40 -0800

    add ruleset_section_title, standard_section, is_primary_rule attributes

--- rct229/rules/section23/section23rule8.py
+++ rct229/rules/section23/section23rule8.py
@@ -39,6 +39,9 @@ class Section23Rule8(RuleDefinitionListIndexedBase):
             index_rmr="baseline",
             id="23-8",
             description="System 5-8 and 11 - part load VAV fan power shall be modeled using either method 1 or 2 in Table G3.1.3.15. This rule will only validate data points from Method-1 Part-load Fan Power Data. However, both methods are equivalent. When modeling inputs are based on Method 2, values should be converted to Method 1 when writing to RMD.",
+            ruleset_section_title="HVAC - Airside",
+            standard_section="Section G3.1.3.15 VAV Fan Part-Load Performance (Systems 5 through 8 and 11)",
+            is_primary_rule=False,
             rmr_context="ruleset_model_instances/0",
             list_path="$..fan_system",
         )
