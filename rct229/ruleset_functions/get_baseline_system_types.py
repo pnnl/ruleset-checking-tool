66def56fadb04c823f3094e8a403c8a5508fac27
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-01-15 14:21:18 -0800

    add more systems in get_baseline_system_types function

--- rct229/ruleset_functions/get_baseline_system_types.py
+++ rct229/ruleset_functions/get_baseline_system_types.py
@@ -4,14 +4,50 @@ from rct229.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.get_d
     get_dict_with_terminal_units_and_zones,
 )
 from rct229.ruleset_functions.baseline_systems.baseline_system_util import HVAC_SYS
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_1 import (
+    is_baseline_system_1,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_1_a import (
+    is_baseline_system_1_a,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_1_c import (
+    is_baseline_system_1_c,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_2 import (
+    is_baseline_system_2,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_3 import (
+    is_baseline_system_3,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_4 import (
+    is_baseline_system_4,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_5 import (
+    is_baseline_system_5,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_6 import (
+    is_baseline_system_6,
+)
 from rct229.ruleset_functions.baseline_systems.is_baseline_system_7 import (
     is_baseline_system_7,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_8 import (
+    is_baseline_system_8,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_10 import (
+    is_baseline_system_10,
 )
 from rct229.ruleset_functions.baseline_systems.is_baseline_system_11_1 import (
     is_baseline_system_11_1,
 )
 from rct229.ruleset_functions.baseline_systems.is_baseline_system_11_2 import (
     is_baseline_system_11_2,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_12 import (
+    is_baseline_system_12,
+)
+from rct229.ruleset_functions.baseline_systems.is_baseline_system_13 import (
+    is_baseline_system_13,
 )
 from rct229.ruleset_functions.get_dict_of_zones_and_terminal_units_served_by_hvac_sys import (
     get_dict_of_zones_and_terminal_units_served_by_hvac_sys,
@@ -64,12 +100,76 @@ def get_baseline_system_types(rmi_b):
             len(dict_with_terminal_units_and_zones[terminal_id]) == 1
             for terminal_id in terminal_unit_id_list
         ):
+            # HVAC system type 1
+            hvac_sys = is_baseline_system_1(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 2
+            hvac_sys = is_baseline_system_2(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 3
+            hvac_sys = is_baseline_system_3(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 4
+            hvac_sys = is_baseline_system_4(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 5
+            hvac_sys = is_baseline_system_5(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 6
+            hvac_sys = is_baseline_system_6(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 7
             hvac_sys = is_baseline_system_7(
                 rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
             )
             if hvac_sys != HVAC_SYS.UNMATCHED:
                 baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
-                # added to the dictionary, move to next iteration
+                continue
+
+            # HVAC system type 8
+            hvac_sys = is_baseline_system_8(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 10
+            hvac_sys = is_baseline_system_10(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
                 continue
 
             # HVAC system type 11.1
@@ -82,6 +182,22 @@ def get_baseline_system_types(rmi_b):
 
             # HVAC system type 11.2
             hvac_sys = is_baseline_system_11_2(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 12
+            hvac_sys = is_baseline_system_12(
+                rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
+            )
+            if hvac_sys != HVAC_SYS.UNMATCHED:
+                baseline_hvac_system_dict[hvac_sys].append(hvac_b_id)
+                continue
+
+            # HVAC system type 13
+            hvac_sys = is_baseline_system_13(
                 rmi_b, hvac_b_id, terminal_unit_id_list, zone_id_list
             )
             if hvac_sys != HVAC_SYS.UNMATCHED:
--- rct229/ruleset_functions/get_baseline_system_types_test.py
+++ rct229/ruleset_functions/get_baseline_system_types_test.py
@@ -1,6 +1,30 @@
 from rct229.ruleset_functions.baseline_systems.baseline_system_util import HVAC_SYS
+from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_1 import (
+    SYS_1_TEST_RMD,
+)
+from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_2 import (
+    SYS_2_TEST_RMD,
+)
+from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_3 import (
+    SYS_3_TEST_RMD,
+)
+from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_4 import (
+    SYS_4_TEST_RMD,
+)
+from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_5 import (
+    SYS_5_TEST_RMD,
+)
+from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_6 import (
+    SYS_6_TEST_RMD,
+)
 from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_7 import (
     SYS_7_TEST_RMD,
+)
+from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_8 import (
+    SYS_8_TEST_RMD,
+)
+from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_10 import (
+    SYS_10_TEST_RMD,
 )
 from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_11_1 import (
     SYS_11_1_TEST_RMD,
@@ -9,6 +33,108 @@ from rct229.ruleset_functions.baseline_systems.test_is_baseline_system_11_2 impo
     SYS_11_2_TEST_RMD,
 )
 from rct229.ruleset_functions.get_baseline_system_types import get_baseline_system_types
+
+
+def test_get_baseline_system_types__system_1():
+    baseline_system_types_dict = get_baseline_system_types(
+        SYS_1_TEST_RMD["ruleset_model_instances"][0]
+    )
+    available_type_lists = [
+        hvac_type
+        for hvac_type in baseline_system_types_dict.keys()
+        if len(baseline_system_types_dict[hvac_type]) > 0
+    ]
+
+    test_types = [HVAC_SYS.SYS_1, HVAC_SYS.SYS_1A, HVAC_SYS.SYS_1B, HVAC_SYS.SYS_1C]
+
+    assert any(
+        [available_type in test_types for available_type in available_type_lists]
+    )
+
+
+def test_get_baseline_system_types__system_2():
+    baseline_system_types_dict = get_baseline_system_types(
+        SYS_2_TEST_RMD["ruleset_model_instances"][0]
+    )
+    available_type_lists = [
+        hvac_type
+        for hvac_type in baseline_system_types_dict.keys()
+        if len(baseline_system_types_dict[hvac_type]) > 0
+    ]
+
+    test_types = [HVAC_SYS.SYS_2]
+
+    assert any(
+        [available_type in test_types for available_type in available_type_lists]
+    )
+
+
+def test_get_baseline_system_types__system_3():
+    baseline_system_types_dict = get_baseline_system_types(
+        SYS_3_TEST_RMD["ruleset_model_instances"][0]
+    )
+    available_type_lists = [
+        hvac_type
+        for hvac_type in baseline_system_types_dict.keys()
+        if len(baseline_system_types_dict[hvac_type]) > 0
+    ]
+
+    test_types = [HVAC_SYS.SYS_3, HVAC_SYS.SYS_3A, HVAC_SYS.SYS_3B, HVAC_SYS.SYS_3C]
+
+    assert any(
+        [available_type in test_types for available_type in available_type_lists]
+    )
+
+
+def test_get_baseline_system_types__system_4():
+    baseline_system_types_dict = get_baseline_system_types(
+        SYS_4_TEST_RMD["ruleset_model_instances"][0]
+    )
+    available_type_lists = [
+        hvac_type
+        for hvac_type in baseline_system_types_dict.keys()
+        if len(baseline_system_types_dict[hvac_type]) > 0
+    ]
+
+    test_types = [HVAC_SYS.SYS_4]
+
+    assert any(
+        [available_type in test_types for available_type in available_type_lists]
+    )
+
+
+def test_get_baseline_system_types__system_5():
+    baseline_system_types_dict = get_baseline_system_types(
+        SYS_5_TEST_RMD["ruleset_model_instances"][0]
+    )
+    available_type_lists = [
+        hvac_type
+        for hvac_type in baseline_system_types_dict.keys()
+        if len(baseline_system_types_dict[hvac_type]) > 0
+    ]
+
+    test_types = [HVAC_SYS.SYS_5, HVAC_SYS.SYS_5B]
+
+    assert any(
+        [available_type in test_types for available_type in available_type_lists]
+    )
+
+
+def test_get_baseline_system_types__system_6():
+    baseline_system_types_dict = get_baseline_system_types(
+        SYS_6_TEST_RMD["ruleset_model_instances"][0]
+    )
+    available_type_lists = [
+        hvac_type
+        for hvac_type in baseline_system_types_dict.keys()
+        if len(baseline_system_types_dict[hvac_type]) > 0
+    ]
+
+    test_types = [HVAC_SYS.SYS_6, HVAC_SYS.SYS_6B]
+
+    assert any(
+        [available_type in test_types for available_type in available_type_lists]
+    )
 
 
 def test_get_baseline_system_types__system_7():
@@ -22,6 +148,40 @@ def test_get_baseline_system_types__system_7():
     ]
 
     test_types = [HVAC_SYS.SYS_7, HVAC_SYS.SYS_7A, HVAC_SYS.SYS_7B, HVAC_SYS.SYS_7C]
+
+    assert any(
+        [available_type in test_types for available_type in available_type_lists]
+    )
+
+
+def test_get_baseline_system_types__system_8():
+    baseline_system_types_dict = get_baseline_system_types(
+        SYS_8_TEST_RMD["ruleset_model_instances"][0]
+    )
+    available_type_lists = [
+        hvac_type
+        for hvac_type in baseline_system_types_dict.keys()
+        if len(baseline_system_types_dict[hvac_type]) > 0
+    ]
+
+    test_types = [HVAC_SYS.SYS_8, HVAC_SYS.SYS_8A, HVAC_SYS.SYS_8B, HVAC_SYS.SYS_8C]
+
+    assert any(
+        [available_type in test_types for available_type in available_type_lists]
+    )
+
+
+def test_get_baseline_system_types__system_10():
+    baseline_system_types_dict = get_baseline_system_types(
+        SYS_10_TEST_RMD["ruleset_model_instances"][0]
+    )
+    available_type_lists = [
+        hvac_type
+        for hvac_type in baseline_system_types_dict.keys()
+        if len(baseline_system_types_dict[hvac_type]) > 0
+    ]
+
+    test_types = [HVAC_SYS.SYS_10]
 
     assert any(
         [available_type in test_types for available_type in available_type_lists]
