cf003092f482e319bcc65a620d2a87920ab4235a
holl695 <charlie.holly@pnnl.com>
2023-02-21 14:52:15 -0800

    Uncommented section5rule2 and sectiono5rule49 in section5 __init__.py

--- rct229/rulesets/ashrae9012019/section5/__init__.py
+++ rct229/rulesets/ashrae9012019/section5/__init__.py
@@ -3,7 +3,7 @@ import importlib
 
 # TODO: Fix section5rule2 and section5rule49 - they currently cause exceptions
 __all__ = [
-    # "section5rule2",
+    "section5rule2",
     "section5rule3",
     "section5rule5",
     "section5rule8",
@@ -29,7 +29,7 @@ __all__ = [
     "section5rule46",
     "section5rule47",
     "section5rule48",
-    # "section5rule49",
+    "section5rule49",
 ]
 
 
