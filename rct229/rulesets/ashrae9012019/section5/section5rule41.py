62b5283b95b0b2cee0e00636a258e533eaa6d4ba
Weili Xu <weili.xu@pnnl.gov>
2023-03-11 07:17:47 -0800

    reformat

--- rct229/rulesets/ashrae9012019/section5/section5rule41.py
+++ rct229/rulesets/ashrae9012019/section5/section5rule41.py
@@ -1,10 +1,15 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
-from rct229.rulesets.ashrae9012019.ruleset_functions.get_opaque_surface_type import get_opaque_surface_type, OpaqueSurfaceType as OST
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_opaque_surface_type import (
+    get_opaque_surface_type,
+    OpaqueSurfaceType as OST,
+)
 
 ABSORPTION_THERMAL_EXTERIOR = 0.9
-PASS_NOT_EQUAL_MSG = "Roof surface emittance in P-RMD matches that in U-RMD but is not equal to 0.9"
+PASS_NOT_EQUAL_MSG = (
+    "Roof surface emittance in P-RMD matches that in U-RMD but is not equal to 0.9"
+)
 PASS_DIFFERS_MSG = "Roof thermal emittance is equal to the prescribed default value of 0.9 but differs from the thermal emittance in the user model"
 
 
@@ -35,9 +40,7 @@ class Section5Rule41(RuleDefinitionListIndexedBase):
 
         def list_filter(self, context_item, data=None):
             surface_p = context_item.proposed
-            return (
-                get_opaque_surface_type(surface_p) == OST.ROOF
-            )
+            return get_opaque_surface_type(surface_p) == OST.ROOF
 
         class RoofRule(RuleDefinitionBase):
             def __init__(self):
@@ -63,17 +66,24 @@ class Section5Rule41(RuleDefinitionListIndexedBase):
                 }
 
             def rule_check(self, context, calc_vals=None, data=None):
-                return calc_vals["absorptance_thermal_exterior_p"] == calc_vals["absorptance_thermal_exterior_u"] or calc_vals["absorptance_thermal_exterior_p"] == ABSORPTION_THERMAL_EXTERIOR
+                return (
+                    calc_vals["absorptance_thermal_exterior_p"]
+                    == calc_vals["absorptance_thermal_exterior_u"]
+                    or calc_vals["absorptance_thermal_exterior_p"]
+                    == ABSORPTION_THERMAL_EXTERIOR
+                )
 
             def get_pass_msg(self, context, calc_vals=None, data=None):
                 """Pre-condition: see rule_check"""
-                absorptance_thermal_exterior_p = calc_vals["absorptance_thermal_exterior_p"]
-                absorptance_thermal_exterior_u = calc_vals["absorptance_thermal_exterior_u"]
+                absorptance_thermal_exterior_p = calc_vals[
+                    "absorptance_thermal_exterior_p"
+                ]
+                absorptance_thermal_exterior_u = calc_vals[
+                    "absorptance_thermal_exterior_u"
+                ]
                 pass_msg = ""
                 if absorptance_thermal_exterior_p != ABSORPTION_THERMAL_EXTERIOR:
                     pass_msg = PASS_NOT_EQUAL_MSG
                 if absorptance_thermal_exterior_u != absorptance_thermal_exterior_p:
                     pass_msg = PASS_DIFFERS_MSG
                 return pass_msg
-
-
