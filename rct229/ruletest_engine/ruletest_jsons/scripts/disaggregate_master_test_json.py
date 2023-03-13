e89f77302febc763d7bd1e502a88f8243a87a202
Karen Walkerman <kwalkerman@gard.com>
2023-03-13 20:25:36 +0100

    remove changes to python files

--- rct229/ruletest_engine/ruletest_jsons/scripts/disaggregate_master_test_json.py
+++ rct229/ruletest_engine/ruletest_jsons/scripts/disaggregate_master_test_json.py
@@ -3,6 +3,6 @@ from rct229.ruletest_engine.ruletest_jsons.scripts.json_generation_utilities imp
 )
 
 # Name of master spreadsheet in rct229.ruletest_engine.ruletest_jsons
-json_name = "envelope_tests.json"
+json_name = "chiller_tests.json"
 ruleset_doc = "ashrae9012019"
 disaggregate_master_ruletest_json(json_name, ruleset_doc)
