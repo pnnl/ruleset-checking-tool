fe1a369bf482fd8c1418cd97c0d936acca5b73c0
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-03-13 16:12:05 -0700

    update rule and RMDs

--- rct229/rulesets/ashrae9012019/section19/section19rule5.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule5.py
@@ -1,75 +1,65 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
-from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.schema.config import ureg
 
-
-MAX_COINCIDENT_UNMET_LOAD_HOUR = 300
-MAX_SUM_HEATING_COOLING_UNMET_HOUR = 300
+MAX_COINCIDENT_UNMET_LOAD_HOUR = 300 * ureg("hr")
+MAX_SUM_HEATING_COOLING_UNMET_HOUR = 300 * ureg("hr")
 UNDETERMINED_MSG = "Conduct manual check that unmet load hours for the proposed design do not exceed 300 (of the 8760 hours simulated)."
 
 
-class Section19Rule5(RuleDefinitionListIndexedBase):
+class Section19Rule5(RuleDefinitionBase):
     """Rule 5 of ASHRAE 90.1-2019 Appendix G Section 19 (HVAC - General)"""
 
     def __init__(self):
         super(Section19Rule5, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, False, True),
-            each_rule=Section19Rule5.OutputRule(),
-            index_rmr="proposed",
+            # each_rule=Section19Rule5.OutputRule(),
+            # index_rmr="proposed",
             id="19-5",
             description="Unmet load hours for the proposed design shall not exceed 300 (of the 8760 hours simulated).",
             ruleset_section_title="HVAC - General",
             standard_section=" Section G3.1.2.3",
             is_primary_rule=True,
             rmr_context="ruleset_model_instances/0",
-            list_path="$.buildings[*].output[*]",
+            required_fields={
+                "$": ["output"],
+            },
+            manual_check_required_msg=UNDETERMINED_MSG,
         )
 
-    class OutputRule(RuleDefinitionBase):
-        def __init__(self):
-            super(Section19Rule5.OutputRule, self).__init__(
-                rmrs_used=UserBaselineProposedVals(False, False, True),
-                manual_check_required_msg=UNDETERMINED_MSG,
-            )
+    def get_calc_vals(self, context, data=None):
+        rmi_p = context.proposed
+        output_instance_p = rmi_p["output"]["output_instance"]
 
-        def get_calc_vals(self, context, data=None):
-            output_p = context.baseline
+        unmet_load_hours_heating_p = output_instance_p["unmet_load_hours_heating"]
+        unmet_load_hours_cooling_p = output_instance_p["unmet_load_hours_cooling"]
+        coincident_unmet_load_hours_p = output_instance_p["unmet_load_hours"]
+        stop = 1
+        return {
+            "unmet_load_hours_heating_p": unmet_load_hours_heating_p,
+            "unmet_load_hours_cooling_p": unmet_load_hours_cooling_p,
+            "coincident_unmet_load_hours_p": coincident_unmet_load_hours_p,
+        }
 
-            unmet_load_hours_heating_p = output_p["OutputInstance"][
-                "unmet_load_hours_heating"
-            ]
-            unmet_load_hours_cooling_p = output_p["OutputInstance"][
-                "unmet_load_hours_cooling"
-            ]
-            coincident_unmet_load_hours_p = output_p["OutputInstance"][
-                "unmet_load_hours"
-            ]
+    # def manual_check_required(self, context, calc_vals, data=None):
+    #     unmet_load_hours_heating_p = calc_vals["unmet_load_hours_heating_p"]
+    #     unmet_load_hours_cooling_p = calc_vals["unmet_load_hours_cooling_p"]
+    #     coincident_unmet_load_hours_p = calc_vals["coincident_unmet_load_hours_p"]
+    #
+    #     return not (
+    #         coincident_unmet_load_hours_p <= MAX_COINCIDENT_UNMET_LOAD_HOUR
+    #         or (
+    #             unmet_load_hours_heating_p + unmet_load_hours_cooling_p
+    #             <= MAX_SUM_HEATING_COOLING_UNMET_HOUR
+    #         )
+    #     )
 
-            return {
-                "unmet_load_hours_heating_p": unmet_load_hours_heating_p,
-                "unmet_load_hours_cooling_p": unmet_load_hours_cooling_p,
-                "coincident_unmet_load_hours_p": coincident_unmet_load_hours_p,
-            }
+    def rule_check(self, context, calc_vals=None, data=None):
+        unmet_load_hours_heating_p = calc_vals["unmet_load_hours_heating_p"]
+        unmet_load_hours_cooling_p = calc_vals["unmet_load_hours_cooling_p"]
+        coincident_unmet_load_hours_p = calc_vals["coincident_unmet_load_hours_p"]
 
-        def manual_check_required(self, context, calc_vals, data=None):
-            unmet_load_hours_heating_p = calc_vals["unmet_load_hours_heating_p"]
-            unmet_load_hours_cooling_p = calc_vals["unmet_load_hours_cooling_p"]
-            coincident_unmet_load_hours_p = calc_vals["coincident_unmet_load_hours_p"]
-
-            return not (
-                coincident_unmet_load_hours_p <= MAX_COINCIDENT_UNMET_LOAD_HOUR
-                or (
-                    unmet_load_hours_heating_p + unmet_load_hours_cooling_p
-                    <= MAX_SUM_HEATING_COOLING_UNMET_HOUR
-                )
-            )
-
-        def rule_check(self, context, calc_vals=None, data=None):
-            unmet_load_hours_heating_p = calc_vals["unmet_load_hours_heating_p"]
-            unmet_load_hours_cooling_p = calc_vals["unmet_load_hours_cooling_p"]
-            coincident_unmet_load_hours_p = calc_vals["coincident_unmet_load_hours_p"]
-
-            return coincident_unmet_load_hours_p <= MAX_COINCIDENT_UNMET_LOAD_HOUR or (
-                unmet_load_hours_heating_p + unmet_load_hours_cooling_p
-                <= MAX_SUM_HEATING_COOLING_UNMET_HOUR
-            )
+        return coincident_unmet_load_hours_p <= MAX_COINCIDENT_UNMET_LOAD_HOUR or (
+            unmet_load_hours_heating_p + unmet_load_hours_cooling_p
+            <= MAX_SUM_HEATING_COOLING_UNMET_HOUR
+        )
--- rct229/rulesets/ashrae9012019/section19/section19rule6.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule6.py
@@ -1,75 +1,64 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.schema.config import ureg
 
-
-MAX_COINCIDENT_UNMET_LOAD_HOUR = 300
-MAX_SUM_HEATING_COOLING_UNMET_HOUR = 300
+MAX_COINCIDENT_UNMET_LOAD_HOUR = 300 * ureg("hr")
+MAX_SUM_HEATING_COOLING_UNMET_HOUR = 300 * ureg("hr")
 UNDETERMINED_MSG = "Conduct manual check that unmet load hours for the baseline design do not exceed 300 (of the 8760 hours simulated)."
 
 
-class Section19Rule6(RuleDefinitionListIndexedBase):
+class Section19Rule6(RuleDefinitionBase):
     """Rule 6 of ASHRAE 90.1-2019 Appendix G Section 19 (HVAC - General)"""
 
     def __init__(self):
         super(Section19Rule6, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, True, False),
-            each_rule=Section19Rule6.OutputRule(),
-            index_rmr="baseline",
             id="19-6",
             description=" Unmet load hours for the baseline design shall not exceed 300 (of the 8760 hours simulated).",
             ruleset_section_title="HVAC - General",
             standard_section=" Section G3.1.2.3",
             is_primary_rule=True,
             rmr_context="ruleset_model_instances/0",
-            list_path="$.buildings[*].output[*]",
+            required_fields={
+                "$": ["output"],
+            },
+            manual_check_required_msg=UNDETERMINED_MSG,
         )
 
-    class OutputRule(RuleDefinitionBase):
-        def __init__(self):
-            super(Section19Rule6.OutputRule, self).__init__(
-                rmrs_used=UserBaselineProposedVals(False, True, False),
-                manual_check_required_msg=UNDETERMINED_MSG,
-            )
+    def get_calc_vals(self, context, data=None):
+        rmi_b = context.baseline
+        output_instance_b = rmi_b["output"]["output_instance"]
 
-        def get_calc_vals(self, context, data=None):
-            output_b = context.baseline
+        unmet_load_hours_heating_b = output_instance_b["unmet_load_hours_heating"]
+        unmet_load_hours_cooling_b = output_instance_b["unmet_load_hours_cooling"]
+        coincident_unmet_load_hours_b = output_instance_b["unmet_load_hours"]
 
-            unmet_load_hours_heating_b = output_b["OutputInstance"][
-                "unmet_load_hours_heating"
-            ]
-            unmet_load_hours_cooling_b = output_b["OutputInstance"][
-                "unmet_load_hours_cooling"
-            ]
-            coincident_unmet_load_hours_b = output_b["OutputInstance"][
-                "unmet_load_hours"
-            ]
+        return {
+            "unmet_load_hours_heating_b": unmet_load_hours_heating_b,
+            "unmet_load_hours_cooling_b": unmet_load_hours_cooling_b,
+            "coincident_unmet_load_hours_b": coincident_unmet_load_hours_b,
+        }
 
-            return {
-                "unmet_load_hours_heating_b": unmet_load_hours_heating_b,
-                "unmet_load_hours_cooling_b": unmet_load_hours_cooling_b,
-                "coincident_unmet_load_hours_b": coincident_unmet_load_hours_b,
-            }
+    # def manual_check_required(self, context, calc_vals, data=None):
+    #     unmet_load_hours_heating_b = calc_vals["unmet_load_hours_heating_b"]
+    #     unmet_load_hours_cooling_b = calc_vals["unmet_load_hours_cooling_b"]
+    #     coincident_unmet_load_hours_b = calc_vals["coincident_unmet_load_hours_b"]
+    #
+    #     return not (
+    #         coincident_unmet_load_hours_b <= MAX_COINCIDENT_UNMET_LOAD_HOUR
+    #         or (
+    #             unmet_load_hours_heating_b + unmet_load_hours_cooling_b
+    #             <= MAX_SUM_HEATING_COOLING_UNMET_HOUR
+    #         )
+    #     )
 
-        def manual_check_required(self, context, calc_vals, data=None):
-            unmet_load_hours_heating_b = calc_vals["unmet_load_hours_heating_b"]
-            unmet_load_hours_cooling_b = calc_vals["unmet_load_hours_cooling_b"]
-            coincident_unmet_load_hours_b = calc_vals["coincident_unmet_load_hours_b"]
+    def rule_check(self, context, calc_vals=None, data=None):
+        unmet_load_hours_heating_b = calc_vals["unmet_load_hours_heating_b"]
+        unmet_load_hours_cooling_b = calc_vals["unmet_load_hours_cooling_b"]
+        coincident_unmet_load_hours_b = calc_vals["coincident_unmet_load_hours_b"]
 
-            return not (
-                coincident_unmet_load_hours_b <= MAX_COINCIDENT_UNMET_LOAD_HOUR
-                or (
-                    unmet_load_hours_heating_b + unmet_load_hours_cooling_b
-                    <= MAX_SUM_HEATING_COOLING_UNMET_HOUR
-                )
-            )
-
-        def rule_check(self, context, calc_vals=None, data=None):
-            unmet_load_hours_heating_b = calc_vals["unmet_load_hours_heating_b"]
-            unmet_load_hours_cooling_b = calc_vals["unmet_load_hours_cooling_b"]
-            coincident_unmet_load_hours_b = calc_vals["coincident_unmet_load_hours_b"]
-
-            return coincident_unmet_load_hours_b <= MAX_COINCIDENT_UNMET_LOAD_HOUR or (
-                unmet_load_hours_heating_b + unmet_load_hours_cooling_b
-                <= MAX_SUM_HEATING_COOLING_UNMET_HOUR
-            )
+        return coincident_unmet_load_hours_b <= MAX_COINCIDENT_UNMET_LOAD_HOUR or (
+            unmet_load_hours_heating_b + unmet_load_hours_cooling_b
+            <= MAX_SUM_HEATING_COOLING_UNMET_HOUR
+        )
