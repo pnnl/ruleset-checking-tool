628281cb32a5c0bdd2c288c7029a05171b3b1b65
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-03-20 12:35:23 -0700

    finish 19-2 development

--- rct229/rulesets/ashrae9012019/section19/section19rule2.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule2.py
@@ -23,6 +23,9 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.baseline_systems.baseline_h
 from rct229.rulesets.ashrae9012019.ruleset_functions.baseline_systems.baseline_hvac_sub_functions.is_hvac_sys_preheating_type_fluid_loop import (
     is_hvac_sys_preheating_type_fluid_loop,
 )
+from rct229.rulesets.ashrae9012019.ruleset_functions.baseline_systems.baseline_system_util import (
+    find_exactly_one_hvac_system,
+)
 from rct229.utils.assertions import getattr_
 from rct229.utils.jsonpath_utils import find_all
 
@@ -44,81 +47,74 @@ class Section19Rule2(RuleDefinitionListIndexedBase):
             standard_section="Section G3.1.2.2",
             is_primary_rule=True,
             rmr_context="ruleset_model_instances/0",
-            list_path="$.buildings[*].fluid_loops[*]",
-        )
-
-    def is_applicable(self, context, data=None):
-        fluid_loop_b = context.baseline
-        fluid_loop_id_b = fluid_loop_b["id"]
-
-        HW_fluid_loop_list = data["HW_fluid_loop_list"]
-        CHW_fluid_loop_list = data["CHW_fluid_loop_list"]
-        CW_fluid_loop_list = data["CW_fluid_loop_list"]
-
-        return (
-            fluid_loop_id_b in HW_fluid_loop_list
-            or fluid_loop_id_b in CHW_fluid_loop_list
-            or CW_fluid_loop_list in CW_fluid_loop_list
+            list_path="$.fluid_loops[*]",
         )
 
     def create_data(self, context, data):
         rmi_b = context.baseline
 
-        hvac_systems_b = find_all(
-            "$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*]",
+        hvac_ids_b = find_all(
+            "$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*].id",
             rmi_b,
         )
 
         HW_fluid_loop_list = []
         CW_fluid_loop_list = []
         CHW_fluid_loop_list = []
-        for hvac_b in hvac_systems_b:
-            hvac_id_b = hvac_b["id"]
+        for hvac_id_b in hvac_ids_b:
             if is_hvac_sys_heating_type_fluid_loop(
-                hvac_b, hvac_id_b
-            ) and is_hvac_sys_fluid_loop_attached_to_boiler(hvac_b, hvac_id_b):
+                rmi_b, hvac_id_b
+            ) and is_hvac_sys_fluid_loop_attached_to_boiler(rmi_b, hvac_id_b):
                 HW_fluid_loop_list.append(
                     getattr_(
-                        hvac_b, "hot_water_loop", "heating_system", "hot_water_loop"
+                        find_exactly_one_hvac_system(rmi_b, hvac_id_b),
+                        "hot_water_loop",
+                        "heating_system",
+                        "hot_water_loop",
                     )
                 )
 
             if is_hvac_sys_preheating_type_fluid_loop(
-                hvac_b, hvac_id_b
-            ) and is_hvac_sys_preheat_fluid_loop_attached_to_boiler(hvac_b, hvac_id_b):
+                rmi_b, hvac_id_b
+            ) and is_hvac_sys_preheat_fluid_loop_attached_to_boiler(rmi_b, hvac_id_b):
                 HW_fluid_loop_list.append(
                     getattr_(
-                        hvac_b, "hot_water_loop", "preheating_system", "hot_water_loop"
+                        find_exactly_one_hvac_system(rmi_b, hvac_id_b),
+                        "hot_water_loop",
+                        "preheat_system",
+                        "hot_water_loop",
                     )
                 )
 
             if is_hvac_sys_cooling_type_fluid_loop(
-                hvac_b, hvac_id_b
-            ) and is_hvac_sys_fluid_loop_attached_to_chiller(hvac_b, hvac_id_b):
+                rmi_b, hvac_id_b
+            ) and is_hvac_sys_fluid_loop_attached_to_chiller(rmi_b, hvac_id_b):
                 CW_fluid_loop_list.append(
                     getattr_(
-                        hvac_b,
+                        find_exactly_one_hvac_system(rmi_b, hvac_id_b),
                         "chilled_water_loop",
                         "cooling_system",
                         "chilled_water_loop",
                     )
                 )
 
-        # for CHW_child_loop in find_all("$.buildings[*].fluid_loops[*]", rmi_b):
-        #     if CHW_child_loop["type"] ==
-        #     CHW_fluid_loop_list.append(CHW_child_loop)
+        for CHW_child_loop in find_all(
+            "$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*].cooling_system.chilled_water_loop",
+            rmi_b,
+        ):
+            CHW_fluid_loop_list.append(CHW_child_loop)
 
         # extend `HW_fluid_loop_list` with terminals
         HW_fluid_loop_list.extend(
             [
-                terminal["id"]
+                terminal["heating_from_loop"]
                 for terminal in find_all(
-                    "$.buildings[*].building_segments[*].terminals[*]", rmi_b
+                    "$.buildings[*].building_segments[*].zones[*].terminals[*]", rmi_b
                 )
                 if (
-                    terminal == HEATING_SOURCE.HOT_WATER
+                    terminal.get("heating_source") == HEATING_SOURCE.HOT_WATER
                     and are_all_terminal_heating_loops_attached_to_boiler(
-                        rmi_b, terminal["id"]
+                        rmi_b, [terminal["id"]]
                     )
                 )
             ]
@@ -134,6 +130,23 @@ class Section19Rule2(RuleDefinitionListIndexedBase):
         def __init__(self):
             super(Section19Rule2.FluidLoopRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, False),
+                required_fields={
+                    "$": ["type"],
+                },
+            )
+
+        def is_applicable(self, context, data=None):
+            fluid_loop_b = context.baseline
+            fluid_loop_id_b = fluid_loop_b["id"]
+
+            HW_fluid_loop_list = data["HW_fluid_loop_list"]
+            CHW_fluid_loop_list = data["CHW_fluid_loop_list"]
+            CW_fluid_loop_list = data["CW_fluid_loop_list"]
+
+            return (
+                fluid_loop_id_b in HW_fluid_loop_list
+                or fluid_loop_id_b in CHW_fluid_loop_list
+                or CW_fluid_loop_list in CW_fluid_loop_list
             )
 
         def get_calc_vals(self, context, data=None):
