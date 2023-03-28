ff15b874219ed7aa6f9e31e417fd0c7dc6350f36
Weili Xu <weili.xu@pnnl.gov>
2023-03-27 23:30:53 -0400

    add inline comments

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_most_used_weekday_hourly_schedule.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_most_used_weekday_hourly_schedule.py
@@ -3,6 +3,7 @@ import pydash
 from rct229.rulesets.ashrae9012019.data.schema_enums import schema_enums
 
 HOURS_IN_DAY = 24
+DAYS_IN_WEEK = 7
 
 DAY_OF_WEEK = schema_enums["DayOfWeekOptions"]
 
@@ -32,15 +33,16 @@ def get_most_used_weekday_hourly_schedule(hourly_data: list, day_of_week_for_jan
     most_used_schedule: list contains 24 hours data.
     """
     assert (
-        len(hourly_data) == 8760
-    ), f"Insufficient data, expected 8760 hours data, but got {len(hourly_data)} instead."
-    # verified this is 8760 hours
+        len(hourly_data) == 8760 or len(hourly_data) == 8784
+    ), f"Insufficient data, expected 8760 or 8784 hours data, but got {len(hourly_data)} instead."
+    # verified this is 8760 or 8784 (leap year) hours
     number_of_days = int(len(hourly_data) / HOURS_IN_DAY)
     daily_data = pydash.chunk(hourly_data, HOURS_IN_DAY)
     days_of_week = [
-        (i + DATE_NUMBER_MAP[day_of_week_for_jan_first]) % 7
+        (i + DATE_NUMBER_MAP[day_of_week_for_jan_first]) % DAYS_IN_WEEK
         for i in range(number_of_days)
     ]
+    # 5 (saturday)
     weekdays_data = [day for i, day in enumerate(daily_data) if days_of_week[i] < 5]
     # Calculate the frequency of each 24-hour schedule
     schedule_frequencies = [tuple(day) for day in weekdays_data]
