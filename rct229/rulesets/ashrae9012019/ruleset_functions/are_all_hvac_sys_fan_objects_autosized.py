eb84121ab1e309b69d05caf3abf35a95faff5fea
Weili Xu <weili.xu@pnnl.gov>
2023-04-03 10:22:40 -0400

    Create are_all_hvac_sys_fan_objects_autosized

---
+++ rct229/rulesets/ashrae9012019/ruleset_functions/are_all_hvac_sys_fan_objects_autosized.py
@@ -0,0 +1,50 @@
+from rct229.rulesets.ashrae9012019.ruleset_functions.baseline_systems.baseline_system_util import (
+    find_exactly_one_hvac_system,
+    find_exactly_one_terminal_unit,
+)
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_dict_of_zones_and_terminal_units_served_by_hvac_sys import (
+    get_dict_of_zones_and_terminal_units_served_by_hvac_sys,
+)
+from rct229.utils.assertions import getattr_
+from rct229.utils.jsonpath_utils import find_all
+
+
+def are_all_hvac_sys_fan_objs_autosized(rmi, hvac_id):
+    """
+
+    Parameters
+    ----------
+    rmi
+    hvac_id
+
+    Returns
+    -------
+
+    """
+    hvac = find_exactly_one_hvac_system(rmi, hvac_id)
+    dict_of_zones_and_terminal_units_served_by_hvac_sys = (
+        get_dict_of_zones_and_terminal_units_served_by_hvac_sys(rmi)
+    )
+
+    return (
+        all(
+            [
+                getattr_(supply_fan, "supply_fan", "is_airflow_autosized")
+                for supply_fan in find_all("$.supply_fans[*]", hvac["fan_system"])
+            ]
+        )
+        if hvac.get("fan_system")
+        else all(
+            [
+                getattr_(
+                    find_exactly_one_terminal_unit(rmi, terminal_id),
+                    "terminal",
+                    "fan",
+                    "is_airflow_autosized",
+                )
+                for terminal_id in dict_of_zones_and_terminal_units_served_by_hvac_sys[
+                    hvac_id
+                ]["terminal_unit_list"]
+            ]
+        )
+    )
---
+++ rct229/rulesets/ashrae9012019/ruleset_functions/are_all_hvac_sys_fan_objects_autosized_test.py
@@ -0,0 +1,177 @@
+import pytest
+
+from rct229.rulesets.ashrae9012019.ruleset_functions.are_all_hvac_sys_fan_objects_autosized import (
+    are_all_hvac_sys_fan_objs_autosized,
+)
+from rct229.schema.validate import schema_validate_rmr
+from rct229.utils.assertions import MissingKeyException
+
+TEST_BUILDING = {
+    "id": "test_rmd",
+    "buildings": [
+        {
+            "id": "building_1",
+            "building_segments": [
+                {
+                    "id": "building_segment_1",
+                    "zones": [
+                        {
+                            "id": "zone_1",
+                            "terminals": [
+                                {
+                                    "id": "terminal_1",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_4",
+                                    "fan": {
+                                        "id": "Terminal Fan 1",
+                                        "is_airflow_autosized": True,
+                                    },
+                                },
+                                {
+                                    "id": "terminal_2",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_5",
+                                    "fan": {
+                                        "id": "Terminal Fan 2",
+                                        "is_airflow_autosized": True,
+                                    },
+                                },
+                                {
+                                    "id": "terminal_3",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_5",
+                                    "fan": {
+                                        "id": "Terminal Fan 3",
+                                        "is_airflow_autosized": False,
+                                    },
+                                },
+                                {
+                                    "id": "terminal_4",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_6",
+                                    "fan": {
+                                        "id": "Terminal Fan 4",
+                                        "is_airflow_autosized": True,
+                                    },
+                                },
+                                {
+                                    "id": "terminal_5",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_6",
+                                    "fan": {
+                                        "id": "Terminal Fan 5",
+                                    },
+                                },
+                            ],
+                        },
+                        {
+                            "id": "zone_2",
+                            "terminals": [
+                                {
+                                    # Failed outcome hvac_2 fan autosized is false.
+                                    "id": "terminal_6",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_2",
+                                    "fan": {
+                                        "id": "Terminal Fan 6",
+                                        "is_airflow_autosized": True,
+                                    },
+                                }
+                            ],
+                        },
+                    ],
+                    "heating_ventilating_air_conditioning_systems": [
+                        {
+                            # Success case - is_airflow_autosized => True
+                            "id": "hvac_1",
+                            "fan_system": {
+                                "id": "VAV Fan System 1",
+                                "fan_control": "VARIABLE_SPEED_DRIVE",
+                                "supply_fans": [
+                                    {"id": "Supply Fan 1", "is_airflow_autosized": True}
+                                ],
+                            },
+                        },
+                        {
+                            # Failed case - is_airflow_autosized => False (at least one one)
+                            "id": "hvac_2",
+                            "fan_system": {
+                                "id": "VAV Fan System 2",
+                                "fan_control": "VARIABLE_SPEED_DRIVE",
+                                "supply_fans": [
+                                    {
+                                        "id": "Supply Fan 2-1",
+                                        "is_airflow_autosized": True,
+                                    },
+                                    {
+                                        "id": "Supply Fan 2-2",
+                                        "is_airflow_autosized": False,
+                                    },
+                                ],
+                            },
+                        },
+                        {
+                            # Raise exception case - is_airflow_autosized => Missing
+                            "id": "hvac_3",
+                            "fan_system": {
+                                "id": "VAV Fan System 3",
+                                "fan_control": "VARIABLE_SPEED_DRIVE",
+                                "supply_fans": [
+                                    {
+                                        "id": "Supply Fan 3-1",
+                                        "is_airflow_autosized": True,
+                                    },
+                                    {
+                                        "id": "Supply Fan 3-2",
+                                    },
+                                ],
+                            },
+                        },
+                        {
+                            # Success case, all terminal fans are autosized
+                            "id": "hvac_4",
+                        },
+                        {
+                            # Failed case, one terminal fan is false
+                            "id": "hvac_5",
+                        },
+                        {
+                            # Raise Exception case, one terminal fan has no autosized data.
+                            "id": "hvac_6",
+                        },
+                    ],
+                }
+            ],
+        }
+    ],
+}
+
+
+TEST_RMD = {"id": "ASHRAE229", "ruleset_model_instances": [TEST_BUILDING]}
+
+
+def test__TEST_RMD__is_valid():
+    schema_validation_result = schema_validate_rmr(TEST_RMD)
+    assert schema_validation_result[
+        "passed"
+    ], f"Schema error: {schema_validation_result['error']}"
+
+
+def test__hvac_fan_all_autosized__success():
+    assert are_all_hvac_sys_fan_objs_autosized(TEST_BUILDING, "hvac_1") is True
+
+
+def test__hvac_fan_partial_autosized__failed():
+    assert are_all_hvac_sys_fan_objs_autosized(TEST_BUILDING, "hvac_2") is False
+
+
+def test__hvac_fan_missing_autosized__exception():
+    with pytest.raises(MissingKeyException):
+        are_all_hvac_sys_fan_objs_autosized(TEST_BUILDING, "hvac_3")
+
+
+def test_hvac_fan_all_terminal_autosized__success():
+    assert are_all_hvac_sys_fan_objs_autosized(TEST_BUILDING, "hvac_4") is True
+
+
+def test_hvac_fan_all_terminal_fan_partial_autosized__failed():
+    assert are_all_hvac_sys_fan_objs_autosized(TEST_BUILDING, "hvac_5") is False
+
+
+def test__hvac_terminal_fan_missing_autosized__exception():
+    with pytest.raises(MissingKeyException):
+        are_all_hvac_sys_fan_objs_autosized(TEST_BUILDING, "hvac_6")
