d39743d315d310d5589c9dafd3262880eef37db1
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-04-05 18:02:41 -0700

    finish function development

---
+++ rct229/rulesets/ashrae9012019/ruleset_functions/baseline_systems/baseline_hvac_sub_functions/get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM.py
@@ -0,0 +1,53 @@
+from rct229.schema.config import ureg
+from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.assertions import assert_
+from rct229.utils.assertions import getattr_
+
+
+def get_fan_object_electric_power():
+    return 1
+
+
+def get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM(fan_system):
+    all_fan_info = {}
+
+    for fan_type in ["supply_fans", "return_fans", "exhaust_fans", "relief_fans"]:
+        total_fan_power = 0.0 * ureg("W")
+        total_fan_cfm = 0.0 * ureg("cfm")
+        fan_qty = 0
+        design_pressure_rise_tempo = []
+
+        for fan in find_all(f"$.{fan_type}[*]", fan_system):
+            fan_elec_power = get_fan_object_electric_power(fan)
+            if fan_elec_power:
+                total_fan_power += fan_elec_power
+
+            fan_cfm = fan.get("design_airflow")
+            if fan_cfm:
+                total_fan_cfm += fan_cfm
+
+            if fan_cfm > 0.0 * ureg("cfm"):  # TODO check if this > can be omitted
+                fan_qty += 1
+
+            fan_design_pressure_rise = fan.get("design_pressure_rise")
+            design_pressure_rise_tempo.append(fan_design_pressure_rise)
+
+        if None in design_pressure_rise_tempo:
+            fan_pressure_drop = "UNDEFINED"
+        elif design_pressure_rise_tempo and all(
+            design_pressure_rise_tempo[0] == x for x in design_pressure_rise_tempo
+        ):
+            fan_pressure_drop = "IDENTICAL"
+        else:
+            fan_pressure_drop = "DIFFERENT"
+
+        all_fan_info.update(
+            {
+                f"{fan_type}_power": total_fan_power,
+                f"{fan_type}_cfm": total_fan_cfm,
+                f"{fan_type}_qty": fan_qty,
+                f"{fan_type}_pressure": fan_pressure_drop,
+            }
+        )
+
+    return all_fan_info
