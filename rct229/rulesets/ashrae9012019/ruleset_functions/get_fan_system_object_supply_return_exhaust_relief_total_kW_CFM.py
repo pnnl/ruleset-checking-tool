da65ac11de8d7a340ef832567c62658999f931c4
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-04-05 23:38:51 -0700

    add doc string

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM.py
@@ -5,7 +5,18 @@ from rct229.schema.config import ureg
 from rct229.utils.jsonpath_utils import find_all
 
 
-def get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM(fan):
+def get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM(fan_system):
+    """
+    Get the supply, return, exhaust, and relief total fan power, CFM, quantity, and information about whether the pressure drop is consistent across the fans if more than one for a fan system object.
+
+    Parameters
+    ----------
+    fan_system: dict fan_system at heating_ventilating_air_conditioning_systems level
+
+    Returns dict dict that includes supply/return/relief/exhaust fans power/cfm/quantify/pressure_rise keys
+    -------
+
+    """
     all_fan_info = {}
 
     for fan_type in ("supply_fans", "return_fans", "exhaust_fans", "relief_fans"):
@@ -14,7 +25,7 @@ def get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM(fan):
         fan_qty = 0
         design_pressure_rise_tempo = []
 
-        for fan in find_all(f"$.{fan_type}[*]", fan):
+        for fan in find_all(f"$.{fan_type}[*]", fan_system):
             fan_elec_power = get_fan_object_electric_power(fan)
             if fan_elec_power:
                 total_fan_power += fan_elec_power
