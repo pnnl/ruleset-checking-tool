80285e3e14f78f7abd7c48d7819480dbca321b6d
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-04-06 14:28:41 -0700

    set min_OA_CFM_schedule_for_zone to None

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_min_oa_cfm_sch_zone.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_min_oa_cfm_sch_zone.py
@@ -19,6 +19,8 @@ def get_min_oa_cfm_sch_zone(rmi, zone_id, is_leap_year: bool = False):
     list: An aggregated OA CFM hourly schedule for the zone (for each hour of the year, for each terminal unit, Terminal.minimum_outdoor_airflow is multiplied by Terminal.minimum_outdoor_airflow_multiplier_schedule, this product is summed across the terminal units for each hour of the year) .
     """
 
+    min_OA_CFM_schedule_for_zone = None  # Returns None if the zone has no terminal or every terminal in the zone do not use minimum_outdoor_airflow_multiplier_schedule
+
     if is_leap_year:
         min_OA_CFM_schedule_for_zone = [0] * 8784
     else:
