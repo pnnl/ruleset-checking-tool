d464c446455e542cc48c698b99f0b76cd50294ce
Weili Xu <weili.xu@pnnl.gov>
2023-04-07 15:29:19 -0400

    update get fan object electric power

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_fan_object_electric_power.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_fan_object_electric_power.py
@@ -1,6 +1,6 @@
 from rct229.rulesets.ashrae9012019.data.schema_enums import schema_enums
 from rct229.schema.config import ureg
-from rct229.utils.assertions import getattr_
+from rct229.utils.assertions import getattr_, assert_
 from rct229.utils.pint_utils import ZERO
 
 
@@ -22,22 +22,28 @@ def get_fan_object_electric_power(fan):
     None if missing key parameters for calculating the fan power
 
     """
-    fan_elec_power = 0.0
+    fan_elec_power = ZERO.POWER
     fan_spec_method = getattr_(fan, "fan", "specification_method")
     if fan_spec_method == FAN_SPECIFICATION_METHOD.SIMPLE:
-        fan_elec_power = fan.get("design_electric_power", None)
+        fan_elec_power = getattr_(fan, "design_electric_power", "design_electric_power")
     elif fan_spec_method == FAN_SPECIFICATION_METHOD.DETAILED:
-        design_pressure_rise = fan.get("design_pressure_rise", ZERO.LENGTH)
+        design_pressure_rise = fan.get("design_pressure_rise", 0.0)
         design_air_flow = fan.get("design_airflow", ZERO.FLOW)
         total_efficiency = fan.get("total_efficiency", 0.0)
         input_power = fan.get("input_power", ZERO.POWER)
         motor_efficiency = fan.get("motor_efficiency", 0.0)
 
-        if input_power > 0.0 and motor_efficiency > 0.0:
+        if input_power > ZERO.POWER and motor_efficiency > 0.0:
             fan_elec_power = input_power / motor_efficiency
         elif total_efficiency > 0.0 and design_pressure_rise > 0.0:
             fan_elec_power = (
                 design_pressure_rise * design_air_flow / total_efficiency
             ).to("watt")
+        else:
+            assert_(
+                False,
+                f"Check Fan: {fan['id']}, Data missing: input_power or motor_efficiency are missing or "
+                f"equal to 0.0, and total_efficiency or design_pressure_rise are missing or equal to 0.0.",
+            )
 
     return fan_elec_power
