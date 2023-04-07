1f4e2ee1fb44c10cb5dd17f0be3e4986a594c723
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2023-04-06 20:49:01 -0400

    YJ review comments addressed

--- rct229/rulesets/ashrae9012019/section5/section5rule39.py
+++ rct229/rulesets/ashrae9012019/section5/section5rule39.py
@@ -1,6 +1,9 @@
+from rct229.rulesets.ashrae9012019.data.schema_enums import schema_enums
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+
+SUBSURFACE_DYNAMIC_GLAZING = schema_enums["SubsurfaceDynamicGlazingOptions"]
 
 
 class Section5Rule39(RuleDefinitionListIndexedBase):
@@ -9,41 +12,35 @@ class Section5Rule39(RuleDefinitionListIndexedBase):
     def __init__(self):
         super(Section5Rule39, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, False, True),
-            each_rule=Section5Rule39.BuildingRule(),
+            each_rule=Section5Rule39.SubsurfaceRule(),
             index_rmr="proposed",
             id="5-39",
             description="Manually controlled dynamic glazing shall use the average of the minimum and maximum SHGC and VT.",
             ruleset_section_title="Envelope",
             standard_section="Section G3.1-5(a)5 Building Envelope Modeling Requirements for the Proposed design",
             is_primary_rule=False,
-            list_path="ruleset_model_instances[0].buildings[*]",
+            list_path="ruleset_model_instances[0].buildings[*].building_segments[*].zones[*].surfaces[*].subsurfaces[*]",
         )
 
-    class BuildingRule(RuleDefinitionListIndexedBase):
+    class SubsurfaceRule(PartialRuleDefinition):
         def __init__(self):
-            super(Section5Rule39.BuildingRule, self).__init__(
+            super(Section5Rule39.SubsurfaceRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, False, True),
-                each_rule=Section5Rule39.BuildingRule.SubsurfaceRule(),
-                index_rmr="proposed",
-                list_path="$.building_segments[*].zones[*].surfaces[*].subsurfaces[*]",
+                required_fields={"$": ["dynamic_glazing_type"]},
             )
 
-        class SubsurfaceRule(PartialRuleDefinition):
-            def __init__(self):
-                super(Section5Rule39.BuildingRule.SubsurfaceRule, self).__init__(
-                    rmrs_used=UserBaselineProposedVals(False, False, True),
-                    required_fields={"$": ["dynamic_glazing_type"]},
-                )
+        def get_calc_vals(self, context, data=None):
+            subsurface_p = context.proposed
+            subsurface_dynamic_glazing_type_p = subsurface_p["dynamic_glazing_type"]
+            return {
+                "subsurface_dynamic_glazing_type_p": subsurface_dynamic_glazing_type_p
+            }
 
-            def get_calc_vals(self, context, data=None):
-                subsurface_p = context.proposed
-                subsurface_dynamic_glazing_type_p = subsurface_p["dynamic_glazing_type"]
-                return {
-                    "subsurface_dynamic_glazing_type_p": subsurface_dynamic_glazing_type_p
-                }
-
-            def applicability_check(self, context, calc_vals, data):
-                subsurface_dynamic_glazing_type_p = calc_vals[
-                    "subsurface_dynamic_glazing_type_p"
-                ]
-                return subsurface_dynamic_glazing_type_p == "MANUAL_DYNAMIC"
+        def applicability_check(self, context, calc_vals, data):
+            subsurface_dynamic_glazing_type_p = calc_vals[
+                "subsurface_dynamic_glazing_type_p"
+            ]
+            return (
+                subsurface_dynamic_glazing_type_p
+                == SUBSURFACE_DYNAMIC_GLAZING.MANUAL_DYNAMIC
+            )
