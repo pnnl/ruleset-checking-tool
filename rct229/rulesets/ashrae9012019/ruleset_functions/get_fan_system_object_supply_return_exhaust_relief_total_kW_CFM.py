6944f053574df3bbbf3c7c1d00d4fc62e909ede5
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-04-10 22:43:11 -0700

    address PR comments

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM.py
@@ -1,8 +1,16 @@
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_fan_object_electric_power import (
     get_fan_object_electric_power,
 )
-from rct229.schema.config import ureg
 from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.pint_utils import ZERO
+
+
+class FanPressureDropCompareCategory:
+    """Enumeration class for fan pressure drop types"""
+
+    UNDEFINED: str = "UNDEFINED"
+    IDENTICAL: str = "IDENTICAL"
+    DIFFERENT: str = "DIFFERENT"
 
 
 def get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(fan_system):
@@ -17,39 +25,38 @@ def get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(fan_system):
     -------
 
     """
-    all_fan_info = {}
 
+    all_fan_info = {}
     for fan_type in ("supply_fans", "return_fans", "exhaust_fans", "relief_fans"):
-        total_fan_power = 0.0 * ureg("W")
-        total_fan_cfm = 0.0 * ureg("cfm")
+        total_fan_power = ZERO.POWER
+        total_fan_airflow = ZERO.FLOW
         fan_qty = 0
-        design_pressure_rise_tempo = []
+        design_pressure_rise_data = []
 
         for fan in find_all(f"$.{fan_type}[*]", fan_system):
-            fan_elec_power = get_fan_object_electric_power(fan)
-            if fan_elec_power:
-                total_fan_power += fan_elec_power
+            total_fan_power += get_fan_object_electric_power(fan)
 
-            fan_cfm = fan.get("design_airflow")
-            if fan_cfm is not None and fan_cfm > 0.0 * ureg("cfm"):
-                total_fan_cfm += fan_cfm
+            fan_airflow = fan.get("design_airflow")
+            if fan_airflow is not None and fan_airflow > ZERO.FLOW:
+                total_fan_airflow += fan_airflow
                 fan_qty += 1
 
-            design_pressure_rise_tempo.append(fan.get("design_pressure_rise"))
+            design_pressure_rise_data.append(fan.get("design_pressure_rise"))
 
-        if None in design_pressure_rise_tempo:
-            fan_pressure_drop = "UNDEFINED"
-        elif design_pressure_rise_tempo and all(
-            design_pressure_rise_tempo[0] == x for x in design_pressure_rise_tempo
-        ):
-            fan_pressure_drop = "IDENTICAL"
-        else:
-            fan_pressure_drop = "DIFFERENT"
+        if design_pressure_rise_data:
+            if None in design_pressure_rise_data:
+                fan_pressure_drop = FanPressureDropCompareCategory.UNDEFINED
+            elif all(
+                design_pressure_rise_data[0] == x for x in design_pressure_rise_data
+            ):
+                fan_pressure_drop = FanPressureDropCompareCategory.IDENTICAL
+            else:
+                fan_pressure_drop = FanPressureDropCompareCategory.DIFFERENT
 
         all_fan_info.update(
             {
                 f"{fan_type}_power": total_fan_power,
-                f"{fan_type}_cfm": total_fan_cfm,
+                f"{fan_type}_airflow": total_fan_airflow,
                 f"{fan_type}_qty": fan_qty,
                 f"{fan_type}_pressure": fan_pressure_drop,
             }
--- rct229/rulesets/ashrae9012019/ruleset_functions/get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM_test.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM_test.py
@@ -1,7 +1,11 @@
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM import (
+    FanPressureDropCompareCategory,
     get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm,
 )
 from rct229.schema.config import ureg
+from rct229.schema.schema_utils import quantify_rmr
+from rct229.schema.validate import schema_validate_rmr
+from rct229.utils.std_comparisons import std_equal
 
 TEST_RMD = {
     "id": "test_rmd",
@@ -50,68 +54,68 @@ TEST_RMD = {
                                 "supply_fans": [
                                     {
                                         "id": "Supply Fan 1",
-                                        "design_airflow": 500 * ureg("cfm"),
+                                        "design_airflow": 500,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 35 * ureg("W"),
-                                        "design_pressure_rise": 0.2 * ureg("W"),
+                                        "design_electric_power": 35,
+                                        "design_pressure_rise": 0.2,
                                     },
                                     {
                                         "id": "Supply Fan 2",
-                                        "design_airflow": 600 * ureg("cfm"),
+                                        "design_airflow": 600,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 50 * ureg("W"),
+                                        "design_electric_power": 50,
                                     },
                                     {
                                         "id": "Supply Fan 2",
-                                        "design_airflow": 100 * ureg("cfm"),
+                                        "design_airflow": 100,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 50 * ureg("W"),
+                                        "design_electric_power": 50,
                                     },
                                 ],
                                 "relief_fans": [
                                     {
                                         "id": "Relief fan 1",
-                                        "design_airflow": 200 * ureg("cfm"),
+                                        "design_airflow": 200,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 15 * ureg("W"),
-                                        "design_pressure_rise": 0.1 * ureg("W"),
+                                        "design_electric_power": 15,
+                                        "design_pressure_rise": 0.1,
                                     },
                                     {
                                         "id": "Relief fan 1",
-                                        "design_airflow": 100 * ureg("cfm"),
+                                        "design_airflow": 100,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 35 * ureg("W"),
-                                        "design_pressure_rise": 0.05 * ureg("W"),
+                                        "design_electric_power": 35,
+                                        "design_pressure_rise": 0.05,
                                     },
                                 ],
                                 "exhaust_fans": [
                                     {
                                         "id": "Exhaust fan 1",
-                                        "design_airflow": 100 * ureg("cfm"),
+                                        "design_airflow": 100,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 35 * ureg("W"),
+                                        "design_electric_power": 35,
                                     },
                                     {
                                         "id": "Exhaust fan 2",
-                                        "design_airflow": 50 * ureg("cfm"),
+                                        "design_airflow": 50,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 25 * ureg("W"),
+                                        "design_electric_power": 25,
                                     },
                                 ],
                                 "return_fans": [
                                     {
                                         "id": "Return fan 1",
-                                        "design_airflow": 700 * ureg("cfm"),
+                                        "design_airflow": 700,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 30 * ureg("W"),
-                                        "design_pressure_rise": 0.1 * ureg("W"),
+                                        "design_electric_power": 30,
+                                        "design_pressure_rise": 0.1,
                                     },
                                     {
                                         "id": "Return fan 2",
-                                        "design_airflow": 60 * ureg("cfm"),
+                                        "design_airflow": 60,
                                         "specification_method": "SIMPLE",
-                                        "design_electric_power": 5 * ureg("W"),
-                                        "design_pressure_rise": 0.1 * ureg("W"),
+                                        "design_electric_power": 5,
+                                        "design_pressure_rise": 0.1,
                                     },
                                 ],
                             },
@@ -128,26 +132,135 @@ TEST_RMD = {
 }
 
 
+TEST_RMD_FULL = {"id": "229", "ruleset_model_instances": [TEST_RMD]}
+
+TEST_FAN_SYSTEM = quantify_rmr(TEST_RMD_FULL)["ruleset_model_instances"][0][
+    "buildings"
+][0]["building_segments"][0]["heating_ventilating_air_conditioning_systems"][0][
+    "fan_system"
+]
+
+
+def test__TEST_RMD__is_valid():
+    schema_validation_result = schema_validate_rmr(TEST_RMD_FULL)
+    assert schema_validation_result[
+        "passed"
+    ], f"Schema error: {schema_validation_result['error']}"
+
+
 def test__get_fan_system_object_supply_return_exhaust_relief_total_kW_CFM():
-    assert get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
-        TEST_RMD["buildings"][0]["building_segments"][0][
-            "heating_ventilating_air_conditioning_systems"
-        ][0]["fan_system"]
-    ) == {
-        "supply_fans_power": 135.0 * ureg("W"),
-        "supply_fans_cfm": 1200.0 * ureg("cfm"),
-        "supply_fans_qty": 3,
-        "supply_fans_pressure": "UNDEFINED",
-        "return_fans_power": 35.0 * ureg("W"),
-        "return_fans_cfm": 760.0 * ureg("cfm"),
-        "return_fans_qty": 2,
-        "return_fans_pressure": "IDENTICAL",
-        "exhaust_fans_power": 60.0 * ureg("W"),
-        "exhaust_fans_cfm": 150.0 * ureg("cfm"),
-        "exhaust_fans_qty": 2,
-        "exhaust_fans_pressure": "UNDEFINED",
-        "relief_fans_power": 50.0 * ureg("W"),
-        "relief_fans_cfm": 300.0 * ureg("cfm"),
-        "relief_fans_qty": 2,
-        "relief_fans_pressure": "DIFFERENT",
-    }
+    # check supply fans
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["supply_fans_power"],
+        135 * ureg("W"),
+    )
+
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["supply_fans_airflow"],
+        1200 * ureg("L/s"),
+    )
+
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["supply_fans_qty"],
+        3,
+    )
+
+    assert (
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["supply_fans_pressure"]
+        == FanPressureDropCompareCategory.UNDEFINED
+    )
+
+    # return fans
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["return_fans_power"],
+        35 * ureg("W"),
+    )
+
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["return_fans_airflow"],
+        760 * ureg("L/s"),
+    )
+
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["return_fans_qty"],
+        2,
+    )
+
+    assert (
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["return_fans_pressure"]
+        == FanPressureDropCompareCategory.IDENTICAL
+    )
+
+    # relief fans
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["relief_fans_power"],
+        50 * ureg("W"),
+    )
+
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["relief_fans_airflow"],
+        300 * ureg("L/s"),
+    )
+
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["relief_fans_qty"],
+        2,
+    )
+
+    assert (
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["relief_fans_pressure"]
+        == FanPressureDropCompareCategory.DIFFERENT
+    )
+
+    # exhaust fans
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["exhaust_fans_power"],
+        60 * ureg("W"),
+    )
+
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["exhaust_fans_airflow"],
+        150 * ureg("L/s"),
+    )
+
+    assert std_equal(
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["exhaust_fans_qty"],
+        2,
+    )
+
+    assert (
+        get_fan_system_object_supply_return_exhaust_relief_total_kw_cfm(
+            TEST_FAN_SYSTEM
+        )["exhaust_fans_pressure"]
+        == FanPressureDropCompareCategory.UNDEFINED
+    )
