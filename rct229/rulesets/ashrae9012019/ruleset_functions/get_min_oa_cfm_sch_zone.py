a914c9f7215a1b3d39a5eddf94168e9d5049d2d5
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-04-10 22:50:41 -0700

    address PR comments

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_min_oa_cfm_sch_zone.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_min_oa_cfm_sch_zone.py
@@ -1,6 +1,9 @@
 from rct229.utils.assertions import assert_, getattr_
 from rct229.utils.jsonpath_utils import find_all, find_exactly_one_with_field_value
 
+LEAP_YEAR_HRS = 8784
+NON_LEAP_YEAR_HRS = 8760
+
 
 def get_min_oa_cfm_sch_zone(rmi, zone_id, is_leap_year: bool = False):
     """Each zone can have multiple terminal units sering it in the proposed RMR and each of these units could supply OA CFM. In order to obtain a zone level OA CFM schedule the OA CFM provided by each terminal unit needs to be aggregated for each hour of the year. This function receives an RMR (B, U, or P) and a zone ID and loops through each terminal unit associated with the zone to create an aggregated 8760 for OA CFM for the zone.
@@ -20,17 +23,18 @@ def get_min_oa_cfm_sch_zone(rmi, zone_id, is_leap_year: bool = False):
           None if the zone has no terminal or every terminal in the zone do not use minimum_outdoor_airflow_multiplier_schedule.
     """
 
-    if is_leap_year:
-        min_OA_CFM_schedule_for_zone = [0] * 8784
-    else:
-        min_OA_CFM_schedule_for_zone = [0] * 8760
+    LEAP_YEAR_HRS = 8784
+    NON_LEAP_YEAR_HRS = 8760
+    year_hrs = LEAP_YEAR_HRS if is_leap_year else NON_LEAP_YEAR_HRS
+
+    min_OA_CFM_schedule_for_zone = [0] * year_hrs
 
     for terminal in find_all(
         f'$.buildings[*].building_segments[*].zones[*][?(@.id = "{zone_id}")].terminals[*]',
         rmi,
     ):
         minimum_outdoor_airflow = getattr_(
-            terminal, "minimum_outdoor_airflow", "minimum_outdoor_airflow"
+            terminal, "terminal", "minimum_outdoor_airflow"
         )
 
         minimum_outdoor_airflow_multiplier_schedule_id = terminal.get(
@@ -45,17 +49,12 @@ def get_min_oa_cfm_sch_zone(rmi, zone_id, is_leap_year: bool = False):
                     minimum_outdoor_airflow_multiplier_schedule_id,
                     rmi,
                 ),
-                "hourly_values",
+                "Schedule",
                 "hourly_values",
             )
 
             assert_(
-                len(minimum_outdoor_airflow_multiplier_schedule)
-                in (
-                    8760,
-                    8784,
-                )
-                and len(min_OA_CFM_schedule_for_zone)
+                len(min_OA_CFM_schedule_for_zone)
                 == len(minimum_outdoor_airflow_multiplier_schedule),
                 f"The length of schedule has to be either 8760 or 8784, but is {len(minimum_outdoor_airflow_multiplier_schedule)}.",
             )
