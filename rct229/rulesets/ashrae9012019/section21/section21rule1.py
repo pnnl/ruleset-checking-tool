90404da54e54852eea415df51f2552803e9f5135
Karen Walkerman <kwalkerman@gard.com>
2023-04-16 13:28:39 +0200

    black formatting

--- rct229/rulesets/ashrae9012019/section21/section21rule1.py
+++ rct229/rulesets/ashrae9012019/section21/section21rule1.py
@@ -13,18 +13,18 @@ class Section21Rule1(PartialRuleDefinition):
             rmrs_used=UserBaselineProposedVals(False, False, True),
             id="21-1",
             description="For systems using purchased hot water or steam, the heating source shall be "
-                        "modeled as purchased hot water or steam in both the proposed design and "
-                        "baseline building design. If any system in the proposed design uses purchased "
-                        "hot water or steam, all baseline systems with hot water coils shall use the "
-                        "same type of purchased hot water or steam.",
+            "modeled as purchased hot water or steam in both the proposed design and "
+            "baseline building design. If any system in the proposed design uses purchased "
+            "hot water or steam, all baseline systems with hot water coils shall use the "
+            "same type of purchased hot water or steam.",
             ruleset_section_title="HVAC - Water Side",
             standard_section="Section G3.1.1.3 Baseline HVAC System Requirements for Systems Utilizing Purchased "
-                             "Chilled Water and/or Purchased Heat",
+            "Chilled Water and/or Purchased Heat",
             is_primary_rule=False,
             rmr_context="ruleset_model_instances/0",
             manual_check_required_msg="Manual Check Required - Proposed is modeled with purchased hot water or steam.  Make "
-                                      "sure the heating source in the baseline building is also purchased hot water or steam.",
-            not_applicable_msg="Rule 21-1 Not Applicable - the proposed is not modeled with Purchased Hot Water or Steam"
+            "sure the heating source in the baseline building is also purchased hot water or steam.",
+            not_applicable_msg="Rule 21-1 Not Applicable - the proposed is not modeled with Purchased Hot Water or Steam",
         )
 
     def applicability_check(self, context, calc_vals, data):
