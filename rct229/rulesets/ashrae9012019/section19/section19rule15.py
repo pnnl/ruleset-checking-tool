da24f6da8411a33dd215a1ac5cb005f1352bdfef
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-04-17 10:59:42 -0700

    update the rule number

--- rct229/rulesets/ashrae9012019/section19/section19rule15.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule15.py
@@ -33,15 +33,15 @@ APPLICABLE_SYS_TYPES = [
 REQ_DESIGN_SUPPLY_AIR_TEMP_SETPOINT = 105.0 * ureg("degF")
 
 
-class Section19Rule17(RuleDefinitionListIndexedBase):
+class Section19Rule15(RuleDefinitionListIndexedBase):
     """Rule 17 of ASHRAE 90.1-2019 Appendix G Section 19 (HVAC - General)"""
 
     def __init__(self):
-        super(Section19Rule17, self).__init__(
+        super(Section19Rule15, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, True, True),
-            each_rule=Section19Rule17.HVACRule(),
+            each_rule=Section19Rule15.HVACRule(),
             index_rmr="baseline",
-            id="19-17",
+            id="19-15",
             description="For baseline system types 9 & 10, the system design supply airflow rates shall be based on the temperature difference between a supply air temperature set point of 105°F and the design space-heating temperature set point, the minimum outdoor airflow rate, or the airflow rate required to comply with applicable codes or accreditation standards, whichever is greater.",
             ruleset_section_title="HVAC - General",
             standard_section="Section G3.1.2.8.2",
@@ -56,11 +56,10 @@ class Section19Rule17(RuleDefinitionListIndexedBase):
         baseline_system_types_dict = get_baseline_system_types(rmi_b)
 
         hvac_info_dict_b = {}
-        for hvac_b in find_all(
+        for hvac_id_b in find_all(
             "$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*].id",
             rmi_b,
         ):
-            hvac_id_b = hvac_b["id"]
             hvac_info_dict_b[hvac_id_b] = {}
             hvac_info_dict_b[hvac_id_b]["all_design_setpoints_105"] = True
             hvac_info_dict_b[hvac_id_b]["proposed_supply_cfm"] = ZERO.FLOW
@@ -117,7 +116,7 @@ class Section19Rule17(RuleDefinitionListIndexedBase):
 
     class HVACRule(RuleDefinitionBase):
         def __init__(self):
-            super(Section19Rule17.HVACRule, self).__init__(
+            super(Section19Rule15.HVACRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, True),
                 required_fields={
                     "$": ["fan_system"],
