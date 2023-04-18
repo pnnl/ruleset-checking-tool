31dc499d816161ee1d9de2bc66ebb618998b1231
Karen Walkerman <kwalkerman@gard.com>
2023-04-18 12:24:24 +0200

    update to include RuleSetModelInstanceRule

--- rct229/rulesets/ashrae9012019/section22/section22rule35.py
+++ rct229/rulesets/ashrae9012019/section22/section22rule35.py
@@ -1,3 +1,4 @@
+from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.rulesets.ashrae9012019.ruleset_functions.check_purchased_chw_hhw_status_dict import (
@@ -5,12 +6,14 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.check_purchased_chw_hhw_sta
 )
 
 
-class Section22Rule35(PartialRuleDefinition):
+class Section22Rule35(RuleDefinitionListIndexedBase):
     """Rule 35 of ASHRAE 90.1-2019 Appendix G Section 22 (Chilled water loop)"""
 
     def __init__(self):
         super(Section22Rule35, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, True, False),
+            each_rule=Section22Rule35.RulesetModelInstanceRule(),
+            index_rmr="baseline",
             id="22-35",
             description="Baseline systems served by purchased chilled water shall not be modeled with"
             " chilled water reset",
@@ -18,12 +21,18 @@ class Section22Rule35(PartialRuleDefinition):
             standard_section="Section G3.1.3.10 Chilled-Water Pumps (Systems 7, 8, 11, 12, and 13)",
             is_primary_rule=False,
             rmr_context="ruleset_model_instances/0",
-            manual_check_required_msg="Manual Check Required - Baseline is modeled with purchased chilled water.  Make sure "
-            "baseline systems served by purchased chilled water are not modeled with chilled water reset.",
+            manual_check_required_msg="Manual Check Required - Baseline is modeled with purchased chilled water.  Make"
+            "sure baseline systems served by purchased chilled water are not modeled with chilled water reset.",
             not_applicable_msg="Rule 22-35 Not Applicable - the baseline is not modeled with Purchased Chilled Water",
         )
 
-    def applicability_check(self, context, calc_vals, data):
-        rmi_b = context.baseline
-        purchased_chw_hhw_status_dict_p = check_purchased_chw_hhw_status_dict(rmi_b)
-        return purchased_chw_hhw_status_dict_p["purchased_cooling"]
+    class RulesetModelInstanceRule(PartialRuleDefinition):
+        def __init__(self):
+            super(Section22Rule35.RulesetModelInstanceRule, self,).__init__(
+                rmrs_used=UserBaselineProposedVals(False, True, False),
+            )
+
+        def applicability_check(self, context, calc_vals, data):
+            rmi_b = context.baseline
+            purchased_chw_hhw_status_dict_p = check_purchased_chw_hhw_status_dict(rmi_b)
+            return purchased_chw_hhw_status_dict_p["purchased_cooling"]
