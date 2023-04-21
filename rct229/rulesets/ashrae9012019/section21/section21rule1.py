4e79e693cb7e6367d61b25f06e5eac38cb0577ce
Karen Walkerman <kwalkerman@gard.com>
2023-04-21 12:39:46 -0400

    RuleDefinitionListIndexedBase

--- rct229/rulesets/ashrae9012019/section21/section21rule1.py
+++ rct229/rulesets/ashrae9012019/section21/section21rule1.py
@@ -1,3 +1,4 @@
+from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.rulesets.ashrae9012019.ruleset_functions.check_purchased_chw_hhw_status_dict import (
@@ -5,12 +6,14 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.check_purchased_chw_hhw_sta
 )
 
 
-class Section21Rule1(PartialRuleDefinition):
+class Section21Rule1(RuleDefinitionListIndexedBase):
     """Rule 1 of ASHRAE 90.1-2019 Appendix G Section 21 (HVAC - Water Side)"""
 
     def __init__(self):
         super(Section21Rule1, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, False, True),
+            each_rule=Section21Rule1.RulesetModelInstanceRule(),
+            index_rmr="proposed",
             id="21-1",
             description="For systems using purchased hot water or steam, the heating source shall be "
             "modeled as purchased hot water or steam in both the proposed design and "
@@ -21,13 +24,20 @@ class Section21Rule1(PartialRuleDefinition):
             standard_section="Section G3.1.1.3 Baseline HVAC System Requirements for Systems Utilizing Purchased "
             "Chilled Water and/or Purchased Heat",
             is_primary_rule=False,
-            rmr_context="ruleset_model_instances/0",
-            manual_check_required_msg="Manual Check Required - Proposed is modeled with purchased hot water or steam.  Make "
-            "sure the heating source in the baseline building is also purchased hot water or steam.",
-            not_applicable_msg="Rule 21-1 Not Applicable - the proposed is not modeled with Purchased Hot Water or Steam",
+            rmr_context="ruleset_model_instances[0]",
+            manual_check_required_msg="Manual Check Required - Proposed is modeled with purchased hot water or steam.  "
+            "Make sure the heating source in the baseline building is also purchased hot water or steam.",
+            not_applicable_msg="Rule 21-1 Not Applicable - the proposed is not modeled with Purchased Hot Water or "
+            "Steam",
         )
 
-    def applicability_check(self, context, calc_vals, data):
-        rmi_p = context.proposed
-        purchased_chw_hhw_status_dict_p = check_purchased_chw_hhw_status_dict(rmi_p)
-        return purchased_chw_hhw_status_dict_p["purchased_heating"]
+    class RulesetModelInstanceRule(PartialRuleDefinition):
+        def __init__(self):
+            super(Section21Rule1.RulesetModelInstanceRule, self,).__init__(
+                rmrs_used=UserBaselineProposedVals(False, False, True),
+            )
+
+        def applicability_check(self, context, calc_vals, data):
+            rmi_p = context.proposed
+            purchased_chw_hhw_status_dict_p = check_purchased_chw_hhw_status_dict(rmi_p)
+            return purchased_chw_hhw_status_dict_p["purchased_heating"]
