06bc52471d97a6aac0bbc0b558aa10e09ffdd3c9
Karen Walkerman <kwalkerman@gard.com>
2023-04-22 09:11:29 -0400

    RuleDefinitionListIndexedBase

--- rct229/rulesets/ashrae9012019/section21/section21rule15.py
+++ rct229/rulesets/ashrae9012019/section21/section21rule15.py
@@ -21,7 +21,7 @@ class Section21Rule15(RuleDefinitionListIndexedBase):
             standard_section="Section G3.1.1.3 Baseline HVAC System Requirements for Systems Utilizing Purchased "
             "Chilled Water and/or Purchased Heat",
             is_primary_rule=False,
-            rmr_context="ruleset_model_instances[0]",
+            list_path="ruleset_model_instances[0]",
             manual_check_required_msg="Manual Check Required - Baseline is modeled with purchased hot water or steam.  "
             "Make sure that that the hot water pump power is 14 W/gpm.",
             not_applicable_msg="Rule 21-15 Not Applicable - the baseline is not modeled with Purchased Hot Water "
