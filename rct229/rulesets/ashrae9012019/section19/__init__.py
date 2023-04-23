fee5f6785aa69d2f781e422496ea96cdb28fb6de
yunjoonjung <yjjung89@gmail.com>
2023-04-22 23:14:52 -0700

    finish development

--- rct229/rulesets/ashrae9012019/section19/__init__.py
+++ rct229/rulesets/ashrae9012019/section19/__init__.py
@@ -25,7 +25,7 @@ __all__ = [
     # "section19rule28",
     # "section19rule29",
     # "section19rule30",
-    # "section19rule31",
+    "section19rule31",
     # "section19rule32",
     # "section19rule34",
     # "section19rule35",
---
+++ rct229/rulesets/ashrae9012019/section19/section19rule31.py
@@ -0,0 +1,78 @@
+from rct229.rule_engine.rule_base import RuleDefinitionBase
+from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
+from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.rulesets.ashrae9012019.data.schema_enums import schema_enums
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_hvac_systems_serving_zone_health_safety_vent_reqs import (
+    get_hvac_systems_serving_zone_health_safety_vent_reqs,
+)
+from rct229.utils.assertions import getattr_
+from rct229.utils.pint_utils import ZERO
+
+FAN_SYSTEM_OPERATION = schema_enums["FanSystemOperationOptions"]
+
+
+class Section19Rule31(RuleDefinitionListIndexedBase):
+    """Rule 31 of ASHRAE 90.1-2019 Appendix G Section 19 (HVAC - General)"""
+
+    def __init__(self):
+        super(Section19Rule31, self).__init__(
+            rmrs_used=UserBaselineProposedVals(False, False, True),
+            each_rule=Section19Rule31.HVACRule(),
+            index_rmr="proposed",
+            id="19-31",
+            description="HVAC fans in the proposed design model shall remain on during unoccupied hours in systems primarily serving computer rooms.",
+            ruleset_section_title="HVAC - General",
+            standard_section=" Section G3.1-4 Schedules exception #3 for the proposed building",
+            is_primary_rule=True,
+            rmr_context="ruleset_model_instances/0",
+            list_path="$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*]",
+        )
+
+    def create_data(self, context, data):
+        rmi_p = context.proposed
+
+        inapplicable_hvac_sys_list_p = get_hvac_systems_primarily_serving_comp_room(
+            rmi_p
+        ) + get_hvac_systems_serving_zone_health_safety_vent_reqs(rmi_p)
+
+        return {"inapplicable_hvac_sys_list_p": inapplicable_hvac_sys_list_p}
+
+    def is_applicable(self, context, data=None):
+        hvac_p = context.proposed
+        hvac_id_p = hvac_p["id"]
+        inapplicable_hvac_sys_list_p = data["inapplicable_hvac_sys_list_p"]
+
+        return hvac_id_p in inapplicable_hvac_sys_list_p
+
+    class HVACRule(RuleDefinitionBase):
+        def __init__(self):
+            super(Section19Rule31.HVACRule, self).__init__(
+                rmrs_used=UserBaselineProposedVals(False, False, True),
+            )
+
+        def get_calc_vals(self, context, data=None):
+            hvac_p = context.proposed
+
+            operation_during_unoccupied_p = getattr_(
+                hvac_p, "HVAC", "fan_system", "operation_during_unoccupied"
+            )
+            minimum_outdoor_airflow_p = getattr_(
+                hvac_p, "HVAC", "fan_system", "minimum_outdoor_airflow"
+            )
+
+            return {
+                "operation_during_unoccupied_p": operation_during_unoccupied_p,
+                "minimum_outdoor_airflow_p": minimum_outdoor_airflow_p,
+            }
+
+        def rule_check(self, context, calc_vals=None, data=None):
+            operation_during_unoccupied_p = calc_vals["operation_during_unoccupied_p"]
+            minimum_outdoor_airflow_p = calc_vals["minimum_outdoor_airflow_p"]
+
+            return (
+                operation_during_unoccupied_p == FAN_SYSTEM_OPERATION.CONTINUOUS
+                and minimum_outdoor_airflow_p > ZERO.FLOW
+            ) or (
+                operation_during_unoccupied_p == FAN_SYSTEM_OPERATION.CYCLING
+                and minimum_outdoor_airflow_p == ZERO.FLOW
+            )
