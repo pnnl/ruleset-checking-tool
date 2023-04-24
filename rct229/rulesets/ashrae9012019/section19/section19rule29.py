99be83f97a485afdb89e5f728f4528cea16af7fd
yunjoonjung <yjjung89@gmail.com>
2023-04-24 09:34:07 -0700

    change getattr_ to required_fields

--- rct229/rulesets/ashrae9012019/section19/section19rule29.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule29.py
@@ -8,7 +8,6 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.get_hvac_systems_primarily_
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_hvac_systems_serving_zone_health_safety_vent_reqs import (
     get_hvac_systems_serving_zone_health_safety_vent_reqs,
 )
-from rct229.utils.assertions import getattr_
 
 FAN_SYSTEM_OPERATION = schema_enums["FanSystemOperationOptions"]
 
@@ -49,6 +48,12 @@ class Section19Rule29(RuleDefinitionListIndexedBase):
         def __init__(self):
             super(Section19Rule29.HVACRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, False),
+                required_fields={
+                    "$": ["fan_system"],
+                    "fan_system": [
+                        "operation_during_occupied",
+                    ],
+                },
             )
 
         def is_applicable(self, context, data=None):
@@ -61,9 +66,9 @@ class Section19Rule29(RuleDefinitionListIndexedBase):
         def get_calc_vals(self, context, data=None):
             hvac_b = context.baseline
 
-            operation_during_unoccupied_b = getattr_(
-                hvac_b, "HVAC", "fan_system", "operation_during_unoccupied"
-            )
+            operation_during_unoccupied_b = hvac_b["fan_system"][
+                "operation_during_unoccupied"
+            ]
 
             return {"operation_during_unoccupied_b": operation_during_unoccupied_b}
 
