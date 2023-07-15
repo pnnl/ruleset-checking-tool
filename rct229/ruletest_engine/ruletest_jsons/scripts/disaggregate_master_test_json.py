318acc19a72519ffb5eb0c00f1182538e5365213
yunjoonjung <yjjung89@gmail.com>
2023-07-14 17:42:03 -0700

    black formatting Juan's code

--- rct229/ruletest_engine/ruletest_jsons/scripts/disaggregate_master_test_json.py
+++ rct229/ruletest_engine/ruletest_jsons/scripts/disaggregate_master_test_json.py
@@ -5,7 +5,7 @@ from rct229.ruletest_engine.ruletest_jsons.scripts.json_generation_utilities imp
 
 # Name of master spreadsheet in rct229.ruletest_engine.ruletest_jsons
 json_name = "envelope_tests.json"
-#output_dir = "system_types"
+# output_dir = "system_types"
 ruleset_doc = "ashrae9012019"
 disaggregate_master_ruletest_json(json_name, ruleset_doc)
-#disaggregate_master_rmd_json(json_name, output_dir, ruleset_doc)
+# disaggregate_master_rmd_json(json_name, output_dir, ruleset_doc)
