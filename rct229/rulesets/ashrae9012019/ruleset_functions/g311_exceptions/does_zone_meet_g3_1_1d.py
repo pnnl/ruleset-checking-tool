5d6ad57f7a81b46f5fc8d800a6765d547966c0ab
Weili Xu <weili.xu@pnnl.gov>
2023-07-21 09:50:12 -0700

    add g3_1_1 function to start

---
+++ rct229/rulesets/ashrae9012019/ruleset_functions/g311_exceptions/does_zone_meet_g3_1_1d.py
@@ -0,0 +1,14 @@
+def does_zone_meet_g3_1_1d(rmi, zone_id, zones_and_systems):
+    """
+    
+
+    Parameters
+    ----------
+    rmi
+    zone_id
+    zones_and_systems
+
+    Returns
+    -------
+
+    """
