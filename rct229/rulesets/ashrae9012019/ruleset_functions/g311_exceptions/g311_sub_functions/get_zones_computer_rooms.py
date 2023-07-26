102a5e2ac1b40ce7d3b48c604bd0daca93627527
Weili Xu <weili.xu@pnnl.gov>
2023-07-26 13:16:38 -0700

    Complete development get_zones_computer_rooms

---
+++ rct229/rulesets/ashrae9012019/ruleset_functions/g311_exceptions/g311_sub_functions/get_zones_computer_rooms.py
@@ -0,0 +1,57 @@
+from pydash import reduce_, curry
+
+from rct229.rulesets.ashrae9012019.data.schema_enums import schema_enums
+from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.pint_utils import ZERO, pint_sum
+
+LightingSpaceOptions2019ASHRAE901TG37 = schema_enums[
+    "LightingSpaceOptions2019ASHRAE901TG37"
+]
+
+
+def get_zone_computer_rooms(rmi):
+    """
+    Returns a dictionary with the zones that have at least one computer room space associated with them in the RMR as the keys.
+    The values associated with each key are in a list form. The list associated with each key contains the computer room
+    floor area as the first item in the list and the total zone floor area as the second item in the list.
+
+    Parameters
+    ----------
+    rmi dict
+        A dictionary representing a RuleModelInstance object as defined by the ASHRAE229 schema
+
+    Returns
+    -------
+    zones_with_computer_room_dict
+        A dictionary with the zones that have at least one computer room space associated with them in the RMR as the keys.
+        The values associated with each key are in a list form. The list associated with each key contains the computer
+        room floor area as the first item in the list and the total zone floor area as the second item in the list.
+    """
+    computer_space_check_func = curry(lambda space: space.get("lighting_space_type") == LightingSpaceOptions2019ASHRAE901TG37.COMPUTER_ROOM)
+    zone_with_computer_room_dict = {}
+
+    for zone in find_all("$.buildings[*].building_segments[*].zones[*]", rmi):
+        zone_has_computer_room_check = reduce_(
+            find_all("$.spaces[*]", zone),
+            lambda check, space:
+            check or computer_space_check_func(space),
+        )
+        zone_computer_room_floor_area = pint_sum([
+            space.get("floor_area", ZERO.AREA)
+            for space in find_all("$.spaces[*]", zone)
+            if computer_space_check_func(space)
+        ], ZERO.AREA)
+
+        total_zone_floor_area = pint_sum([
+            space.get("floor_area", ZERO.AREA)
+            for space in find_all("$.spaces[*]", zone)
+        ], ZERO.AREA)
+
+        if zone_has_computer_room_check:
+            zone_with_computer_room_dict[zone["id"]] = {
+                "zone_computer_room_floor_area": zone_computer_room_floor_area,
+                "total_zone_floor_area": total_zone_floor_area
+            }
+
+    return zone_with_computer_room_dict
+
---
+++ rct229/rulesets/ashrae9012019/ruleset_functions/g311_exceptions/g311_sub_functions/get_zones_computer_rooms_test.py
@@ -0,0 +1,59 @@
+from rct229.rulesets.ashrae9012019.ruleset_functions.g311_exceptions.g311_sub_functions.get_zones_computer_rooms import \
+    get_zone_computer_rooms
+from rct229.schema.config import ureg
+from rct229.schema.schema_utils import quantify_rmr
+from rct229.schema.validate import schema_validate_rmr
+
+TEST_RMI = {
+    "id": "test_rmd",
+    "buildings": [
+        {
+            "id": "Building 1",
+            "building_segments": [
+                {
+                    "id": "Building Segment 1",
+                    "zones": [
+                        {
+                            "id": "Thermal Zone 1",
+                            "spaces": [
+                                {
+                                    "id": "Space 1",
+                                    "function": "LABORATORY",
+                                    "lighting_space_type": "COMPUTER_ROOM",
+                                    "floor_area": 100,
+                                },
+                                {
+                                    "id": "Space 2",
+                                    "floor_area": 100,
+                                }
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+}
+
+
+TEST_RPD_FULL = {"id": "229", "ruleset_model_descriptions": [TEST_RMI]}
+
+TEST_RMD_UNIT = quantify_rmr(TEST_RPD_FULL)["ruleset_model_descriptions"][0]
+
+
+def test__TEST_RMD__is_valid():
+    schema_validation_result = schema_validate_rmr(TEST_RPD_FULL)
+    assert schema_validation_result[
+        "passed"
+    ], f"Schema error: {schema_validation_result['error']}"
+
+
+def test__get_building_total_lab_exhaust_from_zone_exhaust_fan__success():
+    assert get_zone_computer_rooms(
+        TEST_RMD_UNIT
+    ) == {
+        "Thermal Zone 1": {
+            "zone_computer_room_floor_area": 100 * ureg("m2"),
+            "total_zone_floor_area": 200 * ureg("m2")
+        }
+    }
