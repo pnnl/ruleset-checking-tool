d1ca1e14bb3adead43e7c57a9e3026a958922594
Weili Xu <weili.xu@pnnl.gov>
2023-07-26 09:27:52 -0700

    complete dev of test cases

--- rct229/rulesets/ashrae9012019/ruleset_functions/g311_exceptions/does_zone_meet_G3_1_1d.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/g311_exceptions/does_zone_meet_G3_1_1d.py
@@ -22,7 +22,7 @@ from rct229.utils.pint_utils import ZERO, pint_sum
 BUILDING_TOTAL_LAB_EXHAUST_CFM = 15_000 * ureg("ft^3 / min")
 
 
-def does_zone_meet_g3_1_1d(rmi: dict, zone_id: str, zones_and_systems: dict):
+def does_zone_meet_g3_1_1d(rmi: dict, zone_id: str):
     """
     Determines whether a given zone meets the G3_1_1d exception "For laboratory spaces in a building having a total
     laboratory exhaust rate greater than 15,000 cfm, use a single system of type 5 or 7 serving only those spaces."
@@ -34,18 +34,59 @@ def does_zone_meet_g3_1_1d(rmi: dict, zone_id: str, zones_and_systems: dict):
         A dictionary representing a ruleset model instance as defined by the ASHRAE229 schema
     zone_id str
         zone id
-    zones_and_systems dict
-        This is a dict of the existing expected system types from the function get_zone_target_baseline_system
 
     Returns
     -------
     boolean True or False
 
     """
+
+    def sum_total_primary_airflow_from_terminals_func(terminal_list: list):
+        """
+        Return sum of the primary airflow from all terminals in the terminal list
+        Return ZERO.FLOW if none is found.
+        """
+        return pint_sum(
+            [
+                find_exactly_one_terminal_unit(rmi, terminal_id).get(
+                    "primary_airflow", ZERO.FLOW
+                )
+                for terminal_id in terminal_list
+            ],
+            ZERO.FLOW,
+        )
+
+    def sum_zone_primary_airflow_from_terminals_func(terminal_list: list, zone: dict):
+        """
+        Return sum of the primary airflow from all terminals exist both in the terminal_list and zone
+        Return ZERO.FLOW if none is found.
+        """
+        return pint_sum(
+            [
+                find_exactly_one_terminal_unit(rmi, terminal_id).get(
+                    "primary_airflow", ZERO.FLOW
+                )
+                for terminal_id in terminal_list
+                if find_one(f'$.terminals[*][?(@.id="{terminal_id}")]', zone)
+            ],
+            ZERO.FLOW,
+        )
+
+    def sum_hvac_total_exhaust_air_func(hvac_id: str):
+        """Return sum of the design airflow from all exhaust fans in the HVAC."""
+        return pint_sum(
+            find_all(
+                "$.fan_system.exhaust_fans[*].design_airflow",
+                find_exactly_one_hvac_system(rmi, hvac_id),
+            ),
+            ZERO.FLOW,
+        )
+
     laboratory_zones_list = get_building_lab_zones_list(rmi)
     building_total_lab_exhaust = get_building_total_lab_exhaust_from_zone_exhaust_fans(
         rmi
     )
+
     dict_of_zones_and_terminal_units_served_by_hvac_sys = (
         get_dict_of_zones_and_terminal_units_served_by_hvac_sys(rmi)
     )
@@ -59,18 +100,6 @@ def does_zone_meet_g3_1_1d(rmi: dict, zone_id: str, zones_and_systems: dict):
 
             zone_total_exhaust = ZERO.FLOW
             for hvac_id in hvac_sys_list_serving_zone:
-                # sum of the exhaust air in the associated HVAC.
-                hvac_system_total_exhaust_airflow = pint_sum(
-                    find_all(
-                        "$.fan_system.exhaust_fans[*].design_airflow",
-                        find_exactly_one_hvac_system(rmi, hvac_id),
-                    ),
-                    ZERO.FLOW,
-                )
-
-                total_terminal_air_flow = ZERO.FLOW
-                zone_primary_air_flow = ZERO.FLOW
-
                 # The hvac_id seems to be guaranteed in this dictionary.
                 # One alternative is to use getattr_ to avoid any possible errors.
                 terminal_list_hvac_sys = (
@@ -79,19 +108,22 @@ def does_zone_meet_g3_1_1d(rmi: dict, zone_id: str, zones_and_systems: dict):
                     ]
                 )
 
-                for terminal_id in terminal_list_hvac_sys:
-                    terminal_primary_airflow = find_exactly_one_terminal_unit(
-                        rmi, terminal_id
-                    ).get("primary_airflow", ZERO.FLOW)
-                    total_terminal_air_flow += terminal_primary_airflow
-                    # if the terminal is in the lab zone.
-                    if find_one(f'$.terminals[*][?(@.id="{terminal_id}")]', lab_zone):
-                        zone_primary_air_flow += terminal_primary_airflow
+                # sum of the exhaust air in the HVAC.
+                hvac_system_total_exhaust_airflow = sum_hvac_total_exhaust_air_func(
+                    hvac_id
+                )
 
-                if (
-                    zone_primary_air_flow > ZERO.FLOW
-                    and hvac_system_total_exhaust_airflow > ZERO.FLOW
-                ):
+                # sum of primary airflow from terminals that have association with the HVAC.
+                total_terminal_air_flow = sum_total_primary_airflow_from_terminals_func(
+                    terminal_list_hvac_sys
+                )
+
+                # sum of primary airflow from terminals that have association with the lab zone
+                zone_primary_air_flow = sum_zone_primary_airflow_from_terminals_func(
+                    terminal_list_hvac_sys, lab_zone
+                )
+
+                if zone_primary_air_flow > ZERO.FLOW:
                     # note: total_terminal_air_flow > zone_primary_air_flow > ZERO.FLOW
                     zone_total_exhaust += (
                         hvac_system_total_exhaust_airflow
