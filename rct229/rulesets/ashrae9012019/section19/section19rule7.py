0f0c740dd6b998edabfd7204767531bdd39534b0
yunjoonjung <yjjung89@gmail.com>
2023-08-28 09:04:27 -0700

    replace rmi with rpd

--- rct229/rulesets/ashrae9012019/section19/section19rule7.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule7.py
@@ -38,7 +38,7 @@ class Section19Rule7(RuleDefinitionListIndexedBase):
             ruleset_section_title="HVAC - General",
             standard_section="Section G3.1.2.5 and Exception 2",
             is_primary_rule=True,
-            rmr_context="ruleset_model_instances/0",
+            rmr_context="ruleset_model_descriptions/0",
             list_path="$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*]",
         )
 
