de5b14d22a548feeb8f7abb08db6aab3f03d2bae
Weili Xu <weili.xu@pnnl.gov>
2023-09-06 16:24:49 -0700

    update 3_1_1c

--- rct229/rulesets/ashrae9012019/ruleset_functions/g311_exceptions/does_zone_meet_G3_1_1c.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/g311_exceptions/does_zone_meet_G3_1_1c.py
@@ -91,9 +91,8 @@ def does_zone_meet_g3_1_1c(rmi, zone_id, is_leap_year, zones_and_systems):
         # keep only matched system type
         zones_same_floor_same_system_type = list(
             filter(
-                lambda other_zone_id: zones_and_systems[other_zone_id][
-                    "expected_system_type"
-                ]
+                lambda other_zone_id: zones_and_systems.get(other_zone_id)
+                and zones_and_systems[other_zone_id]["expected_system_type"]
                 == expected_system_type,
                 zones_on_same_floor_ids,
             )
@@ -102,23 +101,24 @@ def does_zone_meet_g3_1_1c(rmi, zone_id, is_leap_year, zones_and_systems):
         # a list of conditioned or semi-conditioned zones only
         zone_load_and_eflh_list = [
             (
+                # tuple, 0 is peak load dict, 1 is weekly eflh
                 get_zone_peak_internal_load_floor_area_dict(rmi, other_match_zone_id),
                 get_zone_weekly_eflh(other_match_zone_id),
             )
             for other_match_zone_id in zones_same_floor_same_system_type
-            if other_match_zone_id in zones_and_systems.keys()
         ]
 
         system_total_area = sum(map_(zone_load_and_eflh_list, "0.area"), ZERO.AREA)
         system_total_load = sum(map_(zone_load_and_eflh_list, "0.peak"), ZERO.POWER)
+
         avg_eflh = (
+            # product sum
             np.dot(
                 map_(zone_load_and_eflh_list, lambda zl: zl[1]),
                 map_(zone_load_and_eflh_list, lambda zl: zl[0]["area"].magnitude),
             )
             / system_total_area.magnitude
         )
-
         avg_internal_load = system_total_load / system_total_area
 
         zone_internal_loads = get_zone_peak_internal_load_floor_area_dict(rmi, zone_id)
