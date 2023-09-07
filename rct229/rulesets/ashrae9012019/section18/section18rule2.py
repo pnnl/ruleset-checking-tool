a03f924076cb1d3216756cd635f908e2e282e819
yunjoonjung <yjjung89@gmail.com>
2023-09-07 15:09:38 -0700

    make a structure

---
+++ rct229/rulesets/ashrae9012019/section18/section18rule2.py
@@ -0,0 +1,138 @@
+from rct229.rule_engine.rule_base import RuleDefinitionBase
+from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
+from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.rulesets.ashrae9012019.data.schema_enums import schema_enums
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_proposed_hvac_modeled_with_virtual_cooling import (
+    get_proposed_hvac_modeled_with_virtual_cooling,
+)
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_proposed_hvac_modeled_with_virtual_heating import (
+    get_proposed_hvac_modeled_with_virtual_heating,
+)
+from rct229.rulesets.ashrae9012019.ruleset_functions.baseline_systems.baseline_system_util import (
+    HVAC_SYS,
+)
+from rct229.rulesets.ashrae9012019.ruleset_functions.baseline_system_type_compare import (
+    baseline_system_type_compare,
+)
+from rct229.utils.pint_utils import ZERO
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_baseline_system_types import (
+    get_baseline_system_types,
+)
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_dict_of_zones_and_terminal_units_served_by_hvac_sys import (
+    get_dict_of_zones_and_terminal_units_served_by_hvac_sys,
+)
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_zone_target_baseline_system import (
+    SYSTEMORIGIN,
+    get_zone_target_baseline_system,
+)
+
+FAN_SYSTEM_OPERATION = schema_enums["FanSystemOperationOptions"]
+
+
+APPLICABLE_SYS_TYPES = [
+    HVAC_SYS.SYS_1,
+    HVAC_SYS.SYS_2,
+    HVAC_SYS.SYS_3,
+    HVAC_SYS.SYS_4,
+    HVAC_SYS.SYS_9,
+    HVAC_SYS.SYS_10,
+    HVAC_SYS.SYS_11_1,
+    HVAC_SYS.SYS_12,
+    HVAC_SYS.SYS_13,
+]
+
+
+class Section18Rule2(RuleDefinitionListIndexedBase):
+    """Rule 2 of ASHRAE 90.1-2019 Appendix G Section 18 (HVAC - System Zone Assignment)"""
+
+    def __init__(self):
+        super(Section18Rule2, self).__init__(
+            rmrs_used=UserBaselineProposedVals(False, True, True),
+            each_rule=Section18Rule2.HVACRule(),
+            index_rmr="base;ome",
+            id="18-2",
+            description="Does the modeled system serve the appropriate zones (one system per zone for system types 1, 2, 3, 4, 9, 10, 11, 12, and 13 and "
+            "one system per floor for system types 5, 6, 7, and 8, with the exception of system types 5 or 7 serving laboratory spaces - these systems should serve ALL laboratory zones in the buidling).",
+            ruleset_section_title="HVAC",
+            standard_section="Section 18 HVAC_SystemZoneAssignment",
+            is_primary_rule=True,
+            rmr_context="ruleset_model_descriptions/0",
+            list_path="$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*]",
+        )
+
+    def create_data(self, context, data):
+        rmd_b = context.baseline
+        rmd_p = context.proposed
+        climate_zone_b = data["climatezone"]
+
+        baseline_system_types_dict = get_baseline_system_types(rmd_b)
+
+        applicable_hvac_sys_ids = [
+            hvac_id
+            for sys_type in baseline_system_types_dict
+            for target_sys_type in APPLICABLE_SYS_TYPES
+            if baseline_system_type_compare(sys_type, target_sys_type, False)
+            for hvac_id in baseline_system_types_dict[sys_type]
+        ]
+
+
+        zones_and_terminal_unit_list_dict = (
+            get_dict_of_zones_and_terminal_units_served_by_hvac_sys(rmd_b)
+        )
+        target_baseline_systems = get_zone_target_baseline_system(
+            rmd_b, rmd_p, climate_zone_b, False
+        )
+        # get_lab_zone_hvac_systems = get_lab_zone_hvac_systems(rmd_b, rmd_p) # TODO need to import once the func dev is done
+
+        laboratory_zones_meeting_G3_1_1d = []
+
+        return {}
+
+    class HVACRule(RuleDefinitionBase):
+        def __init__(self):
+            super(Section18Rule2.HVACRule, self).__init__(
+                rmrs_used=UserBaselineProposedVals(False, True, True),
+            )
+
+
+        def get_calc_vals(self, context, data=None):
+            hvac_b = context.baseline
+            hvac_id_b = hvac_b["id"]
+
+            lab_zone_hvac_systems = data["lab_zone_hvac_systems"]
+
+            return
+
+        def manual_check_required(self, context, calc_vals=None, data=None):
+            return
+
+        def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
+
+            undetermined_msg = f"This system serves some lab zones and some non-lab zones in a building which may have more than 15,000 cfm. " \
+                               f"In buildings with > 15,000 cfm of lab exhaust, ALL and only lab zones should be served by system type 5 or 7."
+
+            return undetermined_msg
+
+        def rule_check(self, context, calc_vals=None, data=None):
+            hvac_b = context.baseline
+            hvac_id_b = hvac_b["id"]
+
+
+
+
+            lab_zone_hvac_systems = data["lab_zone_hvac_systems"]
+
+
+            return (hvac_id_b in lab_zone_hvac_systems["lab_zones_only"] and  (
+                 len(lab_zone_hvac_systems["lab_zones_only"]) == 1
+            ) or )
+
+        def get_fail_msg(self, context, calc_vals=None, data=None):
+            hvac_b = context.baseline
+            hvac_id_b = hvac_b["id"]
+
+
+            fail_msg = "This HVAC system serves lab zones in a building with > 15,000 cfm of laboratory exhaust. " \
+                       "The baseline system should be type 5 or 7 and should serve ALL laboratory zones."
+
+            return
