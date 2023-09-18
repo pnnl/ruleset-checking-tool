d2e0dea21faebc2401315dc393291a32b5d01003
yunjoonjung <yjjung89@gmail.com>
2023-09-17 19:32:32 -0700

    address PR comments

--- rct229/rulesets/ashrae9012019/section18/section18rule1.py
+++ rct229/rulesets/ashrae9012019/section18/section18rule1.py
@@ -4,6 +4,9 @@ from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedV
 from rct229.rulesets.ashrae9012019.ruleset_functions.g311_exceptions.g311_sub_functions.get_building_lab_zones_list import (
     get_building_lab_zones_list,
 )
+from rct229.rulesets.ashrae9012019.ruleset_functions.g311_exceptions.g311_sub_functions.get_building_total_lab_exhaust_from_zone_exhaust_fans import (
+    get_building_total_lab_exhaust_from_zone_exhaust_fans,
+)
 from rct229.rulesets.ashrae9012019.ruleset_functions.g311_exceptions.g311_sub_functions.is_zone_likely_a_vestibule import (
     is_zone_likely_a_vestibule,
 )
@@ -24,7 +27,7 @@ EXHAUST_AIRFLOW_15000 = 15000 * ureg("cfm")
 
 
 class Section18Rule1(RuleDefinitionListIndexedBase):
-    """Rule 1 of ASHRAE 90.1-2019 Appendix G Section 18 (HVAC)"""
+    """Rule 1 of ASHRAE 90.1-2019 Appendix G Section 18 (HVAC - System Zone Assignment)"""
 
     def __init__(self):
         super(Section18Rule1, self).__init__(
@@ -33,34 +36,38 @@ class Section18Rule1(RuleDefinitionListIndexedBase):
             index_rmr="baseline",
             id="18-1",
             description="HVAC system type selection is based on ASHRAE 90.1 G3.1.1 (a-h).",
-            ruleset_section_title="HVAC - Chiller",
+            ruleset_section_title="HVAC - System Zone Assignment",
             standard_section="Table G3.1.1",
             is_primary_rule=True,
             rmr_context="ruleset_model_descriptions/0",
             required_fields={
-                "$": ["weather"],
-                "weather": ["climate_zone"],
+                "$": ["weather", "Calendar"],
+                "weather": ["climate_zone", "is_leap_year"],
             },
-            data_items={"climate_zone": ("baseline", "weather/climate_zone")},
+            data_items={
+                "climate_zone": ("baseline", "weather/climate_zone"),
+                "is_leap_year": ("baseline", "weather/is_leap_year"),
+            },
         )
 
     def is_applicable(self, context, data=None):
         rmd_b = context.baseline
         rmd_p = context.proposed
         climate_zone_b = data["climate_zone"]
+        is_leap_year_b = data["is_leap_year"]
 
-        target_baseline_system_dict_b = get_zone_target_baseline_system(
-            rmd_b, rmd_p, climate_zone_b
+        zone_target_baseline_system_dict_b = get_zone_target_baseline_system(
+            rmd_b, rmd_p, climate_zone_b, is_leap_year_b
         )
 
-        return bool(target_baseline_system_dict_b)
+        return bool(zone_target_baseline_system_dict_b)
 
     def create_data(self, context, data):
         rmd_b = context.baseline
         rmd_p = context.proposed
         climate_zone_b = data["climate_zone"]
 
-        target_baseline_system_dict_b = get_zone_target_baseline_system(
+        zone_target_baseline_system_dict_b = get_zone_target_baseline_system(
             rmd_b, rmd_p, climate_zone_b
         )
         baseline_hvac_system_dict_b = get_baseline_system_types(rmd_b)
@@ -73,26 +80,29 @@ class Section18Rule1(RuleDefinitionListIndexedBase):
             zone_id_b: get_list_hvac_systems_associated_with_zone(rmd_b, zone_id_b)
             for zone_id_b in zone_ids_list_b
         }
-
         is_zone_likely_a_vestibule_b = {
             zone_id_b: is_zone_likely_a_vestibule(rmd_b, zone_id_b)
             for zone_id_b in zone_ids_list_b
         }
+        building_total_lab_exhaust_p = (
+            get_building_total_lab_exhaust_from_zone_exhaust_fans(rmd_p)
+        )
 
         return {
-            "target_baseline_system_dict_b": target_baseline_system_dict_b,
+            "zone_target_baseline_system_dict_b": zone_target_baseline_system_dict_b,
             "baseline_hvac_system_dict_b": baseline_hvac_system_dict_b,
             "get_building_lab_zones_list_b": get_building_lab_zones_list_b,
             "hvac_systems_serving_zone_b": hvac_systems_serving_zone_b,
             "is_zone_likely_a_vestibule_b": is_zone_likely_a_vestibule_b,
+            "building_total_lab_exhaust_p": building_total_lab_exhaust_p,
         }
 
     def list_filter(self, context_item, data):
         zone_b = context_item.baseline
         zone_id_b = zone_b["id"]
-        target_baseline_system_dict_b = data["target_baseline_system_dict_b"]
+        zone_target_baseline_system_dict_b = data["zone_target_baseline_system_dict_b"]
 
-        return zone_id_b in target_baseline_system_dict_b[zone_id_b]["system_origin"]
+        return zone_id_b in zone_target_baseline_system_dict_b
 
     class ZoneRule(RuleDefinitionBase):
         def __init__(self):
@@ -104,13 +114,20 @@ class Section18Rule1(RuleDefinitionListIndexedBase):
             zone_b = context.baseline
             zone_id_b = zone_b["id"]
 
+            expected_system_type_b = data["zone_target_baseline_system_dict_b"][
+                zone_id_b
+            ]["expected_system_type"]
             hvac_systems_serving_zone_b = data["hvac_systems_serving_zone_b"][zone_id_b]
-            baseline_hvac_system_dict_b = data["baseline_hvac_system_dict_b"]
+            systems_of_expected_type_list_b = data["baseline_hvac_system_dict_b"][
+                expected_system_type_b
+            ]
 
+            # any HVAC system serving this zone is in the `systems_of_expected_type_list_b`
             is_system_part_of_expected_sys_type_b = any(
-                all(sys_type in sys_type_b for sys_type in hvac_systems_serving_zone_b)
-                for sys_type_b in baseline_hvac_system_dict_b.values()
+                sys_b in systems_of_expected_type_list_b
+                for sys_b in hvac_systems_serving_zone_b
             )
+
             return {
                 "is_system_part_of_expected_sys_type_b": is_system_part_of_expected_sys_type_b
             }
@@ -118,29 +135,18 @@ class Section18Rule1(RuleDefinitionListIndexedBase):
         def manual_check_required(self, context, calc_vals=None, data=None):
             zone_b = context.baseline
             zone_id_b = zone_b["id"]
-
-            hvac_systems_serving_zone_b = data["hvac_systems_serving_zone_b"][zone_id_b]
             is_zone_likely_a_vestibule_b = data["is_zone_likely_a_vestibule_b"]
+            building_total_lab_exhaust_p = data["building_total_lab_exhaust_p"]
 
-            if hvac_systems_serving_zone_b["system_origin"] == SYSTEMORIGIN.G311D:
-                is_undetermined = (
-                    sum(
-                        [
-                            design_airflow_b
-                            for design_airflow_b in find_all(
-                                "$.zonal_exhaust_fan[*].design_airflow", zone_id_b
-                            )
-                        ]
-                    )
-                    <= EXHAUST_AIRFLOW_15000
-                )
+            system_origin_b = data["zone_target_baseline_system_dict_b"][zone_id_b][
+                "system_origin"
+            ]
 
-            elif hvac_systems_serving_zone_b["system_origin"] == SYSTEMORIGIN.G311E:
-                if is_zone_likely_a_vestibule_b[zone_id_b]:
-                    is_undetermined = True
-
-            else:
-                is_undetermined = False
+            is_undetermined = False
+            if system_origin_b == SYSTEMORIGIN.G311D:
+                is_undetermined = building_total_lab_exhaust_p <= EXHAUST_AIRFLOW_15000
+            elif system_origin_b == SYSTEMORIGIN.G311E:
+                is_undetermined = is_zone_likely_a_vestibule_b[zone_id_b]
 
             return is_undetermined
 
@@ -148,12 +154,15 @@ class Section18Rule1(RuleDefinitionListIndexedBase):
             zone_b = context.baseline
             zone_id_b = zone_b["id"]
 
-            hvac_systems_serving_zone_b = data["hvac_systems_serving_zone_b"][zone_id_b]
-            expected_system_type_b = hvac_systems_serving_zone_b[
-                "expected_system_type_b"
+            system_origin_b = data["zone_target_baseline_system_dict_b"][zone_id_b][
+                "system_origin"
             ]
+            expected_system_type_b = data["zone_target_baseline_system_dict_b"][
+                zone_id_b
+            ]["expected_system_type"]
 
-            if hvac_systems_serving_zone_b["system_origin"] == SYSTEMORIGIN.G311D:
+            advisory_note = ""
+            if system_origin_b == SYSTEMORIGIN.G311D:
                 advisory_note = (
                     f"HVAC system type {expected_system_type_b} was selected for this zone based on ASHRAE 90.1 Appendix G3.1.1.d, which reads: "
                     f"For laboratory spaces in a building having a total laboratory exhaust rate greater than 15,000 cfm, use a single system of type 5 or 7 serving only those spaces. "
@@ -161,16 +170,13 @@ class Section18Rule1(RuleDefinitionListIndexedBase):
                     f"If the building laboratory exhaust is greater than 15,000cfm, the system type for this zone should be {expected_system_type_b}."
                 )
 
-            elif hvac_systems_serving_zone_b["system_origin"] == SYSTEMORIGIN.G311E:
+            elif system_origin_b == SYSTEMORIGIN.G311E:
                 advisory_note = (
                     f"HVAC system type {expected_system_type_b} was selected for this zone based on ASHRAE 90.1 Appendix G.3.1.1.e which reads: "
                     f"Thermal zones designed with heating-only systems in the proposed design serving storage rooms, stairwells, vestibules, electrical/mechanical rooms, "
                     f"and restrooms not exhausting or transferring air from mechanically cooled thermal zones in the proposed design shall use system type 9 or 10 in the baseline building design.` "
                     f"We expect that this space is a vestibule, but cannot make a determination with 100% accuracy. If the zone is one of the listed space types, then the system type should be {expected_system_type_b}."
                 )
-
-            else:
-                advisory_note = ""
 
             return advisory_note
 
@@ -185,9 +191,11 @@ class Section18Rule1(RuleDefinitionListIndexedBase):
             zone_b = context.baseline
             zone_id_b = zone_b["id"]
 
-            hvac_systems_serving_zone_b = data["hvac_systems_serving_zone_b"][zone_id_b]
-            system_type_origin_b = hvac_systems_serving_zone_b["system_origin"]
-            expected_system_type_b = hvac_systems_serving_zone_b[
+            zone_target_baseline_system_dict_b = data[
+                "zone_target_baseline_system_dict_b"
+            ][zone_id_b]
+            system_type_origin_b = zone_target_baseline_system_dict_b["system_origin"]
+            expected_system_type_b = zone_target_baseline_system_dict_b[
                 "expected_system_type_b"
             ]
 
@@ -197,9 +205,11 @@ class Section18Rule1(RuleDefinitionListIndexedBase):
             zone_b = context.baseline
             zone_id_b = zone_b["id"]
 
-            hvac_systems_serving_zone_b = data["hvac_systems_serving_zone_b"][zone_id_b]
-            system_type_origin_b = hvac_systems_serving_zone_b["system_origin"]
-            expected_system_type_b = hvac_systems_serving_zone_b[
+            zone_target_baseline_system_dict_b = data[
+                "zone_target_baseline_system_dict_b"
+            ][zone_id_b]
+            system_type_origin_b = zone_target_baseline_system_dict_b["system_origin"]
+            expected_system_type_b = zone_target_baseline_system_dict_b[
                 "expected_system_type_b"
             ]
 
