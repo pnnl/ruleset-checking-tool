6aa2c894b2cbfe0106b1c950cd7a506d697b9610
yunjoonjung <yjjung89@gmail.com>
2023-10-06 13:43:47 -0700

    modify is_applicable level

--- rct229/rulesets/ashrae9012019/section22/section22rule15.py
+++ rct229/rulesets/ashrae9012019/section22/section22rule15.py
@@ -6,6 +6,7 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.get_heat_rejection_loops_co
 )
 from rct229.schema.config import ureg
 from rct229.utils.assertions import getattr_
+from rct229.utils.pint_utils import CalcQ
 from rct229.utils.std_comparisons import std_equal
 
 TEMP_LOW_LIMIT_55F = 55 * ureg("degF")
@@ -37,21 +38,6 @@ class Section22Rule15(RuleDefinitionListIndexedBase):
 
         return {"heat_rejection_loop_ids_b": heat_rejection_loop_ids_b}
 
-    def list_filter(self, context_item, data):
-        heat_rejection_b = context_item.baseline
-        heat_rejection_loop_ids_b = data["heat_rejection_loop_ids_b"]
-        heat_rejection_loop_b = getattr_("$.loop", "loop", heat_rejection_b)
-        design_wetbulb_temp_b = getattr_(
-            "$.design_wetbulb_temperature",
-            "design_wetbulb_temperature",
-            heat_rejection_b,
-        )
-
-        return (
-            heat_rejection_loop_b in heat_rejection_loop_ids_b
-            and TEMP_LOW_LIMIT_55F <= design_wetbulb_temp_b <= TEMP_HIGH_LIMIT_90F
-        )
-
     class HeatRejectionRule(RuleDefinitionBase):
         def __init__(self):
             super(Section22Rule15.HeatRejectionRule, self).__init__(
@@ -61,18 +47,35 @@ class Section22Rule15(RuleDefinitionListIndexedBase):
                 },
             )
 
+        def is_applicable(self, context, data=None):
+            heat_rejection_b = context.baseline
+            heat_rejection_loop_ids_b = data["heat_rejection_loop_ids_b"]
+            heat_rejection_loop_b = getattr_(heat_rejection_b, "loop", "loop")
+            design_wetbulb_temp_b = getattr_(
+                heat_rejection_b,
+                "design_wetbulb_temperature",
+                "design_wetbulb_temperature",
+            )
+
+            return (
+                heat_rejection_loop_b in heat_rejection_loop_ids_b
+                and TEMP_LOW_LIMIT_55F <= design_wetbulb_temp_b <= TEMP_HIGH_LIMIT_90F
+            )
+
         def get_calc_vals(self, context, data=None):
             heat_rejection_b = context.baseline
             approach_b = heat_rejection_b["approach"]
             target_approach_b = heat_rejection_b["design_wetbulb_temperature"]
 
             return {
-                "approach_b": approach_b,
-                "target_approach_b": target_approach_b,
+                "approach_b": CalcQ("temperature", approach_b),
+                "target_approach_b": CalcQ("temperature", target_approach_b),
             }
 
         def rule_check(self, context, calc_vals=None, data=None):
             approach_b = calc_vals["approach_b"]
             target_approach_b = calc_vals["target_approach_b"]
 
-            return std_equal(target_approach_b, approach_b)
+            return std_equal(
+                target_approach_b.to(ureg.kelvin), approach_b.to(ureg.kelvin)
+            )
