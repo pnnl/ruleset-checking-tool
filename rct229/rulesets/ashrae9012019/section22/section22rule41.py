1f5cb5632f172e19e319309af10a3c8067a94105
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-10-06 15:33:04 -0700

    delete unused import

--- rct229/rulesets/ashrae9012019/section22/section22rule41.py
+++ rct229/rulesets/ashrae9012019/section22/section22rule41.py
@@ -1,5 +1,4 @@
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
-from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
 from rct229.rulesets.ashrae9012019.ruleset_functions.check_purchased_chw_hhw_status_dict import (
     check_purchased_chw_hhw_status_dict,
