bea0743a93e6ca6678b1dafd057402ef959238f2
yunjoonjung <yjjung89@gmail.com>
2023-10-06 13:55:48 -0700

    modify is_applicable level

--- rct229/rulesets/ashrae9012019/section22/section22rule18.py
+++ rct229/rulesets/ashrae9012019/section22/section22rule18.py
@@ -1,11 +1,11 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.rulesets.ashrae9012019.data.schema_enums import schema_enums
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_heat_rejection_loops_connected_to_baseline_systems import (
     get_heat_rejection_loops_connected_to_baseline_systems,
 )
 from rct229.utils.assertions import getattr_
-from rct229.rulesets.ashrae9012019.data.schema_enums import schema_enums
 
 HEATREJECTIONFANSPEEDCONTROL = schema_enums["HeatRejectionFanSpeedControlOptions"]
 
@@ -27,16 +27,14 @@ class Section22Rule18(RuleDefinitionListIndexedBase):
             list_path="$.heat_rejections[*]",
         )
 
-    def is_applicable(self, context, data=None):
+    def create_data(self, context, data):
         rmd_b = context.baseline
-        heat_rejection_loop_ids_b = (
-            get_heat_rejection_loops_connected_to_baseline_systems(rmd_b)
-        )
-        heat_rejection_loop_b = getattr_(
-            "$.buildings[*].heat_rejections[*].loop", "loop", rmd_b
-        )
 
-        return heat_rejection_loop_b in heat_rejection_loop_ids_b
+        return {
+            "heat_rejection_loop_ids_b": get_heat_rejection_loops_connected_to_baseline_systems(
+                rmd_b
+            )
+        }
 
     class HeatRejectionRule(RuleDefinitionBase):
         def __init__(self):
@@ -47,9 +45,20 @@ class Section22Rule18(RuleDefinitionListIndexedBase):
                 },
             )
 
+        def is_applicable(self, context, data=None):
+            heat_rejection_b = context.baseline
+            heat_rejection_loop_ids_b = data["heat_rejection_loop_ids_b"]
+            heat_rejection_loop_b = getattr_(
+                heat_rejection_b,
+                "heat_rejections",
+                "loop",
+            )
+
+            return heat_rejection_loop_b in heat_rejection_loop_ids_b
+
         def get_calc_vals(self, context, data=None):
             heat_rejection_b = context.baseline
-            fan_speed_control_b = heat_rejection_b["fan_speed_control_b"]
+            fan_speed_control_b = heat_rejection_b["fan_speed_control"]
 
             return {
                 "fan_speed_control_b": fan_speed_control_b,
