043d7d51c7c0aa79ff9692c5227fa3f7226b5a7e
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-10-27 11:22:49 -0700

    finish dev

--- rct229/rulesets/ashrae9012019/section19/section19rule16.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule16.py
@@ -1,3 +1,4 @@
+from pydash import find_key
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
@@ -16,13 +17,13 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.get_list_hvac_systems_assoc
 )
 from rct229.utils.assertions import getattr_
 from rct229.utils.jsonpath_utils import find_all
-from rct229.utils.pint_utils import ZERO, CalcQ
+from rct229.utils.pint_utils import ZERO
 
 APPLICABLE_SYS_TYPES = [
     HVAC_SYS.SYS_9,
     HVAC_SYS.SYS_10,
 ]
-COOLING_SYSTEM = schema_enums["CoolingSystem"]
+COOLING_SYSTEM = schema_enums["CoolingSystemOptions"]
 
 
 class Section19Rule16((RuleDefinitionListIndexedBase)):
@@ -31,7 +32,7 @@ class Section19Rule16((RuleDefinitionListIndexedBase)):
     def __init__(self):
         super(Section19Rule16, self).__init__(
             rmrs_used=UserBaselineProposedVals(False, True, True),
-            each_rule=Section19Rule16.HVACRule(),
+            each_rule=Section19Rule16.ZoneRule(),
             index_rmr="baseline",
             id="19-16",
             description="For zones served by baseline system types 9 & 10, if the proposed design includes a fan or fans sized and controlled to provide non-mechanical cooling, the baseline building design shall include a separate fan to provide nonmechanical cooling, sized and controlled the same as the proposed design.",
@@ -39,7 +40,23 @@ class Section19Rule16((RuleDefinitionListIndexedBase)):
             standard_section="Section G3.1.2.8.2",
             is_primary_rule=False,
             rmr_context="ruleset_model_descriptions/0",
-            list_path="$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*]",
+            list_path="$.buildings[*].building_segments[*].zones[*]",
+        )
+
+    def is_applicable(self, context, data=None):
+        rmd_b = context.baseline
+        baseline_system_types_dict = get_baseline_system_types(rmd_b)
+        # create a list containing all HVAC systems that are modeled in the rmi_b
+        available_type_list = [
+            hvac_type
+            for hvac_type in baseline_system_types_dict
+            if len(baseline_system_types_dict[hvac_type]) > 0
+        ]
+        return any(
+            [
+                available_type in APPLICABLE_SYS_TYPES
+                for available_type in available_type_list
+            ]
         )
 
     def create_data(self, context, data):
@@ -49,14 +66,14 @@ class Section19Rule16((RuleDefinitionListIndexedBase)):
         hvac_sys_repo_b = {
             zone_id_b: get_list_hvac_systems_associated_with_zone(rmd_b, zone_id_b)
             for zone_id_b in find_all(
-                "$.buildings[*].building_segments[*].zones[*]", rmd_b
+                "$.buildings[*].building_segments[*].zones[*].id", rmd_b
             )
         }
 
         hvac_sys_repo_p = {
             zone_id_p: get_list_hvac_systems_associated_with_zone(rmd_p, zone_id_p)
             for zone_id_p in find_all(
-                "$.buildings[*].building_segments[*].zones[*]", rmd_p
+                "$.buildings[*].building_segments[*].zones[*].id", rmd_p
             )
         }
 
@@ -78,27 +95,13 @@ class Section19Rule16((RuleDefinitionListIndexedBase)):
             "zone_hvac_has_non_mech_cooling_bool_p": zone_hvac_has_non_mech_cooling_bool_p,
         }
 
-    def list_filter(self, context_item, data):
-        hvac_b = context_item.baseline
-        hvac_id_b = hvac_b["id"]
-        hvac_sys_repo_b = data["hvac_sys_repo_b"]
-
-        return hvac_id_b in hvac_sys_repo_b
-
-    class HVACRule(PartialRuleDefinition):
+    class ZoneRule(PartialRuleDefinition):
         def __init__(self):
-            super(Section19Rule16.HVACRule, self).__init__(
+            super(Section19Rule16.ZoneRule, self).__init__(
                 rmrs_used=UserBaselineProposedVals(False, True, True),
-                required_fields={
-                    "$": [
-                        "non_mechanical_cooling_fan_airflow",
-                    ],
-                },
             )
 
         def get_calc_vals(self, context, data=None):
-            baseline_system_types_dict = data["baseline_system_types_dict"]
-
             zone_b = context.baseline
             zone_p = context.proposed
 
@@ -107,28 +110,52 @@ class Section19Rule16((RuleDefinitionListIndexedBase)):
 
             hvac_sys_list_b = data["hvac_sys_repo_b"][zone_id_b]
             hvac_sys_list_p = data["hvac_sys_repo_p"][zone_id_p]
-
             zone_hvac_has_non_mech_cooling_bool_p = data[
                 "zone_hvac_has_non_mech_cooling_bool_p"
             ]
 
-            does_baseline_sys_match_list = [
-                baseline_system_type_compare(system_type, applicable_sys_type, False)
-                for system_type in baseline_system_types_dict
-                for applicable_sys_type in APPLICABLE_SYS_TYPES
+            return {
+                "hvac_sys_list_b": hvac_sys_list_b,
+                "hvac_sys_list_p": hvac_sys_list_p,
+                "zone_hvac_has_non_mech_cooling_bool_p": zone_hvac_has_non_mech_cooling_bool_p,
+            }
+
+        def applicability_check(self, context, calc_vals, data):
+            zone_p = context.proposed
+            zone_hvac_has_non_mech_cooling_bool_p = calc_vals[
+                "zone_hvac_has_non_mech_cooling_bool_p"
+            ]
+            baseline_system_types_dict = data["baseline_system_types_dict"]
+            hvac_sys_list_b = calc_vals["hvac_sys_list_b"]
+
+            sys_types = [
+                find_key(
+                    baseline_system_types_dict, lambda base_sys: hvac_sys in base_sys
+                )
+                for hvac_sys in hvac_sys_list_b
             ]
 
-            # zone_served_by_sys_9_or_10 =
-
-            return {"zone_served_by_sys_9_or_10": zone_served_by_sys_9_or_10}
-
-        def rule_check(self, context, calc_vals=None, data=None):
-            zone_served_by_sys_9_or_10 = calc_vals["zone_served_by_sys_9_or_10"]
-
-            return zone_served_by_sys_9_or_10
+            return (
+                (
+                    zone_p.get("non_mechanical_cooling_fan_airflow") is not None
+                    and zone_p["non_mechanical_cooling_fan_airflow"] != ZERO.FLOW
+                )
+                or zone_hvac_has_non_mech_cooling_bool_p
+            ) and any(
+                [
+                    baseline_system_type_compare(
+                        system_type, applicable_sys_type, False
+                    )
+                    for system_type in sys_types
+                    for applicable_sys_type in APPLICABLE_SYS_TYPES
+                ]
+            )
 
         def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
-            zone_b = context.baseline
-            zone_id_b = zone_b["id"]
+            zone_p = context.proposed
+            zone_id_p = zone_p["id"]
 
-            return ""
+            return (
+                f"{zone_id_p} has non-mechanical cooling in the proposed design and the zone is served by either system 9 or 10 in the baseline, "
+                "conduct a manual check that the baseline building design includes a separate fan to provide nonmechanical cooling, sized and controlled the same as the proposed design."
+            )
