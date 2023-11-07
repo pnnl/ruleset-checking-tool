fcbaf0903b00234cb8956db5d3a80c932f2fb963
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-11-07 12:50:00 -0800

    updated rmrs_used

--- rct229/rulesets/ashrae9012019/section19/section19rule30.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule30.py
@@ -1,6 +1,8 @@
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
+from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_instance
 from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.rulesets.ashrae9012019 import BASELINE_0
 from rct229.rulesets.ashrae9012019.ruleset_functions.baseline_system_type_compare import (
     baseline_system_type_compare,
 )
@@ -19,9 +21,11 @@ class Section19Rule30(RuleDefinitionListIndexedBase):
 
     def __init__(self):
         super(Section19Rule30, self).__init__(
-            rmrs_used=UserBaselineProposedVals(False, True, False),
+            rmrs_used=produce_ruleset_model_instance(
+                USER=False, BASELINE_0=True, PROPOSED=False
+            ),
             each_rule=Section19Rule30.HVACRule(),
-            index_rmr="baseline",
+            index_rmr=BASELINE_0,
             id="19-30",
             description="For Systems 6 and 8, only the terminal-unit fan and reheat coil shall be energized to meet heating set point during unoccupied hours in the baseline design.",
             ruleset_section_title="HVAC - General",
@@ -32,18 +36,20 @@ class Section19Rule30(RuleDefinitionListIndexedBase):
         )
 
     def create_data(self, context, data):
-        rmd_b = context.baseline
+        rmd_b = context.BASELINE_0
 
         return {"baseline_system_types_dict_b": get_baseline_system_types(rmd_b)}
 
     class HVACRule(PartialRuleDefinition):
         def __init__(self):
             super(Section19Rule30.HVACRule, self).__init__(
-                rmrs_used=UserBaselineProposedVals(False, True, False),
+                rmrs_used=produce_ruleset_model_instance(
+                    USER=False, BASELINE_0=True, PROPOSED=False
+                ),
             )
 
         def applicability_check(self, context, calc_vals, data):
-            rmd_b = context.baseline
+            rmd_b = context.BASELINE_0
             baseline_system_types_dict_b = get_baseline_system_types(rmd_b)
 
             return not any(
@@ -57,7 +63,7 @@ class Section19Rule30(RuleDefinitionListIndexedBase):
             )
 
         def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
-            hvac_b = context.baseline
+            hvac_b = context.BASELINE_0
             hvac_id_b = hvac_b["id"]
 
             return f"For {hvac_id_b} perform manual check that only the terminal-unit fan and reheat coil are being energized to meet the heating set point during unoccupied hours."
