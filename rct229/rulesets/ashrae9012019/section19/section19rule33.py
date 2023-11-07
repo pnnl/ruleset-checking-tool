6c9f3dc60d9be6675a2cfa1237cc487207926738
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-11-07 15:09:45 -0800

    updated index_rmr

--- rct229/rulesets/ashrae9012019/section19/section19rule33.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule33.py
@@ -1,7 +1,7 @@
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_instance
-from rct229.rulesets.ashrae9012019 import BASELINE_0
+from rct229.rulesets.ashrae9012019 import PROPOSED
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_proposed_hvac_modeled_with_virtual_cooling import (
     get_proposed_hvac_modeled_with_virtual_cooling,
 )
@@ -17,10 +17,10 @@ class Section19Rule33(RuleDefinitionListIndexedBase):
     def __init__(self):
         super(Section19Rule33, self).__init__(
             rmrs_used=produce_ruleset_model_instance(
-                USER=False, BASELINE_0=True, PROPOSED=True
+                USER=True, BASELINE_0=False, PROPOSED=True
             ),
             each_rule=Section19Rule33.HVACRule(),
-            index_rmr=BASELINE_0,
+            index_rmr=PROPOSED,
             id="19-33",
             description="Where no heating and/or cooling system is to be installed, and a heating or cooling system is being simulated only to meet the requirements described in Section G3.1-10 HVAC Systems proposed column c and d, "
             "heating and/or cooling system fans shall simulated to be cycled ON and OFF to meet heating and cooling loads during occupied hours in the proposed design.",
