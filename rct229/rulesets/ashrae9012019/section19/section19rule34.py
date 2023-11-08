a9890965599c0af2db9da4c6c018a4280e4cb052
yunjoonjung <yunjoon.jung@pnnl.gov>
2023-11-07 16:16:08 -0800

    updated index_rmr

--- rct229/rulesets/ashrae9012019/section19/section19rule34.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule34.py
@@ -1,6 +1,7 @@
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
-from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedVals
+from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_instance
+from rct229.rulesets.ashrae9012019 import BASELINE_0
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_hvac_zone_list_w_area_dict import (
     get_hvac_zone_list_w_area_by_rmi_dict,
 )
@@ -21,9 +22,11 @@ class Section19Rule34(RuleDefinitionListIndexedBase):
 
     def __init__(self):
         super(Section19Rule34, self).__init__(
-            rmrs_used=UserBaselineProposedVals(True, True, True),
+            rmrs_used=produce_ruleset_model_instance(
+                USER=True, BASELINE_0=True, PROPOSED=True
+            ),
             each_rule=Section19Rule34.HVACRule(),
-            index_rmr="baseline",
+            index_rmr=BASELINE_0,
             id="19-34",
             description="Where no heating and/or cooling system is to be installed, and a heating or cooling system is being simulated only to meet the requirements described in this table, "
             "heating and/or cooling system fans shall not be simulated as running continuously during occupied hours but shall be cycled ON and OFF to meet heating and cooling loads during all hours in the baseline design.",
@@ -35,9 +38,9 @@ class Section19Rule34(RuleDefinitionListIndexedBase):
         )
 
     def create_data(self, context, data):
-        rmd_u = context.user
-        rmd_b = context.baseline
-        rmd_p = context.proposed
+        rmd_u = context.USER
+        rmd_b = context.BASELINE_0
+        rmd_p = context.PROPOSED
 
         HVAC_systems_virtual_cooling_list_p = (
             get_proposed_hvac_modeled_with_virtual_cooling(rmd_u, rmd_p)
@@ -80,11 +83,13 @@ class Section19Rule34(RuleDefinitionListIndexedBase):
     class HVACRule(PartialRuleDefinition):
         def __init__(self):
             super(Section19Rule34.HVACRule, self).__init__(
-                rmrs_used=UserBaselineProposedVals(True, True, True),
+                rmrs_used=produce_ruleset_model_instance(
+                    USER=False, BASELINE_0=True, PROPOSED=True
+                ),
             )
 
         def applicability_check(self, context, calc_vals, data):
-            hvac_b = context.baseline
+            hvac_b = context.BASELINE_0
             hvac_id_b = hvac_b["id"]
 
             applicable_hvac_with_virtual_heating_cooling_b = data[
@@ -94,8 +99,8 @@ class Section19Rule34(RuleDefinitionListIndexedBase):
             return hvac_id_b in applicable_hvac_with_virtual_heating_cooling_b
 
         def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
-            hvac_b = context.baseline
-            hvac_p = context.proposed
+            hvac_b = context.BASELINE_0
+            hvac_p = context.PROPOSED
             hvac_id_b = hvac_b["id"]
             hvac_id_p = hvac_p["id"]
 
