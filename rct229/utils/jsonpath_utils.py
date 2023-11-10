037f5bdcdc079d1bfc53f45f6e941d6c43880ccd
Xie <jiarong.xie@pnnl.gov>
2023-11-10 16:26:50 -0500

    remove archive files, Pipfile and Pipfile.lock

--- rct229/utils/jsonpath_utils.py
+++ rct229/utils/jsonpath_utils.py
@@ -11,30 +11,30 @@ def ensure_root(jpath):
     return jpath if jpath.startswith("$") else "$." + jpath
 
 
-def find_all(jpath, obj):
+def find_all(jpath, obj):  ####
     return [m.current_value for m in match(ensure_root(jpath), obj)]
 
 
-def find_all_with_field_value(jpath, field, value, obj):
+def find_all_with_field_value(jpath, field, value, obj):  ##
     return [
         m.current_value
         for m in match(ensure_root(f'{jpath}[?(@.{field}="{value}")]'), obj)
     ]
 
 
-def find_one(jpath, obj, default=None):
+def find_one(jpath, obj, default=None):  ###
     matches = find_all(jpath, obj)
 
     return matches[0] if len(matches) > 0 else default
 
 
-def find_one_with_field_value(jpath, field, value, obj):
+def find_one_with_field_value(jpath, field, value, obj):  ##
     matches = find_all_with_field_value(jpath, field, value, obj)
 
     return matches[0] if len(matches) > 0 else None
 
 
-def find_exactly_one_with_field_value(jpath, field, value, obj):
+def find_exactly_one_with_field_value(jpath, field, value, obj):  ##
     matches = find_all_with_field_value(jpath, field, value, obj)
     # do another search using upper case if no matches
     if not matches:
@@ -46,7 +46,7 @@ def find_exactly_one_with_field_value(jpath, field, value, obj):
     return matches[0]
 
 
-def find_exactly_one(jpath, obj):
+def find_exactly_one(jpath, obj):  #
     matches = find_all(jpath, obj)
     assert (
         len(matches) == 1
