dd8c8fe411007536a0ff6c14807b329c28e9dbe5
Xie <jiarong.xie@pnnl.gov>
2023-11-13 15:07:45 -0500

    remove rmr_context, fix format and convention flaws

--- rct229/rulesets/ashrae9012019/section5/section5rule18.py
+++ rct229/rulesets/ashrae9012019/section5/section5rule18.py
@@ -1,9 +1,11 @@
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
-from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
-from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_instance
+from rct229.rule_engine.rule_list_indexed_base import \
+    RuleDefinitionListIndexedBase
+from rct229.rule_engine.ruleset_model_factory import \
+    produce_ruleset_model_instance
+from rct229.rulesets.ashrae9012019 import BASELINE_0
 from rct229.schema.schema_enums import SchemaEnums
 from rct229.utils.jsonpath_utils import find_all
-from rct229.rulesets.ashrae9012019 import BASELINE_0
 
 GENERAL_STATUS = SchemaEnums.schema_enums["SpaceStatusOptions2019ASHRAE901"]
 
@@ -29,7 +31,6 @@ class Section5Rule18(RuleDefinitionListIndexedBase):
             is_primary_rule=False,
             each_rule=Section5Rule18.ZoneRule(),
             index_rmr=BASELINE_0,
-            rmr_context="ruleset_model_descriptions/0",
             list_path="$.buildings[*].building_segments[*].zones[*]",
         )
 
@@ -53,7 +54,7 @@ class Section5Rule18(RuleDefinitionListIndexedBase):
         def get_calc_vals(self, context, data=None):
             zone_b = context.BASELINE_0
 
-            has_existing_or_altered_space = any(
+            has_existing_or_altered_space_b = any(
                 [
                     space_b["id"]
                     for space_b in find_all("$.spaces[*]", zone_b)
@@ -61,18 +62,22 @@ class Section5Rule18(RuleDefinitionListIndexedBase):
                 ]
             )
 
-            return {"has_existing_or_altered_space": has_existing_or_altered_space}
+            return {"has_existing_or_altered_space_b": has_existing_or_altered_space_b}
 
         def applicability_check(self, context, calc_vals, data):
-            has_existing_or_altered_space = calc_vals["has_existing_or_altered_space"]
+            has_existing_or_altered_space_b = calc_vals[
+                "has_existing_or_altered_space_b"
+            ]
 
-            return has_existing_or_altered_space
+            return has_existing_or_altered_space_b
 
         def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
-            has_existing_or_altered_space = calc_vals["has_existing_or_altered_space"]
+            has_existing_or_altered_space_b = calc_vals[
+                "has_existing_or_altered_space_b"
+            ]
 
             return (
                 f"Part or all of zones listed below is existing or altered. The baseline vertical fenestration "
                 f"area for existing zones must equal to the fenestration area prior to the proposed scope of "
-                f"work. The baseline fenestration area in zone must be checked manually.{has_existing_or_altered_space}"
+                f"work. The baseline fenestration area in zone must be checked manually.{has_existing_or_altered_space_b}"
             )
