8a21c7c0652801ea94c29efb2c230f878dc2cb97
Xie <jiarong.xie@pnnl.gov>
2023-11-13 18:12:46 -0500

    fix style issue

--- rct229/rulesets/ashrae9012019/section5/section5rule18.py
+++ rct229/rulesets/ashrae9012019/section5/section5rule18.py
@@ -1,8 +1,6 @@
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
-from rct229.rule_engine.rule_list_indexed_base import \
-    RuleDefinitionListIndexedBase
-from rct229.rule_engine.ruleset_model_factory import \
-    produce_ruleset_model_instance
+from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
+from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_instance
 from rct229.rulesets.ashrae9012019 import BASELINE_0
 from rct229.schema.schema_enums import SchemaEnums
 from rct229.utils.jsonpath_utils import find_all
