669a15d4e49a4149fa4c5c91dd3e487757fc234a
Xie <jiarong.xie@pnnl.gov>
2023-11-20 22:40:26 -0500

    This ruleset has not been tested

---
+++ rct229/rulesets/ashrae9012019/section5/section5rule39.py
@@ -0,0 +1,40 @@
+from rct229.rule_engine.rule_base import RuleDefinitionBase
+from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_instance
+from rct229.utils.jsonpath_utils import find_all
+
+
+class Section5Rule39(RuleDefinitionBase):
+    """Rule 39 of ASHRAE 90.1-2019 Appendix G Section 5 (Envelope)"""
+
+    def __init__(self):
+        super(Section5Rule39, self).__init__(
+            rmrs_used=produce_ruleset_model_instance(
+                USER=False, BASELINE_0=True, PROPOSED=False
+            ),
+            id="5-39",
+            description="It is acceptable to use either an annual average ground temperature or monthly average ground temperatures for calculation of heat loss through basement floors.",
+            ruleset_section_title="Envelope",
+            standard_section="Section G3.1-14(b) Building Envelope Modeling Requirements for the Proposed design and Baseline",
+            is_primary_rule=True,
+            required_fields={
+                "$": ["weather", "ruleset_model_descriptions"],
+                "$.weather": ["ground_temperature_schedule"],
+            },
+        )
+
+    def get_calc_vals(self, context, data=None):
+        rpd = context.BASELINE_0
+        weather = rpd.get("weather")
+        ground_temperature_schedule_id = weather.get("ground_temperature_schedule")
+        rmds = rpd.get("ruleset_model_descriptions")
+        has_ground_temperature_schedule = any(
+            [
+                schedule["id"] == ground_temperature_schedule_id
+                for schedule in find_all("$.schedules[*]", rmds)
+            ]
+        )
+        return {"has_ground_temperature_schedule": has_ground_temperature_schedule}
+
+    def rule_check(self, context, calc_vals, data=None):
+        has_ground_temperature_schedule = calc_vals["has_ground_temperature_schedule"]
+        return has_ground_temperature_schedule
