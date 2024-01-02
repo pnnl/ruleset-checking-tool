bd8c731fdd35490bea5fa909ce017eef9c6a94e1
Xie <jiarong.xie@pnnl.gov>
2024-01-02 09:41:03 -0500

    revised based on updated RDS

--- rct229/rulesets/ashrae9012019/section5/section5rule39.py
+++ rct229/rulesets/ashrae9012019/section5/section5rule39.py
@@ -1,6 +1,12 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_instance
-from rct229.utils.jsonpath_utils import find_all, find_one
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_opaque_surface_type import (
+    OpaqueSurfaceType as OST,
+)
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_opaque_surface_type import (
+    get_opaque_surface_type,
+)
+from rct229.utils.jsonpath_utils import find_all
 
 
 class Section5Rule39(RuleDefinitionBase):
@@ -12,29 +18,34 @@ class Section5Rule39(RuleDefinitionBase):
                 USER=False, BASELINE_0=True, PROPOSED=False
             ),
             id="5-39",
-            description="It is acceptable to use either an annual average ground temperature or monthly average ground temperatures for calculation of heat loss through basement floors.",
+            description="It is acceptable to use either an annual average ground temperature or monthly average ground temperatures for calculation of heat loss through below-grade walls and basement floors.",
             ruleset_section_title="Envelope",
             standard_section="Section G3.1-14(b) Building Envelope Modeling Requirements for the Proposed design and Baseline",
-            is_primary_rule=True,
+            is_primary_rule=False,
             required_fields={
                 "$": ["weather", "ruleset_model_descriptions"],
-                "$.weather": ["ground_temperature_schedule"],
             },
+        )
+
+    def is_applicable(self, context, data=None):
+        rpd = context.BASELINE_0
+        return any(
+            [
+                surface_b
+                for surface_b in find_all(
+                    "$.ruleset_model_descriptions[0].buildings[*].building_segments[*].zones["
+                    "*].surfaces[*]",
+                    rpd,
+                )
+                if get_opaque_surface_type(surface_b) == OST.BELOW_GRADE_WALL
+            ]
         )
 
     def get_calc_vals(self, context, data=None):
         rpd = context.BASELINE_0
-        ground_temperature_schedule = rpd["weather"]["ground_temperature_schedule"]
-        has_ground_temperature_schedule = any(
-            [
-                schedule["id"] == ground_temperature_schedule
-                for schedule in find_all(
-                    "$.ruleset_model_descriptions[0].schedules[*]", rpd
-                )
-            ]
-        )
-        return {"has_ground_temperature_schedule": has_ground_temperature_schedule}
+        ground_temperature_schedule = rpd["weather"].get("ground_temperature_schedule")
+        return {"ground_temperature_schedule": ground_temperature_schedule}
 
-    def rule_check(self, context, calc_vals, data=None):
-        has_ground_temperature_schedule = calc_vals["has_ground_temperature_schedule"]
-        return has_ground_temperature_schedule
+    def applicability_check(self, context, calc_vals, data):
+        ground_temperature_schedule = calc_vals["ground_temperature_schedule"]
+        return ground_temperature_schedule
