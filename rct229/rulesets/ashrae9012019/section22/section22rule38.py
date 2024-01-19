cf4fb68eaf0fcff3491948fae033f9509ce5bd58
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-01-19 11:37:39 -0800

    Formatted 22-35,37 and 38

--- rct229/rulesets/ashrae9012019/section22/section22rule38.py
+++ rct229/rulesets/ashrae9012019/section22/section22rule38.py
@@ -32,10 +32,7 @@ class Section22Rule38(RuleDefinitionListIndexedBase):
 
     class RulesetModelInstanceRule(PartialRuleDefinition):
         def __init__(self):
-            super(
-                Section22Rule38.RulesetModelInstanceRule,
-                self,
-            ).__init__(
+            super(Section22Rule38.RulesetModelInstanceRule, self,).__init__(
                 rmrs_used=produce_ruleset_model_instance(
                     USER=False, BASELINE_0=True, PROPOSED=False
                 ),
