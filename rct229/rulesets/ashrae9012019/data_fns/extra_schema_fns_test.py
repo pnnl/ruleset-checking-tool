87969d035db9010fc23c8a6cb8d90571476c5bbb
Weili Xu <weili.xu@pnnl.gov>
2024-02-23 11:08:39 -0800

    format

--- rct229/rulesets/ashrae9012019/data_fns/extra_schema_fns_test.py
+++ rct229/rulesets/ashrae9012019/data_fns/extra_schema_fns_test.py
@@ -4,15 +4,6 @@ from rct229.rulesets.ashrae9012019.data_fns.extra_schema_fns import (
     compare_context_pair,
     EXTRA_SCHEMA,
 )
-from rct229.utils.json_utils import load_json
-
-proposed = load_json(
-    "C:\\Users\\xuwe123\\Documents\\GitHub\\ruleset-checking-tool\\examples\\chicago_demo\\proposed_model.json"
-)
-user = load_json(
-    "C:\\Users\\xuwe123\\Documents\\GitHub\\ruleset-checking-tool\\examples\\chicago_demo\\user_model.json"
-)
-error_msg_list = []
 
 TEST_RMI = {
     "id": "test_rmd",
