4f54faa994614253a2be2f751052a11d4674281e
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-03-14 16:30:46 -0400

    add files from 10-7 PR

---
+++ rct229/rulesets/ashrae9012019/data_fns/table_G3_5_4_fns.py
@@ -0,0 +1,29 @@
+from rct229.rulesets.ashrae9012019.data import data
+from rct229.rulesets.ashrae9012019.data_fns.table_utils import find_osstd_table_entry
+
+
+def table_G3_5_4_lookup(equipment_type):
+    """Returns the packaged terminal system efficiency data based on equipment type
+    Parameters
+    ----------
+    equipment_type : str
+        One of the equipment types from table G3.5.4
+    Returns
+    -------
+    dict
+        { minimum_efficiency: Quantity - the minimum COP
+          efficiency_metric: str - the efficiency metric
+        }
+    """
+
+    osstd_entry = find_osstd_table_entry(
+        [
+            ("equipment_type", equipment_type),
+        ],
+        osstd_table=data["ashrae_90_1_table_G3_5_4.json"],
+    )
+
+    return {
+        "minimum_efficiency": osstd_entry["minimum_efficiency"],
+        "efficiency_metric": osstd_entry["efficiency_metric"],
+    }
---
+++ rct229/rulesets/ashrae9012019/data_fns/table_G3_5_4_fns_test.py
@@ -0,0 +1,23 @@
+from rct229.rulesets.ashrae9012019.data_fns.table_G3_5_4_fns import table_G3_5_4_lookup
+
+
+# Testing table_3_5_4------------------------------------------
+def test__table_3_5_4_ptac():
+    assert table_G3_5_4_lookup("PTAC (cooling mode)") == {
+        "minimum_efficiency": 3.2,
+        "efficiency_metric": "FULL_LOAD_COEFFICIENT_OF_PERFORMANCE_NO_FAN",
+    }
+
+
+def test__table_3_5_4_pthp_cooling():
+    assert table_G3_5_4_lookup("PTHP (cooling mode)") == {
+        "minimum_efficiency": 3.1,
+        "efficiency_metric": "FULL_LOAD_COEFFICIENT_OF_PERFORMANCE_NO_FAN",
+    }
+
+
+def test__table_3_5_4_pthp_heating():
+    assert table_G3_5_4_lookup("PTHP (heating mode)") == {
+        "minimum_efficiency": 3.1,
+        "efficiency_metric": "HEAT_PUMP_EFFICIENCY_HIGH_TEMPERATURE_NO_FAN",
+    }
