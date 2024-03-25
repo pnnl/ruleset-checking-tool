e573bac9198dc7e2c423f9c3318504bf5008d97f
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2024-03-25 16:29:30 -0400

    service water heating association check

--- rct229/schema/validate.py
+++ rct229/schema/validate.py
@@ -27,7 +27,45 @@ SCHEMA_OUTPUT_PATH = os.path.join(file_dir, SCHEMA_OUTPUT_KEY)
 
 # def check_fluid_loop_or_piping_association(rmd)
 
-# def check_service_water_heating_association(rmd)
+
+def check_service_water_heating_association(rmd):
+    """
+    Check the association between service water heating distribution systems and the various objects that reference them.
+    Parameters
+    ----------
+    rmd
+
+    Returns list of mismatched service water heating distribution system ids
+    -------
+
+    """
+    mismatch_list = []
+    service_water_heating_id_list = find_all(
+        "$.service_water_heating_distribution_systems[*].id", rmd
+    )
+
+    referenced_service_water_heating_id_list = find_all(
+        "$.buildings[*].building_segments[*].zones[*].spaces[*].service_water_heating_uses[*].served_by_distribution_system",
+        rmd,
+    )
+    referenced_service_water_heating_id_list.extend(
+        find_all(
+            "$.buildings[*].building_segments[*].zones[*].served_by_service_water_heating_system",
+            rmd,
+        )
+    )
+    referenced_service_water_heating_id_list.extend(
+        find_all(
+            "$.service_water_heating_equipment[*].distribution_system",
+            rmd,
+        )
+    )
+
+    for service_water_heating_id in referenced_service_water_heating_id_list:
+        if service_water_heating_id not in service_water_heating_id_list:
+            mismatch_list.append(service_water_heating_id)
+    return mismatch_list
+
 
 # search schedule with key words: Constraint to use when implemented :
 
