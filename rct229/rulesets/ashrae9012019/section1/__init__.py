e33548199b746485b30b3b23a33ab623f90ea2b4
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-04-02 11:51:49 -0700

    Added section 1 setup

---
+++ rct229/rulesets/ashrae9012019/section1/__init__.py
@@ -0,0 +1,20 @@
+# Add all available rule modules in __all__
+import importlib
+
+__all__ = [
+    "section1rule1",
+    # "section1rule2",
+    # "section1rule3",
+    # "section1rule4",
+    # "section1rule5"
+]
+
+
+def __getattr__(name):
+    if name in __all__:
+        return importlib.import_module("." + name, __name__)
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+def __dir__():
+    return sorted(__all__)
