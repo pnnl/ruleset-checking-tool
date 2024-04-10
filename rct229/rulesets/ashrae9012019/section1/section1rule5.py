6d67ef9ca7154622ab85232233ac9b8fdef95f32
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-04-10 10:56:27 -0700

    Added assert_ logic

--- rct229/rulesets/ashrae9012019/section1/section1rule5.py
+++ rct229/rulesets/ashrae9012019/section1/section1rule5.py
@@ -1,3 +1,4 @@
+from pydash import filter_
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_instance
 from rct229.rulesets.ashrae9012019 import (
@@ -8,8 +9,8 @@ from rct229.rulesets.ashrae9012019 import (
     PROPOSED,
     USER,
 )
+from rct229.utils.assertions import assert_
 from rct229.utils.jsonpath_utils import find_one
-
 
 APPLICABLE_LIMIT = 0.05
 
@@ -41,16 +42,6 @@ class Section1Rule5(RuleDefinitionBase):
             standard_section="Section 4.2.1.1",
             is_primary_rule=True,
             rmr_context="ruleset_model_descriptions/0",
-            required_fields={
-                "$": ["output"],
-                "output": [
-                    "total_proposed_building_energy_cost_including_renewable_energy",
-                    "baseline_building_performance_energy_cost",
-                    "total_proposed_building_energy_cost_excluding_renewable_energy",
-                    "performance_cost_index",
-                    "performance_cost_index_target",
-                ],
-            },
         )
 
     def is_applicable(self, context, data=None):
@@ -61,9 +52,9 @@ class Section1Rule5(RuleDefinitionBase):
         rmd_b270 = context.BASELINE_270
         rmd_p = context.PROPOSED
 
-        pbp_set = []  ##
-        bbp_set = []  ##
-        pbp_nre_set = []  ##
+        pbp_set = []
+        bbp_set = []
+        pbp_nre_set = []
         for rmd in (rmd_u, rmd_b0, rmd_b90, rmd_b180, rmd_b270, rmd_p):
             if rmd is not None:
                 pbp_set.append(
@@ -84,6 +75,14 @@ class Section1Rule5(RuleDefinitionBase):
                         rmd,
                     )
                 )
+
+        pbp_set = list(set(filter_(pbp_set, lambda x: x is not None)))
+        bbp_set = list(set(filter_(bbp_set, lambda x: x is not None)))
+        pbp_nre_set = list(set(filter_(pbp_nre_set, lambda x: x is not None)))
+
+        assert_(len(pbp_set) >= 1, "At least one `pbp_set` value must exist.")
+        assert_(len(bbp_set) >= 1, "At least one `bbp_set` value must exist.")
+        assert_(len(pbp_nre_set) >= 1, "At least one `pbp_nre_set` value must exist.")
 
         return (list(set(pbp_nre_set))[0] - list(set(pbp_set))[0]) / list(set(bbp_set))[
             0
@@ -135,6 +134,10 @@ class Section1Rule5(RuleDefinitionBase):
                     )
                 )
 
+        pbp_set = filter_(pbp_set, lambda x: x is not None)
+        bbp_set = filter_(bbp_set, lambda x: x is not None)
+        pbp_nre_set = filter_(pbp_nre_set, lambda x: x is not None)
+
         return {
             "pbp_set": list(set(pbp_set)),
             "bbp_set": list(set(bbp_set)),
@@ -158,7 +161,7 @@ class Section1Rule5(RuleDefinitionBase):
             == len(pci_target_set)
             == 1
             and (pci_set[0] + (pbp_nre_set[0] - pbp_set[0]) / bbp_set[0])
-            <= pci_target_set
+            <= pci_target_set[0]
         )
 
     def get_fail_msg(self, context, calc_vals=None, data=None):
