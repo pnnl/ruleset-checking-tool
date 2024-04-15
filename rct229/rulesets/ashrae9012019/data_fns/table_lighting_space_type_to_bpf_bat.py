3a2882d12af0604782b4869dfba81256ed7da8ea
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-04-15 12:09:59 -0700

    Reverted unexpceted updates

--- rct229/rulesets/ashrae9012019/data_fns/table_lighting_space_type_to_bpf_bat.py
+++ rct229/rulesets/ashrae9012019/data_fns/table_lighting_space_type_to_bpf_bat.py
@@ -5,7 +5,7 @@ from rct229.utils.assertions import assert_
 def lighting_space_type_to_bpf_bat(lgt_space_type):
     lgt_space_type_to__bpf_bat = data["ashrae_90_1_lighting_space_type_bpf_bat"]
     assert_(
-        lgt_space_type in lgt_space_type_to__bpf_bat.keys(),
+        lgt_space_type in lgt_space_type_to__bpf_bat,
         f"Lighting space type {lgt_space_type} does not exist in ashrae_90_1_lighting_space_type_bpf_bat",
     )
 
