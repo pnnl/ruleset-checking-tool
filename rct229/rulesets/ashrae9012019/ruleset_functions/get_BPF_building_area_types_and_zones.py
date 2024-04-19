ec5278f313f3d926466a27f32762a4ae6234ec6f
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-04-19 10:03:27 -0700

    Removed the _initialize_bldg_segment_bpf_bat func

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_BPF_building_area_types_and_zones.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_BPF_building_area_types_and_zones.py
@@ -38,19 +38,6 @@ def get_BPF_building_area_types_and_zones(
                                                        the total area of each building area type: {MULTIFAMILY: {"zone_id": ["zone_2", "zone_3", "zone_4"], "area": 34567, "classification_source": "BUILDING_SEGMENT_LIGHTING"}, HEALTHCARE_HOSPITAL: {"zone_id": ["zone_1","zone_5","zone_6"], "area": 20381, "classification_source": "SPACE_LIGHTING"}
     """
 
-    def _define_bldg_segment_BPF_BAT(
-        building_area_types_with_total_area_and_zones_dict: dict,
-        classification_source: str,
-    ) -> dict:
-
-        building_area_types_with_total_area_and_zones_dict[building_segment_BPF_BAT] = {
-            "zone_id": [],
-            "area": ZERO.AREA,
-            "classification_source": classification_source,
-        }
-
-        return building_area_types_with_total_area_and_zones_dict
-
     building_area_types_with_total_area_and_zones_dict = {}
     for building_segment in find_all("$.buildings[*].building_segments[*]", rmd):
         if building_segment.get(
@@ -83,12 +70,13 @@ def get_BPF_building_area_types_and_zones(
                     building_segment_BPF_BAT
                     not in building_area_types_with_total_area_and_zones_dict
                 ):
-                    building_area_types_with_total_area_and_zones_dict = (
-                        _define_bldg_segment_BPF_BAT(
-                            building_area_types_with_total_area_and_zones_dict,
-                            classification_source,
-                        )
-                    )
+                    building_area_types_with_total_area_and_zones_dict[
+                        building_segment_BPF_BAT
+                    ] = {
+                        "zone_id": [],
+                        "area": ZERO.AREA,
+                        "classification_source": classification_source,
+                    }
 
                 building_area_types_with_total_area_and_zones_dict[
                     building_segment_BPF_BAT
@@ -103,12 +91,13 @@ def get_BPF_building_area_types_and_zones(
                 building_segment_BPF_BAT
                 not in building_area_types_with_total_area_and_zones_dict
             ):
-                building_area_types_with_total_area_and_zones_dict = (
-                    _define_bldg_segment_BPF_BAT(
-                        building_area_types_with_total_area_and_zones_dict,
-                        classification_source,
-                    )
-                )
+                building_area_types_with_total_area_and_zones_dict[
+                    building_segment_BPF_BAT
+                ] = {
+                    "zone_id": [],
+                    "area": ZERO.AREA,
+                    "classification_source": classification_source,
+                }
 
             for zone in find_all("$.zones[*]", building_segment):
                 building_area_types_with_total_area_and_zones_dict[
