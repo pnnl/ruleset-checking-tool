ea40234a2f70c2e58f193f735bab3fd3fa94b10b
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-04-30 10:28:03 -0700

    Dev rule 16-3

--- rct229/rulesets/ashrae9012019/section16/section16rule3.py
+++ rct229/rulesets/ashrae9012019/section16/section16rule3.py
@@ -8,20 +8,20 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.compare_schedules import (
 from rct229.utils.assertions import getattr_
 
 
-class Section16Rule2(RuleDefinitionListIndexedBase):
-    """Rule 2 of ASHRAE 90.1-2019 Appendix G Section 16 (Elevators)"""
+class Section16Rule3(RuleDefinitionListIndexedBase):
+    """Rule 3 of ASHRAE 90.1-2019 Appendix G Section 16 (Elevators)"""
 
     def __init__(self):
-        super(Section16Rule2, self).__init__(
+        super(Section16Rule3, self).__init__(
             rmds_used=produce_ruleset_model_instance(
                 USER=False,
                 BASELINE_0=True,
                 PROPOSED=True,
             ),
-            each_rule=Section16Rule2.BuildingRule(),
+            each_rule=Section16Rule3.BuildingRule(),
             index_rmd="baseline",
-            id="16-2",
-            description="The baseline elevator motor use shall be modeled with the same schedule as the proposed design. Rule Assertion: B-RMD = P-RMD",
+            id="16-3",
+            description="The elevator cab ventilation fan shall be modeled with the same schedule as the elevator motor.",
             ruleset_section_title="Elevators",
             standard_section="Section G3.1",
             is_primary_rule=True,
@@ -32,7 +32,7 @@ class Section16Rule2(RuleDefinitionListIndexedBase):
 
     class BuildingRule(RuleDefinitionBase):
         def __init__(self):
-            super(Section16Rule2.BuildingRule, self).__init__(
+            super(Section16Rule3.BuildingRule, self).__init__(
                 rmds_used=produce_ruleset_model_instance(
                     USER=False,
                     BASELINE_0=True,
@@ -53,33 +53,39 @@ class Section16Rule2(RuleDefinitionListIndexedBase):
             is_leap_year_b = data["is_leap_year_b"]
             mask_schedule = [1] * 8784 if is_leap_year_b else [1] * 8760
 
-            motor_use_schedule_b = getattr_(
-                building_b, "building", "elevators", "cab_motor_multiplier_schedule"
+            cab_ventilation_schedule_b = getattr_(
+                building_b,
+                "building",
+                "elevators",
+                "cab_ventilation_fan_multiplier_schedule",
             )
-            motor_use_schedule_p = getattr_(
-                building_p, "building", "elevators", "cab_motor_multiplier_schedule"
+            cab_ventilation_schedule_p = getattr_(
+                building_p,
+                "building",
+                "elevators",
+                "cab_ventilation_fan_multiplier_schedule",
             )
 
             compare_schedules = compare_schedules(
-                motor_use_schedule_b,
-                motor_use_schedule_p,
+                cab_ventilation_schedule_b,
+                cab_ventilation_schedule_p,
                 mask_schedule,
                 is_leap_year_b,
             )
 
             return {
-                "motor_use_schedule_b": motor_use_schedule_b,
-                "motor_use_schedule_p": motor_use_schedule_p,
+                "cab_ventilation_schedule_b": cab_ventilation_schedule_b,
+                "cab_ventilation_schedule_p": cab_ventilation_schedule_p,
                 "compare_schedules": compare_schedules,
             }
 
         def rule_check(self, context, calc_vals=None, data=None):
             compare_schedules = calc_vals["compare_schedules"]
-            motor_use_schedule_b = calc_vals["motor_use_schedule_b"]
-            motor_use_schedule_p = calc_vals["motor_use_schedule_p"]
+            cab_ventilation_schedule_b = calc_vals["cab_ventilation_schedule_b"]
+            cab_ventilation_schedule_p = calc_vals["cab_ventilation_schedule_p"]
 
             return (
                 compare_schedules["total_hours_matched"]
-                == len(motor_use_schedule_b)
-                == len(motor_use_schedule_p)
+                == len(cab_ventilation_schedule_b)
+                == len(cab_ventilation_schedule_p)
             )
