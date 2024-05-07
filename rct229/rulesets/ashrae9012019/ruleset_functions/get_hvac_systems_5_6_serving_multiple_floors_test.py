80196889911a0eaecf4cb395150793278a1be86d
Weili Xu <weili.xu@pnnl.gov>
2024-05-07 09:01:00 -0700

    fix import error

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_hvac_systems_5_6_serving_multiple_floors_test.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_hvac_systems_5_6_serving_multiple_floors_test.py
@@ -1,8 +1,8 @@
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_hvac_systems_5_6_serving_multiple_floors import (
     get_hvac_systems_5_6_serving_multiple_floors,
 )
-from rct229.schema.schema_utils import quantify_rmr
-from rct229.schema.validate import schema_validate_rmr
+from rct229.schema.schema_utils import quantify_rmd
+from rct229.schema.validate import schema_validate_rmd
 
 TEST_RMI = {
     "id": "Test RMD",
@@ -99,11 +99,11 @@ TEST_RMD_FULL = {
     "data_timestamp": "2024-03-08T09:00Z",
 }
 
-TEST_RMI_UNIT = quantify_rmr(TEST_RMD_FULL)["ruleset_model_descriptions"][0]
+TEST_RMI_UNIT = quantify_rmd(TEST_RMD_FULL)["ruleset_model_descriptions"][0]
 
 
 def test__TEST_RMD__is_valid():
-    schema_validation_result = schema_validate_rmr(TEST_RMD_FULL)
+    schema_validation_result = schema_validate_rmd(TEST_RMD_FULL)
     assert schema_validation_result[
         "passed"
     ], f"Schema error: {schema_validation_result['error']}"
