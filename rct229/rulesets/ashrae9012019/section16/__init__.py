4ad9372d496d3ac7116561d05d32c3c052b16471
Xie, Jiarong <jiarong.xie@pnnl.gov>
2024-05-13 20:56:31 -0400

    set up testing and add test json

---
+++ rct229/rulesets/ashrae9012019/section16/__init__.py
@@ -0,0 +1,16 @@
+import importlib
+
+# Add all available rule modules in __all__
+__all__ = [
+    "section16rule6",
+]
+
+
+def __getattr__(name):
+    if name in __all__:
+        return importlib.import_module("." + name, __name__)
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+def __dir__():
+    return sorted(__all__)
