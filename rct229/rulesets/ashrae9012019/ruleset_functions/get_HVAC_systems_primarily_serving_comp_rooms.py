895cbfc51332bdec486bdac122c577ae6067f216
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-07-10 12:15:55 -0700

    Finish dev

---
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_HVAC_systems_primarily_serving_comp_rooms.py
@@ -0,0 +1,66 @@
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_hvac_zone_list_w_area_dict import (
+    get_hvac_zone_list_w_area_by_rmi_dict,
+)
+from rct229.schema.schema_enums import SchemaEnums
+from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.pint_utils import ZERO
+
+LightingSpaceOptions2019ASHRAE901TG37 = SchemaEnums.schema_enums[
+    "LightingSpaceOptions2019ASHRAE901TG37"
+]
+COMPUTER_ROOM_RATIO_THRESHOLD = 0.5
+
+
+def get_HVAC_systems_primarily_serving_comp_rooms(rmd: dict) -> list[str]:
+    """
+    Returns a list of HVAC systems in which greater than 50% of the area served by the HVAC system is computer room space.
+
+    Parameters
+    ----------
+    rmd: json
+        To develop a list of HVAC systems in which greater than 50% of the area served by the HVAC system is computer room space.
+
+    Returns:
+        hvac_systems_primarily_serving_comp_rooms_list: list
+        A list of hvac systems in which greater than 50% of the area served by the HVAC system is computer room space.
+    -------
+
+    """
+
+    # get each hvac system's serving area and zone list
+    hvac_zone_list_w_area_dict = get_hvac_zone_list_w_area_by_rmi_dict(rmd)
+
+    hvac_systems_primarily_serving_comp_rooms_list = []
+    for hvac_id in find_all(
+        "$.buildings[*].building_segments[*].heating_ventilating_air_conditioning_systems[*].id",
+        rmd,
+    ):
+        hvac_system_serves_computer_room_space = False
+        hvac_sys_computer_room_floor_area = ZERO.AREA
+        if (
+            hvac_id in hvac_zone_list_w_area_dict
+        ):  # To prevent an error when an HVAC system doesn't serve any zone.
+            hvac_total_floor_area = hvac_zone_list_w_area_dict[hvac_id]["total_area"]
+            hvac_zone_list = hvac_zone_list_w_area_dict[hvac_id]["zone_list"]
+
+            for zone_id in hvac_zone_list:
+                for space in find_all(
+                    f'$.buildings[*].building_segments[*].zones[*][?(@.id="{zone_id}")].spaces[*]',
+                    rmd,
+                ):
+                    if (
+                        space.get("lighting_space_type")
+                        == LightingSpaceOptions2019ASHRAE901TG37.COMPUTER_ROOM
+                    ):
+                        hvac_system_serves_computer_room_space = True
+                        space_floor_area = space.get("floor_area", ZERO.AREA)
+                        hvac_sys_computer_room_floor_area += space_floor_area
+
+            if (
+                hvac_system_serves_computer_room_space
+                and hvac_sys_computer_room_floor_area / hvac_total_floor_area
+                > COMPUTER_ROOM_RATIO_THRESHOLD
+            ):
+                hvac_systems_primarily_serving_comp_rooms_list.append(hvac_id)
+
+    return hvac_systems_primarily_serving_comp_rooms_list
---
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_HVAC_systems_primarily_serving_comp_rooms_test.py
@@ -0,0 +1,119 @@
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_HVAC_systems_primarily_serving_comp_rooms import (
+    get_HVAC_systems_primarily_serving_comp_rooms,
+)
+from rct229.schema.schema_utils import quantify_rmd
+from rct229.schema.validate import schema_validate_rmd
+
+TEST_RMD = {
+    "id": "test_rmd",
+    "buildings": [
+        {
+            "id": "bldg_1",
+            "building_open_schedule": "bldg_open_sched_1",
+            "building_segments": [
+                {
+                    "id": "bldg_seg_1",
+                    "zones": [
+                        {
+                            "id": "zone_1",
+                            "spaces": [
+                                {
+                                    "id": "space_1_1",
+                                    "floor_area": 1000,
+                                    "lighting_space_type": "COMPUTER_ROOM",
+                                },
+                                {
+                                    "id": "space_1_2",
+                                    "floor_area": 500,
+                                    "lighting_space_type": "ATRIUM_HIGH",
+                                },
+                            ],
+                            "terminals": [
+                                {
+                                    "id": "terminal_1_1",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_1",
+                                }
+                            ],
+                        },
+                        {
+                            "id": "zone_2",
+                            "spaces": [
+                                {
+                                    "id": "space_2_1",
+                                    "floor_area": 1500,
+                                    "lighting_space_type": "COMPUTER_ROOM",
+                                },
+                                {
+                                    "id": "space_2_2",
+                                    "floor_area": 250,
+                                    "lighting_space_type": "CLASSROOM_LECTURE_HALL_TRAINING_ROOM_ALL_OTHER",
+                                },
+                            ],
+                            "terminals": [
+                                {
+                                    "id": "terminal_2_1",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_2",
+                                }
+                            ],
+                        },
+                        {
+                            "id": "zone_3",
+                            "spaces": [
+                                {
+                                    # The overall area ratio of the computer room < 0.5
+                                    "id": "space_3_1",
+                                    "floor_area": 250,
+                                    "lighting_space_type": "COMPUTER_ROOM",
+                                },
+                                {
+                                    "id": "space_3_2",
+                                    "floor_area": 800,
+                                    "lighting_space_type": "DINING_AREA_BAR_LOUNGE_OR_LEISURE_DINING",
+                                },
+                            ],
+                            "terminals": [
+                                {
+                                    "id": "terminal_3_1",
+                                    "served_by_heating_ventilating_air_conditioning_system": "hvac_3",
+                                }
+                            ],
+                        },
+                    ],
+                    "heating_ventilating_air_conditioning_systems": [
+                        {
+                            "id": "hvac_1",
+                        },
+                        {
+                            "id": "hvac_2",
+                        },
+                        {
+                            "id": "hvac_3",
+                        },
+                    ],
+                }
+            ],
+        }
+    ],
+    "type": "BASELINE_0",
+}
+TEST_RMD = {
+    "id": "229_01",
+    "ruleset_model_descriptions": [TEST_RMD],
+    "data_timestamp": "2024-02-12T09:00Z",
+}
+
+TEST_RMD_UNIT = quantify_rmd(TEST_RMD)["ruleset_model_descriptions"][0]
+
+
+def test__TEST_RMD__is_valid():
+    schema_validation_result = schema_validate_rmd(TEST_RMD)
+    assert schema_validation_result[
+        "passed"
+    ], f"Schema error: {schema_validation_result['error']}"
+
+
+def test__get_hvac_systems_primarily_serving_comp_room__hvac_1():
+    assert get_HVAC_systems_primarily_serving_comp_rooms(TEST_RMD_UNIT) == [
+        "hvac_1",
+        "hvac_2",
+    ]
