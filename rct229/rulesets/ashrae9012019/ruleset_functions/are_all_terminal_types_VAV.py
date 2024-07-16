57f332a8309bc9be40803967b97173bda99e60bd
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-07-16 09:01:57 -0700

    Addressed PR comments

--- rct229/rulesets/ashrae9012019/ruleset_functions/are_all_terminal_types_VAV.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/are_all_terminal_types_VAV.py
@@ -1,4 +1,5 @@
 from rct229.schema.schema_enums import SchemaEnums
+from rct229.utils.assertions import assert_
 from rct229.utils.utility_functions import find_exactly_one_terminal_unit
 
 TERMINAL_TYPE = SchemaEnums.schema_enums["TerminalOptions"]
@@ -11,15 +12,25 @@ def are_all_terminal_types_VAV(rmd: dict, terminal_unit_id_list: list[str]) -> b
 
     Parameters
     ----------
-    rmd: json,
+    rmd: dict,
         To evaluate if the terminal unit types are variable air volume (VAV).
-    terminal_unit_id: list[str],
+    terminal_unit_id_list: list[str],
         List of terminal units to assess.
 
     Returns bool,
         The function returns TRUE if all of the terminal unit types input to this function are variable air volume (VAV) or are equal to null.
         It returns FALSE if any of the terminal units are of a type other than variable air volume (VAV) or null.
     """
+
+    # check `terminal_unit_id_list` type
+    assert_(
+        isinstance(terminal_unit_id_list, list)
+        and all(
+            isinstance(terminal_unit_id, str)
+            for terminal_unit_id in terminal_unit_id_list
+        ),
+        "Please make sure the `terminal_unit_id_list` type is list and has list of strings",
+    )
 
     return all(
         [
--- rct229/rulesets/ashrae9012019/ruleset_functions/are_all_terminal_types_VAV_test.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/are_all_terminal_types_VAV_test.py
@@ -1,10 +1,11 @@
+import pytest
 from rct229.rulesets.ashrae9012019.ruleset_functions.are_all_terminal_types_VAV import (
     are_all_terminal_types_VAV,
 )
 from rct229.schema.validate import schema_validate_rmd
+from rct229.utils.assertions import RCTFailureException
 
-
-TEST_RMI = {
+TEST_RMD = {
     "id": "test_rmd",
     "buildings": [
         {
@@ -50,30 +51,46 @@ TEST_RMI = {
 }
 
 
-TEST_RMD = {
+TEST_RMD_FULL = {
     "id": "ASHRAE229",
-    "ruleset_model_descriptions": [TEST_RMI],
+    "ruleset_model_descriptions": [TEST_RMD],
     "data_timestamp": "2024-02-12T09:00Z",
 }
 
 
 def test__TEST_RMD__is_valid():
-    schema_validation_result = schema_validate_rmd(TEST_RMD)
+    schema_validation_result = schema_validate_rmd(TEST_RMD_FULL)
     assert schema_validation_result[
         "passed"
     ], f"Schema error: {schema_validation_result['error']}"
 
 
-def test__hvac_fan_all_VAV__success():
-    assert are_all_terminal_types_VAV(TEST_RMI, ["terminal_1", "terminal_4"]) is True
+def test__are_all_terminal_types_VAV__terminal_fan_all_VAV():
+    assert are_all_terminal_types_VAV(TEST_RMD, ["terminal_1", "terminal_4"]) is True
 
 
-def test__hvac_fan_not_all_VAV__success():
+def test__are_all_terminal_types_VAV__terminal_fan_not_all_VAV():
     assert (
-        are_all_terminal_types_VAV(TEST_RMI, ["terminal_1", "terminal_2", "terminal_4"])
+        are_all_terminal_types_VAV(TEST_RMD, ["terminal_1", "terminal_2", "terminal_4"])
         is False
     )
 
 
-def test__hvac_fan_include_no_type__success():
-    assert are_all_terminal_types_VAV(TEST_RMI, ["terminal_1", "terminal_3"]) is True
+def test__are_all_terminal_types_VAV__terminal_fan_include_no_type():
+    assert are_all_terminal_types_VAV(TEST_RMD, ["terminal_1", "terminal_3"]) is True
+
+
+def test__are_all_terminal_types_VAV__wrong_terminal_unit_id_list_type():
+    with pytest.raises(
+        RCTFailureException,
+        match="Please make sure the `terminal_unit_id_list` type is list and has list of strings",
+    ):
+        are_all_terminal_types_VAV(TEST_RMD, ("terminal_1", "terminal_3"))
+
+
+def test__are_all_terminal_types_VAV__wrong_terminal_unit_id_list_item_type():
+    with pytest.raises(
+        RCTFailureException,
+        match="Please make sure the `terminal_unit_id_list` type is list and has list of strings",
+    ):
+        are_all_terminal_types_VAV(TEST_RMD, [1, "terminal_3"])
