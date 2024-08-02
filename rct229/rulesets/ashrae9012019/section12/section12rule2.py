8c74d7ab9d19ae24c7774fe39fbb09c12c43c07a
Xie, Jiarong <jiarong.xie@pnnl.gov>
2024-08-01 22:12:47 -0400

    addressed partial PR comments,

--- rct229/rulesets/ashrae9012019/section12/section12rule2.py
+++ rct229/rulesets/ashrae9012019/section12/section12rule2.py
@@ -1,11 +1,14 @@
 from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_description
+from rct229.rule_engine.rulesets import LeapYear
 from rct229.rulesets.ashrae9012019 import BASELINE_0
 from rct229.rulesets.ashrae9012019.ruleset_functions.compare_schedules import (
     compare_schedules,
 )
 from rct229.schema.schema_enums import SchemaEnums
+from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.utility_functions import find_exactly_one_schedule
 
 LIGHTING_SPACE_OPTIONS = SchemaEnums.schema_enums[
     "LightingSpaceOptions2019ASHRAE901TG37"
@@ -33,7 +36,7 @@ class Section12Rule2(RuleDefinitionListIndexedBase):
             rmds_used=produce_ruleset_model_description(
                 USER=False, BASELINE_0=True, PROPOSED=True
             ),
-            each_rule=Section12Rule2.SpaceRule(),
+            each_rule=Section12Rule2.RMDRule(),
             index_rmd=BASELINE_0,
             id="12-2",
             description=(
@@ -42,85 +45,125 @@ class Section12Rule2(RuleDefinitionListIndexedBase):
             ruleset_section_title="Receptacle",
             standard_section="Table G3.1-12 Proposed Building Performance column",
             is_primary_rule=True,
-            list_path="ruleset_model_descriptions[0].buildings[*].building_segments[*].zones[*].spaces[*]",
+            list_path="ruleset_model_descriptions[0]",
             required_fields={"$": ["calendar"], "$.calendar": ["is_leap_year"]},
             data_items={"is_leap_year": (BASELINE_0, "calendar/is_leap_year")},
         )
 
-    class SpaceRule(RuleDefinitionListIndexedBase):
+    class RMDRule(RuleDefinitionListIndexedBase):
         def __init__(self):
-            super(Section12Rule2.SpaceRule, self).__init__(
+            super(Section12Rule2.RMDRule, self).__init__(
                 rmds_used=produce_ruleset_model_description(
                     USER=False, BASELINE_0=True, PROPOSED=True
                 ),
-                each_rule=Section12Rule2.SpaceRule(),
+                each_rule=Section12Rule2.RMDRule.SpaceRule(),
                 index_rmd=BASELINE_0,
-                list_path="$.miscellaneous_equipment[*]",
+                list_path="buildings[*].building_segments[*].zones[*].spaces[*]",
             )
 
         def create_data(self, context, data):
-            space_b = context.BASELINE_0
-            space_type_b = space_b.get("lighting_space_type")
-            return {
-                "space_type_b": space_type_b,
+            rmd_b = context.BASELINE_0
+            mis_equip_schedule_b_dict = {
+                sch_id: find_exactly_one_schedule(rmd_b, sch_id)["hourly_values"]
+                for sch_id in find_all(
+                    "buildings[*].building_segments[*].zones[*].spaces[*].miscellaneous_equipment[*].multiplier_schedule",
+                    rmd_b,
+                )
             }
+            return {"mis_equip_schedule_b_dict": mis_equip_schedule_b_dict}
 
-        class MiscellaneousEquipmentRule(RuleDefinitionBase):
+        class SpaceRule(RuleDefinitionListIndexedBase):
             def __init__(self):
-                super(
-                    Section12Rule2.SpaceRule.MiscellaneousEquipmentRule, self
-                ).__init__(
+                super(Section12Rule2.RMDRule.SpaceRule, self).__init__(
                     rmds_used=produce_ruleset_model_description(
                         USER=False, BASELINE_0=True, PROPOSED=True
                     ),
-                    manual_check_required_msg=MANUAL_CHECK_REQUIRED_MSG,
+                    each_rule=Section12Rule2.RMDRule.SpaceRule.MiscellaneousEquipmentRule(),
+                    index_rmd=BASELINE_0,
+                    list_path="$.miscellaneous_equipment[*]",
                 )
 
-            def get_calc_vals(self, context, data=None):
-                is_leap_year = data["is_leap_year"]
-                misc_equip_b = context.BASELINE_0
-                misc_equip_p = context.PROPOSED
-
-                misc_equip_schedule_b = misc_equip_b.get("multiplier_schedule")
-                misc_equip_schedule_p = misc_equip_p.get("multiplier_schedule")
-                auto_receptacle_control_b = misc_equip_b.get("has_automatic_control")
-                auto_receptacle_control_p = misc_equip_p.get("has_automatic_control")
-                mask_schedule = [1] * 8784 if is_leap_year else [1] * 8760
-                comparison_data = compare_schedules(
-                    misc_equip_schedule_b,
-                    misc_equip_schedule_p,
-                    mask_schedule,
-                    is_leap_year,
-                )
+            def create_data(self, context, data):
+                space_b = context.BASELINE_0
+                space_type_b = space_b.get("lighting_space_type")
                 return {
-                    "auto_receptacle_control_b": auto_receptacle_control_b,
-                    "auto_receptacle_control_p": auto_receptacle_control_p,
-                    "comparison_data": comparison_data,
-                    "misc_equip_schedule_b": misc_equip_schedule_b,
-                    "misc_equip_schedule_p": misc_equip_schedule_p,
+                    "space_type_b": space_type_b,
                 }
 
-            def manual_check_required(self, context, calc_vals=None, data=None):
-                comparison_data = calc_vals["comparison_data"]
-                auto_receptacle_controls_p = calc_vals["auto_receptacle_controls_p"]
-                space_type_b = data["space_type_b"]
-                return comparison_data["eflh_difference"] > 0 and (
-                    auto_receptacle_controls_p
-                    and space_type_b in EXPECTED_RECEPTACLE_CONTROL_SPACE_TYPES
-                    or space_type_b is None
-                )
+            class MiscellaneousEquipmentRule(RuleDefinitionBase):
+                def __init__(self):
+                    super(
+                        Section12Rule2.RMDRule.SpaceRule.MiscellaneousEquipmentRule,
+                        self,
+                    ).__init__(
+                        rmds_used=produce_ruleset_model_description(
+                            USER=False, BASELINE_0=True, PROPOSED=True
+                        ),
+                    )
 
-            def rule_check(self, context, calc_vals=None, data=None):
-                misc_equip_schedule_b = calc_vals["misc_equip_schedule_b"]
-                misc_equip_schedule_p = calc_vals["misc_equip_schedule_p"]
-                comparison_data = calc_vals["comparison_data"]
-                space_type_b = data["space_type_b"]
-                auto_receptacle_controls_p = calc_vals["auto_receptacle_controls_p"]
-                return (
-                    comparison_data["total_hours_matched"]
-                    == len(misc_equip_schedule_b)
-                    == len(misc_equip_schedule_p)
-                    or comparison_data["eflh_difference"] > 0
-                    and auto_receptacle_controls_p
-                    and space_type_b not in EXPECTED_RECEPTACLE_CONTROL_SPACE_TYPES
-                )
+                def get_calc_vals(self, context, data=None):
+                    is_leap_year = data["is_leap_year"]
+                    misc_equip_b = context.BASELINE_0
+                    misc_equip_p = context.PROPOSED
+
+                    space_type_b = data["space_type_b"]
+                    mis_equip_schedule_b_dict = data["mis_equip_schedule_b_dict"]
+
+                    misc_equip_schedule_b = misc_equip_b.get("multiplier_schedule")
+                    misc_equip_schedule_p = misc_equip_p.get("multiplier_schedule")
+                    auto_receptacle_control_b = misc_equip_b.get(
+                        "has_automatic_control"
+                    )
+                    auto_receptacle_control_p = misc_equip_p.get(
+                        "has_automatic_control"
+                    )
+
+                    mask_schedule = (
+                        [1] * LeapYear.LEAP_YEAR_HOURS
+                        if is_leap_year
+                        else [1] * LeapYear.REGULAR_YEAR_HOURS
+                    )
+                    schedules_comparison_output = compare_schedules(
+                        misc_equip_schedule_b,
+                        misc_equip_schedule_p,
+                        mask_schedule,
+                        is_leap_year,
+                    )
+                    return {
+                        "mis_equip_schedule_b_dict": mis_equip_schedule_b_dict,
+                        "space_type_b": space_type_b,
+                        "auto_receptacle_control_b": auto_receptacle_control_b,
+                        "auto_receptacle_control_p": auto_receptacle_control_p,
+                        "schedules_comparison_output": schedules_comparison_output,
+                        "hours_misc_equip_schedule_b": len(misc_equip_schedule_b),
+                        "hours_misc_equip_schedule_p": len(misc_equip_schedule_p),
+                    }
+
+                def manual_check_required(self, context, calc_vals=None, data=None):
+                    schedules_comparison_output = calc_vals[
+                        "schedules_comparison_output"
+                    ]
+                    auto_receptacle_controls_p = calc_vals["auto_receptacle_controls_p"]
+                    space_type_b = calc_vals["space_type_b"]
+                    return schedules_comparison_output["eflh_difference"] > 0 and (
+                        auto_receptacle_controls_p
+                        and space_type_b in EXPECTED_RECEPTACLE_CONTROL_SPACE_TYPES
+                        or space_type_b is None
+                    )
+
+                def rule_check(self, context, calc_vals=None, data=None):
+                    hours_misc_equip_schedule_b = calc_vals["misc_equip_schedule_b"]
+                    hours_misc_equip_schedule_p = calc_vals["misc_equip_schedule_p"]
+                    schedules_comparison_output = calc_vals[
+                        "schedules_comparison_output"
+                    ]
+                    space_type_b = data["space_type_b"]
+                    auto_receptacle_controls_p = calc_vals["auto_receptacle_controls_p"]
+                    return (
+                        schedules_comparison_output["total_hours_matched"]
+                        == hours_misc_equip_schedule_b
+                        == hours_misc_equip_schedule_p
+                        or schedules_comparison_output["eflh_difference"] > 0
+                        and auto_receptacle_controls_p
+                        and space_type_b not in EXPECTED_RECEPTACLE_CONTROL_SPACE_TYPES
+                    )
