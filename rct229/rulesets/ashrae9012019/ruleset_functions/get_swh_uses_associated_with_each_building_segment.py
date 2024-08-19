b8b4656956d52019200844453803f3dd12bffb9a
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-08-19 16:50:38 -0700

    Addressed PR comment

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_swh_uses_associated_with_each_building_segment.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_swh_uses_associated_with_each_building_segment.py
@@ -5,8 +5,8 @@ def get_swh_uses_associated_with_each_building_segment(
     rmd: dict, building_segment_id: str
 ) -> list[str]:
     """
-    This function gets all the SWH uses connected to a building segment. This function is primarily to encapsulate getting service water heating uses in one function so that if a change is made in the schema as to how service water heating use is specified,
-    the RCT only needs to change in one place.
+    This function gets all the SWH uses connected to a building segment or an empty list if no service water heating uses are found in the building segment. This function is primarily to encapsulate getting service water heating uses in one function so that if a change is made in the schema as to how service water heating use is specified,
+    the RCT only needs to change in one place .
 
     Parameters
     ----------
@@ -17,7 +17,7 @@ def get_swh_uses_associated_with_each_building_segment(
 
     Returns
     -------
-    swh_uses: list
+    swh_uses_list: list
         A list containing the ids of all service water heating uses associated with a building segment
     """
 
