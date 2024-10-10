d6def821d7ea8a1ec5aa8214843d175ad5e8b092
Xie, Jiarong <jiarong.xie@pnnl.gov>
2024-10-09 22:07:18 -0400

    address PR comment

--- rct229/rulesets/ashrae9012019/section11/section11rule11.py
+++ rct229/rulesets/ashrae9012019/section11/section11rule11.py
@@ -39,51 +39,39 @@ class Section11Rule11(RuleDefinitionListIndexedBase):
 
         def is_applicable(self, context, data=None):
             rmd_p = context.PROPOSED
-            has_swh_loads = False
-            for building_segment in find_all(
-                "$.buildings[*].building_segments[*]", rmd_p
-            ):
-                service_water_heating_use_ids = (
-                    get_swh_uses_associated_with_each_building_segment(
-                        rmd_p, building_segment["id"]
+            swh_use_loads_p = sum(
+                [
+                    swh_use.get("use", 0.0)
+                    # The json path needs to update if schema changes.
+                    for building_segment in find_all(
+                        "$.buildings[*].building_segments[*]", rmd_p
                     )
-                )
-                for swh_use_id in service_water_heating_use_ids:
-                    swh_use = find_exactly_one_with_field_value(
-                        "$.buildings[*].building_segments[*].zones[*].spaces[*].service_water_heating_uses[*]",
-                        "id",
-                        swh_use_id,
-                        rmd_p,
+                    for swh_use in find_all(
+                        "$.zones[*].spaces[*].service_water_heating_uses[*]",
+                        building_segment,
                     )
-                    if swh_use.get("use", 0) > 0:
-                        has_swh_loads = True
-                        break
-            return not has_swh_loads
+                ]
+            )
+
+            return swh_use_loads_p <= 0.0
 
         def get_calc_vals(self, context, data=None):
             rmd_b = context.BASELINE_0
-            has_swh_loads = False
-            for building_segment in find_all(
-                "$.buildings[*].building_segments[*]", rmd_b
-            ):
-                service_water_heating_use_ids = (
-                    get_swh_uses_associated_with_each_building_segment(
-                        rmd_b, building_segment["id"]
+            swh_use_loads_b = sum(
+                [
+                    swh_use.get("use", 0.0)
+                    # The json path needs to update if schema changes.
+                    for building_segment in find_all(
+                        "$.buildings[*].building_segments[*]", rmd_b
                     )
-                )
-                for swh_use_id in service_water_heating_use_ids:
-                    swh_use = find_exactly_one_with_field_value(
-                        "$.buildings[*].building_segments[*].zones[*].spaces["
-                        "*].service_water_heating_uses[*]",
-                        "id",
-                        swh_use_id,
-                        rmd_b,
+                    for swh_use in find_all(
+                        "$.zones[*].spaces[*].service_water_heating_uses[*]",
+                        building_segment,
                     )
-                    if swh_use.get("use", 0) > 0:
-                        has_swh_loads = True
-                        break
-            return {"has_swh_loads": has_swh_loads}
+                ]
+            )
+            return {"swh_use_loads_b": swh_use_loads_b}
 
         def rule_check(self, context, calc_vals=None, data=None):
-            has_swh_loads = calc_vals["has_swh_loads"]
-            return not has_swh_loads
+            swh_use_loads_b = calc_vals["swh_use_loads_b"]
+            return swh_use_loads_b <= 0.0
