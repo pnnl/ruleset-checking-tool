492114ad36ff2f3799e6e4cb1a2c4bd2fceddeca
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-10-18 14:10:20 -0700

    Finished func dev

--- rct229/rulesets/ashrae9012019/ruleset_functions/get_swh_bats_and_swh_use.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/get_swh_bats_and_swh_use.py
@@ -1,10 +1,16 @@
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_building_segment_swh_bat import (
+    get_building_segment_swh_bat,
+)
+from rct229.rulesets.ashrae9012019.ruleset_functions.get_swh_uses_associated_with_each_building_segment import (
+    get_swh_uses_associated_with_each_building_segment,
+)
 from rct229.utils.jsonpath_utils import find_all
 
 
 def get_swh_bats_and_swh_use(rmd: dict) -> dict:
     """
     This function gets all the SWH Uses and the SWH space types and sorts them into a dictionary where the keys are the ServiceWaterHeatingBuildingAreaOptions2019ASHRAE901
-    and values are a list if ServiceWaterHeatingUse.ids
+    and values are a list of ServiceWaterHeatingUse.ids
 
     Parameters
     ----------
@@ -20,19 +26,14 @@ def get_swh_bats_and_swh_use(rmd: dict) -> dict:
 
     swh_and_swh_use_dict = {}
     for building_segment in find_all("$.buildings[*].building_segments[*]", rmd):
-        swh_bat = get_building_segment_swh_bat(rmd, building_segment["id"])
+        bldg_seg_id = building_segment["id"]
+        swh_bat = get_building_segment_swh_bat(rmd, bldg_seg_id)
         swh_and_swh_use_dict.setdefault(swh_bat, [])
 
         service_water_heating_use_ids = (
-            get_swh_uses_associated_with_each_building_segment(
-                rmd, building_segment["id"]
-            )
+            get_swh_uses_associated_with_each_building_segment(rmd, bldg_seg_id)
         )
 
-        for swh_use_id in find_all(
-            f"$.zones[*].spaces[*].service_water_heating_uses[*].id",
-            building_segment,
-        ):
-            swh_and_swh_use_dict[swh_bat].append(swh_use_id)
+        swh_and_swh_use_dict[swh_bat].extend(service_water_heating_use_ids)
 
     return swh_and_swh_use_dict
