ad24bb8b284e5c2cd1ed3eb374f8ad858f2bf41f
gonz102 <juan.gonzalez@pnnl.gov>
2024-11-05 11:17:19 -0500

    Fixed section 22 raised message errors.

--- rct229/rulesets/ashrae9012019/section22/section22rule37.py
+++ rct229/rulesets/ashrae9012019/section22/section22rule37.py
@@ -23,11 +23,7 @@ class Section22Rule37(RuleDefinitionListIndexedBase):
             ruleset_section_title="HVAC - Chiller",
             standard_section="Section G3.1.3.10 Chilled-Water Pumps (Systems 7, 8, 11, 12, and 13)",
             is_primary_rule=False,
-            list_path="ruleset_model_descriptions[0]",
-            manual_check_required_msg="Manual Check Required - Baseline is modeled with purchased chilled water.  Make "
-            "sure baseline systems served by purchased chilled water are modeled with a variable speed drive pump.",
-            not_applicable_msg="Rule 22-37 Not Applicable - the baseline is not modeled with Purchased Chilled Water",
-        )
+            list_path="ruleset_model_descriptions[0]")
 
     class RulesetModelInstanceRule(PartialRuleDefinition):
         def __init__(self):
@@ -35,6 +31,10 @@ class Section22Rule37(RuleDefinitionListIndexedBase):
                 rmds_used=produce_ruleset_model_description(
                     USER=False, BASELINE_0=True, PROPOSED=False
                 ),
+                manual_check_required_msg="Manual Check Required - Baseline is modeled with purchased chilled water. Make "
+                "sure baseline systems served by purchased chilled water are modeled with a variable speed drive pump.",
+                not_applicable_msg="Rule 22-37 Not Applicable - the baseline is not modeled with Purchased Chilled Water",
+
             )
 
         def applicability_check(self, context, calc_vals, data):
