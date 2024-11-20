f0bb514f8960921ee9886ec4349076f3d774d622
Xie, Jiarong <jiarong.xie@pnnl.gov>
2024-11-20 17:53:47 -0500

    add 11-12 to init file

--- rct229/rulesets/ashrae9012019/section11/__init__.py
+++ rct229/rulesets/ashrae9012019/section11/__init__.py
@@ -15,7 +15,7 @@ __all__ = [
     # "section11rule11",
     # "section11rule12",
     "section11rule11",
-    # "section11rule12",
+    "section11rule12",
     "section11rule13",
     # "section11rule14",
     # "section11rule15",
