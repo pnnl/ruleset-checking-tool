8ef66eab17584398afa22157d25b7cfb229c5575
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-12-16 21:26:35 -0800

    Addressed PR comments

--- rct229/rulesets/ashrae9012019/section11/section11rule9.py
+++ rct229/rulesets/ashrae9012019/section11/section11rule9.py
@@ -1,12 +1,11 @@
 from rct229.rule_engine.partial_rule_definition import PartialRuleDefinition
-from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_description
 from rct229.rulesets.ashrae9012019 import BASELINE_0
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_swh_bats_and_swh_use import (
     get_swh_bats_and_swh_use,
 )
-from rct229.utils.utility_functions import find_exactly_one_service_water_heating_use
+from rct229.utils.jsonpath_utils import find_all
 
 
 class Section11Rule9(RuleDefinitionListIndexedBase):
@@ -15,7 +14,7 @@ class Section11Rule9(RuleDefinitionListIndexedBase):
     def __init__(self):
         super(Section11Rule9, self).__init__(
             rmds_used=produce_ruleset_model_description(
-                USER=False, BASELINE_0=True, PROPOSED=True
+                USER=False, BASELINE_0=True, PROPOSED=False
             ),
             each_rule=Section11Rule9.RMDRule(),
             index_rmd=BASELINE_0,
@@ -27,45 +26,88 @@ class Section11Rule9(RuleDefinitionListIndexedBase):
             list_path="ruleset_model_descriptions[0]",
         )
 
-    class RMDRule(PartialRuleDefinition):
+    class RMDRule(RuleDefinitionListIndexedBase):
         def __init__(self):
             super(Section11Rule9.RMDRule, self).__init__(
                 rmds_used=produce_ruleset_model_description(
-                    USER=True,
+                    USER=False,
                     BASELINE_0=True,
                     PROPOSED=False,
                 ),
+                index_rmd=BASELINE_0,
+                each_rule=Section11Rule9.RMDRule.SWHBATRule(),
             )
 
-        def applicability_check(self, context, calc_vals, data):
-            rmd_u = context.USER
+        def create_data(self, context, data):
             rmd_b = context.BASELINE_0
 
-            shw_bat_uses_dict_u = get_swh_bats_and_swh_use(rmd_u)
-
-            return not all(
-                find_exactly_one_service_water_heating_use(rmd_b, swh_use_id).get(
-                    "use", 0.0
+            service_water_heating_uses_dict_b = {
+                swh_use["id"]: swh_use.get("use", 0.0)
+                for swh_use in find_all(
+                    "$.buildings[*].building_segments[*].zones[*].spaces[*].service_water_heating_uses[*]",
+                    rmd_b,
                 )
-                > 0.0
-                for swh_bat_u in shw_bat_uses_dict_u
-                for swh_use_id in shw_bat_uses_dict_u[swh_bat_u]
-            )
+            }
+            shw_bat_uses_dict_b = get_swh_bats_and_swh_use(rmd_b)
 
-        def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
-            rmd_u = context.USER
-            rmd_b = context.BASELINE_0
+            return {
+                "service_water_heating_uses_dict_b": service_water_heating_uses_dict_b,
+                "shw_bat_uses_dict_b": shw_bat_uses_dict_b,
+            }
 
-            shw_bat_uses_dict_u = get_swh_bats_and_swh_use(rmd_u)
+        def create_context_list(self, context, data=None):
+            shw_bat_uses_dict_b = data["shw_bat_uses_dict_b"]
 
-            swh_bat = ", ".join(
-                swh_bat_u
-                for swh_bat_u in shw_bat_uses_dict_u
-                for swh_use_id in shw_bat_uses_dict_u[swh_bat_u]
-                if find_exactly_one_service_water_heating_use(rmd_b, swh_use_id).get(
-                    "use", 0.0
+            building_area_type_and_uses = {}
+            for (
+                bat_type,
+                SWH_use_id,
+            ) in shw_bat_uses_dict_b.items():
+                building_area_type_and_uses[bat_type] = {}
+                building_area_type_and_uses[bat_type]["id"] = SWH_use_id
+
+            return [
+                produce_ruleset_model_description(
+                    USER=False,
+                    BASELINE_0=building_area_type_and_uses[bat_type],
+                    PROPOSED=False,
                 )
-                == 0.0
-            )
+                for bat_type, SWH_use_id in shw_bat_uses_dict_b.items()
+            ]
 
-            return f"Check that the baseline Service Water Heating System for Building Area Type {swh_bat} is sized according to ASHRAE Standard 90.1 2019, Section 7.4.1."
+        class SWHBATRule(PartialRuleDefinition):
+            def __init__(self):
+                super(Section11Rule9.RMDRule.SWHBATRule, self).__init__(
+                    rmds_used=produce_ruleset_model_description(
+                        USER=False, BASELINE_0=True, PROPOSED=False
+                    ),
+                )
+
+            def applicability_check(self, context, calc_vals, data):
+                swh_BAT_id_list_b = context.BASELINE_0["id"]
+                service_water_heating_uses_dict_b = data[
+                    "service_water_heating_uses_dict_b"
+                ]
+
+                return all(
+                    [
+                        True
+                        if service_water_heating_uses_dict_b[swh_bat_id] > 0.0
+                        else False
+                        for swh_bat_id in swh_BAT_id_list_b
+                    ]
+                )
+
+            def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
+                swh_BAT_id_list_b = context.BASELINE_0["id"]
+                service_water_heating_uses_dict_b = data[
+                    "service_water_heating_uses_dict_b"
+                ]
+
+                swh_bat = ", ".join(
+                    swh_bat_id
+                    for swh_bat_id in swh_BAT_id_list_b
+                    if service_water_heating_uses_dict_b[swh_bat_id] > 0.0
+                )
+
+                return f"Check that the baseline Service Water Heating System for Building Area Type {swh_bat} is sized according to ASHRAE Standard 90.1 2019, Section 7.4.1."
