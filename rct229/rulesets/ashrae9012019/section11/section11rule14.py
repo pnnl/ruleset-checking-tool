49015ce61a4e3c855b3695e9057a284b96540531
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-12-18 10:45:29 -0800

    Addressed PR comments

--- rct229/rulesets/ashrae9012019/section11/section11rule14.py
+++ rct229/rulesets/ashrae9012019/section11/section11rule14.py
@@ -7,6 +7,8 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.get_swh_equipment_associate
 )
 from rct229.schema.config import ureg
 from rct229.utils.jsonpath_utils import find_all, find_one_with_field_value
+from rct229.utils.pint_utils import ZERO
+from rct229.utils.utility_functions import find_exactly_one_service_water_heating_use
 
 MIN_PUMP_POWER = 0.0 * ureg("Btu/hr")
 
@@ -51,21 +53,20 @@ class Section11Rule14(RuleDefinitionListIndexedBase):
                 get_swh_equipment_associated_with_each_swh_distribution_system(rmd_b)
             )
 
-            swh_use_loads_p = all(
+            swh_use_has_loads_p = all(
                 [
-                    True if swh_use_p > 0.0 else False
+                    find_exactly_one_service_water_heating_use(
+                        rmd_p, swh_dist_sys_id_b
+                    ).get("use", 0.0)
+                    > 0.0
                     for swh_dist_id_b in find_all(
                         "$.service_water_heating_distribution_systems[*].id", rmd_b
                     )
                     for swh_dist_sys_id_b in swh_comps_dict_b[swh_dist_id_b].uses
-                    for swh_use_p in find_all(
-                        f'$.buildings[*].building_segments[*].zones[*].spaces[*].service_water_heating_uses[*][?(@.id="{swh_dist_sys_id_b}")].use',
-                        rmd_p,
-                    )
                 ]
             )
 
-            return swh_use_loads_p
+            return swh_use_has_loads_p
 
         def create_data(self, context, data):
             rmd_b = context.BASELINE_0
@@ -99,8 +100,11 @@ class Section11Rule14(RuleDefinitionListIndexedBase):
                                 pump_id,
                                 rmd_b,
                             )
-                            piping_info_b[piping_id]["PUMP_POWER"] = pump.get(
-                                "design_electric_power"
+                            piping_info_b[piping_id].setdefault(
+                                "PUMP_POWER", ZERO.POWER
+                            )
+                            piping_info_b[piping_id]["PUMP_POWER"] += pump.get(
+                                "design_electric_power", ZERO.POWER
                             )
 
             return {
@@ -124,14 +128,14 @@ class Section11Rule14(RuleDefinitionListIndexedBase):
             def get_calc_vals(self, context, data=None):
                 piping_info_b = data["piping_info_b"]
 
-                return {"piping_info_id_b": piping_info_b}
+                return {"piping_info_b": piping_info_b}
 
             def rule_check(self, context, calc_vals=None, data=None):
-                piping_info_b = data["piping_info_b"]
+                piping_info_b = calc_vals["piping_info_b"]
 
                 return all(
-                    piping["IS_RECIRC"] for piping in piping_info_b.values()
-                ) and all(
-                    piping["PUMP_POWER"] > MIN_PUMP_POWER
-                    for piping in piping_info_b.values()
+                    [
+                        piping["IS_RECIRC"] and piping["PUMP_POWER"] > MIN_PUMP_POWER
+                        for piping in piping_info_b.values()
+                    ]
                 )
