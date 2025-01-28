81c261b0007e2336b3917a9b4f5cb919183965a8
Xie, Jiarong <jiarong.xie@pnnl.gov>
2025-01-27 22:06:23 -0500

    change is_airflow_sized_based_on_design_day to is_airflow_calculated

--- rct229/rulesets/ashrae9012019/ruleset_functions/are_all_hvac_sys_fan_objects_autosized.py
+++ rct229/rulesets/ashrae9012019/ruleset_functions/are_all_hvac_sys_fan_objects_autosized.py
@@ -32,7 +32,7 @@ def are_all_hvac_sys_fan_objs_autosized(rmd: dict, hvac_id: str) -> bool:
         all(
             [
                 getattr_(
-                    supply_fan, "supply_fan", "is_airflow_sized_based_on_design_day"
+                    supply_fan, "supply_fan", "is_airflow_calculated"
                 )
                 for supply_fan in find_all("$.fan_system.supply_fans[*]", hvac)
             ]
@@ -44,7 +44,7 @@ def are_all_hvac_sys_fan_objs_autosized(rmd: dict, hvac_id: str) -> bool:
                     find_exactly_one_terminal_unit(rmd, terminal_id),
                     "terminal",
                     "fan",
-                    "is_airflow_sized_based_on_design_day",
+                    "is_airflow_calculated",
                 )
                 for terminal_id in dict_of_zones_and_terminal_units_served_by_hvac_sys[
                     hvac_id
