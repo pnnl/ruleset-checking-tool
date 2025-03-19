67c8ce6d8f66afdc5e24f2a1848f6eaf83843db9
yunjoonjung <yunjoon.jung@pnnl.gov>
2025-03-19 09:25:17 -0700

    Updated logic

--- rct229/rulesets/ashrae9012019/section19/section19rule3.py
+++ rct229/rulesets/ashrae9012019/section19/section19rule3.py
@@ -12,7 +12,7 @@ class Section19Rule3(PartialRuleDefinition):
     def __init__(self):
         super(Section19Rule3, self).__init__(
             rmds_used=produce_ruleset_model_description(
-                USER=False, BASELINE_0=True, PROPOSED=False
+                USER=False, BASELINE_0=True, PROPOSED=True
             ),
             id="19-3",
             description="Weather conditions used in sizing runs to determine baseline equipment capacities shall be based either on design days developed "
@@ -27,7 +27,10 @@ class Section19Rule3(PartialRuleDefinition):
 
     def get_calc_vals(self, context, data=None):
         rpd_b = context.BASELINE_0
+        rpd_p = context.PROPOSED
+
         weather_b = rpd_b["ruleset_model_descriptions"][0]["weather"]
+        weather_p = rpd_p["ruleset_model_descriptions"][0]["weather"]
 
         return {
             "cooling_design_day_type_b": weather_b.get(
@@ -37,7 +40,16 @@ class Section19Rule3(PartialRuleDefinition):
                 "heating_dry_bulb_design_day_type"
             ),
             "evaporation_wet_bulb_design_day_type_b": weather_b.get(
-                "evaporation_wet_bulb_design_day_type_b"
+                "evaporation_wet_bulb_design_day_type"
+            ),
+            "cooling_design_day_type_p": weather_p.get(
+                "cooling_dry_bulb_design_day_type"
+            ),
+            "heating_design_day_type_p": weather_p.get(
+                "heating_dry_bulb_design_day_type"
+            ),
+            "evaporation_wet_bulb_design_day_type_p": weather_p.get(
+                "evaporation_wet_bulb_design_day_type"
             ),
         }
 
@@ -47,10 +59,39 @@ class Section19Rule3(PartialRuleDefinition):
         evaporation_wet_bulb_design_day_type_b = calc_vals[
             "evaporation_wet_bulb_design_day_type_b"
         ]
+        cooling_design_day_type_p = calc_vals["cooling_design_day_type_p"]
+        heating_design_day_type_p = calc_vals["heating_design_day_type_p"]
+        evaporation_wet_bulb_design_day_type_p = calc_vals[
+            "evaporation_wet_bulb_design_day_type_p"
+        ]
 
         return (
             cooling_design_day_type_b
             == evaporation_wet_bulb_design_day_type_b
             == COOLING_DESIGN_DAY.COOLING_1_0
             and heating_design_day_type_b == HEATING_DESIGN_DAY.HEATING_99_6
+        ) and (
+            cooling_design_day_type_p
+            == evaporation_wet_bulb_design_day_type_p
+            == COOLING_DESIGN_DAY.COOLING_1_0
+            and heating_design_day_type_p == HEATING_DESIGN_DAY.HEATING_99_6
         )
+
+    def get_fail_msg(self, context, calc_vals=None, data=None):
+        cooling_design_day_type_p = calc_vals["cooling_design_day_type_p"]
+        heating_design_day_type_p = calc_vals["heating_design_day_type_p"]
+        evaporation_wet_bulb_design_day_type_p = calc_vals[
+            "evaporation_wet_bulb_design_day_type_p"
+        ]
+
+        if not (
+            cooling_design_day_type_p
+            == evaporation_wet_bulb_design_day_type_p
+            == COOLING_DESIGN_DAY.COOLING_1_0
+            and heating_design_day_type_p == HEATING_DESIGN_DAY.HEATING_99_6
+        ):
+            FAIL_MSG = "Fail unless there are no yet to be designed HVAC systems in the proposed design."
+        else:
+            FAIL_MSG = ""
+
+        return FAIL_MSG
