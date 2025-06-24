4479bab3e03ab093d3a3cf09687b39a024388aa1
jugonzal07 <jugonzal07@gmail.com>
2025-06-24 11:31:16 -0400

    Updated rule test generation to allow us to freeze to a branch. Also, properly sorts rule tests within section tests.

--- rct229/ruletest_engine/ruletest_jsons/scripts/generate_rule_test_documentation.py
+++ rct229/ruletest_engine/ruletest_jsons/scripts/generate_rule_test_documentation.py
@@ -4,6 +4,7 @@ from rct229.ruletest_engine.ruletest_jsons.scripts.excel_generation_utilities im
 
 # INPUT:
 ruleset_standard = "ashrae9012019"
+test_json_branch = "RT/JG/schema_update_016_017"
 
 # Create rule test documentation for ruleset standard specified above
-create_rule_test_documentation_spreadsheet(ruleset_standard)
+create_rule_test_documentation_spreadsheet(ruleset_standard, test_json_branch)
