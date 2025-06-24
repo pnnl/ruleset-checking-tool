a57fdb02ebaff6bf1cc0fbae617dcd1e68df9f1f
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-06-24 13:25:52 -0400

    fix schedule length validation

--- rct229/schema/validate_test.py
+++ rct229/schema/validate_test.py
@@ -16,7 +16,7 @@ from rct229.schema.schema_store import SchemaStore, RuleSet
 
 SchemaStore.set_ruleset(RuleSet.ASHRAE9012019_RULESET)
 SchemaEnums.update_schema_enum()
-EXAMPLES_PATH = "examples"
+EXAMPLES_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "examples")
 
 ServiceWaterHeatingUseUnitOptions = SchemaEnums.schema_enums[
     "ServiceWaterHeatingUseUnitOptions"
@@ -274,5 +274,53 @@ def test__non_schema_validate_rpd__mismatched_associated_efficiency_lists_2():
         "passed": False,
         "error": [
             "'Cooling 1' lists at 'efficiency_metric_types' and 'efficiency_metric_values' are not the same length."
+        ],
+    }
+
+
+def test__non_schema_validate_rpd__unexpected_schedule_length():
+    test_rmd = deepcopy(TEST_MISMATCHED_LISTS_RMD)
+    test_rmd["ruleset_model_descriptions"][0]["schedules"] = [
+        {"id": "Schedule 1", "hourly_values": [0.5] * 8759}  # 1 less than 8760
+    ]
+
+    assert non_schema_validate_rpd(test_rmd) == {
+        "passed": False,
+        "error": [
+            "Annual hourly schedules are required to be either 8760 or 8784. The most common schedule length in the project is 8759."
+        ],
+    }
+
+
+def test__non_schema_validate_rpd__mismatched_schedule_length():
+    test_rmd = deepcopy(TEST_MISMATCHED_LISTS_RMD)
+    test_rmd["ruleset_model_descriptions"][0]["schedules"] = [
+        {"id": "Schedule 1", "hourly_values": [0.5] * 8760},
+        {"id": "Schedule 2", "hourly_values": [0.6] * 8760},
+        {"id": "Schedule 3", "hourly_values": [1] * 8784},
+    ]
+
+    assert non_schema_validate_rpd(test_rmd) == {
+        "passed": False,
+        "error": [
+            "Schedule 'Schedule 3' has 8784 hourly values; all annual schedule lengths are expected to match the common length (8760)."
+        ],
+    }
+
+
+def test__non_schema_validate_rpd__mismatched_schedule_length_2():
+    test_rmd = deepcopy(TEST_MISMATCHED_LISTS_RMD)
+    test_rmd["ruleset_model_descriptions"][0]["schedules"] = [
+        {"id": "Schedule 1", "hourly_values": [0.5] * 8760},
+        {"id": "Schedule 2", "hourly_values": [0.6] * 8760},
+        {"id": "Schedule 3", "hourly_values": [1] * 8784},
+        {"id": "Schedule 4", "hourly_values": [0] * 8784},
+    ]
+
+    assert non_schema_validate_rpd(test_rmd) == {
+        "passed": False,
+        "error": [
+            "Schedule 'Schedule 3' has 8784 hourly values; all annual schedule lengths are expected to match the common length (8760).",
+            "Schedule 'Schedule 4' has 8784 hourly values; all annual schedule lengths are expected to match the common length (8760).",
         ],
     }
