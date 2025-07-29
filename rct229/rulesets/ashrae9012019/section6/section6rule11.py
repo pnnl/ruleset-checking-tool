f8bc7ab78d69c9acb5609c8f1e90600950c2fd0a
Xie, Jiarong <jiarong.xie@pnnl.gov>
2025-07-29 12:53:35 -0400

    initial dev

---
+++ rct229/rulesets/ashrae9012019/section6/section6rule11.py
@@ -0,0 +1,103 @@
+from rct229.rule_engine.rule_base import RuleDefinitionBase
+from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
+from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_description
+from rct229.rulesets.ashrae9012019 import PROPOSED
+from rct229.schema.schema_enums import SchemaEnums
+from rct229.utils.jsonpath_utils import find_all
+
+SALES_AREA = SchemaEnums.schema_enums[
+    "LightingSpaceOptions2019ASHRAE901TG37"
+].SALES_AREA
+
+LIGHTING_PURPOSE_TYPE = SchemaEnums.schema_enums[
+    "LightingPurposeOptions2019ASHRAE901"
+].RETAIL_DISPLAY
+
+MANUAL_CHECK_REQUIRED_MSG = (
+    "It could not be determined whether the proposed retail display lighting power is less "
+    "than or equal to the allowance calculated according to the formulas in ASHRAE 90.1 "
+    "Section 9.5.2.2(b)."
+)
+
+
+class PRM9012019Rule22c86(RuleDefinitionListIndexedBase):
+    """Rule 11 of ASHRAE 90.1-2019 Appendix G Section 6 (Lighting)"""
+
+    def __init__(self):
+        super(PRM9012019Rule22c86, self).__init__(
+            rmds_used=produce_ruleset_model_description(
+                USER=False, BASELINE_0=False, PROPOSED=True
+            ),
+            each_rule=PRM9012019Rule22c86.SpaceRule(),
+            index_rmd=PROPOSED,
+            id="6-11",
+            description="Where retail display lighting is included in the proposed building design the display lighting"
+            "additional power shall be less than or equal to the limits established by Section 9.5.2.2(b) ",
+            ruleset_section_title="Lighting",
+            standard_section="G1.2.1b.1 and the methodology described in Table 9.5.2.2(b)",
+            is_primary_rule=True,
+            list_path="$.buildings[*].building_segments[*].zones[*].spaces[*]",
+            rmd_context="ruleset_model_descriptions/0",
+        )
+
+    def is_applicable(self, context, data=None):
+        rmd_p = context.PROPOSED
+        return any(
+            space_p.get("lighting_space_type") is not None
+            for space_p in find_all(
+                "$.buildings[*].building_segments[*].zones[*].spaces[*]", rmd_p
+            )
+        )
+
+    class SpaceRule(RuleDefinitionBase):
+        def __init__(self):
+            super(PRM9012019Rule22c86.SpaceRule, self).__init__(
+                rmds_used=produce_ruleset_model_description(
+                    USER=False, BASELINE_0=False, PROPOSED=True
+                ),
+                required_fields={
+                    "$": ["floor_area", "interior_lighting"],
+                    "interior_lighting[*]": ["purpose_type", "power_per_area"],
+                },
+                precision={
+                    "minimum_retail_display_W": {
+                        "precision": 1,
+                        "unit": "W/",
+                    },
+                    "maximum_retail_display_W": {
+                        "precision": 1,
+                        "unit": "W/",
+                    },
+                },
+                manual_check_required_msg=MANUAL_CHECK_REQUIRED_MSG,
+            )
+
+        def get_calc_vals(self, context, data=None):
+            space_p = context.PROPOSED
+            maximum_retail_display_W = 750 + space_p["floor_area"]
+            minimum_retail_display_W = 750
+            proposed_interior_display_W = sum(
+                space_p["power_per_area"] * space_p["floor_area"]
+                for interior_lighting_p in space_p["interior_lighting"]
+                if interior_lighting_p["purpose_type"] == LIGHTING_PURPOSE_TYPE
+            )
+            return {
+                "maximum_retail_display_W": maximum_retail_display_W,
+                "minimum_retail_display_W": minimum_retail_display_W,
+                "proposed_interior_display_W": proposed_interior_display_W,
+            }
+
+        def manual_check_required(self, context, calc_vals=None, data=None):
+            maximum_retail_display_W = calc_vals["maximum_retail_display_W"]
+            minimum_retail_display_W = calc_vals["minimum_retail_display_W"]
+            proposed_interior_display_W = calc_vals["proposed_interior_display_W"]
+            return (
+                minimum_retail_display_W
+                <= proposed_interior_display_W
+                <= maximum_retail_display_W
+            )
+
+        def rule_check(self, context, calc_vals=None, data=None):
+            minimum_retail_display_W = calc_vals["minimum_retail_display_W"]
+            proposed_interior_display_W = calc_vals["proposed_interior_display_W"]
+            return proposed_interior_display_W < minimum_retail_display_W
