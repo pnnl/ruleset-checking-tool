d5c316abadd7f474cc88bc15fd3c6ddb7aba44c6
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-08-14 10:05:39 -0400

    revert engine.py

--- rct229/rule_engine/engine.py
+++ rct229/rule_engine/engine.py
@@ -237,8 +237,6 @@ def evaluate_rules(
         outcomes.append(outcome)
     else:
         for rule in rules_list:
-            if rule.id not in ["1-7", "1-8", "1-9"]:
-                continue
             print(f"Processing Rule {rule.id}")
             outcome = rule.evaluate(copied_rmds)
             outcomes.append(outcome)
