03adb6d58b6880a61df4d8fa5db3284723ef16a5
yunjoonjung <yunjoon.jung@pnnl.gov>
2025-09-04 15:18:53 -0700

    Corrected the logic

--- rct229/rulesets/ashrae9012022/section6/section6rule11.py
+++ rct229/rulesets/ashrae9012022/section6/section6rule11.py
@@ -2,16 +2,12 @@ from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_description
 from rct229.rulesets.ashrae9012022 import PROPOSED
+from rct229.schema.config import ureg
 from rct229.schema.schema_enums import SchemaEnums
 from rct229.utils.jsonpath_utils import find_all
 
-SALES_AREA = SchemaEnums.schema_enums[
-    "LightingSpaceOptions2019ASHRAE901TG37"
-].SALES_AREA
-
-LIGHTING_PURPOSE_TYPE = SchemaEnums.schema_enums[
-    "LightingPurposeOptions2019ASHRAE901"
-].RETAIL_DISPLAY
+LIGHTING_SPACE = SchemaEnums.schema_enums["LightingSpaceOptions2019ASHRAE901TG37"]
+LIGHTING_PURPOSE = SchemaEnums.schema_enums["LightingPurposeOptions2019ASHRAE901"]
 
 MANUAL_CHECK_REQUIRED_MSG = (
     "It could not be determined whether the proposed retail display lighting power is less "
@@ -44,11 +40,11 @@ class PRM9012022Rule12d80(RuleDefinitionListIndexedBase):
         rmd_p = context.PROPOSED
 
         return any(
-            interior_lighting_p.get("purpose_type") == LIGHTING_PURPOSE_TYPE
+            interior_lighting_p.get("purpose_type") == LIGHTING_PURPOSE.RETAIL_DISPLAY
             for space_p in find_all(
                 "$.buildings[*].building_segments[*].zones[*].spaces[*]", rmd_p
             )
-            if space_p.get("lighting_space_type") == SALES_AREA
+            if space_p.get("lighting_space_type") == LIGHTING_SPACE.SALES_AREA
             for interior_lighting_p in space_p.get("interior_lighting", [])
         )
 
@@ -77,12 +73,23 @@ class PRM9012022Rule12d80(RuleDefinitionListIndexedBase):
 
         def get_calc_vals(self, context, data=None):
             space_p = context.PROPOSED
-            maximum_retail_display_W = 750 + space_p["floor_area"]
-            minimum_retail_display_W = 750
+
+            # 9.5.2.2(b) gives a formula
+            # 750 W + (Retail Area 1 × 0.40 W/ft2) + (Retail Area 2 × 0.40 W/ft2) + (Retail Area 3 × 0.70 W/ft2) + (Retail Area 4 × 1.00 W/ft2)
+            # for retail display lighting that is based on four area categories.
+            # We don't have access to these four area categories in the schema, so we will calculate the maximum and minimum values possible based on this function.
+            # The maximum is calculated based on 100% of the space floor area being type 4: `maximum_retail_display_W = 750 + space.floor_area * (1)
+            maximum_retail_display_W = 750 * ureg("W") + space_p["floor_area"].to(
+                "ft2"
+            ) * 1.0 * ureg("W/ft2")
+            minimum_retail_display_W = 750 * ureg("W")
             proposed_interior_display_W = sum(
-                space_p["power_per_area"] * space_p["floor_area"]
-                for interior_lighting_p in space_p["interior_lighting"]
-                if interior_lighting_p["purpose_type"] == LIGHTING_PURPOSE_TYPE
+                [
+                    interior_lighting_p["power_per_area"] * space_p["floor_area"]
+                    for interior_lighting_p in space_p["interior_lighting"]
+                    if interior_lighting_p["purpose_type"]
+                    == LIGHTING_PURPOSE.RETAIL_DISPLAY
+                ]
             )
             return {
                 "maximum_retail_display_W": maximum_retail_display_W,
@@ -94,6 +101,7 @@ class PRM9012022Rule12d80(RuleDefinitionListIndexedBase):
             maximum_retail_display_W = calc_vals["maximum_retail_display_W"]
             minimum_retail_display_W = calc_vals["minimum_retail_display_W"]
             proposed_interior_display_W = calc_vals["proposed_interior_display_W"]
+
             return (
                 minimum_retail_display_W < proposed_interior_display_W
                 or self.precision_comparison["minimum_retail_display_W"](
@@ -109,4 +117,5 @@ class PRM9012022Rule12d80(RuleDefinitionListIndexedBase):
         def rule_check(self, context, calc_vals=None, data=None):
             minimum_retail_display_W = calc_vals["minimum_retail_display_W"]
             proposed_interior_display_W = calc_vals["proposed_interior_display_W"]
+
             return proposed_interior_display_W < minimum_retail_display_W
