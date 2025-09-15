f8283146398be7cbb7b8cf691ff514aa254d56a2
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-09-15 12:55:56 -0400

    always show res and nonres values in calc_vals

--- rct229/rulesets/ashrae9012019/section5/section5rule8.py
+++ rct229/rulesets/ashrae9012019/section5/section5rule8.py
@@ -118,8 +118,12 @@ class PRM9012019Rule48v87(RuleDefinitionListIndexedBase):
                 )
 
                 target_u_factor = None
-                target_u_factor_res = None
-                target_u_factor_nonres = None
+                target_u_factor_res = table_G34_lookup(
+                    climate_zone, SCC.EXTERIOR_RESIDENTIAL, OST.ABOVE_GRADE_WALL
+                )["u_value"]
+                target_u_factor_nonres = table_G34_lookup(
+                    climate_zone, SCC.EXTERIOR_NON_RESIDENTIAL, OST.ABOVE_GRADE_WALL
+                )["u_value"]
 
                 if scc in [
                     SCC.EXTERIOR_RESIDENTIAL,
@@ -130,12 +134,6 @@ class PRM9012019Rule48v87(RuleDefinitionListIndexedBase):
                         climate_zone, scc, OST.ABOVE_GRADE_WALL
                     )["u_value"]
                 elif scc == SCC.EXTERIOR_MIXED:
-                    target_u_factor_res = table_G34_lookup(
-                        climate_zone, SCC.EXTERIOR_RESIDENTIAL, OST.ABOVE_GRADE_WALL
-                    )["u_value"]
-                    target_u_factor_nonres = table_G34_lookup(
-                        climate_zone, SCC.EXTERIOR_NON_RESIDENTIAL, OST.ABOVE_GRADE_WALL
-                    )["u_value"]
                     if target_u_factor_res == target_u_factor_nonres:
                         target_u_factor = target_u_factor_res
 
