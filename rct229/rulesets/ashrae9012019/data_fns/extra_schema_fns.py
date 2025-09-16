241c2e8597bfff8906548daec39126aacca23443
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-09-16 13:30:28 -0400

    improve compare context messages

--- rct229/rulesets/ashrae9012019/data_fns/extra_schema_fns.py
+++ rct229/rulesets/ashrae9012019/data_fns/extra_schema_fns.py
@@ -97,8 +97,8 @@ def compare_context_pair(
 
     """
     matched = True
-    index_context_str = search_key.split("Equals")[0].strip().replace("AppG ", "").replace("B_RMD", "Baseline").replace("P_RMD", "Proposed").replace("U_RMD", "User")
-    compare_context_str = search_key.split("Equals")[-1].strip().replace("AppG ", "").replace("B_RMD", "Baseline").replace("P_RMD", "Proposed").replace("U_RMD", "User")
+    index_context_str = index_context.replace("_", "-").title()
+    compare_context_str = index_context.replace("_", "-").title()
     if (
         isinstance(index_context, dict)
         and isinstance(compare_context, dict)
