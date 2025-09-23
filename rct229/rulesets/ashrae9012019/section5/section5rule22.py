72800dc70b6a196278d1acf1ccfcb21fbfb64a0f
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-09-23 11:01:02 -0400

    more calc val units

--- rct229/rulesets/ashrae9012019/section5/section5rule22.py
+++ rct229/rulesets/ashrae9012019/section5/section5rule22.py
@@ -14,7 +14,7 @@ from rct229.rulesets.ashrae9012019.ruleset_functions.get_surface_conditioning_ca
 from rct229.rulesets.ashrae9012019.ruleset_functions.get_surface_conditioning_category_dict import (
     get_surface_conditioning_category_dict,
 )
-from rct229.utils.pint_utils import ZERO
+from rct229.utils.pint_utils import ZERO, CalcQ
 
 FAIL_MSG = "Baseline fenestration was modeled with shading projections and/or overhangs, which is incorrect."
 
@@ -111,7 +111,9 @@ class PRM9012019Rule50p59(RuleDefinitionListIndexedBase):
                     return {
                         "has_shading_overhang": subsurface_b["has_shading_overhang"],
                         "has_shading_sidefins": subsurface_b["has_shading_sidefins"],
-                        "depth_of_overhang": subsurface_b.get("depth_of_overhang"),
+                        "depth_of_overhang": CalcQ(
+                            "length", subsurface_b.get("depth_of_overhang")
+                        ),
                     }
 
                 def rule_check(self, context, calc_vals=None, data=None):
