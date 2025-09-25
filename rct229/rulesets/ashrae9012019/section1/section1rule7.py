18986083e6ef4e88f96bc3e2d67b1bea7e8494ca
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-09-25 13:27:28 -0400

    separate changes to compare context pair

--- rct229/rulesets/ashrae9012019/section1/section1rule7.py
+++ rct229/rulesets/ashrae9012019/section1/section1rule7.py
@@ -27,8 +27,8 @@ class PRM9012019Rule37e66(RuleDefinitionBase):
         error_msg_list = []
 
         comparison_result = baseline_equals_proposed(
-            index_context=proposed,
-            compare_context=baseline,
+            index_context=baseline,
+            compare_context=proposed,
             error_msg_list=error_msg_list,
         )
 
