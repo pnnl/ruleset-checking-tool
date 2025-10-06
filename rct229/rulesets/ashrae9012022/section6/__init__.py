4c39f73429753bd6f47c99e143ddebb3f168849f
yunjoonjung <yunjoon.jung@pnnl.gov>
2025-10-06 15:12:21 -0700

    Fixed file name in init

--- rct229/rulesets/ashrae9012022/section6/__init__.py
+++ rct229/rulesets/ashrae9012022/section6/__init__.py
@@ -2,8 +2,8 @@
 import importlib
 
 __all__ = [
-    "section6rule11.py",
-    "section6rule12.py",
+    "section6rule11",
+    "section6rule12",
 ]
 
 
--- rct229/ruletest_engine/ruletest_jsons/ashrae9012022/__init__.py
+++ rct229/ruletest_engine/ruletest_jsons/ashrae9012022/__init__.py
@@ -4,4 +4,5 @@ LIGHTING_DIR = "LTG"
 
 section_rule_to_rule_id = {
     "6-11": f"{LIGHTING_DIR}-11",
+    "6-12": f"{LIGHTING_DIR}-12",
 }
