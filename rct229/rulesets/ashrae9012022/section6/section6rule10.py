10abef8541638e6c064481d6edfc9c9119dab159
yunjoonjung <yunjoon.jung@pnnl.gov>
2025-10-07 11:08:55 -0700

    6-10 initial development

--- rct229/rulesets/ashrae9012022/section6/section6rule10.py
+++ rct229/rulesets/ashrae9012022/section6/section6rule10.py
@@ -2,120 +2,164 @@ from rct229.rule_engine.rule_base import RuleDefinitionBase
 from rct229.rule_engine.rule_list_indexed_base import RuleDefinitionListIndexedBase
 from rct229.rule_engine.ruleset_model_factory import produce_ruleset_model_description
 from rct229.rulesets.ashrae9012022 import PROPOSED
-from rct229.schema.config import ureg
 from rct229.schema.schema_enums import SchemaEnums
-from rct229.utils.jsonpath_utils import find_all
+from rct229.utils.jsonpath_utils import find_all, find_exactly_one_with_field_value
+from rct229.utils.utility_functions import find_exactly_one_schedule
 
-LIGHTING_SPACE = SchemaEnums.schema_enums["LightingSpaceOptions2019ASHRAE901TG37"]
-LIGHTING_PURPOSE = SchemaEnums.schema_enums["LightingPurposeOptions2019ASHRAE901"]
-
-MANUAL_CHECK_REQUIRED_MSG = (
-    "It could not be determined whether the proposed retail display lighting power is less "
-    "than or equal to the allowance calculated according to the formulas in ASHRAE 90.1 "
-    "Section 9.5.2.2(b)."
-)
+SPACE_FUNCTION = SchemaEnums.schema_enums["SpaceFunctionOptions"]
 
 
-class PRM9012022Rule12d80(RuleDefinitionListIndexedBase):
-    """Rule 11 of ASHRAE 90.1-2022 Appendix G Section 6 (Lighting)"""
+class PRM9012022rule12d80(RuleDefinitionListIndexedBase):
+    """Rule 10 of ASHRAE 90.1-2022 Appendix G Section 6 (Lighting)"""
 
     def __init__(self):
-        super(PRM9012022Rule12d80, self).__init__(
+        super(PRM9012022rule12d80, self).__init__(
             rmds_used=produce_ruleset_model_description(
-                USER=False, BASELINE_0=False, PROPOSED=True
+                USER=False, BASELINE_0=True, PROPOSED=True
             ),
-            each_rule=PRM9012022Rule12d80.SpaceRule(),
+            each_rule=PRM9012022rule12d80.SpaceRule(),
             index_rmd=PROPOSED,
-            id="6-11",
-            description="Where retail display lighting is included in the proposed building design the display lighting"
-            "additional power shall be less than or equal to the limits established by Section 9.5.2.2(b) ",
+            id="6-10",
+            description="Lighting and miscellaneous equipment loads and schedules are modeled identically across the baseline and proposed for plenums, crawl spaces, and interstitial spaces.",
             ruleset_section_title="Lighting",
-            standard_section="G1.2.1b.1 and the methodology described in Table 9.5.2.2(b)",
+            standard_section="able G3.1 #1 Baseline Column",
             is_primary_rule=True,
             list_path="$.buildings[*].building_segments[*].zones[*].spaces[*]",
             rmd_context="ruleset_model_descriptions/0",
         )
 
-    def is_applicable(self, context, data=None):
+    def create_data(self, context, data):
+        rmd_b = context.BASELINE_0
         rmd_p = context.PROPOSED
 
-        return any(
-            interior_lighting_p.get("purpose_type") == LIGHTING_PURPOSE.RETAIL_DISPLAY
-            for space_p in find_all(
-                "$.buildings[*].building_segments[*].zones[*].spaces[*]", rmd_p
+        ltg_schedule_b = {
+            int_ltg_sch_id_b: find_exactly_one_schedule(rmd_b, int_ltg_sch_id_b)
+            for int_ltg_sch_id_b in find_all(
+                "$.buildings[*].building_segments[*].zones[*].spaces[*].interior_lighting[*].ltg_multiplier_schedule",
+                rmd_b,
             )
-            if space_p.get("lighting_space_type") == LIGHTING_SPACE.SALES_AREA
-            for interior_lighting_p in space_p.get("interior_lighting", [])
-        )
+        }
+        ltg_schedule_p = {
+            int_ltg_sch_id_p: find_exactly_one_schedule(rmd_b, int_ltg_sch_id_p)
+            for int_ltg_sch_id_p in find_all(
+                "$.buildings[*].building_segments[*].zones[*].spaces[*].interior_lighting[*].ltg_multiplier_schedule",
+                rmd_p,
+            )
+        }
+
+        equip_schedule_b = {
+            equip_ltg_sch_id_b: find_exactly_one_schedule(rmd_b, equip_ltg_sch_id_b)
+            for equip_ltg_sch_id_b in find_all(
+                "$.buildings[*].building_segments[*].zones[*].spaces[*].miscellaneous_equipment[*].multiplier_schedule",
+                rmd_b,
+            )
+        }
+        equip_schedule_p = {
+            equip_ltg_sch_id_p: find_exactly_one_schedule(rmd_b, equip_ltg_sch_id_p)
+            for equip_ltg_sch_id_p in find_all(
+                "$.buildings[*].building_segments[*].zones[*].spaces[*].miscellaneous_equipment[*].multiplier_schedule",
+                rmd_p,
+            )
+        }
+
+        return {
+            "ltg_schedule_b": ltg_schedule_b,
+            "ltg_schedule_p": ltg_schedule_p,
+            "equip_schedule_b": equip_schedule_b,
+            "equip_schedule_p": equip_schedule_p,
+        }
 
     class SpaceRule(RuleDefinitionBase):
         def __init__(self):
-            super(PRM9012022Rule12d80.SpaceRule, self).__init__(
+            super(PRM9012022rule12d80.SpaceRule, self).__init__(
                 rmds_used=produce_ruleset_model_description(
-                    USER=False, BASELINE_0=False, PROPOSED=True
+                    USER=False, BASELINE_0=True, PROPOSED=True
                 ),
                 required_fields={
-                    "$": ["floor_area", "interior_lighting"],
-                    "interior_lighting[*]": ["purpose_type", "power_per_area"],
+                    "$": ["function"],
                 },
-                precision={
-                    "minimum_retail_display_W": {
-                        "precision": 1,
-                        "unit": "W",
-                    },
-                    "maximum_retail_display_W": {
-                        "precision": 1,
-                        "unit": "W",
-                    },
-                },
-                manual_check_required_msg=MANUAL_CHECK_REQUIRED_MSG,
+            )
+
+        def is_applicable(self, context, data=None):
+            space_p = context.PROPOSED
+
+            return space_p["function"] not in (
+                SPACE_FUNCTION.PLENUM,
+                SPACE_FUNCTION.CRAWL_SPACE,
+                SPACE_FUNCTION.INTERSTITIAL_SPACE,
             )
 
         def get_calc_vals(self, context, data=None):
+            space_b = context.BASELINE_0
             space_p = context.PROPOSED
 
-            # 9.5.2.2(b) gives a formula
-            # 750 W + (Retail Area 1 × 0.40 W/ft2) + (Retail Area 2 × 0.40 W/ft2) + (Retail Area 3 × 0.70 W/ft2) + (Retail Area 4 × 1.00 W/ft2)
-            # for retail display lighting that is based on four area categories.
-            # We don't have access to these four area categories in the schema, so we will calculate the maximum and minimum values possible based on this function.
-            # The maximum is calculated based on 100% of the space floor area being type 4: `maximum_retail_display_W = 750 + space.floor_area * (1)
-            maximum_retail_display_W = 750 * ureg("W") + space_p["floor_area"].to(
-                "ft2"
-            ) * 1.0 * ureg("W/ft2")
-            minimum_retail_display_W = 750 * ureg("W")
-            proposed_interior_display_W = sum(
-                [
-                    interior_lighting_p["power_per_area"] * space_p["floor_area"]
-                    for interior_lighting_p in space_p["interior_lighting"]
-                    if interior_lighting_p["purpose_type"]
-                    == LIGHTING_PURPOSE.RETAIL_DISPLAY
-                ]
-            )
+            ltg_schedule_b = data["ltg_schedule_b"]
+            ltg_schedule_p = data["ltg_schedule_p"]
+            equip_schedule_b = data["equip_schedule_b"]
+            equip_schedule_p = data["equip_schedule_p"]
+
+            int_ltg_mismatch = False
+            ltg_purpose_type_match = False
+            ltg_power_per_area_match = False
+            for int_ltg_p in space_p.get("interior_lighting", []):
+                int_ltg_b = find_exactly_one_with_field_value(
+                    "$.interior_lighting[*]", "id", int_ltg_p["id"], space_b
+                )
+                if (
+                    int_ltg_b.get("purpose_type") != int_ltg_b.get("purpose_type")
+                    or int_ltg_b.get("power_per_area")
+                    != int_ltg_p.get("power_per_area")
+                    or int_ltg_b.get("occupancy_control_type")
+                    != int_ltg_p.get("occupancy_control_type")
+                    or int_ltg_b.get("dayltg_control_type")
+                    != int_ltg_p.get("dayltg_control_type")
+                    or ltg_schedule_b[int_ltg_b.get("ltg_multiplier_schedule")]
+                    != ltg_schedule_p[int_ltg_p.get("ltg_multiplier_schedule")]
+                ):
+                    int_ltg_mismatch = True
+
+            mis_equip_mismatch = False
+            for mis_equip_p in space_p.get("miscellaneous_equipment", []):
+                mis_equip_b = find_exactly_one_with_field_value(
+                    "$.miscellaneous_equipment[*]", "id", mis_equip_p["id"], space_b
+                )
+                if (
+                    mis_equip_b.get("energy_type") != mis_equip_p.get("energy_type")
+                    or mis_equip_b.get("power") != mis_equip_p.get("power")
+                    or mis_equip_b.get("sensible_fraction")
+                    != mis_equip_p.get("sensible_fraction")
+                    or mis_equip_b.get("latent_fraction")
+                    != mis_equip_p.get("latent_fraction")
+                    or mis_equip_b.get("remaining_fraction_to_loop")
+                    != mis_equip_p.get("remaining_fraction_to_loop")
+                    or mis_equip_b.get("energy_from_loop")
+                    != mis_equip_p.get("energy_from_loop")
+                    or mis_equip_b.get("type") != mis_equip_p.get("type")
+                    or mis_equip_b.get("automatic_controlled_percentage")
+                    != mis_equip_p.get("automatic_controlled_percentage")
+                    or equip_schedule_b[mis_equip_b.get("multiplier_schedule")]
+                    != equip_schedule_p[mis_equip_p.get("multiplier_schedule")]
+                ):
+                    mis_equip_mismatch = True
+
             return {
-                "maximum_retail_display_W": maximum_retail_display_W,
-                "minimum_retail_display_W": minimum_retail_display_W,
-                "proposed_interior_display_W": proposed_interior_display_W,
+                "int_ltg_mismatch": int_ltg_mismatch,
+                "mis_equip_mismatch": mis_equip_mismatch,
             }
 
-        def manual_check_required(self, context, calc_vals=None, data=None):
-            maximum_retail_display_W = calc_vals["maximum_retail_display_W"]
-            minimum_retail_display_W = calc_vals["minimum_retail_display_W"]
-            proposed_interior_display_W = calc_vals["proposed_interior_display_W"]
+        def rule_check(self, context, calc_vals=None, data=None):
+            int_ltg_mismatch = calc_vals["int_ltg_mismatch"]
+            mis_equip_mismatch = calc_vals["mis_equip_mismatch"]
 
-            return (
-                minimum_retail_display_W < proposed_interior_display_W
-                or self.precision_comparison["minimum_retail_display_W"](
-                    proposed_interior_display_W, minimum_retail_display_W
-                )
-            ) and (
-                proposed_interior_display_W < maximum_retail_display_W
-                or self.precision_comparison["maximum_retail_display_W"](
-                    proposed_interior_display_W, maximum_retail_display_W
-                )
+            return not (int_ltg_mismatch and mis_equip_mismatch)
+
+        def fail_validation(self, context, calc_vals=None, data=None):
+            space_b = context.BASELINE_0
+            space_p = context.PROPOSED
+
+            fail_msg = (
+                f"It is expected that lighting and miscelleneous equipment would be modeled identically "
+                f"across the baseline and proposed for plenums, crawl spaces, and interstitial spaces. A mismatch was observed for {space_p}"
             )
 
-        def rule_check(self, context, calc_vals=None, data=None):
-            minimum_retail_display_W = calc_vals["minimum_retail_display_W"]
-            proposed_interior_display_W = calc_vals["proposed_interior_display_W"]
-
-            return proposed_interior_display_W < minimum_retail_display_W
+            return fail_msg
