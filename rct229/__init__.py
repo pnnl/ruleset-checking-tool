516495585583bd1c111710ceaf96f8bf87bc2407
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-10-23 12:55:46 -0400

    auto register rulesets

--- rct229/__init__.py
+++ rct229/__init__.py
@@ -1,11 +1,3 @@
+import rct229.rulesets
+
 __version__ = "0.4.1"
-
-
-def _initialize_rulesets():
-    from rct229.rulesets import discover_ruleset_plugins, register_rulesets
-
-    discover_ruleset_plugins()
-    register_rulesets()
-
-
-_initialize_rulesets()
