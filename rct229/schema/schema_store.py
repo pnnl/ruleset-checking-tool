bd2a650b5213151857aa6ce8ce1a27dd801f205f
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-10-23 11:36:16 -0400

    attempt CI import order fix

--- rct229/schema/schema_store.py
+++ rct229/schema/schema_store.py
@@ -1,3 +1,4 @@
+from rct229.rulesets import register_rulesets, discover_ruleset_plugins
 from rct229.rule_engine.rulesets import RuleSet
 
 
@@ -33,14 +34,24 @@ class SchemaStore:
     @staticmethod
     def get_enum_schema_by_ruleset(ruleset: str):
 
+        if not hasattr(RuleSet, "ASHRAE9012019_RULESET"):
+            discover_ruleset_plugins()
+            register_rulesets()
+
         match ruleset:
             case RuleSet.ASHRAE9012019_RULESET:
-                return SchemaStore.SCHEMA_9012019_ENUM_KEY
+                return "ASHRAE9012019_enum.schema.json"
             case RuleSet.ASHRAE9012022_RULESET:
-                return SchemaStore.SCHEMA_9012022_ENUM_KEY
+                return "ASHRAE9012022_enum.schema.json"
+            case _:
+                raise ValueError(f"Unknown ruleset: {ruleset}")
 
     @staticmethod
     def get_output_schema_by_ruleset(ruleset: str):
+
+        if not any(attr.endswith("_RULESET") for attr in vars(RuleSet)):
+            discover_ruleset_plugins()
+            register_rulesets()
 
         match ruleset:
             case RuleSet.ASHRAE9012019_RULESET:
