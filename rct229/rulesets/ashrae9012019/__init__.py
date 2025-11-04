5b6e0ca418aac4be032982e73f97e46e25bb14f0
yunjoonjung <yunjoon.jung@pnnl.gov>
2025-11-04 10:46:20 -0800

    Added 11-5 2018 rule

--- rct229/rulesets/ashrae9012019/__init__.py
+++ rct229/rulesets/ashrae9012019/__init__.py
@@ -92,6 +92,7 @@ rules_dict = {
     "prm9012019rule10p28": "section10rule14",
     "prm9012019rule93u32": "section10rule15",
     "prm9012019rule72v93": "section11rule1",
+    "prm9012022rule03z22": "section11rule5",
     "prm9012019rule29n09": "section11rule6",
     "prm9012019rule49y39": "section11rule7",
     "prm9012019rule40i48": "section11rule8",
