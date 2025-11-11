9de67df985021d17cfad30202a403e4f642bbaf6
yunjoonjung <yunjoon.jung@pnnl.gov>
2025-11-11 09:43:27 -0800

    Removed section rule dictionary

--- rct229/ruletest_engine/ruletest_jsons/ashrae9012022/__init__.py
+++ rct229/ruletest_engine/ruletest_jsons/ashrae9012022/__init__.py
@@ -1,11 +1,5 @@
 from pathlib import Path
 
-section_rule_to_rule_id = {
-    "6-11": f"{LIGHTING_DIR}-11",
-    "6-12": f"{LIGHTING_DIR}-12",
-    "21-19": f"{HVAC_HOT_WATER_DIR}-19",
-}
-
 # Identify this ruleset’s root directory
 ROOT_DIR = Path(__file__).parent
 
