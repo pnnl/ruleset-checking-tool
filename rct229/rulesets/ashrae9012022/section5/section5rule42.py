c118cbf7adde6d10d598a878bd9c4de8a80109fc
weilixu <weili.xu@pnnl.gov>
2025-12-02 09:21:03 -0800

    reformatt

--- rct229/rulesets/ashrae9012022/section5/section5rule42.py
+++ rct229/rulesets/ashrae9012022/section5/section5rule42.py
@@ -81,10 +81,7 @@ class PRM9012019Rule82e93(RuleDefinitionListIndexedBase):
 
         class SurfaceRule(PartialRuleDefinition):
             def __init__(self):
-                super(
-                    PRM9012019Rule82e93.BuildingRule.SurfaceRule,
-                    self,
-                ).__init__(
+                super(PRM9012019Rule82e93.BuildingRule.SurfaceRule, self,).__init__(
                     rmds_used=produce_ruleset_model_description(
                         USER=False, BASELINE_0=False, PROPOSED=True
                     ),
