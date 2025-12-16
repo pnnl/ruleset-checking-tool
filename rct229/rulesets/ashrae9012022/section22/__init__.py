191e514f3270da7afe698a785bf428f71b12f6bf
yunjoonjung <yunjoon.jung@pnnl.gov>
2025-12-16 13:01:27 -0800

    Fixed conflicts

--- rct229/rulesets/ashrae9012022/section22/__init__.py
+++ rct229/rulesets/ashrae9012022/section22/__init__.py
@@ -2,18 +2,13 @@
 import importlib
 
 __all__ = [
-<<<<<<< RS/YJ/Rule_22-44-2022
+    "section22rule42",
+    "section22rule43",
     "section22rule44",
 ]
 
 SHORT_NAME = "HVAC-CHW"
 
-=======
-    "section22rule42",
-    "section22rule43",
-]
-
->>>>>>> feature/ashrae-9012022
 
 def __getattr__(name):
     if name in __all__:
