9fc0b30a399413ebc3c55485a304a677622058f9
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-12-22 16:17:25 -0500

    add calc vals

--- rct229/rulesets/ashrae9012019/section21/section21rule10.py
+++ rct229/rulesets/ashrae9012019/section21/section21rule10.py
@@ -125,5 +125,5 @@ class PRM9012019Rule06a67(RuleDefinitionListIndexedBase):
 
         def rule_check(self, context, calc_vals=None, data=None):
             pump_speed_control_type = calc_vals["pump_speed_control_type"]
-            target_speed_control_type = calc_vals["target_speed_control_type"]
+            target_speed_control_type = calc_vals["expected_speed_control_type"]
             return pump_speed_control_type == target_speed_control_type
