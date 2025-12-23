053eec530ded69bd0d078bd752d91d0e459ed9f4
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-12-23 15:05:32 -0500

    handle undetermined cases

--- rct229/rulesets/ashrae9012019/section6/section6rule1.py
+++ rct229/rulesets/ashrae9012019/section6/section6rule1.py
@@ -7,12 +7,21 @@ from rct229.rulesets.ashrae9012019.data_fns.table_G3_8_fns import table_G3_8_loo
 from rct229.utils.jsonpath_utils import find_all
 from rct229.utils.pint_utils import ZERO, CalcQ
 from rct229.utils.std_comparisons import std_equal
+from rct229.schema.schema_enums import SchemaEnums
 
-CASE3_WARNING = "Project passes based on space-by-space method. Verify if project sues space-by-space method."
-CASE4_WARNING = "Project fails based on space-by-space method. LIGHTING_BUILDING_AREA_TYPE is not known to determine building area method allowance."
-CASE5_WARNING = "Project passes based on building area method. Verify if project uses building area method."
-CASE6_WARNING = "Project fails based on building area method. LIGHTING_SPACE_TYPE is not known in all spaces to determine space-by-space method allowance."
-CASE7_WARNING = "LIGHTING_BUILDING_AREA_TYPE is not known and LIGHTING_SPACE_TYPE is not known in all spaces to determine allowance."
+SpaceFunctionOptions = SchemaEnums.schema_enums["SpaceFunctionOptions"]
+NA_SPACE_FUNCTIONS = [
+    SpaceFunctionOptions.CRAWL_SPACE,
+    SpaceFunctionOptions.INTERSTITIAL_SPACE,
+    SpaceFunctionOptions.PLENUM,
+]
+
+CASE1_WARNING = "The baseline model included at least one plenum, crawlspace, or interstitial space with lighting power. Unable to determine whether these spaces should be included in the check."
+CASE3_WARNING = "Project passes based on space-by-space method. Verify if project uses the space-by-space method."
+CASE4_WARNING = "Project fails based on space-by-space method. The lighting building area type is not known to determine building area method allowance."
+CASE5_WARNING = "Project passes based on building area method. Verify if project uses the building area method."
+CASE6_WARNING = "Project fails based on building area method. Lighting space type is not known in all spaces to determine space-by-space method allowance."
+CASE7_WARNING = "The baseline lighting building area type is not known and lighting space types are not known for all spaces to determine allowance."
 
 
 class PRM9012019Rule99c05(RuleDefinitionListIndexedBase):
@@ -26,8 +35,7 @@ class PRM9012019Rule99c05(RuleDefinitionListIndexedBase):
             each_rule=PRM9012019Rule99c05.BuildingSegmentRule(),
             index_rmd=PROPOSED,
             id="6-1",
-            description="The total building interior lighting power shall not exceed the interior lighting power "
-            "allowance determined using either Table G3.7 or G3.8",
+            description="The total building interior lighting power shall not exceed the interior lighting power allowance determined using either Table G3.7 or G3.8",
             ruleset_section_title="Lighting",
             standard_section="Section G1.2.1(b) Mandatory Provisions related to interior lighting power",
             is_primary_rule=True,
@@ -88,11 +96,7 @@ class PRM9012019Rule99c05(RuleDefinitionListIndexedBase):
 
                     # Handle crawlspace / interstitial / plenum
                     space_function = space_p.get("function")
-                    if space_function in [
-                        "CRAWL_SPACE",
-                        "INTERSTITIAL_SPACE",
-                        "PLENUM",
-                    ]:
+                    if space_function in NA_SPACE_FUNCTIONS:
                         if space_total_lpd > ZERO.POWER_PER_AREA:
                             has_crawl_interstitial_plenum_with_lighting = True
                         # Conservative exclusion from allowances
@@ -136,6 +140,56 @@ class PRM9012019Rule99c05(RuleDefinitionListIndexedBase):
                 ),
                 "has_crawl_interstitial_plenum_with_lighting": has_crawl_interstitial_plenum_with_lighting,
             }
+
+        def manual_check_required(self, context, calc_vals=None, data=None):
+            allowable_lpd_bam = calc_vals["allowable_lpd_bam"]
+            check_bam_flag = calc_vals["check_bam_flag"]
+            building_segment_design_lighting_wattage = calc_vals[
+                "building_segment_design_lighting_wattage"
+            ]
+            total_building_segment_area_p = calc_vals["total_building_segment_area_p"]
+            allowable_lighting_wattage_sbs = calc_vals["allowable_lighting_wattage_sbs"]
+            has_na_lighting = calc_vals["has_crawl_interstitial_plenum_with_lighting"]
+
+            # Any crawl / plenum / interstitial space with lighting
+            if has_na_lighting:
+                return True
+
+            allowable_lighting_wattage_bam = (
+                allowable_lpd_bam * total_building_segment_area_p
+                if allowable_lpd_bam
+                else ZERO.POWER
+            )
+
+            # Case 4: Fail Space-by-Space method and unable to check Building Area method
+            if not allowable_lpd_bam and not check_bam_flag and building_segment_design_lighting_wattage > allowable_lighting_wattage_sbs:
+                return True
+
+            # Case 6: Fail Building Area method and unable to check Space-by-Space method
+            if allowable_lpd_bam and check_bam_flag and building_segment_design_lighting_wattage > allowable_lighting_wattage_bam:
+                return True
+
+            # Case 7: Unable to reliably check either method
+            if not allowable_lpd_bam and check_bam_flag:
+                return True
+
+            return False
+
+        def get_manual_check_required_msg(self, context, calc_vals=None, data=None):
+            allowable_lpd_bam = calc_vals["allowable_lpd_bam"]
+            check_bam_flag = calc_vals["check_bam_flag"]
+            has_na_lighting = calc_vals["has_crawl_interstitial_plenum_with_lighting"]
+
+            if has_na_lighting:
+                return CASE1_WARNING
+            elif not allowable_lpd_bam and not check_bam_flag:
+                return CASE4_WARNING
+            elif allowable_lpd_bam and check_bam_flag:
+                return CASE6_WARNING
+            elif not allowable_lpd_bam and check_bam_flag:
+                return CASE7_WARNING
+            else:
+                return None
 
         def rule_check(self, context, calc_vals=None, data=None):
             allowable_lpd_bam = calc_vals["allowable_lpd_bam"]
